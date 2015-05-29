569bc99149147294d7e4d04a8b9b2c420f9ca1ce
Jeremy Mitchell <mitchell852@gmail.com>
2015-05-29 14:30:05 -0600

    applied perltidy

--- traffic_ops/app/lib/MojoPlugins/Email.pm
+++ traffic_ops/app/lib/MojoPlugins/Email.pm
@@ -69,7 +69,7 @@ sub register {
 			my $email_to = shift || confess("Please supply an email address.");
 			my $token    = shift || confess("Please supply a GUID token");
 
-			my $portal_base_url = $self->config->{'portal'}{'base_url'};
+			my $portal_base_url   = $self->config->{'portal'}{'base_url'};
 			my $portal_email_from = $self->config->{'portal'}{'email_from'};
 
 			$self->app->log->info( "MOJO_CONFIG: " . $ENV{MOJO_CONFIG} );
@@ -85,12 +85,19 @@ sub register {
 				$self->db->resultset('Parameter')->search( { -and => [ name => 'tm.instance_name', config_file => 'global' ] } )->get_column('value')
 				->single();
 			$self->stash( tm_user => $tm_user, fbox_layout => 1, mode => 'add' );
-            if ( defined($email_to) ) {
-                if ( defined($portal_email_from) ) {
-                    $self->mail( subject => "Welcome to the " . $instance_name, from => $portal_email_from, to => $email_to, template => 'user/registration', format => 'mail' );
-                } else {
-                    $self->mail( subject => "Welcome to the " . $instance_name, to => $email_to, template => 'user/registration', format => 'mail' );
-                }
+			if ( defined($email_to) ) {
+				if ( defined($portal_email_from) ) {
+					$self->mail(
+						subject  => "Welcome to the " . $instance_name,
+						from     => $portal_email_from,
+						to       => $email_to,
+						template => 'user/registration',
+						format   => 'mail'
+					);
+				}
+				else {
+					$self->mail( subject => "Welcome to the " . $instance_name, to => $email_to, template => 'user/registration', format => 'mail' );
+				}
 
 				my $email_notice = 'Successfully sent registration email to: ' . $email_to;
 				$self->app->log->info($email_notice);
