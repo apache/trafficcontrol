793983a014392a48fb095a733c2dbdeb7abdbc3c
Jan van Doorn <jan_vandoorn@cable.comcast.com>
2015-06-18 15:10:18 -0600

    fix watcher dirs. morbo is back to normal

--- traffic_ops/app/bin/start.sh
+++ traffic_ops/app/bin/start.sh
@@ -19,6 +19,6 @@ mkdir -p log
 if [ -z "$TO_EXTENSIONS_LIB" ]; then
   export PERL5LIB=local/lib/perl5; local/bin/morbo --listen "http://*:3000" -v script/cdn
 else
-  export PERL5LIB=local/lib/perl5; local/bin/morbo --listen "http://*:3000" -v script/cdn -w $TO_EXTENSIONS_LIB  
+  export PERL5LIB=local/lib/perl5; local/bin/morbo --listen "http://*:3000" -v script/cdn -w $TO_EXTENSIONS_LIB -w lib
 fi
 MOJO_LOG_LEVEL=debug
