084a36f99d9ddc39023ae2ea7fa37e01bf8d4878
Hank Beatty <hbeatty@users.noreply.github.com>
2015-07-02 06:34:47 -0400

    added more section break comments
    traffic_ops/rpm_comm/build/build.sh - added more section break comments

--- traffic_ops/rpm_comm/build/build.sh
+++ traffic_ops/rpm_comm/build/build.sh
@@ -305,6 +305,11 @@ if [ -f /etc/profile ]; then
     . /etc/profile
 fi
 
+echo ""
+echo ""
+echo "##################################################################"
+echo "# Configuring cpan"
+echo "##################################################################"
 # Tell cpan to answer yes
 sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
 
