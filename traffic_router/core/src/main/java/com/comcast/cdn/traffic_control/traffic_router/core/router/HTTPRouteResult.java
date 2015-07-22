dce26f55f5a79a04ccf3720c0cfcbe05241a4bc4
Jeff Elsloo <jeffrey_elsloo@cable.comcast.com>
2015-07-22 13:44:43 -0600

    Adds support for arbitrary response headers on HTTP delivery services.

---
+++ traffic_router/core/src/main/java/com/comcast/cdn/traffic_control/traffic_router/core/router/HTTPRouteResult.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.comcast.cdn.traffic_control.traffic_router.core.router;
+
+import java.net.URL;
+
+import com.comcast.cdn.traffic_control.traffic_router.core.ds.DeliveryService;
+
+public class HTTPRouteResult implements RouteResult {
+	private URL url;
+	private DeliveryService deliveryService;
+
+	@Override
+	public Object getResult() {
+		return getUrl();
+	}
+
+	public URL getUrl() {
+		return url;
+	}
+
+	public void setUrl(final URL url) {
+		this.url = url;
+	}
+
+	public DeliveryService getDeliveryService() {
+		return deliveryService;
+	}
+
+	public void setDeliveryService(DeliveryService deliveryService) {
+		this.deliveryService = deliveryService;
+	}
+}
