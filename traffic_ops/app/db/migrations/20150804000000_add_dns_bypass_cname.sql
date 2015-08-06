15913502b24e98bd5c3eb13be8760fdeaa929628
Dan Kirkwood <daniel_kirkwood@cable.comcast.com>
2015-08-06 08:30:25 -0600

    bypass_cname to 255

--- traffic_ops/app/db/migrations/20150804000000_add_dns_bypass_cname.sql
+++ traffic_ops/app/db/migrations/20150804000000_add_dns_bypass_cname.sql
@@ -17,7 +17,7 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-alter table deliveryservice add column `dns_bypass_cname` varchar(45) default NULL;
+alter table deliveryservice add column `dns_bypass_cname` varchar(255) default NULL;
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
