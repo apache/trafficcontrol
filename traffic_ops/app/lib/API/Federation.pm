aba12b98b1cdcc38d50c087bc65338db6b26fe43
Jackie Heitzer <jackie_heitzer@cable.comcast.com>
2015-09-29 14:39:23 -0600

    working copy of federation api.

--- traffic_ops/app/lib/API/Federation.pm
+++ traffic_ops/app/lib/API/Federation.pm
@@ -27,16 +27,17 @@ use Data::Validate::IP qw(is_ipv4 is_ipv6);
 
 sub index {
   my $self = shift;
-  my @data;
+  my $data;
 
   my $rs_data = $self->db->resultset('FederationDeliveryservice')
     ->search( {}, { prefetch => [ 'federation', 'deliveryservice' ] } );
 
   while ( my $row = $rs_data->next ) {
-    my $map;
+    my $xml_id = $row->deliveryservice->xml_id;
+    my $m;
 
-    $map->{'cname'} = $row->federation->cname;
-    $map->{'ttl'}   = $row->federation->ttl;
+    $m->{'cname'} = $row->federation->cname;
+    $m->{'ttl'}   = $row->federation->ttl;
 
     my $id        = $row->federation->id;
     my @resolvers = $self->db->resultset('FederationResolver')->search(
@@ -46,22 +47,65 @@ sub index {
 
     for my $r (@resolvers) {
       my $type = lc $r->type->name;
-      if ( defined $map->{$type} ) {
-        push( $map->{$type}, $r->ip_address );
+      if ( defined $m->{$type} ) {
+        push( $m->{$type}, $r->ip_address );
       }
       else {
-        @{ $map->{$type} } = ();
-        push( $map->{$type}, $r->ip_address );
+        @{ $m->{$type} } = ();
+        push( $m->{$type}, $r->ip_address );
       }
     }
-    push(
-      @data,
-      {   "deliveryService" => $row->deliveryservice->xml_id,
-        "mappings"        => $map
-      }
-    );
-  }
 
-  $self->success( \@data );
+    if ( defined $data ) {
+      my $x = $self->find_delivery_service( $xml_id, $data );
+      if ( !defined $x ) {
+        $data = $self->add_delivery_service( $xml_id, $m, $data );
+      }
+      else {
+        my @map = $x->{'mappings'};
+        push( @map, $m );
+        $x->{'mappings'} = \@map;
+      }
+
+    }
+    else {
+      $data = $self->add_delivery_service( $xml_id, $m, $data );
+    }
+
+  }
+  $self->success( $data );
 }
+
+sub find_delivery_service {
+  my $self   = shift;
+  my $xml_id = shift;
+  my $data   = shift;
+  my $hash;
+
+  foreach my $key (@{$data}) {
+    if ( $key->{'deliveryService'} eq $xml_id ) {
+      $hash = $key;
+    }
+  }
+  return ($hash);
+}
+
+sub add_delivery_service {
+  my $self   = shift;
+  my $xml_id = shift;
+  my $m      = shift;
+  my $data   = shift;
+
+  push( my @map, $m );
+
+  push(
+    @${data},
+    {   "deliveryService" => $xml_id,
+      "mappings"        => \@map
+    }
+  );
+  return $data;
+}
+
+
 1;
