dd543c6171fb8f9d67ad0625f0fe700ce54e7fa1
Mark Torluemke <mark_torluemke@cable.comcast.com>
2015-10-07 22:09:16 +0000

    cdn_id should not be NULL

--- traffic_ops/app/db/migrations/20151007194558_all_cdn.sql
+++ traffic_ops/app/db/migrations/20151007194558_all_cdn.sql
@@ -4,9 +4,15 @@
 
 insert into cdn (name) values ('ALL');
 update server set cdn_id = (select id from cdn where name='ALL') where cdn_id is NULL;
+alter table server drop foreign key fk_cdn2;
+alter table server modify cdn_id INT(11) NOT NULL;
+alter table server add CONSTRAINT `fk_cdn2` FOREIGN KEY (`cdn_id`) REFERENCES `cdn` (`id`);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
 update server set cdn_id = NULL where cdn_id = (select id from cdn where name='ALL');
 delete from cdn where name = 'ALL';
+alter table server drop foreign key fk_cdn2;
+alter table server modify cdn_id INT(11);
+alter table server add CONSTRAINT `fk_cdn2` FOREIGN KEY (`cdn_id`) REFERENCES `cdn` (`id`);
