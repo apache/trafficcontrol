b2cd8408d640aef43bfd3823a98857812913ee7d
Jackie Heitzer <jackie_heitzer@cable.comcast.com>
2015-10-15 15:42:20 -0600

    Revert "Merge pull request #652 from jheitz200/fix/traffic_monitor"
    This reverts commit ba1884e96b3a5288fa716986b98365410b88a434, reversing
    changes made to ce61bdf4a098290310649bb12f4c960b5f499d14.

--- traffic_ops/app/lib/Fixtures/Integration/Cdn.pm
+++ traffic_ops/app/lib/Fixtures/Integration/Cdn.pm
@@ -34,13 +34,6 @@ my %definition_for = (
 			name        => 'cdn_number_2',
 		},
 	},
-	cdn_number_3 => {
-		new   => 'Cdn',
-		using => {
-			id          => 3,
-			name        => 'ALL',
-		},
-	},
 );
 
 sub name {
