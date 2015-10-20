525e214a66c9cf95f3db41688d8ea3f31101569d
Mark Torluemke <mark_torluemke@cable.comcast.com>
2015-10-20 14:48:35 +0000

    oops, wrong order

--- traffic_ops/app/db/migrations/20151020143912_unique_cdn_name.sql
+++ traffic_ops/app/db/migrations/20151020143912_unique_cdn_name.sql
@@ -2,8 +2,8 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 
-alter table cdn add constraint cdn_cdn_UNIQUE unique (name);
 alter table cdn modify name varchar(127);
+alter table cdn add constraint cdn_cdn_UNIQUE unique (name);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
