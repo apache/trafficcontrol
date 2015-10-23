64ab5222ae74a11a9f947c73045cf034d7e09222
Dan Kirkwood <dangogh@gmail.com>
2015-10-23 21:39:52 +0000

    install jdnssec library

---
+++ traffic_router/build/build_rpm.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+#
+# Copyright 2015 Comcast Cable Communications Management, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#----------------------------------------
+function installDnsSec {
+	# download and integrate dnssec library
+	local dnssecversion=0.12
+	local dnssectools=jdnssec-tools
+	local dnssec="$dnssectools-$dnssecversion"
+	local dnssecurl=http://www.verisignlabs.com/dnssec-tools/packages
+
+	curl -o "$dnssec".tar.gz "$dnssecurl/$dnssec".tar.gz || \
+		{ echo "Could not download required $dnssec library: $?"; exit 1; }
+	tar xzvf "$dnssec".tar.gz ||  \
+		{ echo "Could not extract required $dnssec library: $?"; exit 1; }
+
+	fn=$(find $dnssec/. -name jdnssec-tools.jar)
+	if [[ -z $fn ]]; then
+		echo "jdnssec-tools.jar found in $BLDPATH: $?"; exit 1;
+	fi
+	mvn install:install-file -Dfile="$fn" -DgroupId=jdnssec -Dpackaging=jar \
+		-DartifactId=jdnssec-tools -Dversion="$dnssecversion" || \
+		{ echo "Could not install required $dnssec library: $?"; exit 1; }
+}
+
+#----------------------------------------
+function buildRpm () {
+	echo "Building the rpm."
+	version="-DTC_VERSION=$TC_VERSION"
+	targetdir="-Dproject.build.directory=$BLDPATH"
+	cd "$BLDPATH" || { echo "Could not cd to $BLDPATH: $?"; exit 1; }
+
+	installDnsSec
+
+	# Do the rpmbuild
+	mvn -DminimumTPS=1 "$version" "$targetdir" package ||  \
+		{ echo "RPM BUILD FAILED: $?"; exit 1; }
+
+	echo "========================================================================================"
+	echo "RPM BUILD SUCCEEDED, See $DIST/$RPM for the newly built rpm."
+	echo "========================================================================================"
+	echo
+	mkdir -p "$DIST" || { echo "Could not create $DIST: $?"; exit 1; }
+
+	rpm=$(find "$BLDPATH" -name "${PACKAGE}*.rpm")
+	if [[ -z $rpm ]]; then
+		echo "$PACKAGE*.rpm not found anywhere in $BLDPATH: $?"; exit 1;
+	fi
+	/bin/cp "$rpm" "$RPMPATH" || { echo "Could not copy $rpm to $RPMPATH: $?"; exit 1; }
+	/bin/cp "$rpm" "$DIST/." || { echo "Could not copy $rpm to $DIST: $?"; exit 1; }
+
+	# TODO: build src rpm separately -- mvn rpm plugin does not do src rpms
+	#cd "RPMBUILD" && \
+	#	rpmbuild -bs --define "_topdir $(pwd)" \
+        #                 --define "traffic_control_version $TC_VERSION" \
+        #                 --define "build_number $BUILD_NUMBER" -ba SPECS/${PACKAGE}.spec
+	#/bin/cp "$RPMBUILD"/SRPMS/*/*.rpm "$DIST/." || { echo "Could not copy source rpm to $DIST: $?"; exit 1; }
+}
+
+
+#----------------------------------------
+function checkEnvironment() {
+	echo "Verifying the build configuration environment."
+	local script=$(readlink -f "$0")
+	local scriptdir=$(dirname "$script")
+	export TM_DIR=$(dirname "$scriptdir")
+	export TC_DIR=$(dirname "$TM_DIR")
+	functions_sh="$TC_DIR/build/functions.sh"
+	if [[ ! -r $functions_sh ]]; then
+		echo "Error: Can't find $functions_sh"
+		exit 1
+	fi
+	. "$functions_sh"
+
+	# 
+	# get traffic_control src path -- relative to build_rpm.sh script
+	export PACKAGE="traffic_router"
+	export TC_VERSION=$(getVersion "$TC_DIR")
+	export BUILD_NUMBER=${BUILD_NUMBER:-$(getBuildNumber)}
+	export WORKSPACE=${WORKSPACE:-$TC_DIR}
+	export RPMBUILD="$WORKSPACE/rpmbuild"
+	export DIST="$WORKSPACE/dist"
+	export RPM="${PACKAGE}-${TC_VERSION}-${BUILD_NUMBER}.x86_64.rpm"
+
+	echo "=================================================="
+	echo "WORKSPACE: $WORKSPACE"
+	echo "BUILD_NUMBER: $BUILD_NUMBER"
+	echo "TC_VERSION: $TC_VERSION"
+	echo "RPM: $RPM"
+	echo "--------------------------------------------------"
+}
+
+# ---------------------------------------
+function initBuildArea() {
+	echo "Initializing the build area."
+	mkdir -p "$RPMBUILD"/{SPECS,SOURCES,RPMS,SRPMS,BUILD,BUILDROOT} || { echo "Could not create $RPMBUILD: $?"; exit 1; }
+
+	local target="$PACKAGE-$TC_VERSION"
+	# export these so build fcn has them
+	export SRCPATH="$RPMBUILD/SOURCES/$target"
+	export BLDPATH="$RPMBUILD/BUILD/$target"
+	export RPMPATH="$RPMBUILD/RPMS"
+	rm -rf "$BLDPATH" "$SRCPATH"
+	mkdir -p "$SRCPATH" || { echo "Could not create $SRCPATH: $?"; exit 1; }
+	mkdir -p "$BLDPATH" || { echo "Could not create $BLDPATH: $?"; exit 1; }
+
+	# TODO: what can be cut out here?
+	/bin/cp -r "$TM_DIR"/{api,build,connector,core} "$SRCPATH"/. || { echo "Could not copy to $SRCPATH: $?"; exit 1; }
+	/bin/cp  "$TM_DIR"/pom.xml "$SRCPATH" || { echo "Could not copy to $SRCPATH: $?"; exit 1; }
+	/bin/cp -r "$SRCPATH"/* "$BLDPATH"
+
+	# tar/gzip the source
+	tar -czvf "$targetpath.tgz" -C "$RPMBUILD/SOURCES" "$target" || { echo "Could not create tar archive $targetpath.tgz: $?"; exit 1; }
+
+	echo "The build area has been initialized."
+}
+
+# ---------------------------------------
+
+checkEnvironment
+initBuildArea
+buildRpm
