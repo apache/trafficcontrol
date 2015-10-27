475e2a633baa53e0e5eed1b13e0b3d7f450cd1db
Trevor Ackerman <trevorackerman.softdev@gmail.com>
2015-10-27 14:56:35 -0600

    Add standalone embedded jetty application that allows exercising the Traffic Router api

---
+++ traffic_router/application/src/main/java/com/comcast/cdn/traffic_control/traffic_router/RouterAndApiApp.java
@@ -0,0 +1,55 @@
+package com.comcast.cdn.traffic_control.traffic_router;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.bio.SocketConnector;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import javax.management.MBeanServer;
+import java.lang.management.ManagementFactory;
+
+public class RouterAndApiApp
+{
+    public static void main( String[] args ) throws Exception
+    {
+        PropertyConfigurator.configure("standalone/src/test/resources/log4j.properties");
+
+        Server server = new Server();
+        SocketConnector connector = new SocketConnector();
+
+        // Set some timeout options to make debugging easier.
+        connector.setMaxIdleTime(3600 * 1000);
+        connector.setSoLingerTime(-1);
+        connector.setPort(8888);
+        server.addConnector(connector);
+
+        HandlerCollection handlers = new HandlerCollection();
+
+        WebAppContext trafficRouterContext = new WebAppContext("core/src/main/webapp", "/ignoreitall");
+        handlers.addHandler(trafficRouterContext);
+
+        WebAppContext apiContext = new WebAppContext("api/src/main/webapp", "/");
+        handlers.addHandler(apiContext);
+
+        handlers.setServer(server);
+        server.setHandler(handlers);
+
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
+        server.getContainer().addEventListener(mBeanContainer);
+        mBeanContainer.start();
+
+        System.out.println(">>>>>>> Starting everything press 'Q' and enter to stop");
+
+        server.start();
+
+        while (System.in.read() != 'Q');
+
+        System.out.println(">>>>>>>>>> Stopping Jetty");
+
+        server.stop();
+        server.join();
+    }
+}
