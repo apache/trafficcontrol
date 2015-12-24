5dfc6094db7a2653a4ff8463369b6c15076c67fe
Jan van Doorn <jvd@knutsel.com>
2015-12-23 17:17:38 -0700

    licenses, and some small mods

--- todb/crconfig.go
+++ todb/crconfig.go
@@ -1,3 +1,17 @@
+// Copyright 2015 Comcast Cable Communications Management, LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package todb
 
 import (
@@ -189,6 +203,9 @@ type CRConfig struct {
 func boolString(in interface{}) string {
 	if reflect.TypeOf(in) == nil {
 		return "false"
+	}
+	if in == true {
+		return "true"
 	}
 
 	return "false"
