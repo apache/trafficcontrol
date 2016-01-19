fac1e2248a379286c72d004c9e1e040f4714fbf2
Lawrence Wickline <lawrence_wickline@cable.comcast.com>
2016-01-19 15:09:42 -0700

    Speed up display of the Servers Page

--- traffic_ops/app/lib/UI/Cdn.pm
+++ traffic_ops/app/lib/UI/Cdn.pm
@@ -320,6 +320,10 @@ sub aserver {
 	my $self          = shift;
 	my $server_select = shift;
 	my %data          = ( "aaData" => undef );
+	my $pparam =
+		$self->db->resultset('ProfileParameter')->search( { -and => [ 'parameter.name' => 'server_graph_url', 'profile.name' => 'GLOBAL' ] },
+		{ prefetch => [ 'parameter', 'profile' ] } )->single();
+	my $srvg_url = defined($pparam) ? $pparam->parameter->value : '';
 
 	my $rs = $self->db->resultset('Server')->search( undef, { prefetch => [ 'cdn', 'cachegroup', 'type', 'profile', 'status', 'phys_location' ] } );
 	while ( my $row = $rs->next ) {
@@ -333,10 +337,6 @@ sub aserver {
 			my $img     = "";
 
 			if ( $row->type->name eq "MID" || $row->type->name eq "EDGE" ) {
-				my $pparam =
-					$self->db->resultset('ProfileParameter')->search( { -and => [ 'parameter.name' => 'server_graph_url', 'profile.name' => 'GLOBAL' ] },
-					{ prefetch => [ 'parameter', 'profile' ] } )->single();
-				my $srvg_url = defined($pparam) ? $pparam->parameter->value : '';
 				$aux_url = $srvg_url . $row->host_name;
 				$img     = "graph.png";
 			}
