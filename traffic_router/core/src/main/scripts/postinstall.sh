e17b9aab1914816701d10c7d9d6ec7b1a274f73a
Jeff Elsloo <jeffrey_elsloo@cable.comcast.com>
2016-01-22 08:27:57 -0700

    Modified logic for locating cacerts to work with the regular JDK, OpenJDK, and different flavors of RHEL/CentOS 6.x. This fixes #192.

--- traffic_router/core/src/main/scripts/postinstall.sh
+++ traffic_router/core/src/main/scripts/postinstall.sh
@@ -37,11 +37,13 @@ if [ -f /opt/traffic_router/conf/*.crt ]; then
 	cd /opt/traffic_router/conf
 	for file in *.crt; do
 		alias=$(echo $file |sed -e 's/.crt//g' |tr [:upper:] [:lower:])
-		$(dirname $(readlink $(which java)))/keytool -list -alias $alias -keystore $(dirname $(readlink $(which java)))/../jre/lib/security/cacerts -storepass changeit -noprompt > /dev/null
+		cacerts=$(/bin/find $(dirname $(readlink -f $(which java)))/.. -name cacerts)
+		keytool=$(dirname $(readlink -f $(which java)))
+		$keytool -list -alias $alias -keystore $cacerts -storepass changeit -noprompt > /dev/null
 
 		if [ $? -ne 0 ]; then
 			echo "Installing certificate ${file}.."
-			$(dirname $(readlink $(which java)))/keytool -import -trustcacerts -file $file -alias $alias -keystore $(dirname $(readlink $(which java)))/../jre/lib/security/cacerts -storepass changeit -noprompt
+			$keytool -import -trustcacerts -file $file -alias $alias -keystore $cacerts -storepass changeit -noprompt
 		fi
 	done
 fi
