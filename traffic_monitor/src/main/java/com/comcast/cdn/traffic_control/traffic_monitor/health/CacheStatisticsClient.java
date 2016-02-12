c0c37654d88842c16cf369a78876d30945fab3af
Trevor Ackerman <trevorackerman.softdev@gmail.com>
2016-02-12 15:14:05 -0700

    Traffic Monitor no longer tries to avoid creating Http Request objects for getting cache statistics

--- traffic_monitor/src/main/java/com/comcast/cdn/traffic_control/traffic_monitor/health/CacheStatisticsClient.java
+++ traffic_monitor/src/main/java/com/comcast/cdn/traffic_control/traffic_monitor/health/CacheStatisticsClient.java
@@ -3,6 +3,7 @@ package com.comcast.cdn.traffic_control.traffic_monitor.health;
 import com.comcast.cdn.traffic_control.traffic_monitor.config.Cache;
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClientConfig;
+import com.ning.http.client.ProxyServer;
 import com.ning.http.client.Request;
 import org.apache.log4j.Logger;
 
@@ -13,32 +14,23 @@ import java.util.concurrent.Future;
 
 public class CacheStatisticsClient {
 	private static final Logger LOGGER = Logger.getLogger(CacheStatisticsClient.class);
-	private final AsyncHttpClient asyncHttpClient = new AsyncHttpClient(new AsyncHttpClientConfig.Builder().build());
-	private final Map<String, ProxiedRequest> statisticsRequestMap = new HashMap<String, ProxiedRequest>();
+	private final AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 
 	public void fetchCacheStatistics(final Cache cache, final CacheStateUpdater cacheStateUpdater) {
-		Request request = getRequest(cache);
+		int port = cache.getQueryPort() != 0 ? cache.getQueryPort() : 80;
+		final ProxyServer proxyServer = new ProxyServer(cache.getQueryIp(), port);
+
+		Request request = asyncHttpClient
+			.prepareGet(cache.getStatisticsUrl())
+			.setProxyServer(proxyServer)
+			.build();
+
 		try {
 			final Future<Object> future = asyncHttpClient.executeRequest(request, cacheStateUpdater);
 			cacheStateUpdater.setFuture(future);
 		} catch (IOException e) {
-			LOGGER.warn(e,e);
+			LOGGER.warn("Failed to fetch cache statistics from " + request.getUrl(),e);
 		}
-	}
-
-	private Request getRequest(final Cache cache) {
-		ProxiedRequest proxiedRequest;
-
-		if (!statisticsRequestMap.containsKey(cache.getFqdn())) {
-			proxiedRequest = new ProxiedRequest(cache.getQueryIp(), cache.getQueryPort(), cache.getStatisticsUrl(), asyncHttpClient);
-		} else {
-			proxiedRequest = statisticsRequestMap.get(cache.getFqdn()).updateForCache(cache, asyncHttpClient);
-		}
-
-		statisticsRequestMap.put(cache.getFqdn(), proxiedRequest);
-
-
-		return proxiedRequest.getRequest();
 	}
 
 
