e91385fccf4d6e33a871ec0c16e048f5047b9ded
Jeremy Mitchell <mitchell852@gmail.com>
2016-02-24 13:23:47 -0700

    converts user table to a jquery datatable

--- traffic_ops/experimental/ui/app/src/modules/private/administer/users/UsersController.js
+++ traffic_ops/experimental/ui/app/src/modules/private/administer/users/UsersController.js
@@ -2,35 +2,12 @@ var UsersController = function(users, $scope) {
 
     $scope.users = users;
 
-    $scope.predicate = 'fullName';
-    $scope.reverse = false;
-
-    $scope.query = {
-        text: ''
-    };
-
-    // pagination
-    $scope.currentPage = 1;
-    $scope.usersPerPage = $scope.users.length;
-
-    $scope.show = function(count) {
-        $scope.usersPerPage = count;
-    };
-
-    $scope.search = function(user) {
-        var query = $scope.query.text.toLowerCase(),
-            fullName = (user.fullName) ? user.fullName.toLowerCase() : '',
-            username = (user.username) ? user.username.toLowerCase() : '',
-            email = (user.email) ? user.email.toLowerCase() : '',
-            rolename = (user.rolename) ? user.rolename.toLowerCase() : '',
-            isSubstring =
-                (fullName.indexOf(query) !== -1) ||
-                (username.indexOf(query) !== -1) ||
-                (email.indexOf(query) !== -1) ||
-                (rolename.indexOf(query) !== -1);
-
-        return isSubstring;
-    };
+    angular.element(document).ready(function () {
+        $('#usersTable').dataTable({
+            "aLengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
+            "iDisplayLength": -1
+        });
+    });
 
 };
 
