a226cc49a45d9229220718c7ed4ae1239a057ad1
Jeremy Mitchell <mitchell852@gmail.com>
2016-03-01 15:38:07 -0700

    hooks token auth back up

--- traffic_ops/experimental/ui/app/src/common/api/AuthService.js
+++ traffic_ops/experimental/ui/app/src/common/api/AuthService.js
@@ -1,10 +1,11 @@
-var AuthService = function($http, $state, $location, $q, httpService, userModel, messageModel, ENV) {
+var AuthService = function($http, $state, $window, $location, $q, httpService, userModel, messageModel, ENV) {
 
     this.login = function(username, password) {
         userModel.resetUser();
         return httpService.post(ENV.apiEndpoint['login'], { u: username, p: password })
             .then(
                 function(result) {
+                    $window.sessionStorage.token = result.Token;
                     var redirect = decodeURIComponent($location.search().redirect);
                     if (redirect !== 'undefined') {
                         $location.search('redirect', null); // remove the redirect query param
@@ -51,5 +52,5 @@ var AuthService = function($http, $state, $location, $q, httpService, userModel,
 
 };
 
-AuthService.$inject = ['$http', '$state', '$location', '$q', 'httpService', 'userModel', 'messageModel', 'ENV'];
+AuthService.$inject = ['$http', '$state', '$window', '$location', '$q', 'httpService', 'userModel', 'messageModel', 'ENV'];
 module.exports = AuthService;
