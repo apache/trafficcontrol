0911d6e79f6c5542aea3ef89e8b294252aa6472b
Ramiro Arenivar <ramiro.arenivar@gmail.com>
2016-05-05 01:03:50 -0600

    removed some sample video links, moved comment on templates

--- camera5799/client/templates/views/browse_videos.js
+++ camera5799/client/templates/views/browse_videos.js
@@ -20,44 +20,18 @@ Template.browseVideos.onCreated(function() {
     var typeAlert = 'error';
     var showAlert = true;
 
+    // sample videos
     var theVideos = {
         "Videos": [
-            { "name": "testname1", "date": "12/12/16", "url": "http://www.w3schools.com/tags/movie.mp4" },
-            { "name": "testname2", "date": "12/13/16", "url": "http://www.w3schools.com/tags/movie.mp4" },
-            { "name": "testname3", "date": "12/14/16", "url": "http://www.w3schools.com/tags/movie.mp4" },
-            { "name": "testname4", "date": "12/15/16", "url": "http://www.w3schools.com/tags/movie.mp4" }
+            { "name": "sample1", "date": "12/12/16", "url": "http://www.w3schools.com/tags/movie.mp4" },
+            { "name": "sample2", "date": "12/12/16", "url": "http://www.w3schools.com/tags/movie.mp4" }
         ]
     };
 
     AvailableVideos.remove({});
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < 2; i++) {
         AvailableVideos.insert(theVideos.Videos[i]);
     }
-
-
-
-    // Meteor.call('getVideos', token, username, function(err, res) {
-    //     if (err) {
-    //         messageAlert = JSON.stringify(err);
-    //     } else {
-    //         messageAlert = JSON.stringify(res);
-    //         if (res.statusCode == 200) {
-    //             if (res.hasOwnProperty('content')) {
-    //                 res = JSON.parse(res.content);
-    //                 if (res.hasOwnProperty('CameraData')) {
-    //                     showAlert = false;
-    //                     AvailableCameras.remove({});
-    //                     for (var i = 0; i < res.CameraData.length; i++) {
-    //                         AvailableCameras.insert(res.CameraData[i]);
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     if (showAlert) { swal(titleAlert, messageAlert, typeAlert); }
-    //     return res;
-    // });
-
 });
 
 Template.browseVideos.events({
