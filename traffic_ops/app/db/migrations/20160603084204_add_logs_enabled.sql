c7612a682ae2d2799781ecb0796670b68b54943a
Dewayne Richardson <dewrich@gmail.com>
2016-06-03 10:29:21 -0600

    made the column optional

--- traffic_ops/app/db/migrations/20160603084204_add_logs_enabled.sql
+++ traffic_ops/app/db/migrations/20160603084204_add_logs_enabled.sql
@@ -1,7 +1,7 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-alter table deliveryservice add logs_enabled tinyint(1) not null;
+alter table deliveryservice add logs_enabled tinyint(1);
 
 
 -- +goose Down
