3757530081018c7f4dd353498f3fea84ef785d53
Dan Kirkwood <dangogh@gmail.com>
2016-07-27 13:26:24 -0600

    have docker clone repo into mounted volume

--- build/clone-for-docker.sh
+++ build/clone-for-docker.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-git clone ${GITREPO}
-cd `basename ${GITREPO} .git`
-git checkout ${BRANCH}
+
+project=`basename $GITREPO .git`
+
+[ ! -d /repo ] || ( echo "Expected to have volume mounted for /repo"; exit 1)
+
+# Delete previous content if it exists
+rm -rf /repo/$project
+
+git clone --branch $BRANCH $GITREPO
