b853a673c6212a85c7de2c5252fd1ef03b3420ab
Trevor Ackerman <trevorackerman.softdev@gmail.com>
2016-08-08 12:58:03 -0600

    Add 'http to https' as another option for Delivery Service protocol in TO, indicates to redirect incoming http traffic to https

--- traffic_ops/app/lib/UI/Topology.pm
+++ traffic_ops/app/lib/UI/Topology.pm
@@ -413,6 +413,8 @@ sub gen_crconfig_json {
             $data_obj->{'deliveryServices'}->{ $row->xml_id }->{'protocol'}->{'redirectToHttps'} = 'false';
         }
 
+        $data_obj->{'deliveryServices'}->{ $row->xml_id }->{'protocol'} = $row->protocol;
+
         my $geo_provider = $row->geo_provider;
         if ( $geo_provider == 1 ) {
             $data_obj->{'deliveryServices'}->{ $row->xml_id }->{'geolocationProvider'} = 'neustarGeolocationService';
