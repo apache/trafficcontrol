536abd8e1606b83d784a7e98ea284bd05562e8bc
Jackie Heitzer <jackie_heitzer@cable.comcast.com>
2016-08-11 15:04:45 -0600

    fixing tests after merging master

--- traffic_ops/app/db/migrations/20160809092015_update_serverchecks.sql
+++
@@ -1,9 +0,0 @@
-
--- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE servercheck CHANGE `as` `bf` int(11) DEFAULT NULL;
-
-
--- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
-ALTER TABLE servercheck CHANGE `bf` `as` int(11) DEFAULT NULL;
