d62e327011cf53bfb57a8e500e71f9bb02b5f205
Jeremy Mitchell <mitchell852@gmail.com>
2016-09-01 13:10:48 -0600

    no migrations in the branch. removing ones added with master merge.

--- traffic_ops/app/db/migrations/20160817171741_server_add_https_port.sql
+++
@@ -1,8 +0,0 @@
-
--- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
-alter table server add https_port int(10) unsigned default NULL;
-
--- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
-alter table server drop column https_port;
