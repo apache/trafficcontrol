75860bceb782b68ecca21d34453eb971670954e9
Jeremy Mitchell <mitchell852@gmail.com>
2016-09-28 11:19:19 -0600

    perltidy

--- traffic_ops/app/lib/API/Region.pm
+++ traffic_ops/app/lib/API/Region.pm
@@ -34,56 +34,57 @@ sub index {
 	while ( my $row = $rs_data->next ) {
 		push(
 			@data, {
-				"id"   => $row->id,
-				"name" => $row->name,
+				"id"       => $row->id,
+				"name"     => $row->name,
 				"division" => {
-                    "id" => $row->division->id,
-                    "name" => $row->division->name
-                }
+					"id"   => $row->division->id,
+					"name" => $row->division->name
+				}
 			}
 		);
 	}
 	$self->success( \@data );
 }
 
-sub create{
-    my $self = shift;
-    my $division_name = $self->param('division_name');
-    my $params = $self->req->json;
-    if (!defined($params)) {
-        return $self->alert("parameters must be in JSON format,  please check!"); 
-    }
-    if ( !&is_oper($self) ) {
-        return $self->alert("You must be an ADMIN or OPER to perform this operation!");
-    }
+sub create {
+	my $self          = shift;
+	my $division_name = $self->param('division_name');
+	my $params        = $self->req->json;
+	if ( !defined($params) ) {
+		return $self->alert("parameters must be in JSON format,  please check!");
+	}
+	if ( !&is_oper($self) ) {
+		return $self->alert("You must be an ADMIN or OPER to perform this operation!");
+	}
 
-    my $existing_region = $self->db->resultset('Region')->search( { name => $params->{name} } )->get_column('name')->single();
-    if (defined($existing_region)) {
-        return $self->alert("region[". $params->{name} . "] already exists."); 
-    }
+	my $existing_region = $self->db->resultset('Region')->search( { name => $params->{name} } )->get_column('name')->single();
+	if ( defined($existing_region) ) {
+		return $self->alert( "region[" . $params->{name} . "] already exists." );
+	}
 
-    my $divsion_id = $self->db->resultset('Division')->search( { name => $division_name } )->get_column('id')->single();
-    if (!defined($divsion_id)) {
-        return $self->alert("division[". $division_name . "] does not exist."); 
-    }
+	my $divsion_id = $self->db->resultset('Division')->search( { name => $division_name } )->get_column('id')->single();
+	if ( !defined($divsion_id) ) {
+		return $self->alert( "division[" . $division_name . "] does not exist." );
+	}
 
-    my $insert = $self->db->resultset('Region')->create(
-        {
-            name     => $params->{name},
-            division => $divsion_id
-        } );
-    $insert->insert();
-   
-    my $response;
-    my $rs = $self->db->resultset('Region')->find( { id => $insert->id } );
-    if (defined($rs)) {
-        $response->{id}     = $rs->id;
-        $response->{name}   = $rs->name;
-        $response->{divisionName}  = $division_name;
-        $response->{divsionId}    = $rs->division->id;
-        return $self->success($response);
-    }
-    return $self->alert("create region ". $params->{name}." failed.");
+	my $insert = $self->db->resultset('Region')->create(
+		{
+			name     => $params->{name},
+			division => $divsion_id
+		}
+	);
+	$insert->insert();
+
+	my $response;
+	my $rs = $self->db->resultset('Region')->find( { id => $insert->id } );
+	if ( defined($rs) ) {
+		$response->{id}           = $rs->id;
+		$response->{name}         = $rs->name;
+		$response->{divisionName} = $division_name;
+		$response->{divsionId}    = $rs->division->id;
+		return $self->success($response);
+	}
+	return $self->alert( "create region " . $params->{name} . " failed." );
 }
 
 1;
