39519480f57f2508cda08227937b45d30e37c269
Jeremy Mitchell <mitchell852@gmail.com>
2016-09-29 17:02:29 -0600

    puts regions route in the right place

--- traffic_ops/app/lib/TrafficOpsRoutes.pm
+++ traffic_ops/app/lib/TrafficOpsRoutes.pm
@@ -587,8 +587,6 @@ sub api_routes {
 		->to( 'Division#show', namespace => $namespace );
 	$r->post("/api/$version/divisions")->over( authenticated => 1 )
 		->to( 'Division#create', namespace => $namespace );
-	$r->post("/api/$version/divisions/:division_name/regions")->over( authenticated => 1 )
-		->to( 'Region#create', namespace => $namespace );
 
 	# -- FEDERATIONS
 	$r->get( "/internal/api/$version/federations")->over( authenticated => 1 )
@@ -659,6 +657,8 @@ sub api_routes {
 	# Supports ?orderby=key
 	$r->get("/api/$version/regions")->over( authenticated => 1 )
 		->to( 'Region#index', namespace => $namespace );
+	$r->post("/api/$version/divisions/:division_name/regions")->over( authenticated => 1 )
+		->to( 'Region#create', namespace => $namespace );
 
 	# -- ROLES
 	# Supports ?orderby=key
