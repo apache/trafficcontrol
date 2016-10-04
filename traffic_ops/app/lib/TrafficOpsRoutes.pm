eb5dd22e27f33c40e5276a1d7d1c457e1eec5386
Jeremy Mitchell <mitchell852@gmail.com>
2016-10-04 11:12:14 -0600

    limits :id to a number

--- traffic_ops/app/lib/TrafficOpsRoutes.pm
+++ traffic_ops/app/lib/TrafficOpsRoutes.pm
@@ -642,7 +642,7 @@ sub api_routes {
 	# Supports ?orderby=key
 	$r->get( "/api/$version/profiles")->over( authenticated => 1 )
 		->to( 'Profile#index', namespace => $namespace );
-	$r->get( "/api/$version/profiles/:id")->over( authenticated => 1 )
+	$r->get( "/api/$version/profiles/:id" => [ id => qr/\d+/ ] )->over( authenticated => 1 )
 		->to( 'Profile#show', namespace => $namespace );
 	$r->get( "/api/$version/profiles/trimmed")->over( authenticated => 1 )
 		->to( 'Profile#index_trimmed', namespace => $namespace );
