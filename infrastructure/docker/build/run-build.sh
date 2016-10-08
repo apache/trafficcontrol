e427d9a56b8032d748fa84b6b6c520b77f3a273a
Dan Kirkwood <dangogh@gmail.com>
2016-10-08 16:46:41 -0600

    simplify; remove clone volume

--- infrastructure/docker/build/run-build.sh
+++ infrastructure/docker/build/run-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+<<<<<<< HEAD
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,11 +16,14 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+=======
+>>>>>>> c2e0979... simplify; remove clone volume
 
 target=$1
 [[ -z $target ]] && echo "No target specified"
 echo "Building $target"
 
+<<<<<<< HEAD
 echo "GITREPO=${GITREPO:=https://github.com/apache/incubator-trafficcontrol}"
 echo "BRANCH=${BRANCH:=master}"
 
@@ -35,3 +39,15 @@ cp ../dist/* /artifacts/.
 # Clean up for next build
 cd -
 rm -r $dir
+=======
+echo "GITREPO=${GITREPO:=https://github.com/Comcast/traffic_control}"
+echo "BRANCH=${BRANCH:=master}"
+
+set -x
+git clone $GITREPO -b $BRANCH traffic_control
+
+cd traffic_control/$target
+./build/build_rpm.sh
+mkdir -p /artifacts
+cp ../dist/* /artifacts/.
+>>>>>>> c2e0979... simplify; remove clone volume
