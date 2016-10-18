f0d49388ca436d0988821e2cadd9f2007c0ff067
Dan Kirkwood <dangogh@gmail.com>
2016-10-18 15:03:44 -0600

    diff calc for functions path

--- traffic_ops/experimental/ui/build/build_rpm.sh
+++ traffic_ops/experimental/ui/build/build_rpm.sh
@@ -17,9 +17,12 @@
 
 function importFunctions() {
 	local script=$(readlink -f "$0")
+	local suff=${script##*control/}
+	local tcdirlen=$[${#script} - ${#suff}]
+	export TC_DIR=${script:0:$tcdirlen}
 	local scriptdir=$(dirname "$script")
 	export TS_DIR=$(dirname "$scriptdir")
-	export TC_DIR=$(dirname "$TS_DIR")
+
 	functions_sh="$TC_DIR/build/functions.sh"
 	if [[ ! -r $functions_sh ]]; then
 		echo "error: can't find $functions_sh"
