6f0aef4fa6efa13f78563eb166e36ebf0f281185
Dan Kirkwood <dangogh@gmail.com>
2016-10-31 16:42:22 -0600

    removes comcast copyright statements from code comments per Apache incubator guidelines

--- traffic_ops/app/bin/db/setup_kabletown.pl
+++ traffic_ops/app/bin/db/setup_kabletown.pl
@@ -2,7 +2,6 @@
 
 package main;
 #
-# Copyright 2015 Comcast Cable Communications Management, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- traffic_ops/app/lib/Utils/Helper.pm
+++ traffic_ops/app/lib/Utils/Helper.pm
@@ -1,6 +1,5 @@
 package Utils::Helper;
 #
-# Copyright 2015 Comcast Cable Communications Management, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
