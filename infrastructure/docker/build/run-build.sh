1e3a3d4fc80e27e504eb68dd0c733c30348f695d
Dan Kirkwood <dangogh@gmail.com>
2016-11-07 14:50:50 -0700

    merge master

--- infrastructure/docker/build/run-build.sh
+++ infrastructure/docker/build/run-build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -16,14 +15,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-=======
->>>>>>> c2e0979... simplify; remove clone volume
 
 target=$1
 [[ -z $target ]] && echo "No target specified"
 echo "Building $target"
 
-<<<<<<< HEAD
 echo "GITREPO=${GITREPO:=https://github.com/apache/incubator-trafficcontrol}"
 echo "BRANCH=${BRANCH:=master}"
 
@@ -39,15 +35,3 @@ cp ../dist/* /artifacts/.
 # Clean up for next build
 cd -
 rm -r $dir
-=======
-echo "GITREPO=${GITREPO:=https://github.com/Comcast/traffic_control}"
-echo "BRANCH=${BRANCH:=master}"
-
-set -x
-git clone $GITREPO -b $BRANCH traffic_control
-
-cd traffic_control/$target
-./build/build_rpm.sh
-mkdir -p /artifacts
-cp ../dist/* /artifacts/.
->>>>>>> c2e0979... simplify; remove clone volume
