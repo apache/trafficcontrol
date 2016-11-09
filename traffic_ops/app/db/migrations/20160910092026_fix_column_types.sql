fc3f26773c5dd6ac4c62f6fbd338e4cb1014548b
Jeremy Mitchell <mitchell852@gmail.com>
2016-11-09 11:52:19 -0700

    the migration basically includes 2 things -1) converts double to numeric and 2) converts certain smallints to bool

--- traffic_ops/app/db/migrations/20160910092026_fix_column_types.sql
+++ traffic_ops/app/db/migrations/20160910092026_fix_column_types.sql
@@ -5,73 +5,77 @@
 ALTER TABLE cachegroup ALTER COLUMN latitude  TYPE numeric;
 ALTER TABLE cachegroup ALTER COLUMN longitude TYPE numeric;
 
-ALTER TABLE deliveryservice ALTER COLUMN signed TYPE smallint USING (signed::int::smallint);
-ALTER TABLE deliveryservice ALTER COLUMN qstring_ignore TYPE smallint USING (qstring_ignore::int::smallint);
-
-ALTER TABLE deliveryservice
-  ALTER COLUMN geo_limit DROP DEFAULT,
-  ALTER COLUMN geo_limit TYPE smallint USING (geo_limit::int::smallint),
-  ALTER COLUMN geo_limit SET DEFAULT '0';
+ALTER TABLE cdn
+	ALTER COLUMN dnssec_enabled TYPE boolean
+		USING CASE WHEN dnssec_enabled = 0 THEN FALSE
+			WHEN dnssec_enabled = 1 THEN TRUE
+			ELSE NULL;
 
 ALTER TABLE deliveryservice ALTER COLUMN miss_lat                     TYPE numeric;
 ALTER TABLE deliveryservice ALTER COLUMN miss_long                    TYPE numeric;
-ALTER TABLE deliveryservice ALTER COLUMN multi_site_origin            TYPE smallint USING (multi_site_origin::int::smallint);
-ALTER TABLE deliveryservice ALTER COLUMN regional_geo_blocking        TYPE smallint USING (regional_geo_blocking::int::smallint);
-ALTER TABLE deliveryservice ALTER COLUMN logs_enabled                 TYPE smallint USING (logs_enabled::int::smallint);
-ALTER TABLE deliveryservice ALTER COLUMN multi_site_origin_algorithm  TYPE smallint USING (multi_site_origin_algorithm::int::smallint);
-
-ALTER TABLE parameter
-  ALTER COLUMN secure DROP DEFAULT,
-  ALTER COLUMN secure TYPE smallint USING (secure::int::smallint),
-  ALTER COLUMN secure SET DEFAULT '0';
-
-ALTER TABLE server
-  ALTER COLUMN upd_pending DROP DEFAULT,
-  ALTER COLUMN upd_pending TYPE smallint USING (upd_pending::int::smallint),
-  ALTER COLUMN upd_pending SET DEFAULT '0';
-
-ALTER TABLE tm_user
-  ALTER COLUMN new_user DROP DEFAULT,
-  ALTER COLUMN new_user TYPE smallint USING (new_user::int::smallint),
-  ALTER COLUMN new_user SET DEFAULT '1';
-
-ALTER TABLE to_extension  ALTER COLUMN isactive TYPE smallint USING (isactive::int::smallint);
-
-
-
--- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
-
-ALTER TABLE cachegroup ALTER COLUMN latitude  TYPE double precision;
-ALTER TABLE cachegroup ALTER COLUMN longitude TYPE double precision;
-
-ALTER TABLE deliveryservice ALTER COLUMN signed         TYPE boolean USING (signed::int::boolean);
-ALTER TABLE deliveryservice ALTER COLUMN qstring_ignore TYPE boolean USING (qstring_ignore::int::boolean);
 
 ALTER TABLE deliveryservice
-  ALTER COLUMN geo_limit DROP DEFAULT,
-  ALTER COLUMN geo_limit TYPE boolean USING (geo_limit::int::boolean),
-  ALTER COLUMN geo_limit SET DEFAULT FALSE;
+	ALTER COLUMN active TYPE boolean
+		USING CASE WHEN active = 0 THEN FALSE
+			WHEN active = 1 THEN TRUE
+			ELSE NULL;
 
-ALTER TABLE deliveryservice ALTER COLUMN miss_lat                     TYPE double precision;
-ALTER TABLE deliveryservice ALTER COLUMN miss_long                    TYPE double precision;
-ALTER TABLE deliveryservice ALTER COLUMN multi_site_origin            TYPE boolean USING (multi_site_origin::int::boolean);
-ALTER TABLE deliveryservice ALTER COLUMN regional_geo_blocking        TYPE boolean USING (regional_geo_blocking::int::boolean);
-ALTER TABLE deliveryservice ALTER COLUMN multi_site_origin_algorithm  TYPE boolean USING (multi_site_origin_algorithm::int::boolean);
+ALTER TABLE deliveryservice
+	ALTER COLUMN signed TYPE boolean
+		USING CASE WHEN signed = 0 THEN FALSE
+			WHEN signed = 1 THEN TRUE
+			ELSE NULL;
+
+ALTER TABLE deliveryservice
+	ALTER COLUMN ipv6_routing_enabled TYPE boolean
+		USING CASE WHEN ipv6_routing_enabled = 0 THEN FALSE
+			WHEN ipv6_routing_enabled = 1 THEN TRUE
+			ELSE NULL;
+
+ALTER TABLE deliveryservice
+	ALTER COLUMN multi_site_origin TYPE boolean
+		USING CASE WHEN multi_site_origin = 0 THEN FALSE
+			WHEN multi_site_origin = 1 THEN TRUE
+			ELSE NULL;
+
+ALTER TABLE deliveryservice
+	ALTER COLUMN regional_geo_blocking TYPE boolean
+		USING CASE WHEN regional_geo_blocking = 0 THEN FALSE
+			WHEN regional_geo_blocking = 1 THEN TRUE
+			ELSE NULL;
+
+ALTER TABLE deliveryservice
+	ALTER COLUMN logs_enabled TYPE boolean
+		USING CASE WHEN logs_enabled = 0 THEN FALSE
+			WHEN logs_enabled = 1 THEN TRUE
+			ELSE NULL;
+
+ALTER TABLE deliveryservice
+	ALTER COLUMN logs_enabled TYPE boolean
+		USING CASE WHEN logs_enabled = 0 THEN FALSE
+			WHEN logs_enabled = 1 THEN TRUE
+			ELSE NULL;
 
 ALTER TABLE parameter
-  ALTER COLUMN secure DROP DEFAULT,
-  ALTER COLUMN secure TYPE boolean USING (secure::int::boolean),
-  ALTER COLUMN secure SET DEFAULT FALSE;
+	ALTER COLUMN secure TYPE boolean
+		USING CASE WHEN secure = 0 THEN FALSE
+			WHEN secure = 1 THEN TRUE
+			ELSE NULL;
 
 ALTER TABLE server
-  ALTER COLUMN upd_pending DROP DEFAULT,
-  ALTER COLUMN upd_pending TYPE boolean USING (upd_pending::int::boolean),
-  ALTER COLUMN upd_pending SET DEFAULT FALSE;
+	ALTER COLUMN upd_pending TYPE boolean
+		USING CASE WHEN upd_pending = 0 THEN FALSE
+			WHEN upd_pending = 1 THEN TRUE
+			ELSE NULL;
 
 ALTER TABLE tm_user
-  ALTER COLUMN new_user DROP DEFAULT,
-  ALTER COLUMN new_user TYPE boolean USING (new_user::int::boolean),
-  ALTER COLUMN new_user SET DEFAULT TRUE;
+	ALTER COLUMN new_user TYPE boolean
+		USING CASE WHEN new_user = 0 THEN FALSE
+			WHEN new_user = 1 THEN TRUE
+			ELSE NULL;
 
-ALTER TABLE to_extension  ALTER COLUMN isactive TYPE boolean USING (isactive::int::boolean);
+ALTER TABLE to_extension
+	ALTER COLUMN isactive TYPE boolean
+		USING CASE WHEN isactive = 0 THEN FALSE
+			WHEN isactive = 1 THEN TRUE
+			ELSE NULL;
