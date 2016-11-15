943aebb4e8c9694205d957994b82c3b88937afaf
Jeremy Mitchell <mitchell852@gmail.com>
2016-11-15 14:12:32 -0700

    moves the sql uses to fix datatypes for a migrated database into a non-migration file. this sql file is a one-off only to be applied to a database converted from mysql.

--- traffic_ops/app/db/convert_data_types.sql
+++ traffic_ops/app/db/convert_data_types.sql
@@ -1,7 +1,3 @@
-
--- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
-
 ALTER TABLE cachegroup ALTER COLUMN latitude  TYPE numeric;
 ALTER TABLE cachegroup ALTER COLUMN longitude TYPE numeric;
 
@@ -64,6 +60,14 @@ ALTER TABLE deliveryservice
 			END,
   ALTER COLUMN logs_enabled SET DEFAULT FALSE;
 
+  ALTER TABLE goose_db_version
+  ALTER COLUMN is_applied DROP DEFAULT,
+	ALTER COLUMN is_applied TYPE boolean
+		USING CASE WHEN is_applied = 1 THEN TRUE
+			ELSE FALSE
+			END,
+  ALTER COLUMN is_applied SET DEFAULT FALSE;
+
 ALTER TABLE parameter
   ALTER COLUMN secure DROP DEFAULT,
 	ALTER COLUMN secure TYPE boolean
@@ -95,78 +99,3 @@ ALTER TABLE to_extension
 			ELSE FALSE
 			END,
     ALTER COLUMN isactive SET DEFAULT FALSE;
-
--- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
-
-ALTER TABLE cachegroup ALTER COLUMN latitude  TYPE double precision;
-ALTER TABLE cachegroup ALTER COLUMN longitude TYPE double precision;
-
-ALTER TABLE cdn
-  ALTER COLUMN dnssec_enabled DROP DEFAULT,
-	ALTER COLUMN dnssec_enabled TYPE SMALLINT
-   USING CASE WHEN dnssec_enabled THEN 1 ELSE 0 END,
-  ALTER COLUMN dnssec_enabled SET DEFAULT 0;
-
-ALTER TABLE deliveryservice ALTER COLUMN miss_lat  TYPE double precision;
-ALTER TABLE deliveryservice ALTER COLUMN miss_long TYPE double precision;
-
-ALTER TABLE deliveryservice
-  ALTER COLUMN active DROP DEFAULT,
-	ALTER COLUMN active TYPE SMALLINT
-   USING CASE WHEN active THEN 1 ELSE 0 END,
-  ALTER COLUMN active SET DEFAULT 0;
-
-ALTER TABLE deliveryservice
-  ALTER COLUMN signed DROP DEFAULT,
-	ALTER COLUMN signed TYPE SMALLINT
-   USING CASE WHEN signed THEN 1 ELSE 0 END,
-  ALTER COLUMN signed SET DEFAULT 0;
-
-ALTER TABLE deliveryservice
-  ALTER COLUMN ipv6_routing_enabled DROP DEFAULT,
-	ALTER COLUMN ipv6_routing_enabled TYPE SMALLINT
-   USING CASE WHEN ipv6_routing_enabled THEN 1 ELSE 0 END,
-  ALTER COLUMN ipv6_routing_enabled SET DEFAULT 0;
-
-ALTER TABLE deliveryservice
-  ALTER COLUMN multi_site_origin DROP DEFAULT,
-	ALTER COLUMN multi_site_origin TYPE SMALLINT
-   USING CASE WHEN multi_site_origin THEN 1 ELSE 0 END,
-  ALTER COLUMN multi_site_origin SET DEFAULT 0;
-
-ALTER TABLE deliveryservice
-  ALTER COLUMN regional_geo_blocking DROP DEFAULT,
-	ALTER COLUMN regional_geo_blocking TYPE SMALLINT
-   USING CASE WHEN regional_geo_blocking THEN 1 ELSE 0 END,
-  ALTER COLUMN regional_geo_blocking SET DEFAULT 0;
-
-ALTER TABLE deliveryservice
-  ALTER COLUMN logs_enabled DROP DEFAULT,
-	ALTER COLUMN logs_enabled TYPE SMALLINT
-   USING CASE WHEN logs_enabled THEN 1 ELSE 0 END,
-  ALTER COLUMN logs_enabled SET DEFAULT 0;
-
-ALTER TABLE parameter
-  ALTER COLUMN secure DROP DEFAULT,
-	ALTER COLUMN secure TYPE SMALLINT
-   USING CASE WHEN secure THEN 1 ELSE 0 END,
-  ALTER COLUMN secure SET DEFAULT 0;
-
-ALTER TABLE server
-  ALTER COLUMN upd_pending DROP DEFAULT,
-	ALTER COLUMN upd_pending TYPE SMALLINT
-   USING CASE WHEN upd_pending THEN 1 ELSE 0 END,
-  ALTER COLUMN upd_pending SET DEFAULT 0;
-
-ALTER TABLE tm_user
-  ALTER COLUMN new_user DROP DEFAULT,
-	ALTER COLUMN new_user TYPE SMALLINT
-   USING CASE WHEN new_user THEN 1 ELSE 0 END,
-  ALTER COLUMN new_user SET DEFAULT 0;
-
-ALTER TABLE to_extension
-  ALTER COLUMN isactive DROP DEFAULT,
-	ALTER COLUMN isactive TYPE SMALLINT
-   USING CASE WHEN isactive THEN 1 ELSE 0 END,
-  ALTER COLUMN isactive SET DEFAULT 0;
