8251d983fe2e20ceaa57ece9e908de301c8a008b
Dan Kirkwood <dangogh@gmail.com>
2016-11-18 17:47:45 -0700

    add more casts

--- traffic_ops/app/db/pg-migration/runpgloader.sh
+++ traffic_ops/app/db/pg-migration/runpgloader.sh
@@ -10,4 +10,9 @@ for c in "$POSTGRES_HOST 5432" "$MYSQL_HOST 3306"; do
 	done
 done
 
-pgloader -v --cast 'type tinyint to smallint drop typemod' mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST/traffic_ops_db postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB
+pgloader -v \
+	--cast 'type tinyint to smallint drop typemod' \
+	--cast 'type varchar to text drop typemod' \
+	--cast 'type double to numeric drop typemod' \
+	mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST/traffic_ops_db \
+	postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB
