624f1ba16d99bb0fc3d00dee4e5e363e2e8d4fdd
Dan Kirkwood <dangogh@gmail.com>
2016-12-04 01:44:03 +0000

    add another step to convert bools

--- traffic_ops/app/db/pg-migration/mysql/run.sh
+++
@@ -1,8 +0,0 @@
-docker run \
-        --name mysql \
-        -p 3306:3306 \
-	-v $(pwd)/mysql/conf.d:/etc/mysql/conf.d \
-	-v $(pwd)/mysql/initdb.d:/docker-entrypoint-initdb.d \
-        -d mysql
-
-
