a595c104bcf774da7098fa35c535045abf48e628
Robert Butts <robert.o.butts@gmail.com>
2016-12-08 15:24:44 -0700

    Add TM2 cache.TestStatsMarshall

--- traffic_monitor/experimental/traffic_monitor/cache/cache_test.go
+++ traffic_monitor/experimental/traffic_monitor/cache/cache_test.go
@@ -20,9 +20,14 @@ package cache
  */
 
 import (
+	"encoding/json"
+	"net/url"
 	"testing"
+	"time"
 
+	"github.com/apache/incubator-trafficcontrol/traffic_monitor/experimental/traffic_monitor/enum"
 	"github.com/apache/incubator-trafficcontrol/traffic_monitor/experimental/traffic_monitor/peer"
+	"github.com/apache/incubator-trafficcontrol/traffic_monitor/experimental/traffic_monitor/srvhttp"
 	todata "github.com/apache/incubator-trafficcontrol/traffic_monitor/experimental/traffic_monitor/trafficopsdata"
 )
 
@@ -32,5 +37,55 @@ func TestHandlerPrecompute(t *testing.T) {
 	}
 	if !NewPrecomputeHandler(todata.NewThreadsafe(), peer.NewCRStatesPeersThreadsafe()).Precompute() {
 		t.Errorf("expected NewPrecomputeHandler().Precompute() true, actual false")
+	}
+}
+
+type DummyFilterNever struct {
+}
+
+func (f DummyFilterNever) UseStat(name string) bool {
+	return false
+}
+
+func (f DummyFilterNever) UseCache(name enum.CacheName) bool {
+	return false
+}
+
+func (f DummyFilterNever) WithinStatHistoryMax(i int) bool {
+	return false
+}
+
+func TestStatsMarshall(t *testing.T) {
+	hist := randResultHistory()
+	filter := DummyFilterNever{}
+	params := url.Values{}
+	beforeStatsMarshall := time.Now()
+	bytes, err := StatsMarshall(hist, filter, params)
+	afterStatsMarshall := time.Now()
+	if err != nil {
+		t.Fatalf("StatsMarshall return expected nil err, actual err: %v", err)
+	}
+	// if len(bytes) > 0 {
+	// 	t.Errorf("expected empty bytes, actual: %v", string(bytes))
+	// }
+
+	stats := Stats{}
+	if err := json.Unmarshal(bytes, &stats); err != nil {
+		t.Fatalf("unmarshalling expected nil err, actual err: %v", err)
+	}
+
+	if stats.CommonAPIData.QueryParams != "" {
+		t.Errorf(`unmarshalling stats.CommonAPIData.QueryParams expected "", actual %v`, stats.CommonAPIData.QueryParams)
+	}
+
+	statsDate, err := time.Parse(srvhttp.CommonAPIDataDateFormat, stats.CommonAPIData.DateStr)
+	if err != nil {
+		t.Errorf(`stats.CommonAPIData.DateStr expected format %v, actual %v`, srvhttp.CommonAPIDataDateFormat, stats.CommonAPIData.DateStr)
+	}
+	if beforeStatsMarshall.Round(time.Second).After(statsDate) || statsDate.After(afterStatsMarshall.Round(time.Second)) { // round to second, because CommonAPIDataDateFormat is second-precision
+		t.Errorf(`unmarshalling stats.CommonAPIData.DateStr expected between %v and %v, actual %v`, beforeStatsMarshall, afterStatsMarshall, stats.CommonAPIData.DateStr)
+	}
+	if len(stats.Caches) > 0 {
+		t.Errorf(`unmarshalling stats.Caches expected empty, actual %+v`, stats.Caches)
 	}
 }
