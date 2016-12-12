3b16167e3627f5ef2706581ad8dd93b7bdceb601
Dan Kirkwood <dangogh@gmail.com>
2016-12-12 15:06:16 -0700

    move shebang back to top

--- traffic_ops/app/db/pg-migration/runpgloader.sh
+++ traffic_ops/app/db/pg-migration/runpgloader.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -x
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -11,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-#!/bin/bash -x
 
 # make sure postgres and mysql ports are both active
 echo "POSTGRES_HOST=$POSTGRES_HOST MYSQL_HOST=$MYSQL_HOST"
