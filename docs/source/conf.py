86bed539323e6a6671ce6163852a447afeb13a67
Jan van Doorn <jan_vandoorn@cable.comcast.com>
2016-12-22 10:42:15 -0700

    change docs logo and black sidebar; some small edits to remove errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,7 +126,7 @@ html_theme_path = ["_themes", ]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../../traffic_ops/app/public/images/tc_logo.png'
+html_logo = '../../traffic_ops/app/public/images/tc_logo_c_only.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
