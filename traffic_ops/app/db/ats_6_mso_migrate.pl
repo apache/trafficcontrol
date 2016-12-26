dd9abfede9e5d9b954d424e55268c231e88fcc94
Jan van Doorn <jan_vandoorn@cable.comcast.com>
2016-12-26 10:45:47 -0700

    Move domain_name to cdn table - schema

--- traffic_ops/app/db/ats_6_mso_migrate.pl
+++ traffic_ops/app/db/ats_6_mso_migrate.pl
@@ -33,47 +33,74 @@ my $dsn = "DBI:$driver:dbname=$database;host=127.0.0.1;port=5432";
 my $dbh = DBI->connect( $dsn, $userid, $password, { RaiseError => 1 } )
 	or die $DBI::errstr;
 
-print "Opened database successfully\n";
-
-my $sql = 'select id,xml_id,mid_header_rewrite from deliveryservice where multi_site_origin=true;';
+# elsif ( $multi_site_origin_algorithm == 1 ) {
+# 					$mso_algorithm = "false";
+# 				}
+# 				elsif ( $multi_site_origin_algorithm == 2 ) {
+# 					$mso_algorithm = "strict";
+# 				}
+# 				elsif ( $multi_site_origin_algorithm == 3 ) {
+# 					$mso_algorithm = "true";
+# 				}
+# 				elsif ( $multi_site_origin_algorithm == 4 ) {
+# 					$mso_algorithm = "latched";
+# 				}
+# 				else {
+# 					$mso_algorithm = "consistent_hash";
+my @mso_algorithm_choices = ( "consistent_hash", "false", "strict", "true", "latched" );
+my $sql = 'select id,xml_id,mid_header_rewrite,multi_site_origin,multi_site_origin_algorithm from deliveryservice where multi_site_origin=true;';
 
 my $sth = $dbh->prepare_cached($sql);
 $sth->execute || die "Couldn't execute statement: " . $sth->errstr;
 while ( my @data = $sth->fetchrow_array() ) {
-	if ( !defined( $data[2] ) ) {
+	if ( defined( $data[3] ) && !$data[3] ) {
 		next;
 	}
-	my @lines = split( /__RETURN__/, $data[2] );
 	my %profile_ids;
-	foreach my $line (@lines) {
-		if ( !defined( $profile_ids{ $data[1] } ) ) {
-			my $insp = $dbh->prepare('INSERT INTO PROFILE ("name", "description") VALUES(?, ?);');
-			$insp->bind_param( 1, "MIDMSO_" . $data[1] );
-			$insp->bind_param( 2, "Profile for " . $data[1] . " MSO settings" );
-			$insp->execute();
-			my $profile_id = $dbh->last_insert_id( undef, undef, "profile", undef );
-			$profile_ids{ $data[1] } = $profile_id;
-		}
-		if ( $line =~ /set-config proxy.config.http.parent_origin/ ) {
-			my $setting = $line;
-			$setting =~ s/set-config //;
-			print $data[1] . " ->" . $setting . "\n";
-			$setting =~ s/^ *//;
-			my ( $name, $value ) = split( /\s+/, $setting );
-			print $name . " -> " . $value . "\n";
-			my $insh = $dbh->prepare('INSERT INTO PARAMETER ("name", "config_file", "value") VALUES (?, ?, ?);');
-			$insh->bind_param( 1, $name );
-			$insh->bind_param( 2, 'parent.config' );
-			$insh->bind_param( 3, $value );
-			$insh->execute();
-			my $param_id = $dbh->last_insert_id( undef, undef, "parameter", undef );
-			print "Last inserted: " . $param_id;
-			my $inspp = $dbh->prepare('INSERT INTO PROFILE_PARAMETER ("parameter", "profile") VALUES (?, ?);');
-			$inspp->bind_param( 1, $param_id );
-			$inspp->bind_param( 2, $profile_ids{ $data[1] } );
-			$inspp->execute();
+	if ( !defined( $profile_ids{ $data[1] } ) ) {
+		my $insp = $dbh->prepare('INSERT INTO PROFILE ("name", "description") VALUES(?, ?);');
+		$insp->bind_param( 1, "MSO_SELECT_" . $data[1] );
+		$insp->bind_param( 2, "Profile for " . $data[1] . " MSO settings" );
+		$insp->execute();
+		my $profile_id = $dbh->last_insert_id( undef, undef, "profile", undef );
+		$profile_ids{ $data[1] } = $profile_id;
+	}
+	if ( defined( $data[2] ) ) {
+		my @lines = split( /__RETURN__/, $data[2] );
+		foreach my $line (@lines) {
+			if ( $line =~ /set-config proxy.config.http.parent_origin/ ) {
+				my $setting = $line;
+				$setting =~ s/set-config\s+proxy.config.http.parent_origin.//;
+				print $data[1] . " ->" . $setting . "\n";
+				$setting =~ s/^ *//;
+				my ( $name, $value ) = split( /\s+/, $setting );
+				print $name . " -> " . $value . "\n";
+				my $insh = $dbh->prepare('INSERT INTO PARAMETER ("name", "config_file", "value") VALUES (?, ?, ?);');
+				$insh->bind_param( 1, $name );
+				$insh->bind_param( 2, 'parent.config' );
+				$insh->bind_param( 3, $value );
+				$insh->execute();
+				my $param_id = $dbh->last_insert_id( undef, undef, "parameter", undef );
+				print "Last inserted: " . $param_id;
+				my $inspp = $dbh->prepare('INSERT INTO PROFILE_PARAMETER ("parameter", "profile") VALUES (?, ?);');
+				$inspp->bind_param( 1, $param_id );
+				$inspp->bind_param( 2, $profile_ids{ $data[1] } );
+				$inspp->execute();
+			}
 		}
 	}
+	my $insh = $dbh->prepare('INSERT INTO PARAMETER ("name", "config_file", "value") VALUES (?, ?, ?);');
+	$insh->bind_param( 1, "algorithm" );
+	$insh->bind_param( 2, 'parent.config' );
+	my $mso_num = defined( $data[4] ) ? $data[4] : 0;
+	$insh->bind_param( 3, $mso_algorithm_choices[$mso_num] );
+	$insh->execute();
+	my $param_id = $dbh->last_insert_id( undef, undef, "parameter", undef );
+	print "Last inserted: " . $param_id;
+	my $inspp = $dbh->prepare('INSERT INTO PROFILE_PARAMETER ("parameter", "profile") VALUES (?, ?);');
+	$inspp->bind_param( 1, $param_id );
+	$inspp->bind_param( 2, $profile_ids{ $data[1] } );
+	$inspp->execute();
 }
 
 $dbh->disconnect();
