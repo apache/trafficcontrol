fb7c63f4e9b31b44bc3c01b463b9c5bd4de0ee33
Jan van Doorn <jan_vandoorn@cable.comcast.com>
2016-12-31 13:02:44 -0700

    Add type ENUM and set the profile for DS in script

--- traffic_ops/app/db/ats_6_mso_migrate.pl
+++ traffic_ops/app/db/ats_6_mso_migrate.pl
@@ -17,7 +17,7 @@
 # Script to create profiles / parameters from the header_rewrite settings to move to ATS 6.2
 # support.
 #
-# Please be careful using this script, it was created for a very specific usecase, and only
+# Please be careful using this script, it was created for a very specific use case, and only
 # tested our env - JvD
 
 use strict;
@@ -33,34 +33,22 @@ my $dsn = "DBI:$driver:dbname=$database;host=127.0.0.1;port=5432";
 my $dbh = DBI->connect( $dsn, $userid, $password, { RaiseError => 1 } )
 	or die $DBI::errstr;
 
-# elsif ( $multi_site_origin_algorithm == 1 ) {
-# 					$mso_algorithm = "false";
-# 				}
-# 				elsif ( $multi_site_origin_algorithm == 2 ) {
-# 					$mso_algorithm = "strict";
-# 				}
-# 				elsif ( $multi_site_origin_algorithm == 3 ) {
-# 					$mso_algorithm = "true";
-# 				}
-# 				elsif ( $multi_site_origin_algorithm == 4 ) {
-# 					$mso_algorithm = "latched";
-# 				}
-# 				else {
-# 					$mso_algorithm = "consistent_hash";
 my @mso_algorithm_choices = ( "consistent_hash", "false", "strict", "true", "latched" );
-my $sql = 'select id,xml_id,mid_header_rewrite,multi_site_origin,multi_site_origin_algorithm from deliveryservice where multi_site_origin=true;';
+my $sql = 'select id,xml_id,mid_header_rewrite,multi_site_origin,multi_site_origin_algorithm,cdn_id from deliveryservice where multi_site_origin=true;';
 
+my %profile_ids;
 my $sth = $dbh->prepare_cached($sql);
 $sth->execute || die "Couldn't execute statement: " . $sth->errstr;
 while ( my @data = $sth->fetchrow_array() ) {
 	if ( defined( $data[3] ) && !$data[3] ) {
 		next;
 	}
-	my %profile_ids;
 	if ( !defined( $profile_ids{ $data[1] } ) ) {
-		my $insp = $dbh->prepare('INSERT INTO PROFILE ("name", "description") VALUES(?, ?);');
-		$insp->bind_param( 1, "MSO_SELECT_" . $data[1] );
-		$insp->bind_param( 2, "Profile for " . $data[1] . " MSO settings" );
+		my $insp = $dbh->prepare('INSERT INTO PROFILE ("name", "description", "type", "cdn") VALUES(?, ?, ?, ?);');
+		$insp->bind_param( 1, "DS_" . $data[1] );
+		$insp->bind_param( 2, "Profile for delivery service " . $data[1] );
+		$insp->bind_param( 3, "DS_PROFILE");
+		$insp->bind_param( 4, $data[5]);
 		$insp->execute();
 		my $profile_id = $dbh->last_insert_id( undef, undef, "profile", undef );
 		$profile_ids{ $data[1] } = $profile_id;
@@ -70,22 +58,22 @@ while ( my @data = $sth->fetchrow_array() ) {
 		foreach my $line (@lines) {
 			if ( $line =~ /set-config proxy.config.http.parent_origin/ ) {
 				my $setting = $line;
-				$setting =~ s/set-config\s+proxy.config.http.parent_origin.//;
-				print $data[1] . " ->" . $setting . "\n";
+				$setting =~ s/set-config\s+proxy.config.http.//;
+				# print $data[1] . " ->" . $setting . "\n";
 				$setting =~ s/^ *//;
 				my ( $name, $value ) = split( /\s+/, $setting );
-				print $name . " -> " . $value . "\n";
+				# print $name . " -> " . $value . "\n";
 				my $insh = $dbh->prepare('INSERT INTO PARAMETER ("name", "config_file", "value") VALUES (?, ?, ?);');
 				$insh->bind_param( 1, $name );
 				$insh->bind_param( 2, 'parent.config' );
 				$insh->bind_param( 3, $value );
 				$insh->execute();
 				my $param_id = $dbh->last_insert_id( undef, undef, "parameter", undef );
-				print "Last inserted: " . $param_id;
+				# print "Last inserted: " . $param_id;
 				my $inspp = $dbh->prepare('INSERT INTO PROFILE_PARAMETER ("parameter", "profile") VALUES (?, ?);');
 				$inspp->bind_param( 1, $param_id );
 				$inspp->bind_param( 2, $profile_ids{ $data[1] } );
-				$inspp->execute();
+				$inspp->execute() || die "Couldn't execute statement: " . $inspp->errstr;
 			}
 		}
 	}
@@ -94,13 +82,26 @@ while ( my @data = $sth->fetchrow_array() ) {
 	$insh->bind_param( 2, 'parent.config' );
 	my $mso_num = defined( $data[4] ) ? $data[4] : 0;
 	$insh->bind_param( 3, $mso_algorithm_choices[$mso_num] );
-	$insh->execute();
+	$insh->execute() || die "Couldn't execute statement: " . $insh->errstr;
 	my $param_id = $dbh->last_insert_id( undef, undef, "parameter", undef );
-	print "Last inserted: " . $param_id;
+	# print "Last inserted: " . $param_id;
 	my $inspp = $dbh->prepare('INSERT INTO PROFILE_PARAMETER ("parameter", "profile") VALUES (?, ?);');
 	$inspp->bind_param( 1, $param_id );
 	$inspp->bind_param( 2, $profile_ids{ $data[1] } );
-	$inspp->execute();
+	$inspp->execute() || die "Couldn't execute statement: " . $inspp->errstr;
+}
+
+# drop all CCR profiles
+my $upd = $dbh->prepare('UPDATE deliveryservice SET profile=NULL;');
+$upd->execute() || die "Couldn't execute statement: " . $upd->errstr;
+
+# and update the ones that have a delivery service profile.
+foreach my $deliveryservice ( keys %profile_ids ) {
+	my $upd = $dbh->prepare('UPDATE deliveryservice SET profile=? WHERE xml_id=?;');
+	$upd->bind_param( 1, $profile_ids{$deliveryservice} );
+	$upd->bind_param( 2, $deliveryservice );
+	$upd->execute() || die "Couldn't execute statement: " . $upd->errstr;
 }
 
 $dbh->disconnect();
+
--- traffic_ops/app/db/migrations/20161226000000_cdn_domain_name.sql
+++ traffic_ops/app/db/migrations/20161226000000_cdn_domain_name.sql
@@ -16,6 +16,15 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 
+-- INSERT INTO TYPE (name, description, use_in_table) VALUES ('SERVER_PROFILE', 'Profile to be assigned to server', 'profile');
+-- INSERT INTO TYPE (name, description, use_in_table) VALUES ('DS_PROFILE', 'Profile to be assigned to deliveryservice', 'profile');
+-- better to use ENUMs, I think
+
+CREATE TYPE profile_type AS ENUM ('SERVER_PROFILE', 'DS_PROFILE');
+ALTER TABLE public.profile ADD COLUMN type profile_type;
+UPDATE public.profile SET type='SERVER_PROFILE';
+ALTER TABLE public.profile ALTER type SET NOT NULL;
+
 ALTER TABLE public.cdn ADD COLUMN domain_name text;
 UPDATE cdn SET domain_name=domainlist.value
   FROM (SELECT distinct cdn_id,value FROM server,parameter WHERE type=(SELECT id FROM type WHERE name='EDGE') 
@@ -27,6 +36,7 @@ UPDATE public.cdn SET domain_name='-' WHERE name='ALL';
 ALTER TABLE public.cdn ALTER COLUMN domain_name SET NOT NULL;
 
 ALTER TABLE public.profile ADD COLUMN cdn bigint;
+
 ALTER TABLE public.profile
   ADD CONSTRAINT fk_cdn1 FOREIGN KEY (cdn)
       REFERENCES public.cdn (id) MATCH SIMPLE
@@ -45,9 +55,18 @@ UPDATE profile set cdn=domainlist.cdn_id
     AND parameter.id in (select parameter from profile_parameter where profile=profile.id)) as domainlist
 WHERE id = domainlist.profile_id;
 
+ALTER TABLE deliveryservice ALTER profile DROP NOT NULL;
+
+
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
 ALTER TABLE public.cdn DROP COLUMN domain_name;
 
 ALTER TABLE public.profile DROP COLUMN cdn;
+
+ALTER TABLE deliveryservice ALTER profile SET NOT NULL;
+
+ALTER TABLE public.profile DROP COLUMN type;
+
+DROP type profile_type;
