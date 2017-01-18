2b74c9413b950c2c1ece9c154811376fe7fdfb1e
Derek Gelinas <derek_gelinas@cable.comcast.com>
2017-01-18 14:35:01 -0500

    Create ATS Config API for master branch

--- traffic_ops/app/lib/TrafficOpsRoutes.pm
+++ traffic_ops/app/lib/TrafficOpsRoutes.pm
@@ -474,6 +474,12 @@ sub api_routes {
 	$r->get("/api/$version/logs/:days/days")->over( authenticated => 1 )->to( 'ChangeLog#index', namespace => $namespace );
 	$r->get("/api/$version/logs/newcount")->over( authenticated => 1 )->to( 'ChangeLog#newlogcount', namespace => $namespace );
 
+	# -- CONFIG FILES
+	$r->get("/api/$version/server/:id/configfiles/ats")->over( authenticated => 1 )->to ( 'ApacheTrafficServer#ort', namespace => 'API::Configs' );
+	$r->get("/api/$version/profile/:id/configfiles/ats/#filename")->over( authenticated => 1 )->to ( 'ApacheTrafficServer#get_profile_config', namespace => 'API::Configs' );
+	$r->get("/api/$version/server/:id/configfiles/ats/#filename")->over( authenticated => 1 )->to ( 'ApacheTrafficServer#get_server_config', namespace => 'API::Configs' );
+	$r->get("/api/$version/cdn/:id/configfiles/ats/#filename")->over( authenticated => 1 )->to ( 'ApacheTrafficServer#get_cdn_config', namespace => 'API::Configs' );
+
 	# -- DELIVERYSERVICES
 	# -- DELIVERYSERVICES: CRUD
 	$r->get("/api/$version/deliveryservices")->over( authenticated => 1 )->to( 'Deliveryservice#index', namespace => $namespace );
