46a32b859c913c8ec95eb3cef619ae670cedd28e
Robert Butts <robert.o.butts@gmail.com>
2017-02-14 17:17:13 -0700

    Fix TM2 to overwrite monitoring.json with CRConfig

--- traffic_monitor_golang/traffic_monitor/trafficopswrapper/trafficopswrapper.go
+++ traffic_monitor_golang/traffic_monitor/trafficopswrapper/trafficopswrapper.go
@@ -20,9 +20,11 @@ package trafficopswrapper
  */
 
 import (
+	"encoding/json"
 	"fmt"
 	"sync"
 
+	"github.com/apache/incubator-trafficcontrol/traffic_monitor_golang/traffic_monitor/crconfig"
 	to "github.com/apache/incubator-trafficcontrol/traffic_ops/client"
 )
 
@@ -34,6 +36,7 @@ type ITrafficOpsSession interface {
 	URL() (string, error)
 	User() (string, error)
 	Servers() ([]to.Server, error)
+	Profiles() ([]to.Profile, error)
 	Parameters(profileName string) ([]to.Parameter, error)
 	DeliveryServices() ([]to.DeliveryService, error)
 	CacheGroups() ([]to.CacheGroup, error)
@@ -83,8 +86,8 @@ func (s TrafficOpsSessionThreadsafe) CRConfigRaw(cdn string) ([]byte, error) {
 	return b, e
 }
 
-// TrafficMonitorConfigMap returns the Traffic Monitor config map from the Traffic Ops. This is safe for multiple goroutines.
-func (s TrafficOpsSessionThreadsafe) TrafficMonitorConfigMap(cdn string) (*to.TrafficMonitorConfigMap, error) {
+// TrafficMonitorConfigMapRaw returns the Traffic Monitor config map from the Traffic Ops, directly from the monitoring.json endpoint. This is not usually what is needed, rather monitoring needs the snapshotted CRConfig data, which is filled in by `TrafficMonitorConfigMap`. This is safe for multiple goroutines.
+func (s TrafficOpsSessionThreadsafe) TrafficMonitorConfigMapRaw(cdn string) (*to.TrafficMonitorConfigMap, error) {
 	s.m.Lock()
 	defer s.m.Unlock()
 	if s.session == nil || *s.session == nil {
@@ -92,6 +95,185 @@ func (s TrafficOpsSessionThreadsafe) TrafficMonitorConfigMap(cdn string) (*to.Tr
 	}
 	d, e := (*s.session).TrafficMonitorConfigMap(cdn)
 	return d, e
+}
+
+// TrafficMonitorConfigMap returns the Traffic Monitor config map from the Traffic Ops. This is safe for multiple goroutines.
+func (s TrafficOpsSessionThreadsafe) TrafficMonitorConfigMap(cdn string) (*to.TrafficMonitorConfigMap, error) {
+	mc, err := s.TrafficMonitorConfigMapRaw(cdn)
+	if err != nil {
+		fmt.Printf("DEBUG4 TrafficMonitorConfigMap err: %v\n", err)
+		return nil, fmt.Errorf("getting monitor config map: %v", err)
+	}
+
+	crcData, err := s.CRConfigRaw(cdn)
+	if err != nil {
+		fmt.Printf("DEBUG4 CRConfigRaw err: %v\n", err)
+		return nil, fmt.Errorf("getting CRConfig: %v", err)
+	}
+
+	crConfig := crconfig.CRConfig{}
+	if err := json.Unmarshal(crcData, &crConfig); err != nil {
+		fmt.Printf("DEBUG4 CRConfig Unmarshal err: %v\n", err)
+		return nil, fmt.Errorf("Error unmarshalling CRConfig JSON: %v", err)
+		return nil, err
+	}
+
+	mc, err = CreateMonitorConfig(crConfig, mc)
+	if err != nil {
+		fmt.Printf("DEBUG4 CreateMonitorConfig err: %v\n", err)
+		return nil, fmt.Errorf("Error creating Traffic Monitor Config: %v", err)
+	}
+
+	// mcMap, err := to.TrafficMonitorTransformToMap(mc)
+	// if err != nil {
+	// 	fmt.Printf("DEBUG4 TrafficMonitorTransformToMap err: %v\n", err)
+	// 	return nil, fmt.Errorf("Error transforming Traffic Monitor Config to Map: %v", err)
+	// }
+
+	// debug
+
+	// if bytes, err := json.Marshal(mcMap); err != nil {
+	// 	fmt.Printf("DEBUG4 error marshalling map: %v\n", err)
+	// } else {
+	// 	fmt.Printf("DEBUG4 New Map: %v\n\n", string(bytes))
+	// }
+
+	return mc, nil
+}
+
+func CreateMonitorConfig(crConfig crconfig.CRConfig, mc *to.TrafficMonitorConfigMap) (*to.TrafficMonitorConfigMap, error) {
+	// mc := to.TrafficMonitorConfig{}
+
+	// cgs, err := s.CacheGroups()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Error getting CacheGroups: %v", err)
+	// }
+
+	// allProfiles, err := s.Profiles()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Error getting Profiles: %v", err)
+	// }
+
+	// Dump the "live" monitoring.json servers, and populate with the "snapshotted" CRConfig
+	mc.TrafficServer = map[string]to.TrafficServer{}
+	for name, srv := range crConfig.ContentServers {
+		mc.TrafficServer[name] = to.TrafficServer{
+			Profile:       *srv.Profile,
+			IP:            *srv.Ip,
+			Status:        string(*srv.Status),
+			CacheGroup:    *srv.CacheGroup,
+			IP6:           *srv.Ip6,
+			Port:          *srv.Port,
+			HostName:      name,
+			FQDN:          *srv.Fqdn,
+			InterfaceName: *srv.InterfaceName,
+			Type:          *srv.ServerType,
+			HashID:        *srv.HashId,
+		}
+	}
+
+	// for _, cg := range cgs {
+	// 	mc.CacheGroups = append(mc.CacheGroups, to.TMCacheGroup{
+	// 		Name: cg.Name,
+	// 		Coordinates: to.Coordinates{
+	// 			Latitude:  cg.Latitude,
+	// 			Longitude: cg.Longitude,
+	// 		},
+	// 	})
+	// }
+
+	// monitorProfile := ""
+
+	// Dump the "live" monitoring.json monitors, and populate with the "snapshotted" CRConfig
+	mc.TrafficMonitor = map[string]to.TrafficMonitor{}
+	for name, mon := range crConfig.Monitors {
+		// monitorProfile = *mon.Profile
+		mc.TrafficMonitor[name] = to.TrafficMonitor{
+			Port:     *mon.Port,
+			IP6:      *mon.IP6,
+			IP:       *mon.IP,
+			HostName: name,
+			FQDN:     *mon.FQDN,
+			Profile:  *mon.Profile,
+			Location: *mon.Location,
+			Status:   string(*mon.Status),
+		}
+	}
+
+	// monitorParams, err := s.Parameters(monitorProfile)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Error getting profile %v parameters: %v", monitorProfile, err)
+	// }
+	// mc.Config = map[string]interface{}{}
+	// for _, param := range monitorParams {
+	// 	if numParam, err := strconv.ParseFloat(param.Value, 64); err == nil {
+	// 		mc.Config[param.Name] = numParam
+	// 	} else {
+	// 		mc.Config[param.Name] = param.Value
+	// 	}
+	// }
+
+	// Dump the "live" monitoring.json DeliveryServices, and populate with the "snapshotted" CRConfig
+	mc.DeliveryService = map[string]to.TMDeliveryService{}
+	for name, _ := range crConfig.DeliveryServices {
+		mc.DeliveryService[name] = to.TMDeliveryService{
+			XMLID:              name,
+			TotalTPSThreshold:  0,          // TODO verify
+			Status:             "Reported", // TODO verify
+			TotalKbpsThreshold: 0,          // TODO verify
+		}
+	}
+
+	// mc.Profiles = []to.TMProfile{}
+	// for _, prof := range allProfiles {
+	// 	if strings.HasPrefix(prof.Name, "EDGE") || strings.HasPrefix(prof.Name, "TEAK") {
+	// 		mc.Profiles = append(mc.Profiles, to.TMProfile{
+	// 			Name: prof.Name,
+	// 			Type: "EDGE",
+	// 		})
+	// 	} else if strings.HasPrefix(prof.Name, "MID") {
+	// 		mc.Profiles = append(mc.Profiles, to.TMProfile{
+	// 			Name: prof.Name,
+	// 			Type: "MID",
+	// 		})
+	// 	}
+	// }
+	// for profI, prof := range mc.Profiles {
+	// 	// MID2_TOP_v5.3.2-757
+	// 	prof.Parameters.Thresholds = map[string]to.HealthThreshold{}
+	// 	// TODO lock
+	// 	params, err := s.Parameters(prof.Name)
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("Error getting profile %v parameters: %v", prof, err)
+	// 	}
+	// 	fmt.Printf("DEBUG5 profile %v len(params) %v\n", prof.Name, len(params))
+	// 	for _, param := range params {
+	// 		if param.Name == "health.connection.timeout" {
+	// 			i, err := strconv.Atoi(param.Value)
+	// 			if err != nil {
+	// 				return nil, fmt.Errorf("Error getting profile %v parameter %v: %v", prof, param.Name, err)
+	// 			}
+	// 			prof.Parameters.HealthConnectionTimeout = i
+	// 		} else if param.Name == "health.polling.url" {
+	// 			prof.Parameters.HealthPollingURL = param.Value
+	// 		} else if param.Name == "history.count" {
+	// 			i, err := strconv.Atoi(param.Value)
+	// 			if err != nil {
+	// 				return nil, fmt.Errorf("Error getting profile %v parameter %v: %v", prof, param.Name, err)
+	// 			}
+	// 			prof.Parameters.HistoryCount = i
+	// 		} else if strings.HasPrefix(param.Name, "health.threshold.") {
+	// 			stat := param.Name[len("health.threshold."):]
+	// 			thresh, err := to.StrToThreshold(param.Value)
+	// 			if err != nil {
+	// 				return nil, fmt.Errorf("Error getting profile %v parameter %v: %v", prof, param.Name, err)
+	// 			}
+	// 			prof.Parameters.Thresholds[stat] = thresh
+	// 		}
+	// 	}
+	// 	mc.Profiles[profI] = prof
+	// }
+	return mc, nil
 }
 
 // Set sets the internal Traffic Ops session. This is safe for multiple goroutines, being aware they will race.
@@ -108,6 +290,15 @@ func (s TrafficOpsSessionThreadsafe) Servers() ([]to.Server, error) {
 		return nil, ErrNilSession
 	}
 	return (*s.session).Servers()
+}
+
+func (s TrafficOpsSessionThreadsafe) Profiles() ([]to.Profile, error) {
+	s.m.Lock()
+	defer s.m.Unlock()
+	if s.session == nil || *s.session == nil {
+		return nil, ErrNilSession
+	}
+	return (*s.session).Profiles()
 }
 
 func (s TrafficOpsSessionThreadsafe) Parameters(profileName string) ([]to.Parameter, error) {
