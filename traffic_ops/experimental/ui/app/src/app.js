393fcb2662c019526a871a2ea9688fc153533663
Jeremy Mitchell <mitchell852@gmail.com>
2017-02-15 22:04:48 -0700

    adds a generic dialog with a dropdown

--- traffic_ops/experimental/ui/app/src/app.js
+++ traffic_ops/experimental/ui/app/src/app.js
@@ -152,6 +152,7 @@ var trafficOps = angular.module('trafficOps', [
         require('./common/modules/dialog/confirm').name,
         require('./common/modules/dialog/delete').name,
         require('./common/modules/dialog/reset').name,
+        require('./common/modules/dialog/select').name,
         require('./common/modules/header').name,
         require('./common/modules/message').name,
         require('./common/modules/navigation').name,
