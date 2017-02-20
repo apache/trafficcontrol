13f4d0fa692670d8846e8d5d0b72ff8e56c3d0c9
Derek Gelinas <derek_gelinas@cable.comcast.com>
2017-02-20 10:11:00 -0500

    fix the license

--- traffic_ops/app/lib/Schema/Result/Deliveryservice.pm
+++ traffic_ops/app/lib/Schema/Result/Deliveryservice.pm
@@ -1,22 +1,23 @@
 use utf8;
-#
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#
-#
-
 package Schema::Result::Deliveryservice;
+
+# Licensed to the Apache Software Foundation (ASF) under one     +# Licensed under the Apache License, Version 2.0 (the "License");
+# or more contributor license agreements.  See the NOTICE file     +# you may not use this file except in compliance with the License.
+# distributed with this work for additional information    +# You may obtain a copy of the License at
+# regarding copyright ownership.  The ASF licenses this file     +#
+# to you under the Apache License, Version 2.0 (the    +#     http://www.apache.org/licenses/LICENSE-2.0
+# "License"); you may not use this file except in compliance     +#
+# with the License.  You may obtain a copy of the License at     +# Unless required by applicable law or agreed to in writing, software
+#      +# distributed under the License is distributed on an "AS IS" BASIS,
+#   http://www.apache.org/licenses/LICENSE-2.0     +# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#      +# See the License for the specific language governing permissions and
+# Unless required by applicable law or agreed to in writing,     +# limitations under the License.
+# software distributed under the License is distributed on an    +#
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY     +#
+# KIND, either express or implied.  See the License for the    +#
+# specific language governing permissions and limitations   
+# under the License.
+
 
 # Created by DBIx::Class::Schema::Loader
 # DO NOT MODIFY THE FIRST PART OF THIS FILE
