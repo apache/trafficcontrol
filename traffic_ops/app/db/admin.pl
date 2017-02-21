560323626423d32dd53effce5cb1da6a99bb6abf
Dan Kirkwood <dangogh@gmail.com>
2017-02-21 09:52:07 -0700

    require user to run as db superuser -- not necessarily postgres user

--- traffic_ops/app/db/admin.pl
+++ traffic_ops/app/db/admin.pl
@@ -159,7 +159,7 @@ sub migrate {
 	my ($command) = @_;
 
 	print "Migrating database...\n";
-	if ( system( "goose --env=$environment $command" ) != 0 ) {
+	if ( system("goose --env=$environment $command") != 0 ) {
 		die "Can't run goose\n";
 	}
 }
@@ -186,8 +186,8 @@ sub dropdb {
 
 sub createdb {
 	createuser();
-	my $db_exists = `psql -h $host_ip -p $host_port -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$db_name'"`;
-	if ( $db_exists ) {
+	my $db_exists = `psql -h $host_ip -p $host_port -tAc "SELECT 1 FROM pg_database WHERE datname='$db_name'"`;
+	if ($db_exists) {
 		print "Database $db_name already exists\n";
 		return;
 	}
@@ -198,26 +198,26 @@ sub createdb {
 }
 
 sub createuser {
-	my $user_exists = `psql -h $host_ip -p $host_port -U postgres postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$db_username'"`;
-	if ( $user_exists ) {
+	my $user_exists = `psql -h $host_ip -p $host_port postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$db_username'"`;
+	if ($user_exists) {
 		print "Role $db_username already exists\n";
 		return;
 	}
 
 	my $cmd = "CREATE USER $db_username WITH SUPERUSER CREATEROLE CREATEDB ENCRYPTED PASSWORD '$db_password'";
-	if ( system( qq{psql -h $host_ip -p $host_port -U postgres -tAc "$cmd"} ) != 0 ) {
+	if ( system(qq{psql -h $host_ip -p $host_port -tAc "$cmd"}) != 0 ) {
 		die "Can't create user $db_username\n";
 	}
 }
 
 sub dropuser {
-	if ( system("dropuser -h $host_ip -p $host_port -U postgres -i -e $db_username;") != 0 ) {
+	if ( system("dropuser -h $host_ip -p $host_port -i -e $db_username;") != 0 ) {
 		die "Can't drop user $db_username\n";
 	}
 }
 
 sub showusers {
-	if ( system("psql -h $host_ip -p $host_port -U postgres -ec '\\du';") != 0 ) {
+	if ( system("psql -h $host_ip -p $host_port -ec '\\du';") != 0 ) {
 		die "Can't show users";
 	}
 }
