c615fa42ff09d285a1995a196ba474dc7bf22a31
Amir Yeshurun <amiry@qwilt.com>
2017-02-26 22:51:53 +0200

    original webfront, auth services taken form https://github.com/rarenivar/project5799

---
+++ traffic_ops/experimental/webfront/startfakes.sh
@@ -0,0 +1,7 @@
+go run simpleserver/simpleserver.go  8001 &
+go run simpleserver/simpleserver.go  8002 &
+go run simpleserver/simpleserver.go  8003 &
+go run simpleserver/simpleserver.go  8004 &
+go run simpleserver/simpleserver.go  8005 &
+go run simpleserver/simpleserver.go  8006 &
+go run simpleserver/simpleserver.go  8007
