0e756b367d0abdc2387899d40d74d3c2e4cc58b9
Robert Butts <robert.o.butts@gmail.com>
2017-03-13 14:09:34 -0600

    Fix TM2 to mark System.NotAvailable caches polled

--- traffic_monitor_golang/traffic_monitor/threadsafe/polledcaches.go
+++ traffic_monitor_golang/traffic_monitor/threadsafe/polledcaches.go
@@ -119,7 +119,8 @@ func (t *UnpolledCaches) SetPolled(results []cache.Result, lastStats dsdata.Last
 				continue
 			}
 
-			if !result.Available || result.Error != nil {
+			// TODO fix "whether a cache has ever been polled" to be generic somehow. The result.System.NotAvailable check is duplicated in health.EvalCache, and is fragile. What if another "successfully polled but unavailable" flag were added?
+			if !result.Available || result.Error != nil || result.Astats.System.NotAvailable {
 				log.Debugf("polled %v\n", cache)
 				delete(unpolledCaches, cache)
 				break innerLoop
