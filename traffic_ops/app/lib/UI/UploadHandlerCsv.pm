8c5ac3b52626b5824ecae3965df1d3bc3bd5194b
nir-sopher <nirsopher@gmail.com>
2017-03-16 14:55:12 +0200

    Fix failure in t_integration/server.t by adding a replaceNamedLookupValues before check_server_input call

--- traffic_ops/app/lib/UI/UploadHandlerCsv.pm
+++ traffic_ops/app/lib/UI/UploadHandlerCsv.pm
@@ -326,6 +326,7 @@ sub processCSV {
 					}
 					else {
 						my $paramHashRef = &getParamHashRef( \@p, $lineNumber );
+						$paramHashRef = &replaceNamedLookupValues( $paramHashRef, $cdnHashRef, $cachegroupHashRef, $typeHashRef, $profileHashRef, $physLocationHashRef );
 
 						# print Dumper($paramHashRef);
 						$processCSVErrors .= &UI::Server::check_server_input( $self, $paramHashRef );
@@ -366,6 +367,7 @@ sub processCSV {
 				}
 				else {
 					my $paramHashRef = &getParamHashRef( \@p, $lineNumber );
+					$paramHashRef = &replaceNamedLookupValues( $paramHashRef, $cdnHashRef, $cachegroupHashRef, $typeHashRef, $profileHashRef, $physLocationHashRef );
 					$processCSVErrors .= &UI::Server::check_server_input( $self, $paramHashRef );
 					my $enteredCdn          = $p[9];
 					my $enteredCachegroup   = $p[10];
