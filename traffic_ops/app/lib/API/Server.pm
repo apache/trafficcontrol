7dd50c2d5ec25d8bedc7c8e9e4dab20db61e5a30
Robert Butts <robert.o.butts@gmail.com>
2017-03-17 10:31:27 -0600

    Add TO Server detail route CDN

--- traffic_ops/app/lib/API/Server.pm
+++ traffic_ops/app/lib/API/Server.pm
@@ -633,9 +633,8 @@ sub details_v11 {
 	my $isadmin   = &is_admin($self);
 	my $host_name = $self->param('name');
 	my $rs_data   = $self->db->resultset('Server')->search( { host_name => $host_name },
-		{ prefetch => [ 'cachegroup', 'type', 'profile', 'status', 'phys_location', 'hwinfos', { 'deliveryservice_servers' => 'deliveryservice' } ], } );
+																											 { prefetch => [ 'cdn', 'cachegroup', 'type', 'profile', 'status', 'phys_location', 'hwinfos', { 'deliveryservice_servers' => 'deliveryservice' } ], } );
 	while ( my $row = $rs_data->next ) {
-
 		my $serv = {
 			"id"             => $row->id,
 			"hostName"       => $row->host_name,
@@ -670,6 +669,7 @@ sub details_v11 {
 			"iloPassword"    => $isadmin ? $row->ilo_password : "********",
 			"routerHostName" => $row->router_host_name,
 			"routerPortName" => $row->router_port_name,
+			"cdnName"        => $row->cdn->name,
 		};
 		my $hw_rs = $row->hwinfos;
 		while ( my $hwinfo_row = $hw_rs->next ) {
