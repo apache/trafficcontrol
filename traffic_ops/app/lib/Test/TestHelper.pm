3cbbb81f6260087d0516f7e23dcec7da54d2f044
nir-sopher <nirsopher@gmail.com>
2017-03-20 10:41:08 +0200

    adding tenant to the user API

--- traffic_ops/app/lib/Test/TestHelper.pm
+++ traffic_ops/app/lib/Test/TestHelper.pm
@@ -65,6 +65,12 @@ use constant STEERING_PASSWORD_1 => 'password';
 use constant STEERING_USER_2 => 'steering2';
 use constant STEERING_PASSWORD_2 => 'password';
 
+use constant ADMIN_ROOT_USER          => 'admin-root';
+use constant ADMIN_ROOT_USER_PASSWORD => 'password';
+
+use constant PORTAL_ROOT_USER          => 'portal-root';
+use constant PORTAL_ROOT_USER_PASSWORD => 'password';
+
 sub load_all_fixtures {
 	my $self    = shift;
 	my $fixture = shift;
