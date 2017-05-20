6819ccde9842e874885e52240faca215319c06b3
Dan Kirkwood <dangogh@gmail.com>
2017-05-19 21:42:59 -0600

    add license to sql file
    (cherry picked from commit eb7ec6db3bb7c465dc808020b6d2614fec97a975)

--- traffic_ops/install/data/profiles/trafficserver_edge.sql
+++ traffic_ops/install/data/profiles/trafficserver_edge.sql
@@ -1,3 +1,18 @@
+/*
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
 INSERT INTO profile (name, description, type) VALUES ('EDGE_PROFILE','Edge Cache','ATS_PROFILE') ON CONFLICT (name) DO NOTHING;
 INSERT INTO parameter (name, config_file, value) VALUES ('algorithm','parent.config','consistent_hash') ON CONFLICT (name,config_file, value) DO NOTHING;
 INSERT INTO profile_parameter (profile, parameter) VALUES ( (select id from profile where name = 'EDGE_PROFILE'), (select id from parameter where name = 'algorithm' and config_file = 'parent.config' and value = 'consistent_hash') )  ON CONFLICT (profile, parameter) DO NOTHING;
