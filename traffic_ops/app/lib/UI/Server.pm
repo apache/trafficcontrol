64916261e2e7586ca0383e60af4dba331ac37d50
Derek Gelinas <derek_gelinas@cable.comcast.com>
2017-05-23 20:55:48 +0000

    fix some hash logic errors

--- traffic_ops/app/lib/UI/Server.pm
+++ traffic_ops/app/lib/UI/Server.pm
@@ -449,11 +449,10 @@ sub check_server_input {
 
 	my $profile = $self->db->resultset('Profile')->search( { 'me.id' => $paramHashRef->{'profile'}}, { prefetch => ['cdn'] } )->single();
 	my $cdn = $self->db->resultset('Cdn')->search( { 'me.id' => $paramHashRef->{'cdn'} } )->single();
-	my $profile_cdn = $profile->cdn;
-	if ( !defined($profile_cdn) ) {
+	if ( !defined($profile->cdn) ) {
 		$err .= "the " . $paramHashRef->{'profile'} . " profile is not in the " . $cdn->name . " CDN." . $sep;
 	}
-	if ( $profile_cdn != $cdn->id ) {
+	elsif ( $profile->cdn->id != $cdn->id ) {
 		$err .= "the " . $paramHashRef->{'profile'} . " profile is not in the " . $cdn->name . " CDN." . $sep;
 	}
 	return $err;
