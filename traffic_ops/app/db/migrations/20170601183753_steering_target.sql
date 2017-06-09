c25547c720661b530ac6d9b13193368f495c425c
Derek Gelinas <derek_gelinas@cable.comcast.com>
2017-06-09 17:36:22 +0000

    move patches from migration

--- traffic_ops/app/db/migrations/20170601183753_steering_target.sql
+++ traffic_ops/app/db/migrations/20170601183753_steering_target.sql
@@ -3,11 +3,10 @@
 -- SQL in section 'Up' is executed when this migration is applied
 ALTER TABLE steering_target RENAME COLUMN weight TO value;
 ALTER TABLE steering_target ADD COLUMN IF NOT EXISTS type bigint;
-ALTER TABLE steering_target ALTER COLUMN type SET NOT NULL;
 ALTER TABLE steering_target ADD CONSTRAINT steering_target_type_fkey
   FOREIGN KEY ("type")
   REFERENCES "type" (id);
-UPDATE steering_target SET "type" = (SELECT id FROM type WHERE name = 'STEERING_WEIGHT');
+
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
