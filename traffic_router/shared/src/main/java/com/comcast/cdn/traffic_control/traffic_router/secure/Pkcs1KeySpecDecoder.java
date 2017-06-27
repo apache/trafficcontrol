6ebd7a428fb7fe9025de12c061e2b1342040be46
David Neuman <david.neuman64@gmail.com>
2017-06-27 13:52:35 -0600

    Added bouncy castle provider to help convert PKCS1 keys to PKCS8.  OpenSSL only supports PKCS8 format keys and TO creates PCKS1 keys.

--- traffic_router/shared/src/main/java/com/comcast/cdn/traffic_control/traffic_router/secure/Pkcs1KeySpecDecoder.java
+++ traffic_router/shared/src/main/java/com/comcast/cdn/traffic_control/traffic_router/secure/Pkcs1KeySpecDecoder.java
@@ -15,6 +15,7 @@
 
 package com.comcast.cdn.traffic_control.traffic_router.secure;
 
+import org.apache.log4j.Logger;
 import sun.security.util.DerInputStream;
 import sun.security.util.DerValue;
 
@@ -22,17 +23,21 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.spec.KeySpec;
-import java.security.spec.RSAMultiPrimePrivateCrtKeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+//import java.security.spec.RSAMultiPrimePrivateCrtKeySpec;
 import java.security.spec.RSAPublicKeySpec;
 import java.util.Base64;
+
+import static com.comcast.cdn.traffic_control.traffic_router.secure.Pkcs1.FOOTER;
+import static com.comcast.cdn.traffic_control.traffic_router.secure.Pkcs1.HEADER;
 
 public class Pkcs1KeySpecDecoder {
 	// https://tools.ietf.org/html/rfc3447#appendix-A.1.1
 
-	static public final String HEADER = "-----BEGIN RSA PRIVATE KEY-----";
-	static public final String FOOTER = "-----END RSA PRIVATE KEY-----";
+
 	static final int PRIVATE_SEQUENCE_LENGTH = 9;
 	static final int PUBLIC_SEQUENCE_LENGTH = 2;
+	private static final Logger LOGGER = Logger.getLogger(Pkcs1KeySpecDecoder.class);
 
 	public KeySpec decode(final String data) throws IOException, GeneralSecurityException {
 		final String pemData = data.replaceAll(HEADER, "").replaceAll(FOOTER, "").replaceAll("\\s", "");
@@ -47,7 +52,7 @@ public class Pkcs1KeySpecDecoder {
 		if (derSequence.length == PUBLIC_SEQUENCE_LENGTH) {
 			final BigInteger n = derSequence[0].getBigInteger();
 			final BigInteger e = derSequence[1].getBigInteger();
-			return new RSAPublicKeySpec(n,e);
+			return new RSAPublicKeySpec(n, e);
 		}
 
 		// man 3 rsa
@@ -55,15 +60,23 @@ public class Pkcs1KeySpecDecoder {
 		// http://linux.die.net/man/3/rsa
 
 		// We don't need the version data at derSequence[0]
-		final BigInteger n = derSequence[1].getBigInteger();
-		final BigInteger e = derSequence[2].getBigInteger();
-		final BigInteger d = derSequence[3].getBigInteger();
-		final BigInteger p = derSequence[4].getBigInteger();
-		final BigInteger q = derSequence[5].getBigInteger();
-		final BigInteger dmp1 = derSequence[6].getBigInteger();
-		final BigInteger dmq1 = derSequence[7].getBigInteger();
-		final BigInteger iqmp = derSequence[8].getBigInteger();
+//		final BigInteger n = derSequence[1].getBigInteger();
+//		final BigInteger e = derSequence[2].getBigInteger();
+//		final BigInteger d = derSequence[3].getBigInteger();
+//		final BigInteger p = derSequence[4].getBigInteger();
+//		final BigInteger q = derSequence[5].getBigInteger();
+//		final BigInteger dmp1 = derSequence[6].getBigInteger();
+//		final BigInteger dmq1 = derSequence[7].getBigInteger();
+//		final BigInteger iqmp = derSequence[8].getBigInteger();
+//
+//		return new RSAMultiPrimePrivateCrtKeySpec(n, e, d, p, q, dmp1, dmq1, iqmp, null);
 
-		return new RSAMultiPrimePrivateCrtKeySpec(n, e, d, p, q, dmp1, dmq1, iqmp, null);
+		//Convert to PKCS8 since OpenSSL doesn't support PKCS1.  This works because of the BouncyCastle security provider.
+		try {
+			return new PKCS8EncodedKeySpec(Base64.getDecoder().decode((data.getBytes())));
+		} catch (Exception e) {
+			LOGGER.error("Error converting to PKCS8 Encoded Key Spec " + e.getClass().getCanonicalName() + ": " + e.getMessage(), e);
+		}
+		return null;
 	}
 }
