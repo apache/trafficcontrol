3f4c70166b507f1884c326ea90bfa569bb5fc95b
Dan Kirkwood <dangogh@gmail.com>
2017-07-13 16:17:46 -0600

    adds integration tests to docker setup for traffic_ops

--- traffic_ops/app/bin/tests/runtests.sh
+++ traffic_ops/app/bin/tests/runtests.sh
@@ -17,14 +17,10 @@
 # set up db based on given conf file
 
 set -x
-testdir=$1
-dbenv=$2
-dbhost=$3
-dbport=${4:-5432}
 export PERL5LIB=/opt/traffic_ops/app/lib:/opt/traffic_ops/app/local/lib/perl5
 
 usage() {
-        echo "Usage: $(basename $0) <test dir> <dbenv> <host> <port>"
+        echo "Usage: $(basename $0) <test dir> <test env> <host> <port>"
         echo "  e.g. $(basename $0) ./t test db 5432"
 }
 
@@ -36,41 +32,40 @@ finish() {
 
 trap finish EXIT
 
-dbconf=/opt/traffic_ops/app/conf/$dbenv/database.conf
+dbconf=/opt/traffic_ops/app/conf/$TESTENV/database.conf
 if [[ ! -f $dbconf ]]; then
         usage
         msg="$dbconf should be a file"
         exit 1
 fi
 
-if [[ ! -d $testdir ]]; then
+if [[ ! -d $TESTDIR ]]; then
         usage
-        msg="$testdir should be a directory"
+        msg="$TESTDIR should be a directory"
         exit 1
 fi
 
-while ! nc $dbhost $dbport </dev/null; do # &>/dev/null; do
-        dig $dbhost +short
-        echo "waiting for $dbhost:$dbport"
+while ! nc $DBHOST $DBPORT </dev/null; do # &>/dev/null; do
+        echo "waiting for $DBHOST:$DBPORT"
         sleep 3
 done
 
 # get dbname, user, password from database.conf;  update it with hostname of db container
-read dbname user pw <<<$(python -c "import json; d=json.load(open('$dbconf')); d['hostname']='$dbhost'; d['port']='$dbport'; json.dump(d, open('$dbconf', 'w')); print d['dbname'],d['user'],d['password']") || exit $?
+read dbname user pw <<<$(python -c "import json; d=json.load(open('$dbconf')); d['hostname']='$DBHOST'; d['port']='$DBPORT'; json.dump(d, open('$dbconf', 'w')); print d['dbname'],d['user'],d['password']") || exit $?
 
 # update dbconf.yml
-sed -E -i "s/host=[^ ]+/host=$dbhost/" /opt/traffic_ops/app/db/dbconf.yml
+sed -E -i "s/host=[^ ]+/host=$DBHOST/" /opt/traffic_ops/app/db/dbconf.yml
 
 # create user if doesn't exist
-st=$(psql -h$dbhost -p$dbport -Upostgres -tAc "SELECT 1 from pg_roles WHERE rolname='$user'") || exit $?
+st=$(psql -h$DBHOST -p$DBPORT -Upostgres -tAc "SELECT 1 from pg_roles WHERE rolname='$user'") || exit $?
 if [[ $st != 1 ]]; then
-        psql -h$dbhost -Upostgres -etAc "CREATE USER $user with LOGIN ENCRYPTED PASSWORD '$pw'" || exit $?
+        psql -h$DBHOST -Upostgres -etAc "CREATE USER $user with LOGIN ENCRYPTED PASSWORD '$pw'" || exit $?
 fi
 
 
-st=$(psql -h$dbhost -p$dbport -Upostgres -tAc "SELECT 1 FROM pg_database WHERE datname='$dbname'") || exit $?
+st=$(psql -h$DBHOST -p$DBPORT -Upostgres -tAc "SELECT 1 FROM pg_database WHERE datname='$dbname'") || exit $?
 if [[ $st != 1 ]]; then
-        createdb -h$dbhost -p$dbport -Upostgres -e --owner $user $dbname || exit $?
+        createdb -h$DBHOST -p$DBPORT -Upostgres -e --owner $user $dbname || exit $?
 fi
 
 cd /opt/traffic_ops/app
@@ -87,6 +82,6 @@ export GOPATH=/opt/traffic_ops/go
 PATH=$PATH:$GOPATH/bin:$GOROOT/bin
 
 export PGOPTIONS='--client-min-messages=warning'
-./db/admin.pl --env=test reset
+./db/admin.pl --env=$TESTENV reset
 
-prove -qrp $testdir
+prove -qrp $TESTDIR
