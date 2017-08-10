afcf2a2e319b8df7526d7fa22951d5ab4eaf13bf
Dylan Volz <Dylan_Volz@comcast.com>
2017-08-10 11:15:19 -0600

    add the ability to manage ssl keys to TPv2

---
+++ traffic_portal/app/src/common/api/DeliveryServiceSslKeysService.js
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+var DeliveryServiceSslKeysService = function($http, $q, locationUtils, messageModel, ENV) {
+	this.generateSslKeys = function(deliveryService, sslKeys, generateSslKeyForm) {
+		if (sslKeys.version != 'undefined'){
+			generateSslKeyForm.version = parseInt(sslKeys.version) + 1;
+		} else {
+			generateSslKeyForm.version = 1;
+		}
+
+		generateSslKeyForm.cdn = deliveryService.cdnName;
+		generateSslKeyForm.deliveryservice = deliveryService.id;
+		generateSslKeyForm.key = "ds_" + deliveryService.xmlId;
+
+		var request = $q.defer();
+        $http.post(ENV.api['root'] + "deliveryservices/sslkeys/generate", generateSslKeyForm)
+        .then(
+            function(result) {
+            	messageModel.setMessages([ { level: 'success', text: 'New SSL Keys generated' } ], true);
+                request.resolve(result.data.response);
+            },
+            function(fault) {
+            	messageModel.setMessages(fault.data.alerts, false);
+                request.reject(fault);
+            }
+        );
+        return request.promise;
+	};
+
+	this.addSslKeys = function(sslKeys) {
+		var request = $q.defer();
+        $http.post(ENV.api['root'] + "deliveryservices/sslkeys/add", sslKeys)
+        .then(
+            function(result) {
+            	messageModel.setMessages([ { level: 'success', text: 'New SSL Keys added' } ], false);
+                request.resolve(result.data.response);
+            },
+            function(fault) {
+            	messageModel.setMessages(fault.data.alerts, false);
+                request.reject(fault);
+            }
+        );
+        return request.promise;
+	};
+
+	this.getSslKeys = function(dsXmlId) {
+		var request = $q.defer();
+        $http.get(ENV.api['root'] + "deliveryservices/xmlId/" + dsXmlId + "/sslkeys")
+        .then(
+            function(result) {
+                request.resolve(result.data.response);
+            },
+            function(fault) {
+            	messageModel.setMessages(fault.data.alerts, false);
+                request.reject(fault);
+            }
+        );
+        return request.promise;
+	};
+};
+
+DeliveryServiceSslKeysService.$inject = ['$http', '$q', 'locationUtils', 'messageModel', 'ENV'];
+module.exports = DeliveryServiceSslKeysService;
