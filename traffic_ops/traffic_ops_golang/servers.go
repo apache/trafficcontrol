a699f51a233046544707b7c2b403a05859b9149b
Dewayne Richardson <dewrich@apache.org>
2017-09-07 08:33:52 -0600

    moved where clause back into handler

--- traffic_ops/traffic_ops_golang/servers.go
+++ traffic_ops/traffic_ops_golang/servers.go
@@ -74,7 +74,7 @@ func getServers(v url.Values, db *sqlx.DB, privLevel int) ([]Server, error) {
 	var rows *sqlx.Rows
 	var err error
 
-	wc := newWhereClause(v)
+	wc := newServersWhereClause(v)
 	query := SelectStatement{
 		Select: selectQuery(),
 		Where:  wc,
@@ -170,4 +170,37 @@ JOIN profile p ON s.profile = p.id
 JOIN status st ON s.status = st.id
 JOIN type t ON s.type = t.id`
 	return query
+}
+
+func newServersWhereClause(v url.Values) WhereClause {
+
+	whereClause := WhereClause{}
+
+	switch {
+	case v.Get("cachegroup") != "":
+		whereClause.SetCondition(Condition{"s.cachegroup", EQUAL, v.Get("cachegroup")})
+
+	// Support what should have been the cachegroupId as well
+	case v.Get("cachegroupId") != "":
+		whereClause.SetCondition(Condition{"s.cachegroup", EQUAL, v.Get("cachegroupId")})
+
+	case v.Get("cdn") != "":
+		whereClause.SetCondition(Condition{"s.cdn_id", EQUAL, v.Get("cdn")})
+
+	case v.Get("physLocation") != "":
+		whereClause.SetCondition(Condition{"s.phys_location", EQUAL, v.Get("physLocation")})
+
+	case v.Get("physLocationId") != "":
+		whereClause.SetCondition(Condition{"s.phys_location", EQUAL, v.Get("physLocationId")})
+
+	case v.Get("profileId") != "":
+		whereClause.SetCondition(Condition{"s.profile", EQUAL, v.Get("profileId")})
+
+	case v.Get("type") != "":
+		whereClause.SetCondition(Condition{"s.type", EQUAL, v.Get("type")})
+
+	case v.Get("typeId") != "":
+		whereClause.SetCondition(Condition{"s.type", EQUAL, v.Get("typeId")})
+	}
+	return whereClause
 }
