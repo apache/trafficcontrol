5f122613d11eb0b7f498e1f387596e46e6c9d52f
Dan Kirkwood <dangogh@gmail.com>
2017-09-14 14:57:58 -0600

    back out log dep

--- traffic_ops/tocookie/tocookie.go
+++ traffic_ops/tocookie/tocookie.go
@@ -21,8 +21,6 @@ import (
 	"fmt"
 	"strings"
 	"time"
-
-	"github.com/apache/incubator-trafficcontrol/traffic_monitor_golang/common/log"
 )
 
 const GeneratedByStr = "trafficcontrol-go-tocookie"
@@ -80,9 +78,6 @@ func Parse(secret, cookie string) (*Cookie, error) {
 	}
 
 	if cookieData.ExpiresUnix-time.Now().Unix() < 0 {
-		now := time.Now()
-		then := time.Unix(cookieData.ExpiresUnix, 0)
-		log.Errorf("signature expired: %s < %s", then.Format(time.RFC3339), now.Format(time.RFC3339))
 		return nil, fmt.Errorf("signature expired")
 	}
 
