ed5ae6107a1e19c2a7991f6d49bc5bd4ef48153c
Robert O Butts <rob@apache.org>
2017-09-15 13:06:21 -0600

    Grove Add service, RPM

---
+++ grove/build/build_rpm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BUILDDIR="$HOME/rpmbuild"
+
+# prep build environment
+rm -rf $BUILDDIR
+mkdir -p $BUILDDIR/{BUILD,RPMS,SOURCES}
+echo "$BUILDDIR" > ~/.rpmmacros
+
+# build
+go build -v
+
+# tar
+tar -cvzf $BUILDDIR/SOURCES/grove-0.1.tgz grove grove.cfg grove.service
+
+# build RPM
+rpmbuild -ba grove.spec
+
+# copy build RPM to .
+cp $BUILDDIR/RPMS/x86_64/grove-0.1.x86_64.rpm .
