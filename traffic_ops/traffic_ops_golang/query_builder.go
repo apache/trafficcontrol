bf0804ae43ea6f947129dfe99e277ff713c9088c
Dewayne Richardson <dewrich@apache.org>
2017-09-25 10:13:12 -0600

    moved license comments up

--- traffic_ops/traffic_ops_golang/query_builder.go
+++ traffic_ops/traffic_ops_golang/query_builder.go
@@ -1,15 +1,9 @@
 package main
 
-import (
-	"net/url"
-
-	"github.com/apache/incubator-trafficcontrol/traffic_monitor_golang/common/log"
-)
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
+ * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
@@ -23,7 +17,13 @@ import (
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
-*/
+ */
+
+import (
+	"net/url"
+
+	"github.com/apache/incubator-trafficcontrol/traffic_monitor_golang/common/log"
+)
 
 func BuildQuery(v url.Values, selectStmt string, queryParamsToSQLCols map[string]string) (string, map[string]interface{}) {
 	var sqlQuery string
