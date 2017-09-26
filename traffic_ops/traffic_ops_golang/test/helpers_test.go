ec8c64db085316c12e606a50c43006cab53132ad
Dan Kirkwood <dangogh@gmail.com>
2017-09-26 12:54:34 -0600

    add test for test helper

---
+++ traffic_ops/traffic_ops_golang/test/helpers_test.go
@@ -0,0 +1,42 @@
+package test
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import "testing"
+
+type thingy struct {
+	Cat string `json:"cat" db:"cat"`
+	Hat int    `json:"hat" db:"hat"`
+}
+
+// Extract the tag annotations from a struct into a string array
+func TestColsFromStructByTag(t *testing.T) {
+	res := ColsFromStructByTag("db", thingy{})
+	cols := []string{"cat", "hat"}
+
+	if len(res) != len(cols) {
+		t.Errorf("Expected %d columns, got %d", len(cols), len(res))
+	}
+	for i, c := range cols {
+		if c != res[i] {
+			t.Errorf("Expected %s, got %s", c, res[i])
+		}
+	}
+}
