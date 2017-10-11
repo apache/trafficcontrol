1703f089eaafe2775907975c2b3629a11cc2dca3
Kevin Mackenzie <dev.kjmack305@gmail.com>
2017-10-11 11:44:17 -0400

    Added Post method to cfg API
    This route takes a json representation of the config information and add it to the database.  This has not been tested with a request yet, because there is not yet a method in the ort script to do that.

--- traffic_ops/traffic_ops_golang/cfgdiffs.go
+++ traffic_ops/traffic_ops_golang/cfgdiffs.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"strconv"
+	"time"
 	
 	"github.com/apache/incubator-trafficcontrol/lib/go-log"
 
@@ -51,6 +52,68 @@ func cfgDiffsHandler(db *sqlx.DB) RegexHandlerFunc {
                 w.Header().Set("Content-Type", "application/json")
                 fmt.Fprintf(w, "%s", respBts);
         }
+}
+
+func postCfgDiffsHandler(db *sqlx.DB) AuthRegexHandlerFunc {
+        return func(w http.ResponseWriter, r *http.Request, p PathParams, username string, privLevel int) {
+                handleErr := func(err error, status int) {
+                        log.Errorf("%v %v\n", r.RemoteAddr, err)
+                        w.WriteHeader(status)
+                        fmt.Fprintf(w, http.StatusText(status))
+                }
+
+                serverID, err := strconv.ParseInt(p["id"], 10, 64)
+		if err != nil {
+			handleErr(err, http.StatusBadRequest)
+			return
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		var diffs CfgFileDiffs
+		err = decoder.Decode(&diffs)
+		if err != nil {
+			handleErr(err, http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+	
+                err = postCfgDiffs(db, serverID, diffs)
+                if err != nil {
+                        handleErr(err, http.StatusInternalServerError)
+                        return
+                }
+        }
+}
+
+func cfgDiffsExist(db *sqlx.DB, serverID int64, configName string) (bool, error) {
+	query := `SELECT 
+EXISTS(
+SELECT 1
+FROM config_diffs me 
+WHERE me.server_id = $1 AND me.config_name = $2)`
+
+	rows, err := db.Query(query, serverID, configName)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var exists sql.NullString
+		
+		err = rows.Scan(&exists)
+		if err != nil {
+			return false, err
+		}
+
+		if exists.String == "t" {
+			return true, nil
+		} else {
+			return false, nil
+		}
+	} else {
+		return false, nil;// this is an issue...
+	}
 }
 
 func getCfgDiffs(db *sqlx.DB, serverID int64) ([]CfgFileDiffs, error) {
@@ -104,4 +167,33 @@ func getCfgDiffsJson(serverID int64, db * sqlx.DB) ([]CfgFileDiffs, error) {
 	}
 	
 	return cfgDiffs, nil
+}
+
+func postCfgDiffs(db *sqlx.DB, serverID int64, diffs CfgFileDiffs) (error) {
+	query := `INSERT INTO 
+config_diffs(server_id, config_name, db_lines, local_lines, last_checked)
+VALUES($1, $2, json_array_elements_text($3), json_array_elements_text($4), $5)`
+		
+	dbLinesJson, err := json.Marshal(diffs.DBLines)
+	if err != nil {
+		return err
+	}
+	localLinesJson, err := json.Marshal(diffs.LocalLines)
+	if err != nil {
+		return err
+	}
+
+	rows, err := db.Query(query, 
+		serverID, 
+		diffs.FileName, 
+		dbLinesJson,
+		localLinesJson,
+		time.Now().UTC())
+
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	return nil
 }
