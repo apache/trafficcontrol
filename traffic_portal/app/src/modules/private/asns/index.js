2f4b8b0d9edd29eb7c37872b4f110acc0785189b
Jeremy Mitchell <mitchell852@gmail.com>
2017-11-10 12:25:31 -0700

    removes unused controllers

--- traffic_portal/app/src/modules/private/asns/index.js
+++ traffic_portal/app/src/modules/private/asns/index.js
@@ -18,7 +18,6 @@
  */
 
 module.exports = angular.module('trafficPortal.private.asns', [])
-    .controller('AsnsController', require('./AsnsController'))
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('trafficPortal.private.asns', {
@@ -26,8 +25,7 @@ module.exports = angular.module('trafficPortal.private.asns', [])
                 abstract: true,
                 views: {
                     privateContent: {
-                        templateUrl: 'modules/private/asns/asns.tpl.html',
-                        controller: 'AsnsController'
+                        templateUrl: 'modules/private/asns/asns.tpl.html'
                     }
                 }
             })
