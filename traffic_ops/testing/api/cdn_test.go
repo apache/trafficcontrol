9c6c809a53ac74860ae762b7c02d1c146a90f794
Dewayne Richardson <dewrich@apache.org>
2017-11-29 13:32:52 -0700

    added functionality for Post end-to-end

--- traffic_ops/testing/api/cdn_test.go
+++ traffic_ops/testing/api/cdn_test.go
@@ -17,34 +17,51 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"testing"
 
-	tc "github.com/apache/incubator-trafficcontrol/lib/go-tc"
+	log "github.com/apache/incubator-trafficcontrol/lib/go-log"
+	"github.com/apache/incubator-trafficcontrol/lib/go-tc"
+	"github.com/apache/incubator-trafficcontrol/traffic_ops/client"
 )
 
-func TestCDNsPOST(t *testing.T) {
+func TestCDNs(t *testing.T) {
+
+	TestPostCDNs(t)
+	TestPutCDNs(t)
+	TestGetCDNs(t)
+
+}
+
+func TestPostCDNs(t *testing.T) {
+
+	for _, cdn := range testData.CDNs {
+		alerts, _, err := TOSession.CreateCDN(cdn)
+		log.Debugln("Alerts: %v", alerts)
+		if err != nil {
+			t.Errorf("could not POST to cdns: %v\n", err)
+		}
+	}
+
+}
+
+func TestPutCDNs(t *testing.T) {
 
 	for _, cdn := range testData.CDNs {
 
 		b, err := json.Marshal(cdn)
 		if err != nil {
-			t.Errorf("Could not marshal data %v\n", err)
+			t.Errorf("could not marshal data %v\n", err)
 		}
-		fmt.Printf("b ---> %v\n", string(b))
-		// TODO: drichardson centralize these routes
-		resp, err := Request(*TOSession, http.MethodPost, "/api/1.2/cdns", b)
-		fmt.Printf("err ---> %v\n", err)
-		fmt.Printf("resp ---> %v\n", resp)
+		resp, err := Request(*TOSession, http.MethodPost, client.API_v2_CDNs, b)
 		if err != nil {
-			t.Errorf("Could not POST to cdns: %v\n", err)
+			t.Errorf("could not POST to cdns: %v\n", err)
 		}
 		defer resp.Body.Close()
 
 		var alerts tc.Alerts
 		if err := json.NewDecoder(resp.Body).Decode(&alerts); err != nil {
-			t.Errorf("Could not decode alert response.  Error is: %v\n", err)
+			t.Errorf("could not decode alert response: %v\n", err)
 		}
 		if resp.StatusCode != http.StatusOK {
 			t.Errorf("Response was not successful %v\n", err)
@@ -54,90 +71,28 @@ func TestCDNsPOST(t *testing.T) {
 	for _, cdn := range testData.CDNs {
 		alerts, _, err := TOSession.DeleteCDNByName(cdn.Name)
 		if err != nil {
-			t.Errorf("Cannot delete CDN by name: %v - %v\n", err, alerts)
+			t.Errorf("cannot DELETE CDN by name: %v - %v\n", err, alerts)
 		}
 	}
 
 }
 
-//TestCDNGETs compares the results of the CDN api and CDN client
-func TestCDNs(t *testing.T) {
-	resp, err := Request(*TOSession, http.MethodGet, "/api/1.2/cdns.json", nil)
-	if err != nil {
-		t.Errorf("Could not get cdns.json reponse was: %v\n", err)
-	}
+func TestGetCDNs(t *testing.T) {
 
-	defer resp.Body.Close()
-	var apiCDNRes tc.CDNsResponse
-	if err := json.NewDecoder(resp.Body).Decode(&apiCDNRes); err != nil {
-		t.Errorf("Could not decode CDN json.  Error is: %v\n", err)
-	}
-	apiCDNs := apiCDNRes.Response
-
-	//get CDNs data from client
-	clientCDNs, err := TOSession.CDNs()
-	if err != nil {
-		t.Errorf("Could not get CDNs from client.  Error is: %v\n", err)
-	}
-
-	if len(apiCDNs) != len(clientCDNs) {
-		t.Errorf("Array lengths from client and API are different...API = %s, Client = %s\n", apiCDNs, clientCDNs)
-	}
-
-	for _, apiCDN := range apiCDNs {
-		matchFound := false
-		for _, clientCDN := range clientCDNs {
-			if clientCDN.Name != apiCDN.Name {
-				continue
-			}
-			matchFound = true
-			//compare API results and client results
-			if apiCDN.Name != clientCDN.Name {
-				t.Errorf("CDN Name from client and API are different...API = %s, Client = %s\n", apiCDN.Name, clientCDN.Name)
-			}
-			if apiCDN.LastUpdated != clientCDN.LastUpdated {
-				t.Errorf("CDN Last Updated from client and API are different...API = %s, Client = %s\n", apiCDN.Name, clientCDN.Name)
-			}
-		}
-		if !matchFound {
-			t.Errorf("A match for %s from the API was not found in the client results\n", apiCDN.Name)
+	for _, cdn := range testData.CDNs {
+		alerts, _, err := TOSession.GetCDNByName(cdn.Name)
+		if err != nil {
+			t.Errorf("cannot GET CDN by name: %v - %v\n", err, alerts)
 		}
 	}
 }
 
-//TestCDNName ensures the client returns a CDN by name
-func TestCDNName(t *testing.T) {
-	//Get CDNs Data from API
-	resp, err := Request(*TOSession, http.MethodGet, "/api/1.2/cdns.json", nil)
-	if err != nil {
-		t.Errorf("Could not get cdns.json reponse was: %v\n", err)
-	}
+func TestDeleteCDNs(t *testing.T) {
 
-	defer resp.Body.Close()
-	var apiCDNRes tc.CDNsResponse
-	if err := json.NewDecoder(resp.Body).Decode(&apiCDNRes); err != nil {
-		t.Errorf("Could not decode CDN json.  Error is: %v\n", err)
-	}
-
-	apiCDNs := apiCDNRes.Response
-	apiName := apiCDNs[0].Name
-	apiLastUpdated := apiCDNs[0].LastUpdated
-
-	//get CDNs data from client
-	clientCDN, err := TOSession.CDNName(apiName)
-
-	if len(clientCDN) != 1 {
-		t.Errorf("The length of the client CDN response %v is greater than 1!\n", len(apiCDNs))
-	}
-
-	clientName := clientCDN[0].Name
-	clientLastUpdated := clientCDN[0].LastUpdated
-
-	//compare API results and client results
-	if apiName != clientName {
-		t.Errorf("CDN Name from client and API are different...API = %s, Client = %s\n", apiName, clientName)
-	}
-	if apiLastUpdated != clientLastUpdated {
-		t.Errorf("CDN Last Updated from client and API are different...API = %s, Client = %s\n", apiName, clientName)
+	for _, cdn := range testData.CDNs {
+		alerts, _, err := TOSession.DeleteCDNByName(cdn.Name)
+		if err != nil {
+			t.Errorf("cannot DELETE CDN by name: %v - %v\n", err, alerts)
+		}
 	}
 }
--- traffic_ops/testing/api/db.go
+++ traffic_ops/testing/api/db.go
@@ -22,191 +22,190 @@ func openConnection(cfg *Config) (*sql.DB, error) {
 	db, err = sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s", cfg.DB.User, cfg.DB.Password, cfg.DB.Hostname, cfg.DB.Name, sslStr))
 	if err != nil {
 		log.Errorf("opening database: %v\n", err)
-		return nil, fmt.Errorf("Transaction Failed: %s", err)
+		return nil, fmt.Errorf("transaction failed: %s", err)
 	}
 	return db, err
 }
 
-func setupData(cfg *Config, db *sql.DB) error {
+func setupUserData(cfg *Config, db *sql.DB) error {
 
-	log.Debugln("Setting up Data")
+	log.Debugln("Setting up initial user data")
 	var err error
 
-	//res, err = tx.Exec("CREATE EXTENSION IF NOT EXISTS pgcrypto")
-	//if err != nil {
-	//log.Errorf("Transaction Failed %v %v", err, res)
-	//}
-
 	tx, err := db.Begin()
+	if err != nil {
+		return fmt.Errorf("transaction begin failed %v %v ", err, tx)
+	}
 
 	res, err := tx.Exec("INSERT INTO role (id, name, description, priv_level) VALUES (1, 'disallowed','Block all access',0) ON CONFLICT DO NOTHING")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 
 	res, err = tx.Exec("INSERT INTO role (id, name, description, priv_level) VALUES (2, 'read-only user','Block all access', 10) ON CONFLICT DO NOTHING")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 
 	res, err = tx.Exec("INSERT INTO role (id, name, description, priv_level) VALUES (3, 'operations','Block all access', 20) ON CONFLICT DO NOTHING")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("INSERT INTO role (id, name, description, priv_level) VALUES (4, 'admin','super-user', 30) ON CONFLICT DO NOTHING")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("INSERT INTO role (id, name, description, priv_level) VALUES (5, 'portal','Portal User', 2) ON CONFLICT DO NOTHING")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("INSERT INTO role (id, name, description, priv_level) VALUES (6, 'migrations','database migrations user - DO NOT REMOVE', 20) ON CONFLICT DO NOTHING")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("INSERT INTO role (id, name, description, priv_level) VALUES (7, 'federation','Role for Secondary CZF', 15) ON CONFLICT DO NOTHING")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 
 	encryptedPassword, err := auth.DerivePassword(cfg.TOUserPassword)
 	if err != nil {
-		return fmt.Errorf("Password encryption failed %v %v", err)
+		return fmt.Errorf("password encryption failed %v", err)
 	}
-	userInsert := `INSERT INTO tm_user (username, local_passwd, confirm_local_passwd, role) VALUES ('admin','` + encryptedPassword + `','` + encryptedPassword + `', 4)`
-	res, err = tx.Exec(userInsert)
+	itm := `INSERT INTO tm_user (username, local_passwd, confirm_local_passwd, role) VALUES ('admin','` + encryptedPassword + `','` + encryptedPassword + `', 4)`
+	res, err = tx.Exec(itm)
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 
 	tx.Commit()
 	if err != nil {
-		return fmt.Errorf("Commit Failed %v %v", err, res)
+		return fmt.Errorf("commit failed %v %v", err, res)
 	}
 	return nil
 }
 
 // ensures that the data is cleaned up for a fresh run
 func teardownData(cfg *Config, db *sql.DB) error {
+	log.Debugln("Tearing down data")
 	tx, err := db.Begin()
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v ", err, tx)
+		return fmt.Errorf("transaction begin failed %v %v ", err, tx)
 	}
 
 	res, err := tx.Exec("DELETE FROM TO_EXTENSION")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 
 	res, err = tx.Exec("DELETE FROM STATICDNSENTRY")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM JOB")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM JOB_AGENT")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM JOB_STATUS")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM LOG")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM ASN")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM DELIVERYSERVICE_TMUSER")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM TM_USER")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM ROLE")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM DELIVERYSERVICE_REGEX")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM REGEX")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM DELIVERYSERVICE_SERVER")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM DELIVERYSERVICE")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM SERVER")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM PHYS_LOCATION")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM REGION")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM DIVISION")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM PROFILE")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM PARAMETER")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM PROFILE_PARAMETER")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM CACHEGROUP")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM TYPE")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM STATUS")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM SNAPSHOT")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM CDN")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 	res, err = tx.Exec("DELETE FROM TENANT")
 	if err != nil {
-		return fmt.Errorf("Transaction Failed %v %v", err, res)
+		return fmt.Errorf("exec failed %v %v", err, res)
 	}
 
 	tx.Commit()
 	if err != nil {
-		return fmt.Errorf("Commit Failed %v %v", err, res)
+		return fmt.Errorf("commit failed %v %v", err, res)
 	}
 	return err
 }
