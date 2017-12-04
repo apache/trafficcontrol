69200a7cad278f078a927699a974d6bb58cd398f
Dewayne Richardson <dewrich@apache.org>
2017-12-04 12:53:18 -0700

    added better command line options for the fixtures and refactored

--- traffic_ops/testing/api/config.go
+++ traffic_ops/testing/api/config.go
@@ -33,20 +33,43 @@ type Config struct {
 }
 
 type TrafficOps struct {
-	URL          string `json:"TOURL" envconfig:"TO_URL" default:"https://localhost:8443"`
-	User         string `json:"TOUser" envconfig:"TO_USER"`
-	UserPassword string `json:"TOPassword" envconfig:"TO_USER_PASSWORD"`
-	Insecure     bool   `json:"sslInsecure" envconfig:"SSL_INSECURE"`
+	// URL - The point to the Traffic Ops instance being tested
+	URL string `json:"URL" envconfig:"TO_URL" default:"https://localhost:8443"`
+
+	// User - The Traffic Ops test user hitting the API
+	User string `json:"User" envconfig:"TO_USER"`
+
+	// UserPassword - The Traffic Ops test user password hitting the API
+	UserPassword string `json:"Password" envconfig:"TO_USER_PASSWORD"`
+
+	// Insecure - ignores insecure ssls certs that were self-generated
+	Insecure bool `json:"sslInsecure" envconfig:"SSL_INSECURE"`
 }
 
 type TrafficOpsDB struct {
-	Name        string `json:"dbname" envconfig:"TODB_NAME"`
-	Hostname    string `json:"hostname" envconfig:"TODB_HOSTNAME"`
-	User        string `json:"user" envconfig:"TODB_USER"`
-	Password    string `json:"password" envconfig:"TODB_PASSWORD"`
-	Port        string `json:"port" envconfig:"TODB_PORT"`
-	DBType      string `json:"type" envconfig:"TODB_TYPE"`
-	SSL         bool   `json:"ssl" envconfig:"TODB_SSL"`
+	// Name - Traffic Ops Database name where the test data will be setup
+	Name string `json:"dbname" envconfig:"TODB_NAME"`
+
+	// Hostname - Traffic Ops Database hostname where Postgres is running
+	Hostname string `json:"hostname" envconfig:"TODB_HOSTNAME"`
+
+	// User - database user that Traffic Ops is using to point to the 'to_test' database
+	User string `json:"user" envconfig:"TODB_USER"`
+
+	// Password - database password for the User above
+	Password string `json:"password" envconfig:"TODB_PASSWORD"`
+
+	// Port - Postgres port running that has the to_test schema
+	Port string `json:"port" envconfig:"TODB_PORT"`
+
+	// DBType - will be 'Pg' by default but tells the Golang database driver
+	//          the database type
+	DBType string `json:"type" envconfig:"TODB_TYPE"`
+
+	// SSL - Flag that tells the database driver that the Postgres instances has TLS enabled
+	SSL bool `json:"ssl" envconfig:"TODB_SSL"`
+
+	// Description - database description
 	Description string `json:"description" envconfig:"TODB_DESCRIPTION"`
 }
 
