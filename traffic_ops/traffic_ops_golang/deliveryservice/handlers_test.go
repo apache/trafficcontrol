aba394a47e8ac7119e5459ba218f324618b0b25b
Dewayne Richardson <dewrich@apache.org>
2018-01-18 11:40:05 -0700

    removed the nullable refs and made the ds nullable struct the default usage

--- traffic_ops/traffic_ops_golang/deliveryservice/handlers_test.go
+++ traffic_ops/traffic_ops_golang/deliveryservice/handlers_test.go
@@ -32,7 +32,7 @@ import (
 // TestValidateErrors ...
 func TestValidateErrors(t *testing.T) {
 
-	ds := GetRefTypeNullable()
+	ds := GetRefType()
 	if err := json.Unmarshal([]byte(errorTestCase()), &ds); err != nil {
 		fmt.Printf("err ---> %v\n", err)
 		return
