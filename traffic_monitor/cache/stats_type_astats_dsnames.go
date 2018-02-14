cdbafa9158ddbc1b2f7790508d5197de4edf2e21
Robert Butts <rob@apache.org>
2018-02-13 21:12:56 -0700

    Add Apache license headers

--- traffic_monitor/cache/stats_type_astats_dsnames.go
+++ traffic_monitor/cache/stats_type_astats_dsnames.go
@@ -1,21 +1,40 @@
 package cache
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 // stats_type_astats_dsnames is a Stat format similar to the default Astats, but with Fully Qualified Domain Names replaced with Delivery Service names (xml_id)
 // That is, stat names are of the form: `"plugin.remap_stats.delivery-service-name.stat-name"`
 
-import(
+import (
+	"fmt"
 	"strconv"
 	"strings"
-	"fmt"
 
-	"github.com/apache/incubator-trafficcontrol/lib/go-tc"
 	"github.com/apache/incubator-trafficcontrol/lib/go-log"
+	"github.com/apache/incubator-trafficcontrol/lib/go-tc"
 	"github.com/apache/incubator-trafficcontrol/traffic_monitor/dsdata"
 	"github.com/apache/incubator-trafficcontrol/traffic_monitor/todata"
 )
 
 func init() {
- 	AddStatsType("astats-dsnames", astatsParse, astatsdsnamesPrecompute)
+	AddStatsType("astats-dsnames", astatsParse, astatsdsnamesPrecompute)
 }
 
 func astatsdsnamesPrecompute(cache tc.CacheName, toData todata.TOData, rawStats map[string]interface{}, system AstatsSystem) PrecomputedData {
