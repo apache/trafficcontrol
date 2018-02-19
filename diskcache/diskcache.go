90f07d6614bc2cd3a14b65e86b2f194265a8e499
Robert Butts <rob@apache.org>
2018-02-18 20:57:26 -0700

    Add disk cache

---
+++ diskcache/diskcache.go
@@ -0,0 +1,157 @@
+package diskcache
+
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"sync/atomic"
+	"time"
+
+	"github.com/apache/incubator-trafficcontrol/grove/cacheobj"
+
+	"github.com/apache/incubator-trafficcontrol/lib/go-log"
+
+	bolt "github.com/coreos/bbolt"
+)
+
+type DiskCache struct {
+	db           *bolt.DB
+	sizeBytes    uint64
+	maxSizeBytes uint64
+	lru          *LRU
+}
+
+const BucketName = "b"
+
+func New(path string, sizeBytes uint64) (*DiskCache, error) {
+	db, err := bolt.Open(path, 0600, &bolt.Options{Timeout: 5 * time.Second})
+	if err != nil {
+		return nil, errors.New("opening database '" + path + "': " + err.Error())
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		if _, err := tx.CreateBucketIfNotExists([]byte(BucketName)); err != nil {
+			return errors.New("creating bucket: " + err.Error())
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, errors.New("creating bucket for database '" + path + "': " + err.Error())
+	}
+
+	return &DiskCache{db: db, maxSizeBytes: sizeBytes, lru: NewLRU()}, nil
+}
+
+// Add takes a key and value to add. Returns whether an eviction occurred
+// The size is taken to fulfill the Cache interface, but the DiskCache doesn't use it.
+// Instead, we compute size from the serialized bytes stored to disk.
+//
+// Note DiskCache.Add does garbage collection in a goroutine, and thus it is not possible to determine eviction without impacting performance. This always returns false.
+func (c *DiskCache) Add(key string, val *cacheobj.CacheObj) bool {
+	log.Debugf("DiskCache Add CALLED key '%+v' size '%+v'\n", key, val.Size)
+	eviction := false
+
+	buf := bytes.Buffer{}
+	if err := gob.NewEncoder(&buf).Encode(val); err != nil {
+		log.Errorln("DiskCache.Add encoding cache object: " + err.Error())
+		return eviction
+	}
+	valBytes := buf.Bytes()
+
+	err := c.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(BucketName))
+		if b == nil {
+			return errors.New("bucket does not exist")
+		}
+		return b.Put([]byte(key), valBytes)
+	})
+	if err != nil {
+		log.Errorln("DiskCache.Add inserting '" + key + "' in database: " + err.Error())
+		return eviction
+	}
+
+	c.lru.Add(key, uint64(len(valBytes)))
+
+	newSizeBytes := atomic.AddUint64(&c.sizeBytes, uint64(len(valBytes)))
+	if newSizeBytes > c.maxSizeBytes {
+		go c.gc(newSizeBytes)
+	}
+
+	log.Debugf("DiskCache Add SUCCESS key '%+v' size '%+v'\n", key, val.Size)
+	return eviction
+}
+
+// gc does garbage collection, deleting stored entries until the DiskCache's size is less than maxSizeBytes. This is threadsafe, and should be called in a goroutine to avoid blocking the caller.
+// The given cacheSizeBytes must be `c.Size()`; it's passed here, because gc should be called immediately after an insert updates the size, so it saves an atomic instruction to pass rather than calling Size() again.
+func (c *DiskCache) gc(cacheSizeBytes uint64) {
+	for cacheSizeBytes > c.maxSizeBytes {
+		log.Debugf("DiskCache.gc cacheSizeBytes %+v > c.maxSizeBytes %+v\n", cacheSizeBytes, c.maxSizeBytes)
+		key, sizeBytes, exists := c.lru.RemoveOldest() // TODO change lru to use strings
+		if !exists {
+			// should never happen
+			log.Errorf("sizeBytes %v > %v maxSizeBytes, but LRU is empty!? Setting cache size to 0!\n", cacheSizeBytes, c.maxSizeBytes)
+			atomic.StoreUint64(&c.sizeBytes, 0)
+			return
+		}
+
+		log.Debugf("DiskCache.gc deleting key '" + key + "'")
+		err := c.db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(BucketName))
+			if b == nil {
+				return errors.New("bucket does not exist")
+			}
+			b.Delete([]byte(key))
+
+			return b.Delete([]byte(key))
+		})
+		if err != nil {
+			log.Errorln("removing '" + key + "' from cache: " + err.Error())
+		}
+
+		cacheSizeBytes = atomic.AddUint64(&c.sizeBytes, ^uint64(sizeBytes-1)) // subtract sizeBytes
+	}
+}
+
+// Get takes a key, and returns its value, and whether it was found
+func (c *DiskCache) Get(key string) (*cacheobj.CacheObj, bool) {
+	log.Debugln("DiskCache.Get key '" + key + "'")
+	valBytes := []byte(nil)
+
+	err := c.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(BucketName))
+		if b == nil {
+			return errors.New("bucket does not exist")
+		}
+		valBytes = b.Get([]byte(key))
+		return nil
+	})
+	if err != nil {
+		log.Errorln("DiskCache.Get getting '" + key + "' from cache: " + err.Error())
+		return nil, false
+	}
+
+	if valBytes == nil {
+		log.Debugln("DiskCache.Get key '" + key + "' CACHE MISS")
+		return nil, false
+	}
+
+	buf := bytes.NewBuffer(valBytes)
+	val := cacheobj.CacheObj{}
+	if err := gob.NewDecoder(buf).Decode(&val); err != nil {
+		log.Errorln("DiskCache.Get decoding '" + key + "' from cache: " + err.Error())
+		return nil, false
+	}
+
+	c.lru.Add(key, uint64(len(valBytes))) // TODO directly call c.ll.MoveToFront
+
+	log.Debugln("DiskCache.Get key '" + key + "' CACHE HIT")
+	return &val, true
+}
+
+func (c *DiskCache) Size() uint64 {
+	return atomic.LoadUint64(&c.sizeBytes)
+}
+
+func (c *DiskCache) Close() {
+	c.db.Close()
+}
---
+++ tiercache/tiercache.go
@@ -0,0 +1,58 @@
+package tiercache
+
+import (
+	"github.com/apache/incubator-trafficcontrol/grove/cacheobj"
+	"github.com/apache/incubator-trafficcontrol/grove/icache"
+
+	"github.com/apache/incubator-trafficcontrol/lib/go-log"
+)
+
+// TierCache wraps two icache.Caches and implements icache.Cache. Adding adds to both caches. Getting tries the first cache first, and if it isn't found, it then tries the second. Thus, the first should be smaller and faster (memory) and the second should be larger and slower (disk).
+//
+// This is more suitable for more less-frequently-requested items, with a separate cache for fewer frequently-requested items.
+//
+// An alternative implementation would be to Add to the first only, and when an object is evicted from the first, then store in the second. That would be more efficient for any given frequency, but less efficient for known infrequent objects.
+type TierCache struct {
+	first  icache.Cache
+	second icache.Cache
+}
+
+// New creates a new TierCache with the given first and second caches to use.
+func New(first, second icache.Cache) *TierCache {
+	return &TierCache{first: first, second: second}
+}
+
+// Get returns the object if it's in the first cache. Else, it returns the object from the second cache. Else, false.
+func (c *TierCache) Get(key string) (*cacheobj.CacheObj, bool) {
+	log.Debugln("TierCache.Get '" + key + "' calling")
+	v, ok := c.first.Get(key)
+	log.Debugf("TierCache.Get '"+key+"' FOUND FIRST: %+v\n", ok)
+	if !ok {
+		v, ok = c.second.Get(key)
+		if ok {
+			// if it was in second but not first, add back to first (LRU behavior)
+			c.first.Add(key, v)
+		}
+		log.Debugf("TierCache.Get '"+key+"' FOUND SECOND: %+v\n", ok)
+	}
+	return v, ok
+}
+
+// Add adds to both internal caches. Returns whether either reported an eviction.
+func (c *TierCache) Add(key string, val *cacheobj.CacheObj) bool {
+	aevict := c.first.Add(key, val)
+	bevict := c.second.Add(key, val)
+	return aevict || bevict
+}
+
+// Size returns the size of the second cache. This is because, since all objects are added to both, they are presumed to have the same content, and the second is presumed to be larger.
+//
+// For example, if the first is a memory cache and the second is a disk cache, it's most useful to report the size used on disk.
+func (c *TierCache) Size() uint64 {
+	return c.second.Size()
+}
+
+func (c *TierCache) Close() {
+	c.first.Close()
+	c.second.Close()
+}
