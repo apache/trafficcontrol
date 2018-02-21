a625fda763897b5dbf4b56b44845790ca5b8ffe2
Rawlin Peters <rawlin_peters@comcast.com>
2018-02-20 18:01:22 -0700

    Implement geo-sorting of CLIENT_STEERING targets
    For GEO targets in a CLIENT_STEERING DS, sort the resulting list from
    most optimal to least optimal, where "optimal" means shortest distance
    from client -> edge -> origin.
    
    If there is a mix of STEERING_WEIGHT, STEERING_ORDER, and GEO targets,
    the ordering would be:
    1. negative STEERING_ORDER
    2. GEO
    3. STEERING_WEIGHT
    4. positive STEERING_ORDER

--- traffic_router/core/src/main/java/com/comcast/cdn/traffic_control/traffic_router/core/ds/SteeringRegistry.java
+++ traffic_router/core/src/main/java/com/comcast/cdn/traffic_control/traffic_router/core/ds/SteeringRegistry.java
@@ -56,9 +56,15 @@ public class SteeringRegistry {
 		registry.putAll(newSteerings);
 		for (final Steering steering : steerings) {
 			for (final SteeringTarget target : steering.getTargets()) {
-				if (target.getWeight() > 0) {
+				if (target.getGeolocation() != null && target.getGeoOrder() != 0) {
+					LOGGER.info("Steering " + steering.getDeliveryService() + " target " + target.getDeliveryService() + " now has geolocation [" + target.getLatitude() + ", "  + target.getLongitude() + "] and geoOrder " + target.getGeoOrder());
+				} else if (target.getGeolocation() != null && target.getWeight() > 0) {
+					LOGGER.info("Steering " + steering.getDeliveryService() + " target " + target.getDeliveryService() + " now has geolocation [" + target.getLatitude() + ", "  + target.getLongitude() + "] and weight " + target.getWeight());
+				} else if (target.getGeolocation() != null) {
+					LOGGER.info("Steering " + steering.getDeliveryService() + " target " + target.getDeliveryService() + " now has geolocation [" + target.getLatitude() + ", "  + target.getLongitude() + "]");
+				} else if (target.getWeight() > 0) {
 					LOGGER.info("Steering " + steering.getDeliveryService() + " target " + target.getDeliveryService() + " now has weight " + target.getWeight());
-				} else if (target.getOrder() > 0 || target.getOrder() < 0) { // this target has a specific order set
+				} else if (target.getOrder() != 0) { // this target has a specific order set
 					LOGGER.info("Steering " + steering.getDeliveryService() + " target " + target.getDeliveryService() + " now has order " + target.getOrder());
 				} else {
 					LOGGER.info("Steering " + steering.getDeliveryService() + " target " + target.getDeliveryService() + " now has weight " + target.getWeight() + " and order " + target.getOrder());
