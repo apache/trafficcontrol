6d3c34e169e846c9617e250eb78a2019b247b711
Dan Kirkwood <dangogh@apache.org>
2018-02-23 16:28:14 -0700

    backup plan for GetAuditName

--- traffic_ops/traffic_ops_golang/api/change_log.go
+++ traffic_ops/traffic_ops_golang/api/change_log.go
@@ -48,6 +48,7 @@ func CreateChangeLog(level string, action string, i Identifier, user auth.Curren
 	query := `INSERT INTO log (level, message, tm_user) VALUES ($1, $2, $3)`
 	id, _ := i.GetID()
 	message := action + " " + i.GetType() + ": " + i.GetAuditName() + " id: " + strconv.Itoa(id)
+	log.Debugf("about to exec ", query, " with ", message)
 	_, err := db.Exec(query, level, message, user.ID)
 	if err != nil {
 		log.Errorf("received error: %++v from audit log insertion", err)
