24f08139c5ca93d296336000a6aa7ad00f96faf4
ASchmidt <Andrew_Schmidt@comcast.com>
2018-03-14 16:38:47 -0600

    Moved tomcat_tr to traffic_router/tomcat-rpm and fixed

---
+++ traffic_router/tomcat-rpm/build_rpm.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#----------------------------------------
+function checkEnvironment() {
+	echo "Verifying the build configuration environment."
+	local script=$(readlink -f "$0")
+	local scriptdir=$(dirname "$script")
+	export TR_DIR=$(dirname "$scriptdir")
+	export TC_DIR=$(dirname "$TR_DIR")
+	functions_sh="$TC_DIR/build/functions.sh"
+	if [[ ! -r $functions_sh ]]; then
+		echo "Error: Can't find $functions_sh"
+		exit 1
+	fi
+	. "$functions_sh"
+
+	# 
+	# get traffic_control src path -- relative to build_rpm.sh script
+	export PACKAGE="tomcat"
+        export BUILD_NUMBER=${BUILD_NUMBER:-$(getBuildNumber)}
+        export TC_VERSION=$(getVersion "$TC_DIR")
+	export TOMCAT_VERSION=8.5
+	export TOMCAT_RELEASE=29
+	export WORKSPACE=${WORKSPACE:-$TC_DIR}
+	export RPMBUILD="$WORKSPACE/rpmbuild"
+	export DIST="$WORKSPACE/dist"
+	export RPM="${PACKAGE}-${TOMCAT_VERSION}.${TOMCAT_RELEASE}-${BUILD_NUMBER}.x86_64.rpm"
+
+	echo "=================================================="
+	echo "WORKSPACE: $WORKSPACE"
+	echo "TOMCAT_RELEASE: $TOMCAT_RELEASE"
+	echo "TOMCAT_VERSION: $TOMCAT_VERSION"
+        echo "BUILD_NUMBER: $BUILD_NUMBER"
+	echo "RPM: $RPM"
+	echo "--------------------------------------------------"
+}
+
+# ---------------------------------------
+function initBuildArea() {
+        echo "Initializing the build area."
+        mkdir -p "$RPMBUILD"/{SPECS,SOURCES,RPMS,SRPMS,BUILD,BUILDROOT} || { echo "Could not create $RPMBUILD: $?"; exit 1; }
+        export VERSION=$TOMCAT_VERSION
+        export RELEASE=$TOMCAT_RELEASE
+
+        wget http://archive.apache.org/dist/tomcat/tomcat-8/v$VERSION.$RELEASE/bin/apache-tomcat-$VERSION.$RELEASE.tar.gz -O "$RPMBUILD"/SOURCES/apache-tomcat-$VERSION.$RELEASE.tar.gz
+
+        cp "$TR_DIR/tomcat-rpm/tomcat.service" "$RPMBUILD/SOURCES/" || { echo "Could not copy source files: $?"; exit 1; }
+        cp "$TR_DIR/tomcat-rpm/tomcat.spec" "$RPMBUILD/SPECS/" || { echo "Could not copy spec files: $?"; exit 1; }
+
+        echo "The build area has been initialized."
+}
+
+#----------------------------------------
+function buildRpmTomcat () {
+        echo "Building the rpm."
+
+        cd $RPMBUILD
+        rpmbuild --define "_topdir $(pwd)" \
+                 --define "build_number $BUILD_NUMBER" \
+                 --define "tomcat_version $TOMCAT_VERSION.$TOMCAT_RELEASE" \
+                 -ba SPECS/tomcat.spec || \
+                 { echo "RPM BUILD FAILED: $?"; exit 1; }
+        local rpm=$(find ./RPMS -name $RPM)
+        if [[ -z $rpm ]]; then
+                echo "Could not find rpm file $RPM in $(pwd)"
+                exit 1;
+        fi
+        echo "========================================================================================"
+        echo "RPM BUILD SUCCEEDED, See $DIST/$RPM for the newly built rpm."
+        echo "========================================================================================"
+        echo
+        mkdir -p "$DIST" || { echo "Could not create $DIST: $?"; exit 1; }
+
+        cp "$rpm" "$DIST/." || { echo "Could not copy $rpm to $DIST: $?"; exit 1; }
+}
+
+checkEnvironment npm node
+initBuildArea
+buildRpmTomcat
---
+++ traffic_router/tomcat-rpm/tomcat.spec
@@ -0,0 +1,63 @@
+%define debug_package %{nil}
+
+Name:       tomcat
+Version:    %{tomcat_version}
+Release:    %{build_number}
+Summary:    Apache Tomcat Servlet/JSP Engine 8.5+, RI for Servlet 3.1/JSP 2.3 API
+License:    Apache Software License
+URL:        https://github.com/apache/incubator-trafficcontrol/
+Source:     %{_sourcedir}/apache-tomcat-%{version}.tar.gz
+Requires:   jdk >= 1.8
+
+%define tomcat_home /opt/tomcat
+
+%description
+This rpm is a minimal install of the Tomcat servlet container version 8.5. 
+It gets installed to /opt/tomcat and contains no webapps of its own. 
+To use it create your own CATALINA_BASE directory and place your application 
+specific webapps and server.xml there. 
+You will also need your own systemd unit file for starting your application 
+with the correct setting for CATALINA_BASE. 
+
+Built:@BUILT@
+
+%prep
+%setup -q -n apache-tomcat-%{version}
+
+%build
+
+%install
+install -d -m 755 ${RPM_BUILD_ROOT}/%{tomcat_home}/
+cp -R * ${RPM_BUILD_ROOT}/%{tomcat_home}/
+
+# Remove all webapps. 
+rm -rf ${RPM_BUILD_ROOT}/%{tomcat_home}/webapps/*
+
+# Remove *.bat
+rm -f ${RPM_BUILD_ROOT}/%{tomcat_home}/bin/*.bat
+
+# Drop sysd script
+install -d -m 755 ${RPM_BUILD_ROOT}/%{_sysconfdir}/systemd/system
+install    -m 755 %_sourcedir/tomcat.service ${RPM_BUILD_ROOT}/%{_sysconfdir}/systemd/system/tomcat.service 
+
+%clean
+rm -rf ${RPM_BUILD_ROOT}
+
+%pre
+
+%files
+%defattr(-,root,root)
+%{tomcat_home}
+%{_sysconfdir}/systemd/system/tomcat.service 
+
+%post
+systemctl daemon-reload
+echo "Tomcat for Traffic Router installed successfully."
+echo ""
+echo "Start with 'sudo systemctl start traffic_router'"
+
+%preun
+
+%postun
+
+%changelog
