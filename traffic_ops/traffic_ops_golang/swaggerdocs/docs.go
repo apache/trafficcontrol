41a1d98ca24b17444354031b9a46048951108185
Dewayne Richardson <dewrich@apache.org>
2018-03-22 08:22:23 -0600

    added swagger files

---
+++ traffic_ops/traffic_ops_golang/swaggerdocs/docs.go
@@ -0,0 +1,52 @@
+/*
+ Package docs TrafficOps API
+
+ The following REST APIs are used by the Traffic Portal UI along with several Traffic Control backend component and services.
+
+ This documentation is fully compliant with the Swagger 2.0 specification
+
+     Schemes: http, https
+	 Host: localhost:8443
+     BasePath: /api/1.3
+     Version: 1.3
+     License: Apache 2 https://github.com/apache/incubator-trafficcontrol/blob/master/LICENSE
+     Contact: Traffic Control Dev List<dev@trafficcontrol.incubator.apache.org> http://traffic-control-cdn.readthedocs.io/en/latest/index.html
+
+     Consumes:
+     - application/json
+
+     Produces:
+     - application/json
+
+     Security:
+     - Cookie:
+
+     SecurityDefinitions:
+     Cookie:
+          type: apiKey
+          name: cookie
+          in: header
+
+
+ swagger:meta
+*/
+package swaggerdocs
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
