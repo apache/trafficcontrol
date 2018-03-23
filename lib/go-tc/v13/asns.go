e795087f34bffa5ed9afd8b6f1dc1cede4d12c60
Dewayne Richardson <dewrich@apache.org>
2018-03-23 10:42:41 -0600

    added swagger documented asns struct

---
+++ lib/go-tc/v13/asns.go
@@ -0,0 +1,90 @@
+package v13
+
+import tc "github.com/apache/incubator-trafficcontrol/lib/go-tc"
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Autonomous System Numbers
+//
+// A List of ASNs Response
+// swagger:response ASNsResponse
+// in: body
+type ASNsResponse struct {
+	// in: body
+	Response []ASN `json:"response"`
+}
+
+// A Single ASN Response for Update and Create to depict what changed
+// swagger:response ASNResponse
+// in: body
+type ASNResponse struct {
+	// in: body
+	Response ASN `json:"response"`
+}
+
+type ASN struct {
+	// The ASN to retrieve
+	//
+	// Autonomous System Numbers per APNIC for identifying a service provider
+	// required: true
+	ASN int `json:"asn" db:"asn"`
+
+	// Related cachegroup name
+	//
+	Cachegroup string `json:"cachegroup" db:"cachegroup"`
+
+	// Related cachegroup id
+	//
+	CachegroupID int `json:"cachegroupId" db:"cachegroup_id"`
+
+	// ID of the ASN
+	//
+	// required: true
+	ID int `json:"id" db:"id"`
+
+	// LastUpdated
+	//
+	LastUpdated string `json:"lastUpdated" db:"last_updated"`
+}
+
+type ASNNullable struct {
+	// The ASN to retrieve
+	//
+	// Autonomous System Numbers per APNIC for identifying a service provider
+	// required: true
+	ASN *int `json:"asn" db:"asn"`
+
+	// Related cachegroup name
+	//
+	Cachegroup *string `json:"cachegroup" db:"cachegroup"`
+
+	// Related cachegroup id
+	//
+	CachegroupID *int `json:"cachegroupId" db:"cachegroup_id"`
+
+	// ID of the ASN
+	//
+	// required: true
+	ID *int `json:"id" db:"id"`
+
+	// LastUpdated
+	//
+	LastUpdated *tc.TimeNoMod `json:"lastUpdated" db:"last_updated"`
+}
