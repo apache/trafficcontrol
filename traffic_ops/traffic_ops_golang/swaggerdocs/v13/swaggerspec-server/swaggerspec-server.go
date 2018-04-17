533b0a3fd6158ddcd3ed5a70b23079afc79a977b
Dewayne Richardson <dewrich@apache.org>
2018-04-17 10:29:30 -0600

    converted the swaggerdocs generation into a Docker infrastructure

--- traffic_ops/traffic_ops_golang/swaggerdocs/v13/swaggerspec-server/swaggerspec-server.go
+++ traffic_ops/traffic_ops_golang/swaggerdocs/v13/swaggerspec-server/swaggerspec-server.go
@@ -7,18 +7,20 @@ import (
 )
 
 func main() {
+	hostName := flag.String("h", "localhost", "hostname to serve on")
 	port := flag.String("p", "8000", "port to serve on")
 	flag.Parse()
 
-	swaggerFile := "swagger.json"
+	swaggerFile := "/swaggerspec/swagger.json"
 
-	http.HandleFunc("/swagger.json", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(swaggerFile, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie, Cookie")
 		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT,DELETE")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		http.ServeFile(w, r, swaggerFile)
 	})
-	log.Printf("Serving %s on HTTP port: %s\n", swaggerFile, *port)
+	log.Printf("Serving swagger spec file here: http://%s:%s%s\n", *hostName, *port, swaggerFile)
+	log.Printf("Serving Swagger UI here: http://localhost:8080\n")
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
