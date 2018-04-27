7033dd3c1b3537cb3e612de45040f1de6da627ff
Rawlin Peters <rawlin_peters@comcast.com>
2018-04-26 18:06:27 -0600

    Implement Origin client methods

--- lib/go-tc/v13/origins.go
+++ lib/go-tc/v13/origins.go
@@ -24,12 +24,13 @@ import (
  */
 
 type OriginsResponse struct {
-	Response []Origin `json:"response"`
+	Response []OriginNullable `json:"response"`
 }
 
 // OriginDetailResponse is the JSON object returned for a single origin
-type OriginsDetailResponse struct {
-	Response Origin `json:"response"`
+type OriginDetailResponse struct {
+	Response OriginNullable `json:"response"`
+	tc.Alerts
 }
 
 type Origin struct {
@@ -72,4 +73,71 @@ type OriginNullable struct {
 	Protocol          *string       `json:"protocol" db:"protocol"`
 	Tenant            *string       `json:"tenant" db:"tenant"`
 	TenantID          *int          `json:"tenantId" db:"tenant_id"`
+}
+
+type OriginBuilder struct {
+	originBuild OriginNullable
+}
+
+func NewOriginBuilder() *OriginBuilder {
+	return &OriginBuilder{}
+}
+
+func (o *OriginBuilder) Build() OriginNullable {
+	return o.originBuild
+}
+
+func (o *OriginBuilder) CachegroupID(id int) *OriginBuilder {
+	o.originBuild.CachegroupID = &id
+	return o
+}
+
+func (o *OriginBuilder) CoordinateID(id int) *OriginBuilder {
+	o.originBuild.CoordinateID = &id
+	return o
+}
+
+func (o *OriginBuilder) DeliveryServiceID(id int) *OriginBuilder {
+	o.originBuild.DeliveryServiceID = &id
+	return o
+}
+
+func (o *OriginBuilder) FQDN(fqdn string) *OriginBuilder {
+	o.originBuild.FQDN = &fqdn
+	return o
+}
+
+func (o *OriginBuilder) IP6Address(ip string) *OriginBuilder {
+	o.originBuild.IP6Address = &ip
+	return o
+}
+
+func (o *OriginBuilder) IPAddress(ip string) *OriginBuilder {
+	o.originBuild.IPAddress = &ip
+	return o
+}
+
+func (o *OriginBuilder) Name(name string) *OriginBuilder {
+	o.originBuild.Name = &name
+	return o
+}
+
+func (o *OriginBuilder) Port(port int) *OriginBuilder {
+	o.originBuild.Port = &port
+	return o
+}
+
+func (o *OriginBuilder) ProfileID(id int) *OriginBuilder {
+	o.originBuild.ProfileID = &id
+	return o
+}
+
+func (o *OriginBuilder) Protocol(proto string) *OriginBuilder {
+	o.originBuild.Protocol = &proto
+	return o
+}
+
+func (o *OriginBuilder) TenantID(id int) *OriginBuilder {
+	o.originBuild.TenantID = &id
+	return o
 }
