e068e34b57fdf4350041b99e377a7fe92b9320d9
Dan Kirkwood <dangogh@gmail.com>
2018-06-01 16:35:17 +0000

    straighten out vars; write configs

--- infrastructure/cdn-in-a-box/traffic_ops/dbInit.sh
+++ infrastructure/cdn-in-a-box/traffic_ops/dbInit.sh
@@ -26,5 +26,6 @@ while ! nc $DB_SERVER $DB_PORT </dev/null; do # &>/dev/null; do
         echo "waiting for $DB_SERVER:$DB_PORT"
         sleep 3
 done
+
 psql -h $DB_SERVER -U postgres -c "CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_USER_PASS'"
 createdb $DB_NAME -h $DB_SERVER -U postgres --owner $DB_USER
---
+++ infrastructure/cdn-in-a-box/traffic_ops/run-go.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# Script for running the Dockerfile for Traffic Ops.
+# The Dockerfile sets up a Docker image which can be used for any new Traffic Ops container;
+# This script, which should be run when the container is run (it's the ENTRYPOINT), will configure the container.
+#
+# The following environment variables must be set, ordinarily by `docker run -e` arguments:
+# DB_SERVER
+# DB_PORT
+# DB_ROOT_PASS
+# DB_USER
+# DB_USER_PASS
+# DB_NAME
+# ADMIN_USER
+# ADMIN_PASS
+# CERT_COUNTRY
+# CERT_STATE
+# CERT_CITY
+# CERT_COMPANY
+# DOMAIN
+
+# TODO:  Unused -- should be removed?  TRAFFIC_VAULT_PASS
+
+# Check that env vars are set
+
+set -x
+envvars=( DB_SERVER DB_PORT DB_ROOT_PASS DB_USER DB_USER_PASS ADMIN_USER ADMIN_PASS CERT_COUNTRY CERT_STATE CERT_CITY CERT_COMPANY DOMAIN)
+for v in $envvars
+do
+	if [[ -z $$v ]]; then echo "$v is unset"; exit 1; fi
+done
+
+# Write config files
+if [[ -x /config.sh ]]; then
+	/config.sh
+fi
+
+while ! nc trafficops-perl 60443 </dev/null; do # &>/dev/null; do
+        echo "waiting for trafficops-perl:60443"
+        sleep 3
+done
+
+cd /opt/traffic_ops/app
+ls -l bin
+CDNCONF=/opt/traffic_ops/app/conf/cdn.conf
+DBCONF=/opt/traffic_ops/app/conf/production/database.conf
+mkdir -p /var/log/traffic_ops
+./bin/traffic_ops_golang -cfg $CDNCONF -dbcfg $DBCONF
