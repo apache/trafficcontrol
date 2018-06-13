490c484f80956e9253480012d09381231c86108c
Dan Kirkwood <dangogh@apache.org>
2018-06-13 17:06:00 -0600

    install cert for TP

--- infrastructure/cdn-in-a-box/traffic_portal/run.sh
+++ infrastructure/cdn-in-a-box/traffic_portal/run.sh
@@ -10,6 +10,12 @@ LOGFILE="/var/log/traffic_portal/traffic_portal.log"
 MIN_UPTIME="5000"
 SPIN_SLEEP_TIME="2000"
 
+
+key=/etc/pki/tls/private/localhost.key
+cert=/etc/pki/tls/certs/localhost.crt
+ca=/etc/pki/tls/certs/ca-bundle.crt
+openssl req -newkey rsa:2048 -nodes -keyout $key -x509 -days 365 -out $cert -subj "/C=$CERT_COUNTRY/ST=$CERT_STATE/L=$CERT_CITY/O=$CERT_COMPANY"
+
 # Add node to the path for situations in which the environment is passed.
 PATH=$FOREVER_BIN_DIR:$NODE_BIN_DIR:$PATH
 forever \
