aff5fbcb3a46f8433dedf64140a878a069a2a433
Dylan Volz <Dylan_Volz@comcast.com>
2018-07-17 14:58:48 -0600

    properly issue delete query in transaction

--- traffic_ops/traffic_ops_golang/deliveryservice/servers/servers.go
+++ traffic_ops/traffic_ops_golang/deliveryservice/servers/servers.go
@@ -368,7 +368,7 @@ func GetReplaceHandler(db *sqlx.DB) http.HandlerFunc {
 
 		if *payload.Replace {
 			// delete existing
-			rows, err := db.Queryx("DELETE FROM deliveryservice_server WHERE deliveryservice = $1", *dsId)
+			rows, err := tx.Queryx("DELETE FROM deliveryservice_server WHERE deliveryservice = $1", *dsId)
 			if err != nil {
 				log.Errorf("unable to remove the existing servers assigned to the delivery service: %s", err)
 				handleErrs(http.StatusInternalServerError, err)
