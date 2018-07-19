e47a60cdb17da36a8b207891c04656edf42ca703
Brennan Fieck <Brennan_WilliamFieck@comcast.com>
2018-07-19 18:49:56 +0000

    Traffic Monitor now starts, runs, and can pull its config from Ops.
    CrConfig is still missing

--- infrastructure/cdn-in-a-box/traffic_monitor/run.sh
+++ infrastructure/cdn-in-a-box/traffic_monitor/run.sh
@@ -21,131 +21,117 @@
 # This script, which should be run when the container is run (it's the ENTRYPOINT), will configure the container.
 #
 # The following environment variables must be set (ordinarily by `docker run -e` arguments):
-# TRAFFIC_OPS_URI
-# TRAFFIC_OPS_USER
-# TRAFFIC_OPS_PASS
+# TO_HOST
+# TO_PORT
+# TM_USER
+# TM_PASSWORD
 
 # Check that env vars are set
-envvars=( TRAFFIC_OPS_URI TRAFFIC_OPS_USER TRAFFIC_OPS_PASS )
+
+set -e
+set -x
+set -m
+
+envvars=( TP_HOST TO_HOST TO_PORT TM_USER TM_PASSWORD TO_ADMIN_USER TO_ADMIN_PASSWORD)
 for v in $envvars
 do
 	if [[ -z $$v ]]; then echo "$v is unset"; exit 1; fi
 done
 
-start() {
-	service traffic_monitor start
-	touch /opt/traffic_monitor/var/log/traffic_monitor.log
-	exec tail -f /opt/traffic_monitor/var/log/traffic_monitor.log
+IP=$(ip addr | grep 'global' | grep -v "inet6" | head -n1 | awk '{print $2}' | cut -f1 -d'/')
+NETMASK=$(ifconfig eth0 | grep 'inet ' | tr -s ' ' | cut -d ' ' -f4)
+GATEWAY="$(ip route | grep default | awk '{print $3}')"
+
+cat > /opt/traffic_monitor/conf/traffic_ops.cfg <<- ENDOFMESSAGE
+{
+	"username": "$TM_USER",
+	"password": "$TM_PASSWORD",
+	"url": "https://$TO_HOST:$TO_PORT",
+	"insecure": true,
+	"cdnName": "CDN-in-a-Box",
+	"httpListener": ":80"
 }
-
-init() {
-	[ ! -z $IP ]            || IP=$(ip addr | grep 'global' | awk '{print $2}' | cut -f1 -d'/')
-	[ ! -z $DOMAIN ]        || DOMAIN="localdomain"
-	[ ! -z $CACHEGROUP ]    || CACHEGROUP="mid-east"
-	[ ! -z $TYPE ]          || TYPE="RASCAL"
-	[ ! -z $PROFILE ]       || PROFILE="RASCAL_CDN1"
-	[ ! -z $PHYS_LOCATION ] || PHYS_LOCATION="plocation-nyc-1"
-	[ ! -z $INTERFACE ]     || INTERFACE="eth0"
-	[ ! -z $NETMASK ]       || NETMASK="255.255.0.0"
-	[ ! -z $MTU ]           || MTU="9000"
-	[ ! -z $PORT ]          || PORT="80"
-	[ ! -z $GATEWAY ]       || GATEWAY="$(ip route | grep default | awk '{print $3}')"
-	[ ! -z $CDN ]           || CDN="cdn"
-	echo "IP: $IP"
-	echo "Domain: $DOMAIN"
-	echo "Cachegroup: $CACHEGROUP"
-	echo "Type: $TYPE"
-	echo "Profile: $PROFILE"
-	echo "PhysLocation: $PHYS_LOCATION"
-	echo "Interface: $INTERFACE"
-	echo "NetMask: $NETMASK"
-	echo "MTU: $MTU"
-	echo "Port: $PORT"
-	echo "Gateway: $GATEWAY"
-	echo "CDN: $CDN"
-	echo "Create Server: $CREATE_TO_SERVER"
-
-	mkdir -p /opt/traffic_monitor/conf
-	cat > /opt/traffic_monitor/conf/traffic_monitor.cfg <<- ENDOFMESSAGE
-		{
-				"cache_health_polling_interval_ms": 6000,
-				"cache_stat_polling_interval_ms": 6000,
-				"monitor_config_polling_interval_ms": 15000,
-				"http_timeout_ms": 2000,
-				"peer_polling_interval_ms": 5000,
-				"peer_optimistic": true,
-				"max_events": 200,
-				"max_stat_history": 5,
-				"max_health_history": 5,
-				"health_flush_interval_ms": 20,
-				"stat_flush_interval_ms": 20,
-				"log_location_event": "/opt/traffic_monitor/var/log/event.log",
-				"log_location_error": "/opt/traffic_monitor/var/log/traffic_monitor.log",
-				"log_location_warning": "/opt/traffic_monitor/var/log/traffic_monitor.log",
-				"log_location_info": "null",
-				"log_location_debug": "null",
-				"serve_read_timeout_ms": 10000,
-				"serve_write_timeout_ms": 10000,
-				"http_poll_no_sleep": false,
-				"static_file_dir": "/opt/traffic_monitor/static/"
-		}
 ENDOFMESSAGE
 
-	cat > /opt/traffic_monitor/conf/traffic_ops.cfg <<- ENDOFMESSAGE
-		{
-				"username": "$TRAFFIC_OPS_USER",
-				"password": "$TRAFFIC_OPS_PASS",
-				"url": "$TRAFFIC_OPS_URI",
-				"insecure": true,
-				"cdnName": "$CDN",
-				"httpListener": ":$PORT"
-				}
-	ENDOFMESSAGE
+sed -ie "s;MY_HOSTNAME;$(hostname -s);g" /server.json
+sed -ie "s;MY_DOMAINNAME;$(dnsdomainname);g" /server.json
+sed -ie "s;MY_GATEWAY;$GATEWAY;g" /server.json
+sed -ie "s;MY_NETMASK;$NETMASK;g" /server.json
+sed -ie "s;MY_IP;$IP;g" /server.json
 
-	TO_COOKIE="$(curl -v -s -k -X POST --data '{ "u":"'"$TRAFFIC_OPS_USER"'", "p":"'"$TRAFFIC_OPS_PASS"'" }' $TRAFFIC_OPS_URI/api/1.2/user/login 2>&1 | grep 'Set-Cookie' | sed -e 's/.*mojolicious=\(.*\); expires.*/\1/')"
-	echo "Got Cookie: $TO_COOKIE"
+while ! nc "$TO_HOST" "$TO_PORT" </dev/null; do
+	echo "waiting for traffic_ops_golang:6443"
+	sleep 3
+done
 
-	if [ ! -z "$CREATE_TO_SERVER" ] ; then
-		echo "Creating Server in Traffic Ops!"
-		# curl -v -k -X POST -H "Cookie: mojolicious=$TO_COOKIE" -F "filename=Traffic_Monitor_Dockerfile_profile.traffic_ops" -F "profile_to_import=@/Traffic_Monitor_Dockerfile_profile.traffic_ops" $TRAFFIC_OPS_URI/profile/doImport
+RESPONSE=$(curl -sk -d '{ "u":"'"$TM_USER"'", "p":"'"$TM_PASSWORD"'" }' https://$TP_HOST/api/1.3/user/login)
+while [[ "$RESPONSE" == '{"alerts":[{"text":"Invalid username or password.","level":"error"}]}' ]]; do
+	echo "Waiting for traffic_ops_init"
+	RESPONSE=$(curl -sk -d '{ "u":"'"$TM_USER"'", "p":"'"$TM_PASSWORD"'" }' https://$TP_HOST/api/1.3/user/login)
+	sleep 3
+done
+curl -ksc cookie.jar -d "{\"u\":\"$TO_ADMIN_USER\",\"p\":\"${TO_ADMIN_PASSWORD}\"}" https://$TP_HOST/api/1.3/user/login
+echo "Got Cookie: $(tail -n1 cookie.jar | tr '\t' ' ')"
 
-		CACHEGROUP_ID="$( curl -s -k -X GET -H "Cookie: mojolicious=$TO_COOKIE" $TRAFFIC_OPS_URI/api/1.2/cachegroups.json | jq '.response | .[] | select(.name=='"\"$CACHEGROUP\""') | .id')"
-		echo "Got cachegroup ID: $CACHEGROUP_ID"
+# Gets our CDN ID
+CDN=$(curl -ksb cookie.jar https://trafficportal/api/1.3/cdns)
+CDN=$(echo $CDN | tr '}' '\n' | grep CDN-in-a-Box | tr ',' '\n' | grep '"id"' | cut -d : -f2)
+while [[ -z "$CDN" ]]; do
+	echo "waiting for trafficops setup to complete..."
+	sleep 3
+	CDN=$(curl -ksb cookie.jar https://trafficportal/api/1.3/cdns)
+	CDN=$(echo $CDN | tr '}' '\n' | grep CDN-in-a-Box | tr ',' '\n' | grep '"id"' | cut -d : -f2)
+done
 
-		SERVER_TYPE_ID="$(curl -s -k -X GET -H "Cookie: mojolicious=$TO_COOKIE" $TRAFFIC_OPS_URI/api/1.2/types.json | jq '.response | .[] | select(.name=='"\"$TYPE\""') | .id')"
-		echo "Got server type ID: $SERVER_TYPE_ID"
+# Now we upload a profile for later use
+sed -ie "s;CDN_ID;$CDN;g" /profile.json
+cat /profile.json
+PROFILE=$(curl -ksb cookie.jar -d @/profile.json https://trafficportal/api/1.3/profiles)
+PROFILENAME=$(echo $PROFILE | tr ',' '\n' | grep '"name"' | cut -d : -f2 | tr -d '"')
+PROFILEID=$(echo $PROFILE | tr ',{' '\n' | grep '"id"' | cut -d : -f2)
+curl -ksb cookie.jar -d @/parameters.json https://trafficportal/api/1.3/profiles/name/$PROFILENAME/parameters
+echo
 
-		SERVER_PROFILE_ID="$(curl -s -k -X GET -H "Cookie: mojolicious=$TO_COOKIE" $TRAFFIC_OPS_URI/api/1.2/profiles.json | jq '.response | .[] | select(.name=='"\"$PROFILE\""') | .id')"
-		echo "Got server profile ID: $SERVER_PROFILE_ID"
+# Gets the location ID
+location=$(curl -ksb cookie.jar https://trafficportal/api/1.3/phys_locations)
+while [[ "$location" == '{"response":[]}' ]]; do
+	echo "Waiting for location setup"
+	sleep 3
+	location=$(curl -ksb cookie.jar https://trafficportal/api/1.3/phys_locations)
+done
+location=$(echo $location | tr ']' '\n' | grep CDN_in_a_Box | tr ',' '\n' | grep '"id"' | cut -d ':' -f2)
 
-		PHYS_LOCATION_ID="$(curl -s -k -X GET -H "Cookie: mojolicious=$TO_COOKIE" $TRAFFIC_OPS_URI/api/1.2/phys_locations.json | jq '.response | .[] | select(.shortName=='"\"$PHYS_LOCATION\""') | .id')"
-		echo "Got phys location ID: $PHYS_LOCATION_ID"
+# Gets the id of a RASCAL server type
+TYPE=$(curl -ksb cookie.jar https://trafficportal/api/1.3/types)
+TYPE=$(echo $TYPE | tr '}' '\n' | grep '"RASCAL"' | tr ',' '\n' | grep '"id"' | cut -d : -f2)
 
-		CDN_ID="$(curl -s -k -X GET -H "Cookie: mojolicious=$TO_COOKIE" $TRAFFIC_OPS_URI/api/1.2/cdns.json | jq '.response | .[] | select(.name=='"\"$CDN\""') | .id')"
-		echo "Got cdn ID: $CDN_ID"
+# Gets the id of the 'ONLINE' status
+ONLINE=$(curl -ksb cookie.jar https://trafficportal/api/1.3/statuses)
+ONLINE=$(echo $ONLINE | tr '}' '\n' | grep ONLINE | tr ',' '\n' | grep '"id"' | cut -d : -f2)
 
-		# Create Server in Traffic Ops
-		curl -v -k -X POST -H "Cookie: mojolicious=$TO_COOKIE" --data-urlencode "host_name=$HOSTNAME" --data-urlencode "domain_name=$DOMAIN" --data-urlencode "interface_name=$INTERFACE" --data-urlencode "ip_address=$IP" --data-urlencode "ip_netmask=$NETMASK" --data-urlencode "ip_gateway=$GATEWAY" --data-urlencode "interface_mtu=$MTU" --data-urlencode "cdn=$CDN_ID" --data-urlencode "cachegroup=$CACHEGROUP_ID" --data-urlencode "phys_location=$PHYS_LOCATION_ID" --data-urlencode "type=$SERVER_TYPE_ID" --data-urlencode "profile=$SERVER_PROFILE_ID" --data-urlencode "tcp_port=$PORT" --data-urlencode "offline_reason=creation" $TRAFFIC_OPS_URI/server/create
+# Gets the cachegroup ID
+CACHEGROUP=$(curl -ksb cookie.jar https://trafficportal/api/1.3/cachegroups)
+while [[ CACHEGROUP == '{"response":[]}' ]]; do
+	echo "waiting for trafficops setup to complete..."
+	sleep 3
+	CACHEGROUP=$(curl -ksb cookie.jar https://trafficportal/api/1.3/cachegroups)
+done
+CACHEGROUP=$(echo $CACHEGROUP | tr '{' '\n' | grep CDN_in_a_Box_Mid | tr ',' '\n' | grep '"id"' | cut -d : -f2)
 
-		# Add Monitor IP to `allow_ip` Parameters
-		IP_ALLOW_PARAMS=$(curl -Lsk --cookie "mojolicious=$TO_COOKIE" $TRAFFIC_OPS_URI/api/1.2/parameters?name=allow_ip | jq '.response | .[] | .id, .value')
-		while IFS= read -r id; do
-			IFS= read -r ipallow
-			ipallow=$(echo ${ipallow} | sed -e 's/^"//' -e 's/"$//')
-			IPALLOW_UPDATE_JSON="{\"id\": ${id}, \"value\": \"${ipallow},${IP}\"}"
-			curl -Lsk --cookie "mojolicious=$TO_COOKIE" -H 'Content-Type: application/json' -X PUT -d "$IPALLOW_UPDATE_JSON" $TRAFFIC_OPS_URI/api/1.2/parameters/${id}
-		done <<< "$IP_ALLOW_PARAMS"
-	fi
+# Now put it all together and send it up
+sed -ie "s;MY_LOCATION;$location;g" /server.json
+sed -ie "s;MY_TYPE;$TYPE;g" /server.json
+sed -ie "s;MY_CDN_ID;$CDN;g" /server.json
+sed -ie "s;MY_STATUS;$ONLINE;g" /server.json
+sed -ie "s;CACHE_GROUP_ID;$CACHEGROUP;g" /server.json
+sed -ie "s;MY_PROFILE_ID;$PROFILEID;g" /server.json
+cat /server.json
+curl -ksb cookie.jar -d @/server.json https://trafficportal/api/1.3/servers
+echo
 
-	SERVER_ID="$(curl -s -k -X GET -H "Cookie: mojolicious=$TO_COOKIE" $TRAFFIC_OPS_URI/api/1.2/servers.json | jq '.response | .[] | select(.hostName=='"\"$HOSTNAME\""') | .id')"
-	echo "Got server ID: $SERVER_ID"
+touch /opt/traffic_monitor/var/log/traffic_monitor.log
 
-	# Set Server to Online in Traffic Ops
-	curl -v -k -H "Content-Type: application/x-www-form-urlencoded" -H "Cookie: mojolicious=$TO_COOKIE" -X POST --data-urlencode "id=$SERVER_ID" --data-urlencode "status=ONLINE" $TRAFFIC_OPS_URI/server/updatestatus
-
-	echo "INITIALIZED=1" >> /etc/environment
-}
-
-source /etc/environment
-if [ -z "$INITIALIZED" ]; then init; fi
-start
+cd /opt/traffic_monitor
+/opt/traffic_monitor/bin/traffic_monitor -opsCfg /opt/traffic_monitor/conf/traffic_ops.cfg -config /opt/traffic_monitor/conf/traffic_monitor.cfg &
+disown
+exec tail -f /opt/traffic_monitor/var/log/traffic_monitor.log
