cf4650c37d47b6ce38919f3ad2b434aca1a01935
Brennan Fieck <Brennan_WilliamFieck@comcast.com>
2018-07-24 13:37:44 +0000

    traffic_ops/setup.sh now using environment config for auth/port

--- infrastructure/cdn-in-a-box/traffic_ops/setup.sh
+++ infrastructure/cdn-in-a-box/traffic_ops/setup.sh
@@ -20,16 +20,16 @@ set -x
 set -e
 
 # First, we need to authenticate
-curl -skc cookie.jar -d "{\"u\":\"$TO_ADMIN_USER\",\"p\":\"twelve\"}" https://localhost:6443/api/1.3/user/login
+curl -skc cookie.jar -d "{\"u\":\"$TO_ADMIN_USER\",\"p\":\"$TO_ADMIN_PASSWORD\"}" https://localhost:$TO_PORT/api/1.3/user/login
 echo
 # Now set up a new CDN...
-curl -skb cookie.jar -d @/cdns.json https://localhost:6443/api/1.3/cdns
+curl -skb cookie.jar -d @/cdns.json https://$TO_PORT:6443/api/1.3/cdns
 echo
 #... and a delivery service
-curl -skb cookie.jar -d @/deliveryservices.json https://localhost:6443/api/1.3/deliveryservices
+curl -skb cookie.jar -d @/deliveryservices.json https://localhost:$TO_PORT/api/1.3/deliveryservices
 echo
 #... and a cachegroup
-MID_LOC_ID=$(curl -skb cookie.jar https://localhost:6443/api/1.3/types | sed -re 's/\},\{/\n/g' | grep MID_LOC | tr ',' '\n' | grep '"id"' | cut -d : -f2)
+MID_LOC_ID=$(curl -skb cookie.jar https://localhost:$TO_PORT/api/1.3/types | sed -re 's/\},\{/\n/g' | grep MID_LOC | tr ',' '\n' | grep '"id"' | cut -d : -f2)
 
 # I have literally no idea why this is happening and it's so infuriating
 if [[ -f '/cachegroup.jsone' ]]; then
@@ -38,16 +38,16 @@ fi
 
 
 sed -ie "s/MID_LOC_ID/${MID_LOC_ID}/g" /cachegroup.json
-curl -skb cookie.jar -d @/cachegroup.json https://localhost:6443/api/1.3/cachegroups
+curl -skb cookie.jar -d @/cachegroup.json https://localhost:$TO_PORT/api/1.3/cachegroups
 echo
 #... and a division
-curl -skb cookie.jar -d '{"name":"CDN_in_a_Box"}' https://localhost:6443/api/1.3/divisions
+curl -skb cookie.jar -d '{"name":"CDN_in_a_Box"}' https://localhost:$TO_PORT/api/1.3/divisions
 echo
 #... and a region
-curl -skb cookie.jar -d '{"name":"CDN_in_a_Box"}' https://localhost:6443/api/1.3/divisions/CDN_in_a_Box/regions
+curl -skb cookie.jar -d '{"name":"CDN_in_a_Box"}' https://localhost:$TO_PORT/api/1.3/divisions/CDN_in_a_Box/regions
 echo
 #... and a physical location
-curl -skb cookie.jar -d @/phys_location.json https://localhost:6443/api/1.3/regions/CDN_in_a_Box/phys_locations
+curl -skb cookie.jar -d @/phys_location.json https://localhost:$TO_PORT/api/1.3/regions/CDN_in_a_Box/phys_locations
 echo
 
 
