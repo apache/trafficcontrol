e28b1776d09d750fc584744ad9f65dc3850c7ac2
Brennan Fieck <Brennan_WilliamFieck@comcast.com>
2018-07-24 13:35:22 +0000

    Mid cache now uses environment config

--- infrastructure/cdn-in-a-box/mid/setup.sh
+++ infrastructure/cdn-in-a-box/mid/setup.sh
@@ -38,52 +38,52 @@ sed -ie "s;MY_GATEWAY;$gateway;g" /server.json
 sed -ie "s;MY_NETMASK;$netmask;g" /server.json
 sed -ie "s;MY_IP;$myIP;g" /server.json
 
-while ! nc trafficops 6443 </dev/null; do
-	echo "waiting for traffic_ops_golang:6443"
+while ! nc $TO_HOST $TO_PORT </dev/null; do
+	echo "waiting for Traffic Ops"
 	sleep 3
 done
 
 # Now network things. First need to authenticate
-curl -ksc cookie.jar -d '{"u":"admin","p":"twelve"}' https://trafficportal/api/1.3/user/login
+curl -ksc cookie.jar -d "{\"u\":\"$TO_ADMIN_USER\",\"p\":\"$TO_ADMIN_PASSWORD\"}" https://$TP_HOST/api/1.3/user/login
 echo
 
 # Gets our CDN ID
-CDN=$(curl -ksb cookie.jar https://trafficportal/api/1.3/cdns)
+CDN=$(curl -ksb cookie.jar https://$TP_HOST/api/1.3/cdns)
 CDN=$(echo $CDN | tr '}' '\n' | grep CDN-in-a-Box | tr ',' '\n' | grep '"id"' | cut -d : -f2)
 while [[ -z "$CDN" ]]; do
 	echo "waiting for trafficops setup to complete..."
 	sleep 3
-	CDN=$(curl -ksb cookie.jar https://trafficportal/api/1.3/cdns)
+	CDN=$(curl -ksb cookie.jar https://$TP_HOST/api/1.3/cdns)
 	CDN=$(echo $CDN | tr '}' '\n' | grep CDN-in-a-Box | tr ',' '\n' | grep '"id"' | cut -d : -f2)
 done
 
 
 # Now we upload a profile for later use
 sed -ie "s;CDN_ID;$CDN;g" /mid_profile.json
-PROFILE=$(curl -ksb cookie.jar -d @/mid_profile.json https://trafficportal/api/1.3/profiles)
+PROFILE=$(curl -ksb cookie.jar -d @/mid_profile.json https://$TP_HOST/api/1.3/profiles)
 PROFILENAME=$(echo $PROFILE | tr ',' '\n' | grep '"name"' | cut -d : -f2 | tr -d '"')
 PROFILEID=$(echo $PROFILE | tr ',{' '\n' | grep '"id"' | cut -d : -f2)
-curl -ksb cookie.jar -d @/mid_parameters.json https://trafficportal/api/1.3/profiles/name/$PROFILENAME/parameters
+curl -ksb cookie.jar -d @/mid_parameters.json https://$TP_HOST/api/1.3/profiles/name/$PROFILENAME/parameters
 echo
 
 # Gets the location ID
-location=$(curl -ksb cookie.jar https://trafficportal/api/1.3/phys_locations)
+location=$(curl -ksb cookie.jar https://$TP_HOST/api/1.3/phys_locations)
 location=$(echo $location | tr ']' '\n' | grep CDN_in_a_Box | tr ',' '\n' | grep '"id"' | cut -d ':' -f2)
 
 # Gets the id of a MID server type
-TYPE=$(curl -ksb cookie.jar https://trafficportal/api/1.3/types)
+TYPE=$(curl -ksb cookie.jar https://$TP_HOST/api/1.3/types)
 TYPE=$(echo $TYPE | tr '}' '\n' | grep '"MID"' | tr ',' '\n' | grep '"id"' | cut -d : -f2)
 
 # Gets the id of the 'REPORTED' status
-REPORTED=$(curl -ksb cookie.jar https://trafficportal/api/1.3/statuses)
+REPORTED=$(curl -ksb cookie.jar https://$TP_HOST/api/1.3/statuses)
 REPORTED=$(echo $REPORTED | tr '}' '\n' | grep REPORTED | tr ',' '\n' | grep '"id"' | cut -d : -f2)
 
 # Gets the cachegroup ID
-CACHEGROUP=$(curl -ksb cookie.jar https://trafficportal/api/1.3/cachegroups)
+CACHEGROUP=$(curl -ksb cookie.jar https://$TP_HOST/api/1.3/cachegroups)
 while [[ CACHEGROUP == '{"response":[]}' ]]; do
 	echo "waiting for trafficops setup to complete..."
 	sleep 3
-	CACHEGROUP=$(curl -ksb cookie.jar https://trafficportal/api/1.3/cachegroups)
+	CACHEGROUP=$(curl -ksb cookie.jar https://$TP_HOST/api/1.3/cachegroups)
 done
 CACHEGROUP=$(echo $CACHEGROUP | tr '{' '\n' | grep CDN_in_a_Box | tr ',' '\n' | grep '"id"' | cut -d : -f2)
 
@@ -95,7 +95,7 @@ sed -ie "s;REPORTED_ID;$REPORTED;g" /server.json
 sed -ie "s;CACHE_GROUP_ID;$CACHEGROUP;g" /server.json
 sed -ie "s;MY_PROFILE_ID;$PROFILEID;g" /server.json
 cat /server.json
-curl -ksb cookie.jar -d @/server.json https://trafficportal/api/1.3/servers
+curl -ksb cookie.jar -d @/server.json https://$TP_HOST/api/1.3/servers
 echo
 
 fg
