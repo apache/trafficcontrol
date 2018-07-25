67b148745b6ec05648a7ce51a00113e7476b1e3d
Jesse Rivas <jesse_rivas@comcast.com>
2018-07-25 14:50:43 -0600

    editStaticDnsEntry

--- traffic_portal/app/src/common/modules/table/deliveryServiceStaticDnsEntries/TableDeliveryServiceStaticDnsEntriesController.js
+++ traffic_portal/app/src/common/modules/table/deliveryServiceStaticDnsEntries/TableDeliveryServiceStaticDnsEntriesController.js
@@ -27,6 +27,10 @@ var TableDeliveryServiceStaticDnsEntriesController = function(deliveryService, s
 		$state.reload(); // reloads all the resolves for the view
 	};
 
+    $scope.editStaticDnsEntry = function(dsId, staticDnsEntryId) {
+        locationUtils.navigateToPath('/delivery-services/' + dsId + '/static-dns-entry/' + staticDnsEntryId);
+    };
+
 	$scope.navigateToPath = locationUtils.navigateToPath;
 
 	angular.element(document).ready(function () {
