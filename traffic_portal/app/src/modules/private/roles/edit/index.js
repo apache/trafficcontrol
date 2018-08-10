95044f16cc1b22dc9503a914f7cf8f3dbb59ad1e
Jeremy Mitchell <mitchell852@gmail.com>
2018-08-10 11:27:05 -0600

    turn caps on/off based upon setting in tp_properties.json

--- traffic_portal/app/src/modules/private/roles/edit/index.js
+++ traffic_portal/app/src/modules/private/roles/edit/index.js
@@ -29,9 +29,6 @@ module.exports = angular.module('trafficPortal.private.roles.edit', [])
 						resolve: {
 							roles: function($stateParams, roleService) {
 								return roleService.getRoles({ id: $stateParams.roleId });
-							},
-							useCapabilities: function(parameterService) {
-								return parameterService.getParameters({ name: 'use_capabilities', configFile: 'global' });
 							}
 						}
 					}
