86c93dd6f7388a5ba7e89ace3b8a59851d8b92e0
Jeffrey Bevill <Jeffrey_Bevill@comcast.com>
2018-08-13 11:52:58 -0600

    Traffic Ops - admin.pl load_schema proper SQL statement eval and exit codes - Fix #2667

--- traffic_ops/app/db/create_tables.sql
+++ traffic_ops/app/db/create_tables.sql
@@ -35,13 +35,6 @@ SET row_security = off;
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
 SET search_path = public, pg_catalog;
 
 --
