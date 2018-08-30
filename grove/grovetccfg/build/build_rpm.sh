a12353f4223e6281d3911532a69db2e8d743502a
John Rushford <jrushford@apache.org>
2018-08-30 17:45:55 +0000

    Modify the grove build scripts to insure that grove is built using a go 1.11 or later compiler.  This addresses a performance problem with TLS requests using RSA certificates, see Issue #2780.

--- grove/grovetccfg/build/build_rpm.sh
+++ grove/grovetccfg/build/build_rpm.sh
@@ -14,9 +14,9 @@
 
 #----------------------------------------
 function importFunctions() {
-  TC_DIR=$(git rev-parse --show-toplevel)
-  [ ! -z "$TC_DIR" ] || { echo "Cannot find repository root." >&2 ; exit 1; }
-  export TC_DIR
+	TC_DIR=$(git rev-parse --show-toplevel)
+	[ ! -z "$TC_DIR" ] || { echo "Cannot find repository root." >&2 ; exit 1; }
+	export TC_DIR
 	functions_sh="$TC_DIR/build/functions.sh"
 	if [[ ! -r $functions_sh ]]; then
 		echo "Error: Can't find $functions_sh"
@@ -32,67 +32,74 @@ function checkGroveEnvironment() {
 	local scriptdir=$(dirname "$script")
 
 	export GROVETC_DIR=$(dirname "$scriptdir")
-  export GROVE_DIR=$(dirname "$GROVETC_DIR")
-  export GROVE_VERSION=`cat ${GROVE_DIR}/VERSION`
+	export GROVE_DIR=$(dirname "$GROVETC_DIR")
+	export GROVE_VERSION=`cat ${GROVE_DIR}/VERSION`
 	export PACKAGE="grovetccfg"
 	export BUILD_NUMBER=${BUILD_NUMBER:-$(getBuildNumber)}
 	export RPMBUILD="${GROVE_DIR}/rpmbuild"
 	export DIST="${TC_DIR}/dist"
 	export RPM="${PACKAGE}-${GROVE_VERSION}-${BUILD_NUMBER}.x86_64.rpm"
 
+	# grovetccfg needs to be built with go 1.11 or greater
+	verify_and_set_go_version
+	if [[ $? -ne 1 ]]; then
+		exit 0
+	fi
+
 	echo "=================================================="
-  echo "TC_DIR: $TC_DIR"
-  echo "PACKAGE: $PACKAGE"
-  echo "GROVE_DIR: $GROVE_DIR"
-  echo "GROVETC_DIR: $GROVETC_DIR"
+	echo "GO_VERSION: $GO_VERSION"
+	echo "TC_DIR: $TC_DIR"
+	echo "PACKAGE: $PACKAGE"
+	echo "GROVE_DIR: $GROVE_DIR"
+	echo "GROVETC_DIR: $GROVETC_DIR"
 	echo "GROVE_VERSION: $GROVE_VERSION"
 	echo "BUILD_NUMBER: $BUILD_NUMBER"
-  echo "DIST: $DIST"
+	echo "DIST: $DIST"
 	echo "RPM: $RPM"
-  echo "RPMBUILD: $RPMBUILD"
+	echo "RPMBUILD: $RPMBUILD"
 	echo "--------------------------------------------------"
 }
 
 # ---------------------------------------
 function initBuildArea() {
-  cd "$GROVETC_DIR"
+	cd "$GROVETC_DIR"
 
-  # prep build environment
-  [ -e $RPMBUILD ] && rm -rf $RPMBUILD
-  [ ! -e $RPMBUILD ] || { echo "Failed to clean up rpm build directory '$RPMBUILD': $?" >&2; exit 1; }
-  mkdir -p $RPMBUILD/{BUILD,RPMS,SOURCES} || { echo "Failed to create build directory '$RPMBUILD': $?" >&2; exit 1; }
+	# prep build environment
+	[ -e $RPMBUILD ] && rm -rf $RPMBUILD
+	[ ! -e $RPMBUILD ] || { echo "Failed to clean up rpm build directory '$RPMBUILD': $?" >&2; exit 1; }
+	mkdir -p $RPMBUILD/{BUILD,RPMS,SOURCES} || { echo "Failed to create build directory '$RPMBUILD': $?" >&2; exit 1; }
 }
 
 # ---------------------------------------
 function buildRpmGrove() {
-  # build
-  go get -v -d . || { echo "Failed to go get dependencies: $?" >&2; exit 1; }
-  go build -v -ldflags "-X main.Version=$GROVE_VERSION" || { echo "Failed to build $PACKAGE: $?" >&2; exit 1; }
+	# build
+	$GO get -v -d . || { echo "Failed to go get dependencies: $?" >&2; exit 1; }
+	$GO build -v -ldflags "-X main.Version=$GROVE_VERSION" || { echo "Failed to build $PACKAGE: $?" >&2; exit 1; }
 
-  # tar
-  tar -cvzf $RPMBUILD/SOURCES/${PACKAGE}-${GROVE_VERSION}.tgz ${PACKAGE}|| { echo "Failed to create archive for rpmbuild: $?" >&2; exit 1; }
+	# tar
+	tar -cvzf $RPMBUILD/SOURCES/${PACKAGE}-${GROVE_VERSION}.tgz ${PACKAGE}|| { echo "Failed to create archive for rpmbuild: $?" >&2; exit 1; }
 
-  # Work around bug in rpmbuild. Fixed in rpmbuild 4.13.
-  # See: https://github.com/rpm-software-management/rpm/commit/916d528b0bfcb33747e81a57021e01586aa82139
-  # Takes ownership of the spec file.
-  spec=build/${PACKAGE}.spec
-  spec_owner=$(stat -c%u $spec)
-  spec_group=$(stat -c%g $spec)
-  if ! id $spec_owner >/dev/null 2>&1; then
+	# Work around bug in rpmbuild. Fixed in rpmbuild 4.13.
+	# See: https://github.com/rpm-software-management/rpm/commit/916d528b0bfcb33747e81a57021e01586aa82139
+	# Takes ownership of the spec file.
+	spec=build/${PACKAGE}.spec
+	spec_owner=$(stat -c%u $spec)
+	spec_group=$(stat -c%g $spec)
+	if ! id $spec_owner >/dev/null 2>&1; then
 	  chown $(id -u):$(id -g) build/${PACKAGE}.spec
 
 	  function give_spec_back {
 		chown ${spec_owner}:${spec_group} build/${PACKAGE}.spec
 	  }
 	  trap give_spec_back EXIT
-  fi
+	fi
 
-  # build RPM
-  rpmbuild --define "_topdir $RPMBUILD" --define "version ${GROVE_VERSION}" --define "build_number ${BUILD_NUMBER}" -ba build/${PACKAGE}.spec || { echo "rpmbuild failed: $?" >&2; exit 1; }
+	# build RPM
+	rpmbuild --define "_topdir $RPMBUILD" --define "version ${GROVE_VERSION}" --define "build_number ${BUILD_NUMBER}" -ba build/${PACKAGE}.spec || { echo "rpmbuild failed: $?" >&2; exit 1; }
 
-  # copy build RPM to .
-  [ -e $DIST ] || mkdir -p $DIST
-  cp $RPMBUILD/RPMS/x86_64/${RPM} $DIST
+	# copy build RPM to .
+	[ -e $DIST ] || mkdir -p $DIST
+	cp $RPMBUILD/RPMS/x86_64/${RPM} $DIST
 }
 
 importFunctions
