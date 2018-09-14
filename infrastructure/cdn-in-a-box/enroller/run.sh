f5731f014c5583c900d70bd3dbccba2d47778cae
Jeffrey Bevill <Jeffrey_Bevill@comcast.com>
2018-09-14 13:56:57 -0600

    Add shared CA support for all container services

--- infrastructure/cdn-in-a-box/enroller/run.sh
+++ infrastructure/cdn-in-a-box/enroller/run.sh
@@ -24,6 +24,20 @@ export TO_URL=https://$TO_FQDN:$TO_PORT
 export TO_USER=$TO_ADMIN_USER
 export TO_PASSWORD=$TO_ADMIN_PASSWORD
 
+# Wait on SSL certificate generation
+until [ -f "$CERT_DONE_FILE" ] 
+do
+     echo "Waiting on Shared SSL certificate generation"
+     sleep 3
+done
+
+# Source the CIAB-CA shared SSL environment
+source "$CERT_ENV_FILE"
+ 
+# Copy the CIAB-CA certificate to the traffic_router conf so it can be added to the trust store
+cp $CERT_CA_CERT_FILE /usr/local/share/ca-certificates
+update-ca-certificates
+
 # Traffic Ops must be accepting connections before enroller can start
 until nc -z $TO_HOST $TO_PORT </dev/null >/dev/null && to-ping; do
     echo "Waiting for $TO_URL"
