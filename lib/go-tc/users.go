c71626afbbad10724d22b7fff45be6ee3af01679
moltzaum <matthew@moltzau.net>
2018-09-20 09:13:01 -0600

    User client and tests

--- lib/go-tc/users.go
+++ lib/go-tc/users.go
@@ -19,114 +19,111 @@ package tc
  * under the License.
  */
 
-import (
-	"time"
-)
-
-// UsersResponse ...
-type UsersResponse struct {
-	Response []User `json:"response"`
+// UserCredentials contains Traffic Ops login credentials
+type UserCredentials struct {
+	Username string `json:"u"`
+	Password string `json:"p"`
 }
 
-// User contains information about a given user in Traffic Ops.
-type User struct {
-	Username         string    `json:"username,omitempty"`
-	PublicSSHKey     string    `json:"publicSshKey,omitempty"`
-	Role             int       `json:"role,omitempty"`
-	RoleName         string    `json:"rolename,omitempty"`
-	ID               int       `json:"id,omitempty"`
-	UID              int       `json:"uid,omitempty"`
-	GID              int       `json:"gid,omitempty"`
-	Company          string    `json:"company,omitempty"`
-	Email            string    `json:"email,omitempty"`
-	FullName         string    `json:"fullName,omitempty"`
-	NewUser          bool      `json:"newUser,omitempty"`
-	LastUpdated      string    `json:"lastUpdated,omitempty"`
+// UserV13 contains non-nullable TO user information
+type UserV13 struct {
+	Username         string    `json:"username"`
+	PublicSSHKey     string    `json:"publicSshKey"`
+	Role             int       `json:"role"`
+	RoleName         string    `json:"rolename"`
+	ID               int       `json:"id"`
+	UID              int       `json:"uid"`
+	GID              int       `json:"gid"`
+	Company          string    `json:"company"`
+	Email            string    `json:"email"`
+	FullName         string    `json:"fullName"`
+	NewUser          bool      `json:"newUser"`
+	LastUpdated      string    `json:"lastUpdated"`
 	AddressLine1     string    `json:"addressLine1"`
 	AddressLine2     string    `json:"addressLine2"`
 	City             string    `json:"city"`
 	Country          string    `json:"country"`
 	PhoneNumber      string    `json:"phoneNumber"`
 	PostalCode       string    `json:"postalCode"`
-	RegistrationSent time.Time `json:"registrationSent"`
+	RegistrationSent TimeNoMod `json:"registrationSent"`
 	StateOrProvince  string    `json:"stateOrProvince"`
 	Tenant           string    `json:"tenant"`
 	TenantID         int       `json:"tenantId"`
 }
 
-// Credentials contains Traffic Ops login credentials
-type UserCredentials struct {
-	Username string `json:"u"`
-	Password string `json:"p"`
-}
-
-// TODO reconcile APIUser and User
-
-type APIUser struct {
-	AddressLine1     *string    `json:"addressLine1", db:"address_line1"`
-	AddressLine2     *string    `json:"addressLine2" db:"address_line2"`
-	City             *string    `json:"city" db:"city"`
-	Company          *string    `json:"company,omitempty" db:"company"`
-	Country          *string    `json:"country" db:"country"`
-	Email            *string    `json:"email" db:"email"`
-	FullName         *string    `json:"fullName" db:"full_name"`
-	GID              *int       `json:"gid" db:"gid"`
-	ID               *int       `json:"id" db:"id"`
-	LastUpdated      *time.Time `json:"lastUpdated" db:"last_updated"`
-	NewUser          *bool      `json:"newUser" db:"new_user"`
-	PhoneNumber      *string    `json:"phoneNumber" db:"phone_number"`
-	PostalCode       *string    `json:"postalCode" db:"postal_code"`
-	PublicSSHKey     *string    `json:"publicSshKey" db:"public_ssh_key"`
-	RegistrationSent *time.Time `json:"registrationSent" db:"registration_sent"`
-	Role             *int       `json:"role" db:"role"`
-	RoleName         *string    `json:"rolename"`
-	StateOrProvince  *string    `json:"stateOrProvince" db:"state_or_province"`
-	Tenant           *string    `json:"tenant"`
-	TenantID         *int       `json:"tenantId" db:"tenant_id"`
-	UID              *int       `json:"uid" db:"uid"`
-	UserName         *string    `json:"username" db:"username"`
-}
-
-type APIUserPost struct {
-	APIUser
-	ConfirmLocalPassword *string `json:"confirmLocalPassword" db:"confirm_local_passwd"`
-	LocalPassword        *string `json:"localPassword" db:"local_passwd"`
-}
-
-type APIUsersResponse struct {
-	Response []APIUser `json:"response"`
-}
-
-type UserDeliveryServiceDeleteResponse struct {
-	Alerts []Alert `json:"alerts"`
-}
-
-// UserCurrent contains all the user info about the current user, as returned by /api/1.x/user/current
-type UserCurrent struct {
-	AddressLine1    *string `json:"addressLine1"`
-	AddressLine2    *string `json:"addressLine2"`
-	City            *string `json:"city"`
-	Company         *string `json:"company,omitempty"`
-	Country         *string `json:"country"`
-	Email           *string `json:"email,omitempty"`
-	FullName        *string `json:"fullName,omitempty"`
-	GID             *int    `json:"gid,omitempty"`
-	ID              *int    `json:"id,omitempty"`
-	LastUpdated     *string `json:"lastUpdated,omitempty"`
-	LocalUser       *bool   `json:"localUser"`
-	NewUser         *bool   `json:"newUser,omitempty"`
-	PhoneNumber     *string `json:"phoneNumber"`
-	PostalCode      *string `json:"postalCode"`
-	PublicSSHKey    *string `json:"publicSshKey,omitempty"`
-	Role            *int    `json:"role,omitempty"`
-	RoleName        *string `json:"roleName,omitempty"`
-	StateOrProvince *string `json:"stateOrProvince"`
+// commonUserFields is unexported, but its contents are still visible when it is embedded
+// LastUpdated is a new field for some structs
+type commonUserFields struct {
+	AddressLine1    *string `json:"addressLine1" db:"address_line1"`
+	AddressLine2    *string `json:"addressLine2" db:"address_line2"`
+	City            *string `json:"city" db:"city"`
+	Company         *string `json:"company" db:"company"`
+	Country         *string `json:"country" db:"country"`
+	Email           *string `json:"email" db:"email"`
+	FullName        *string `json:"fullName" db:"full_name"`
+	GID             *int    `json:"gid"`
+	ID              *int    `json:"id" db:"id"`
+	NewUser         *bool   `json:"newUser" db:"new_user"`
+	PhoneNumber     *string `json:"phoneNumber" db:"phone_number"`
+	PostalCode      *string `json:"postalCode" db:"postal_code"`
+	PublicSSHKey    *string `json:"publicSshKey" db:"public_ssh_key"`
+	Role            *int    `json:"role" db:"role"`
+	RoleName        *string `json:"rolename"`
+	StateOrProvince *string `json:"stateOrProvince" db:"state_or_province"`
 	Tenant          *string `json:"tenant"`
-	TenantID        *uint64 `json:"tenantId"`
-	UID             *int    `json:"uid,omitempty"`
-	UserName        *string `json:"username,omitempty"`
+	TenantID        *int    `json:"tenantId" db:"tenant_id"`
+	UID             *int    `json:"uid"`
+	//Username        *string    `json:"username" db:"username"`  //not including major change due to naming incompatibility
+	LastUpdated *TimeNoMod `json:"lastUpdated" db:"last_updated"` //including minor change to add field
+}
+
+// User fields in v14 have been updated to be nullable. omitempty is used to protect password data
+type User struct {
+	Username             *string    `json:"username" db:"username"`
+	RegistrationSent     *TimeNoMod `json:"registrationSent" db:"registration_sent"`
+	ConfirmLocalPassword *string    `json:"confirmLocalPasswd,omitempty" db:"confirm_local_passwd"`
+	LocalPassword        *string    `json:"localPasswd,omitempty" db:"local_passwd"`
+	commonUserFields
+}
+
+// UserCurrent represents the profile for the authenticated user
+type UserCurrent struct {
+	UserName  *string `json:"username"`
+	LocalUser *bool   `json:"localUser"`
+	commonUserFields
+}
+
+// ------------------- Response structs -------------------- //
+//  Response structs should only be used in the client       //
+//  The client's use of these will eventually be deprecated  //
+// --------------------------------------------------------- //
+
+type UsersResponseV13 struct {
+	Response []UserV13 `json:"response"`
+}
+
+type UsersResponse struct {
+	Response []User `json:"response"`
+}
+
+type CreateUserResponse struct {
+	Response User `json:"response"`
+	Alerts
+}
+
+type UpdateUserResponse struct {
+	Response User `json:"response"`
+	Alerts
+}
+
+type DeleteUserResponse struct {
+	Alerts
 }
 
 type UserCurrentResponse struct {
 	Response UserCurrent `json:"response"`
+}
+
+type UserDeliveryServiceDeleteResponse struct {
+	Alerts
 }
