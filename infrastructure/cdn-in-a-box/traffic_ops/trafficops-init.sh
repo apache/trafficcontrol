76cdc5af66bab43ec3647ef986b4188fb903c970
Dan Kirkwood <dangogh@apache.org>
2018-09-21 11:01:53 -0600

    enroller handles deliveryservice_servers
    (cherry picked from commit 26f4ec6d771f57e6e1a13b4c46cf1672411f6ad5)

--- infrastructure/cdn-in-a-box/traffic_ops/trafficops-init.sh
+++ infrastructure/cdn-in-a-box/traffic_ops/trafficops-init.sh
@@ -36,7 +36,7 @@ while ! to-ping 2>/dev/null; do
 done
 
 # NOTE: order dependent on foreign key references, e.g. profiles must be loaded before parameters
-endpoints="cdns types divisions regions phys_locations tenants users cachegroups deliveryservices profiles parameters servers"
+endpoints="cdns types divisions regions phys_locations tenants users cachegroups deliveryservices profiles parameters servers deliveryservice_servers"
 vars=$(awk -F = '/^\w/ {printf "$%s ",$1}' /variables.env)
 
 load_data_from() {
