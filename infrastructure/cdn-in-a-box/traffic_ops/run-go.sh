768b5af424b7fb66df6e9833d7ad09cb7a23573d
Jeffrey Bevill <Jeffrey_Bevill@comcast.com>
2018-09-24 16:02:08 -0600

    Only start TM after CDN snapshot
    (cherry picked from commit 355f4eec3352abc885f0bf31d691795ddec01038)

--- infrastructure/cdn-in-a-box/traffic_ops/run-go.sh
+++ infrastructure/cdn-in-a-box/traffic_ops/run-go.sh
@@ -73,8 +73,51 @@ TO_USER=$TO_ADMIN_USER TO_PASSWORD=$TO_ADMIN_PASSWORD to-enroll $(hostname -s) &
 
 ./bin/traffic_ops_golang -cfg $CDNCONF -dbcfg $DBCONF -riakcfg $RIAKCONF &
 
-sleep 5
-
 to-enroll "to" ALL || (while true; do echo "enroll failed."; sleep 3 ; done)
+
+### Workaround: Start DeliveryService and Edge association
+while true; do
+  edge_name="$(to-get 'api/1.3/servers/hostname/edge/details' 2>/dev/null | jq -r -c '.response|.hostName')"
+  ds_name="$(to-get 'api/1.3/deliveryservices' 2>/dev/null | jq -r -c '.response[].xmlId')"
+
+  if [ -n "$edge_name" ] && [ "$ds_name" ] ; then
+    tmp_file=$(mktemp)
+    echo "{ \"xmlId\" : \"$ds_name\", \"serverNames\": [ \"$edge_name\" ] }" > $tmp_file
+    cp $tmp_file /shared/enroller/deliveryservice_servers/
+    break
+  else 
+    echo "Waiting for delivery service and edge to exist..."
+  fi
+
+  sleep 2
+done
+
+while true; do
+  echo "Verifying that edge was associated to delivery service..."
+
+  edge_name="$(to-get 'api/1.3/servers/hostname/edge/details' 2>/dev/null | jq -r -c '.response|.hostName')"
+  ds_name="$(to-get 'api/1.3/deliveryservices' 2>/dev/null | jq -r -c '.response[].xmlId')"
+  ds_id="$(to-get 'api/1.3/deliveryservices' 2>/dev/null | jq -r -c '.response[].id')"
+  edge_verify=$(to-get "/api/1.2/deliveryservices/$ds_id/servers" | jq -r '.response[]|.hostName')
+
+  if [[ $edge_verify = $edge_name ]] ; then
+    break
+  fi
+
+  sleep 2
+done
+
+# Snapshot the CDN 
+until [ $(to-get '/CRConfig-Snapshots/CDN-in-a-Box/CRConfig.json' 2>/dev/null | jq -c -e '.config|length') -gt 0 ] ; do 
+  sleep 2
+  echo "Do Snapshot for CDN-in-a-Box..."; 
+  to-put 'api/1.3/cdns/2/snapshot'
+done
+
+# Queue Updates 
+sleep 1
+to-post 'api/1.3/cdns/2/queue_update' '{"action":"queue"}'
+
+### Workaround: End DeliveryService and Edge association
 
 exec tail -f /dev/null
