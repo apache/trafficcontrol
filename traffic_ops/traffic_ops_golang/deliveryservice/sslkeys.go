26a30b2261aa8548f63cdb2aa28fc1ddfb183478
Rawlin Peters <rawlin_peters@comcast.com>
2018-09-24 16:15:34 -0600

    Fix DS SSL API request validation, tenancy checks, versions
    Validate API requests for cert generation and addition separately, add
    tenancy checks to cert generation, only store the numeric version of the
    SSL key in the Riak record (not "latest"), and update the
    deliveryservice.ssl_key_version to the requested version of the ssl key
    add/generate request.

--- traffic_ops/traffic_ops_golang/deliveryservice/sslkeys.go
+++ traffic_ops/traffic_ops_golang/deliveryservice/sslkeys.go
@@ -28,6 +28,7 @@ import (
 	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/api"
 	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/config"
 	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/riaksvc"
+	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/tenant"
 )
 
 func GenerateSSLKeys(w http.ResponseWriter, r *http.Request) {
@@ -38,14 +39,26 @@ func GenerateSSLKeys(w http.ResponseWriter, r *http.Request) {
 	}
 	defer inf.Close()
 
-	req := tc.DeliveryServiceSSLKeysReq{}
+	req := tc.DeliveryServiceGenSSLKeysReq{}
 	if err := api.Parse(r.Body, inf.Tx.Tx, &req); err != nil {
 		api.HandleErr(w, r, inf.Tx.Tx, http.StatusBadRequest, errors.New("parsing request: "+err.Error()), nil)
 		return
 	}
-
+	if userErr, sysErr, errCode := tenant.Check(inf.User, *req.DeliveryService, inf.Tx.Tx); userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, errCode, userErr, sysErr)
+		return
+	}
 	if err := generatePutRiakKeys(req, inf.Tx.Tx, inf.Config); err != nil {
 		api.HandleErr(w, r, inf.Tx.Tx, http.StatusInternalServerError, nil, errors.New("generating and putting SSL keys: "+err.Error()))
+		return
+	}
+	version, err := req.Version.ToInt()
+	if err != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, http.StatusBadRequest, errors.New("generating SSL keys for delivery service '"+*req.DeliveryService+"': "+err.Error()), nil)
+		return
+	}
+	if err := updateSSLKeyVersion(*req.DeliveryService, version, inf.Tx.Tx); err != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, http.StatusInternalServerError, nil, errors.New("generating SSL keys for delivery service '"+*req.DeliveryService+"': "+err.Error()))
 		return
 	}
 	api.WriteResp(w, r, "Successfully created ssl keys for "+*req.DeliveryService)
@@ -53,7 +66,7 @@ func GenerateSSLKeys(w http.ResponseWriter, r *http.Request) {
 
 // generatePutRiakKeys generates a certificate, csr, and key from the given request, and insert it into the Riak key database.
 // The req MUST be validated, ensuring required fields exist.
-func generatePutRiakKeys(req tc.DeliveryServiceSSLKeysReq, tx *sql.Tx, cfg *config.Config) error {
+func generatePutRiakKeys(req tc.DeliveryServiceGenSSLKeysReq, tx *sql.Tx, cfg *config.Config) error {
 	dsSSLKeys := tc.DeliveryServiceSSLKeys{
 		CDN:             *req.CDN,
 		DeliveryService: *req.DeliveryService,
@@ -66,22 +79,13 @@ func generatePutRiakKeys(req tc.DeliveryServiceSSLKeysReq, tx *sql.Tx, cfg *conf
 		Key:             *req.Key,
 		Version:         *req.Version,
 	}
-	if req.Certificate != nil {
-		dsSSLKeys.Certificate = *req.Certificate
-	} else {
-		csr, crt, key, err := GenerateCert(*req.HostName, *req.Country, *req.City, *req.State, *req.Organization, *req.BusinessUnit)
-		if err != nil {
-			return errors.New("generating certificate: " + err.Error())
-		}
-		dsSSLKeys.Certificate = tc.DeliveryServiceSSLKeysCertificate{Crt: string(crt), Key: string(key), CSR: string(csr)}
+	csr, crt, key, err := GenerateCert(*req.HostName, *req.Country, *req.City, *req.State, *req.Organization, *req.BusinessUnit)
+	if err != nil {
+		return errors.New("generating certificate: " + err.Error())
 	}
+	dsSSLKeys.Certificate = tc.DeliveryServiceSSLKeysCertificate{Crt: string(crt), Key: string(key), CSR: string(csr)}
 	if err := riaksvc.PutDeliveryServiceSSLKeysObj(dsSSLKeys, tx, cfg.RiakAuthOptions); err != nil {
 		return errors.New("putting riak keys: " + err.Error())
-	}
-
-	dsSSLKeys.Version = riaksvc.DSSSLKeyVersionLatest
-	if err := riaksvc.PutDeliveryServiceSSLKeysObj(dsSSLKeys, tx, cfg.RiakAuthOptions); err != nil {
-		return errors.New("putting latest riak keys: " + err.Error())
 	}
 	return nil
 }
