d8cf653eaecb0074db8a860ba9393982d8ff5cad
Robert Butts <rob@apache.org>
2018-09-27 18:16:18 -0600

    Remove unused symbols

--- lib/go-util/num_test.go
+++ lib/go-util/num_test.go
@@ -179,21 +179,3 @@ func TestBytesLenSplit(t *testing.T) {
 		}
 	}
 }
-
-func TestJSONNumAsStrUnmarshalJSON(t *testing.T) {
-	type T struct {
-		V JSONNumAsStr `json:"v"`
-	}
-	jNum := `{"v":42}`
-	expected := T{V: "42"}
-	actual := T{}
-	json.Unmarshal([]byte(jNum), &actual)
-	if expected != actual {
-		t.Errorf("JSONNumAsStr number unmarshal expected %v actual %v", expected, actual)
-	}
-	jStr := `{"v":"42"}`
-	json.Unmarshal([]byte(jStr), &actual)
-	if expected != actual {
-		t.Errorf("JSONNumAsStr string unmarshal expected %v actual %v", expected, actual)
-	}
-}
