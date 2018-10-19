f8d32912a914155abb83847f21a7c14f3d708b4d
moltzaum <matthew@moltzau.net>
2018-10-19 16:37:00 -0600

    Dockerized TO integration tests

---
+++ infrastructure/cdn-in-a-box/to_integration/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# Check that env vars are set
+envvars=( DB_SERVER DB_PORT DB_ROOT_PASS DB_USER DB_USER_PASS ADMIN_USER ADMIN_PASS)
+for v in $envvars
+do
+	if [[ -z $$v ]]; then echo "$v is unset"; exit 1; fi
+done
+
+source to-access.sh
+
+TO_URL="https://$TO_FQDN:$TO_PORT"
+while ! to-ping 2>/dev/null; do
+   echo waiting for trafficops
+   sleep 3
+done
+
+# if [[ -r ]]; then; source config.sh; done    traffic_ops/run.sh
+# if [[ -x ]]; then;./config.sh; done          traffic_ops/run-go.sh
+source config.sh
+
+./traffic_ops_integration -cfg=traffic-ops-test.conf
+exit $?
+
