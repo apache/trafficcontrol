8c9e28cff1ef7046c826d4281306ffdd0995f5c4
moltzaum <matthew@moltzau.net>
2018-10-25 15:25:05 -0600

    Response to PR comments

--- infrastructure/cdn-in-a-box/traffic_ops_integration_test/config.sh
+++ infrastructure/cdn-in-a-box/traffic_ops_integration_test/config.sh
@@ -24,7 +24,7 @@
 envvars=( DB_SERVER DB_PORT DB_ROOT_PASS DB_USER DB_USER_PASS ADMIN_USER ADMIN_PASS DOMAIN TO_PERL_HOST TO_PERL_PORT TO_HOST TO_PORT TP_HOST)
 for v in $envvars
 do
-	if [[ -z $$v ]]; then echo "$v is unset"; exit 1; fi
+    if [[ -z $$v ]]; then echo "$v is unset"; exit 1; fi
 done
 
 cat <<-EOF >/opt/integration/app/traffic-ops-test.conf
@@ -43,7 +43,7 @@ cat <<-EOF >/opt/integration/app/traffic-ops-test.conf
     },
     "trafficOps": {
         "URL": "https://$TO_HOST:$TO_PORT",
-	    "password": "$ADMIN_PASS",
+        "password": "$ADMIN_PASS",
         "users": {
             "disallowed": "disallowed",
             "operations": "operations",
