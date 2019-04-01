7bfb65e1aa05cbc81396817542c007e5b45c3071
ocket8888 <ocket8888@gmail.com>
2019-04-01 10:15:22 -0600

    ORT.py now implements a REVALIDATE mode that only updates configuration files relevant to content invalidation. (#3371)
    * ORT.py now implements a REVALIDATE mode that only updates configuration files relevant to content invalidation.
    
    * Fixed missing string format escape sequence

--- infrastructure/cdn-in-a-box/ort/traffic_ops_ort/main_routines.py
+++ infrastructure/cdn-in-a-box/ort/traffic_ops_ort/main_routines.py
@@ -260,6 +260,9 @@ def processConfigurationFiles(conf:Configuration) -> bool:
 	for file in myFiles:
 		try:
 			file = config_files.ConfigFile(file, conf.TOURL)
+			if conf.mode is conf.Modes.REVALIDATE and file.fname != "regex_revalidate.config":
+				logging.info("Skipping file %s because is not a revalidation file", file.fname)
+				continue
 			logging.info("\n============ Processing File: %s ============", file.fname)
 			if file.update(conf) and file.fname in services.FILES_THAT_REQUIRE_RELOADS:
 				services.NEEDED_RELOADS.add(services.FILES_THAT_REQUIRE_RELOADS[file.fname])
