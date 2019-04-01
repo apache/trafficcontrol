d7bd2db73641904b48375b61923a22dbfe0df5ed
ocket8888 <ocket8888@gmail.com>
2019-04-01 10:19:35 -0600

    Added model for Roles (i.e. 'Role models', heh)

--- experimental/traffic-portal-angular7/src/app/models/user.ts
+++ experimental/traffic-portal-angular7/src/app/models/user.ts
@@ -16,26 +16,38 @@
  * Reprents a Traffic Ops user (`tm_user` in the database)
 */
 export class User {
-	addressLine1?: string;
-	addressLine2?: string;
-	city?: string;
-	company?: string;
-	country?: string;
-	email: string;
-	fullName?: string;
-	gid?: number;
-	id: number;
-	lastUpdated?: Date;
-	localUser: boolean;
-	newUser: boolean;
-	phoneNumber?: string;
-	postalCode?: string;
-	publicSshKey?: string;
-	role?: number;
-	roleName?: string;
+	addressLine1?:    string;
+	addressLine2?:    string;
+	city?:            string;
+	company?:         string;
+	country?:         string;
+	email:            string;
+	fullName?:        string;
+	gid?:             number;
+	id:               number;
+	lastUpdated?:     Date;
+	localUser:        boolean;
+	newUser:          boolean;
+	phoneNumber?:     string;
+	postalCode?:      string;
+	publicSshKey?:    string;
+	role?:            number;
+	roleName?:        string;
 	stateOrProvince?: string;
-	tenant?: string;
-	tenantId?: number;
-	uid?: number;
-	username: string;
+	tenant?:          string;
+	tenantId?:        number;
+	uid?:             number;
+	username:         string;
+}
+
+
+/**
+ * Represents a role that a user may have
+*/
+export class Role {
+	capabilities: Array<string>;
+	description?: string;
+	id:           number;
+	name:         string;
+	privLevel:    number;
 }
