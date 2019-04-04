fb873038976a363d337b673d5547cae5b2e7f751
ocket8888 <ocket8888@gmail.com>
2019-04-04 11:50:31 -0600

    Added testing support for Firefox and Opera

--- experimental/traffic-portal-angular7/src/karma.conf.js
+++ experimental/traffic-portal-angular7/src/karma.conf.js
@@ -16,30 +16,39 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine', '@angular-devkit/build-angular'],
+		plugins: [
+			require('karma-jasmine'),
+			require('karma-chrome-launcher'),
+			require('karma-firefox-launcher'),
+			require('karma-opera-launcher'),
+			require('karma-jasmine-html-reporter'),
+			require('karma-coverage-istanbul-reporter'),
+			require('@angular-devkit/build-angular/plugins/karma')
+		],
+		client: {
+			clearContext: false // leave Jasmine Spec Runner output visible in browser
+		},
+		browsers: [
+			'Chrome',
+			'Firefox',
+			'Opera'
+		],
+		browserConsoleLog: {
+			terminal: true
+		},
+		coverageIstanbulReporter: {
+			dir: require('path').join(__dirname, '../coverage'),
+			reports: ['html', 'lcovonly', 'text-summary'],
+			fixWebpackSourcePaths: true
+		},
+		reporters: ['progress', 'kjhtml'],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		singleRun: false
+	});
 };
