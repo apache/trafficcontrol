306927317fe33500c9bba8cab1a53242b67b92c4
ocket8888 <ocket8888@gmail.com>
2019-05-28 13:14:22 -0600

    Removed restangular from ./UserService.js

--- traffic_portal/app/src/common/api/UserService.js
+++ traffic_portal/app/src/common/api/UserService.js
@@ -17,143 +17,153 @@
  * under the License.
  */
 
-var UserService = function(Restangular, $http, $location, $q, authService, httpService, locationUtils, userModel, messageModel, ENV) {
-
-    var service = this;
+var UserService = function($http, locationUtils, userModel, messageModel, ENV) {
 
     this.getCurrentUser = function() {
-        var deferred = $q.defer();
-
-        $http.get(ENV.api['root'] + "user/current")
-            .then(
-                function(result) {
-                    userModel.setUser(result.data.response);
-                    deferred.resolve(result.data.response);
-                },
-                function(fault) {
-                    deferred.reject(fault);
-                }
-            );
-
-        return deferred.promise;
+        return $http.get(ENV.api['root'] + "user/current").then(
+            function(result) {
+                userModel.setUser(result.data.response);
+                return result.data.response;
+            },
+            function(err) {
+                throw err;
+            }
+        );
     };
 
     this.resetPassword = function(email) {
-        return $http.post(ENV.api['root'] + "user/reset_password", { email: email })
-            .then(
-                function(result) {
-                    messageModel.setMessages(result.data.alerts, false);
-                },
-                function(fault) {
-                    messageModel.setMessages(fault.data.alerts, false);
-                }
-            );
+        return $http.post(ENV.api['root'] + "user/reset_password", { email: email }).then(
+            function(result) {
+                messageModel.setMessages(result.data.alerts, false);
+                return result;
+            },
+            function(err) {
+                messageModel.setMessages(err.data.alerts, false);
+                throw err;
+            }
+        );
     };
 
     this.getUsers = function(queryParams) {
-        return Restangular.all('users').getList(queryParams);
+        return $http.get(ENV.api['root'] + 'users', {params: queryParams}).then(
+            function (result) {
+                return result.data.response;
+            },
+            function (err) {
+                console.error(err);
+                throw err;
+            }
+        )
     };
 
     this.getUser = function(id) {
-        return Restangular.one("users", id).get();
+        return $http.get(ENV.api['root'] + 'users', {params: {id: id}}).then(
+            function (result) {
+                return result.data.response[0];
+            },
+            function (err) {
+                console.error(err);
+                throw err;
+            }
+        )
     };
 
     this.createUser = function(user) {
-        return Restangular.service('users').post(user)
-            .then(
-                function(result) {
-                    messageModel.setMessages([ { level: 'success', text: 'User created' } ], true);
-                    locationUtils.navigateToPath('/users');
-                },
-                function(fault) {
-                    messageModel.setMessages(fault.data.alerts, false);
-                }
-            );
+        return $http.post(ENV.api['root'] + 'users', user).then(
+            function(result) {
+                messageModel.setMessages([ { level: 'success', text: 'User created' } ], true);
+                locationUtils.navigateToPath('/users');
+                return result;
+            },
+            function(err) {
+                messageModel.setMessages(err.data.alerts, false);
+                throw err;
+            }
+        );
     };
 
     this.updateUser = function(user) {
-        return $http.put(ENV.api['root'] + "users/" + user.id, user)
-            .then(
-                function(result) {
-                    if (userModel.user.id == user.id) {
-                        // if you are updating the currently logged in user...
-                        userModel.setUser(user);
-                    }
-                    messageModel.setMessages(result.data.alerts, false);
-                },
-                function(fault) {
-                    messageModel.setMessages(fault.data.alerts, false);
+        return $http.put(ENV.api['root'] + "users/" + user.id, user).then(
+            function(result) {
+                if (userModel.user.id === user.id) {
+                    // if you are updating the currently logged in user...
+                    userModel.setUser(user);
                 }
-            );
+                messageModel.setMessages(result.data.alerts, false);
+                return result;
+            },
+            function(err) {
+                messageModel.setMessages(err.data.alerts, false);
+                throw err;
+            }
+        );
     };
 
     this.updateCurrentUser = function(user) {
-        return $http.post(ENV.api['root'] + "user/current/update", { user: user })
-            .then(
-                function() {
-                    userModel.setUser(user);
-                    messageModel.setMessages([ { level: 'success', text: 'Current user updated' } ], false);
-                },
-                function() {
-                    messageModel.setMessages([ { level: 'error', text: 'Current user updated failed' } ], false);
-                }
-            );
+        return $http.post(ENV.api['root'] + "user/current/update", { user: user }).then(
+            function(result) {
+                userModel.setUser(user);
+                messageModel.setMessages([ { level: 'success', text: 'Current user updated' } ], false);
+                return result;
+            },
+            function(err) {
+                messageModel.setMessages([ { level: 'error', text: 'Current user updated failed' } ], false);
+                throw err;
+            }
+        );
     };
 
     this.getUnassignedUserDeliveryServices = function(userId) {
-        var deferred = $q.defer();
-
-        $http.get(ENV.api['root'] + "user/" + userId + "/deliveryservices/available")
-            .then(
-                function(result) {
-                    deferred.resolve(result.data.response);
-                },
-                function(fault) {
-                    deferred.reject(fault);
-                }
-            );
-
-        return deferred.promise;
+        return $http.get(ENV.api['root'] + "user/" + userId + "/deliveryservices/available").then(
+            function(result) {
+                return result.data.response;
+            },
+            function(err) {
+                console.error(err);
+                throw err;
+            }
+        );
     };
 
     this.deleteUserDeliveryService = function(userId, dsId) {
-        return httpService.delete(ENV.api['root'] + 'deliveryservice_user/' + dsId + '/' + userId)
-            .then(
-                function() {
-                    messageModel.setMessages([ { level: 'success', text: 'User and delivery service were unlinked.' } ], false);
-                },
-                function(fault) {
-                    messageModel.setMessages(fault.data.alerts, true);
-                }
-            );
+        return $http.delete(ENV.api['root'] + 'deliveryservice_user/' + dsId + '/' + userId).then(
+            function(result) {
+                messageModel.setMessages([ { level: 'success', text: 'User and delivery service were unlinked.' } ], false);
+                return result;
+            },
+            function(err) {
+                messageModel.setMessages(err.data.alerts, true);
+                throw err;
+            }
+        );
     };
 
     this.assignUserDeliveryServices = function(userId, deliveryServices) {
-        return Restangular.service('deliveryservice_user').post( { userId: userId, deliveryServices: deliveryServices, replace: true } )
-            .then(
-                function() {
-                    messageModel.setMessages([ { level: 'success', text: 'Delivery services linked to user' } ], false);
-                },
-                function(fault) {
-                    messageModel.setMessages(fault.data.alerts, false);
-                }
-            );
+        return $http.post(ENV.api['root'] + 'deliveryservice_user', { userId: userId, deliveryServices: deliveryServices, replace: true } ).then(
+            function(result) {
+                messageModel.setMessages([ { level: 'success', text: 'Delivery services linked to user' } ], false);
+                return result;
+            },
+            function(err) {
+                messageModel.setMessages(err.data.alerts, false);
+                throw err;
+            }
+        );
     };
 
     this.registerUser = function(registration) {
-        return $http.post(ENV.api['root'] + "users/register", registration)
-            .then(
-                function(result) {
-                    messageModel.setMessages(result.data.alerts, false);
-                },
-                function(fault) {
-                    messageModel.setMessages(fault.data.alerts, false);
-                }
-            );
+        return $http.post(ENV.api['root'] + "users/register", registration).then(
+            function(result) {
+                messageModel.setMessages(result.data.alerts, false);
+                return result;
+            },
+            function(err) {
+                messageModel.setMessages(err.data.alerts, false);
+                throw err;
+            }
+        );
     };
-
-
 };
 
-UserService.$inject = ['Restangular', '$http', '$location', '$q', 'authService', 'httpService', 'locationUtils', 'userModel', 'messageModel', 'ENV'];
+UserService.$inject = ['$http', 'locationUtils', 'userModel', 'messageModel', 'ENV'];
 module.exports = UserService;
