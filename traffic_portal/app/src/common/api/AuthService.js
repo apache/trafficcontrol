dc761097398a1efd932a1ceaef743a1594109419
mjacks258 <matthew_jackson@comcast.com>
2019-06-25 08:07:59 -0600

    updated so code is in POST body

--- traffic_portal/app/src/common/api/AuthService.js
+++ traffic_portal/app/src/common/api/AuthService.js
@@ -51,8 +51,8 @@ var AuthService = function($rootScope, $http, $state, $location, userModel, mess
         );
     };
 
-    this.oauthLogin = function(authCodeTokenUrl) {
-        return $http.post(ENV.api['root'] + 'user/login/oauth', { authCodeTokenUrl: authCodeTokenUrl })
+    this.oauthLogin = function(authCodeTokenUrl, code) {
+        return $http.post(ENV.api['root'] + 'user/login/oauth', { authCodeTokenUrl: authCodeTokenUrl, code: code})
             .then(
                 function(result) {
                     $rootScope.$broadcast('authService::login');
