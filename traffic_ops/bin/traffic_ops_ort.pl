36c0e0e1499acdd76017f25d58bc80d4af3150ca
Derek Gelinas <derek_gelinas@cable.comcast.com>
2019-07-16 17:53:05 -0400

    fixes trafficserver status check (#3730)

--- traffic_ops/bin/traffic_ops_ort.pl
+++ traffic_ops/bin/traffic_ops_ort.pl
@@ -504,7 +504,7 @@ sub start_service {
 	}
 	my $running_string = "";
 	if ( $pkg_name eq "trafficserver" ) {
-		$running_string = "traffic_manager";
+		$running_string = "traffic_manager|traffic_cop";
 	}
 	else {
 		$running_string = $pkg_name;
@@ -599,7 +599,7 @@ sub restart_service {
 	}
 	my $running_string = "";
 	if ( $pkg_name eq "trafficserver" ) {
-		$running_string = "traffic_manager";
+		$running_string = "traffic_manager|traffic_cop";
 	}
 	if ( $running_string ne "" ) {
 		if ( $pkg_running =~ m/$running_string \(pid  (\d+)\) is running.../ ) {
