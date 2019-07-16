4a68fe0a187c4ee8646674ae685514dc7c6a96f5
mjacks258 <matthew_jackson@comcast.com>
2019-07-16 08:37:51 -0600

    vendored dgrijalva/jwt-go and lestrrat-go/jwx

---
+++ traffic_ops/traffic_ops_golang/vendor/github.com/lestrrat-go/jwx/jwe/content_crypt.go
@@ -0,0 +1,59 @@
+package jwe
+
+import (
+	"github.com/lestrrat-go/jwx/internal/debug"
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/pkg/errors"
+)
+
+func (c GenericContentCrypt) Algorithm() jwa.ContentEncryptionAlgorithm {
+	return c.alg
+}
+
+func (c GenericContentCrypt) Encrypt(cek, plaintext, aad []byte) ([]byte, []byte, []byte, error) {
+	if debug.Enabled {
+		debug.Printf("ContentCrypt.Encrypt: cek        = %x (%d)", cek, len(cek))
+		debug.Printf("ContentCrypt.Encrypt: ciphertext = %x (%d)", plaintext, len(plaintext))
+		debug.Printf("ContentCrypt.Encrypt: aad        = %x (%d)", aad, len(aad))
+	}
+	iv, encrypted, tag, err := c.cipher.encrypt(cek, plaintext, aad)
+	if err != nil {
+		if debug.Enabled {
+			debug.Printf("cipher.encrypt failed")
+		}
+
+		return nil, nil, nil, errors.Wrap(err, `failed to crypt content`)
+	}
+
+	return iv, encrypted, tag, nil
+}
+
+func (c GenericContentCrypt) Decrypt(cek, iv, ciphertext, tag, aad []byte) ([]byte, error) {
+	return c.cipher.decrypt(cek, iv, ciphertext, tag, aad)
+}
+
+func NewAesCrypt(alg jwa.ContentEncryptionAlgorithm) (*GenericContentCrypt, error) {
+	if debug.Enabled {
+		debug.Printf("AES Crypt: alg = %s", alg)
+	}
+	cipher, err := NewAesContentCipher(alg)
+	if err != nil {
+		return nil, errors.Wrap(err, `aes crypt: failed to create content cipher`)
+	}
+
+	if debug.Enabled {
+		debug.Printf("AES Crypt: cipher.keysize = %d", cipher.KeySize())
+	}
+
+	return &GenericContentCrypt{
+		alg:     alg,
+		cipher:  cipher,
+		cekgen:  NewRandomKeyGenerate(cipher.KeySize() * 2),
+		keysize: cipher.KeySize() * 2,
+		tagsize: 16,
+	}, nil
+}
+
+func (c GenericContentCrypt) KeySize() int {
+	return c.keysize
+}
---
+++ traffic_ops/traffic_ops_golang/vendor/github.com/lestrrat-go/jwx/jwe/key_generate.go
@@ -0,0 +1,109 @@
+package jwe
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"encoding/binary"
+	"io"
+
+	"github.com/lestrrat-go/jwx/internal/concatkdf"
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/pkg/errors"
+)
+
+// Bytes returns the byte from this ByteKey
+func (k ByteKey) Bytes() []byte {
+	return []byte(k)
+}
+
+// KeySize returns the size of the key
+func (g StaticKeyGenerate) KeySize() int {
+	return len(g)
+}
+
+// KeyGenerate returns the key
+func (g StaticKeyGenerate) KeyGenerate() (ByteSource, error) {
+	buf := make([]byte, g.KeySize())
+	copy(buf, g)
+	return ByteKey(buf), nil
+}
+
+// NewRandomKeyGenerate creates a new KeyGenerator that returns
+// random bytes
+func NewRandomKeyGenerate(n int) RandomKeyGenerate {
+	return RandomKeyGenerate{keysize: n}
+}
+
+// KeySize returns the key size
+func (g RandomKeyGenerate) KeySize() int {
+	return g.keysize
+}
+
+// KeyGenerate generates a random new key
+func (g RandomKeyGenerate) KeyGenerate() (ByteSource, error) {
+	buf := make([]byte, g.keysize)
+	if _, err := io.ReadFull(rand.Reader, buf); err != nil {
+		return nil, errors.Wrap(err, "failed to read from rand.Reader")
+	}
+	return ByteKey(buf), nil
+}
+
+// NewEcdhesKeyGenerate creates a new key generator using ECDH-ES
+func NewEcdhesKeyGenerate(alg jwa.KeyEncryptionAlgorithm, pubkey *ecdsa.PublicKey) (*EcdhesKeyGenerate, error) {
+	var keysize int
+	switch alg {
+	case jwa.ECDH_ES:
+		return nil, errors.New("unimplemented")
+	case jwa.ECDH_ES_A128KW:
+		keysize = 16
+	case jwa.ECDH_ES_A192KW:
+		keysize = 24
+	case jwa.ECDH_ES_A256KW:
+		keysize = 32
+	default:
+		return nil, errors.Wrap(ErrUnsupportedAlgorithm, "invalid ECDH-ES key generation algorithm")
+	}
+
+	return &EcdhesKeyGenerate{
+		algorithm: alg,
+		keysize:   keysize,
+		pubkey:    pubkey,
+	}, nil
+}
+
+// KeySize returns the key size associated with this generator
+func (g EcdhesKeyGenerate) KeySize() int {
+	return g.keysize
+}
+
+// KeyGenerate generates new keys using ECDH-ES
+func (g EcdhesKeyGenerate) KeyGenerate() (ByteSource, error) {
+	priv, err := ecdsa.GenerateKey(g.pubkey.Curve, rand.Reader)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to generate key for ECDH-ES")
+	}
+
+	pubinfo := make([]byte, 4)
+	binary.BigEndian.PutUint32(pubinfo, uint32(g.keysize)*8)
+
+	z, _ := priv.PublicKey.Curve.ScalarMult(g.pubkey.X, g.pubkey.Y, priv.D.Bytes())
+	kdf := concatkdf.New(crypto.SHA256, []byte(g.algorithm.String()), z.Bytes(), []byte{}, []byte{}, pubinfo, []byte{})
+	kek := make([]byte, g.keysize)
+	kdf.Read(kek)
+
+	return ByteWithECPrivateKey{
+		PrivateKey: priv,
+		ByteKey:    ByteKey(kek),
+	}, nil
+}
+
+// HeaderPopulate populates the header with the required EC-DSA public key
+// information ('epk' key)
+func (k ByteWithECPrivateKey) HeaderPopulate(h *Header) {
+	key, err := jwk.New(&k.PrivateKey.PublicKey)
+	if err == nil {
+		h.Set("epk", key)
+	}
+}
