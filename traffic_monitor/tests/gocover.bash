d7826e440872be42d53476954b66027fddcd9f15
Lavanya Bathina <lbathi200@cosml-1547677.cable.comcast.com>
2019-08-15 13:59:57 -0600

    updated docker files and gocover.bash.

--- traffic_monitor/tests/gocover.bash
+++ traffic_monitor/tests/gocover.bash
@@ -15,14 +15,14 @@
 #----------------------------------------
 set -e
 go get "github.com/wadey/gocovmerge"
-echo "" > result.txt
+touch result.txt
 packages=( "$@" )
 coverage_out=()
 i=1
 for pkg in ${packages[@]} ; do
     for d in $(go list $pkg | grep -v vendor); do
         file="$i.out"
-        go test -v -coverprofile=$file $d >> result.txt
+        go test -v -coverprofile=$file $d | tee -a result.txt
         cat result.txt
         if [ -f $file ]; then
             coverage_out+=( $file )
@@ -33,4 +33,4 @@ done
 gocovmerge ${coverage_out[*]} > coverage.out
 go tool cover -func=coverage.out
 cat result.txt | go-junit-report --package-name=golang.test.tm --set-exit-code > /junit/golang.test.tm.xml 
-chmod 777 -R /junit && cat /junit/golang.test.tm.xml
+cat /junit/golang.test.tm.xml
