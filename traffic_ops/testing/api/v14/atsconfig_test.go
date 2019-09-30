e58ef146e9a4dd6d4ef34763a57fa29f24ba586f
Robert Butts <rob@apache.org>
2019-09-30 09:59:44 -0600

    Add TO Tests for ATS config endpoints

--- traffic_ops/testing/api/v14/atsconfig_test.go
+++ traffic_ops/testing/api/v14/atsconfig_test.go
@@ -21,6 +21,8 @@ import (
 
 func TestATSConfigs(t *testing.T) {
 	WithObjs(t, []TCObj{CDNs, Types, Tenants, Parameters, Profiles, Statuses, Divisions, Regions, PhysLocations, CacheGroups, Servers, DeliveryServices}, func() {
+		defer DeleteTestDeliveryServiceServersCreated(t)
+		CreateTestDeliveryServiceServers(t)
 		GetTestATSConfigs(t)
 	})
 }
@@ -50,33 +52,59 @@ func GetTestATSConfigs(t *testing.T) {
 		t.Fatalf("Getting server by name '" + server.HostName + "' config list: " + err.Error() + "\n")
 	}
 
-	_, _, err = TOSession.GetATSServerConfig(server.ID, "remap.config")
-	if err != nil {
-		t.Fatalf("Getting server '" + server.HostName + "' config remap.config: " + err.Error() + "\n")
+	serverConfigs := []string{
+		"ip_allow.config",
+		"hosting.config",
+		"packages",
+		"chkconfig",
+		"remap.config",
+		"parent.config",
+	}
+	for _, serverConfig := range serverConfigs {
+		if _, _, err = TOSession.GetATSServerConfig(server.ID, serverConfig); err != nil {
+			t.Errorf("Getting server '" + server.HostName + "' config '" + serverConfig + "': " + err.Error() + "\n")
+		}
+		if _, _, err = TOSession.GetATSServerConfigByName(server.HostName, serverConfig); err != nil {
+			t.Errorf("Getting server by name '" + server.HostName + "' config '" + serverConfig + "': " + err.Error() + "\n")
+		}
 	}
 
-	_, _, err = TOSession.GetATSServerConfigByName(server.HostName, "remap.config")
-	if err != nil {
-		t.Fatalf("Getting server by name '" + server.HostName + "' config remap.config: " + err.Error() + "\n")
+	profileConfigs := []string{
+		"12M_facts",
+		"50-ats.rules",
+		"astats.config",
+		"cache.config",
+		"drop_qstring.config",
+		"logging.config",
+		"logging.yaml",
+		"logs_xml.config",
+		"plugin.config",
+		"records.config",
+		"storage.config",
+		"sysctl.conf",
+		"volume.config",
+	}
+	for _, profileConfig := range profileConfigs {
+		if _, _, err = TOSession.GetATSProfileConfig(server.ProfileID, profileConfig); err != nil {
+			t.Errorf("Getting profile '" + server.Profile + "' config '" + profileConfig + "': " + err.Error() + "\n")
+		}
+		if _, _, err = TOSession.GetATSProfileConfigByName(server.Profile, profileConfig); err != nil {
+			t.Errorf("Getting profile by name '" + server.Profile + "' config '" + profileConfig + "': " + err.Error() + "\n")
+		}
 	}
 
-	_, _, err = TOSession.GetATSProfileConfig(server.ProfileID, "storage.config")
-	if err != nil {
-		t.Fatalf("Getting profile '" + server.Profile + "' config storage.config: " + err.Error() + "\n")
+	cdnConfigs := []string{
+		"regex_revalidate.config",
+		"bg_fetch.config",
+		"set_dscp_42.config",
+		"ssl_multicert.config",
 	}
-
-	_, _, err = TOSession.GetATSProfileConfigByName(server.Profile, "storage.config")
-	if err != nil {
-		t.Fatalf("Getting profile by name '" + server.Profile + "' config storage.config: " + err.Error() + "\n")
-	}
-
-	_, _, err = TOSession.GetATSCDNConfig(server.CDNID, "bg_fetch.config")
-	if err != nil {
-		t.Fatalf("Getting cdn '" + server.CDNName + "' config bg_fetch.config: " + err.Error() + "\n")
-	}
-
-	_, _, err = TOSession.GetATSCDNConfigByName(server.CDNName, "bg_fetch.config")
-	if err != nil {
-		t.Fatalf("Getting cdn by name '" + server.CDNName + "' config bg_fetch.config: " + err.Error() + "\n")
+	for _, cdnConfig := range cdnConfigs {
+		if _, _, err = TOSession.GetATSCDNConfig(server.CDNID, cdnConfig); err != nil {
+			t.Errorf("Getting cdn '" + server.CDNName + "' config '" + cdnConfig + "': " + err.Error() + "\n")
+		}
+		if _, _, err = TOSession.GetATSCDNConfigByName(server.CDNName, cdnConfig); err != nil {
+			t.Errorf("Getting cdn by name '" + server.CDNName + "' config '" + cdnConfig + "': " + err.Error() + "\n")
+		}
 	}
 }
