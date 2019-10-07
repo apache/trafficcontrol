b766e0e738842c7545edc862c1af14586f396298
Jeremy Mitchell <mitchell852@gmail.com>
2019-10-07 15:30:42 -0600

    when comparing profiles, only show parameter diffs by default but provide the option to see all params as well

--- traffic_portal/app/src/common/modules/table/profiles/TableProfilesController.js
+++ traffic_portal/app/src/common/modules/table/profiles/TableProfilesController.js
@@ -161,10 +161,10 @@ var TableProfilesController = function(profiles, $scope, $state, $location, $uib
         });
     };
 
-    $scope.compareProfiles = function() {
+    $scope.diffProfiles = function() {
         var params = {
-            title: 'Compare Profile Parameters',
-            message: "Please select 2 profiles to compare",
+            title: 'Diff Profile Parameters',
+            message: 'Please select 2 profiles to diff',
             labelFunction: function(item) { return item['name'] + ' (' + item['type'] + ')' }
         };
         var modalInstance = $uibModal.open({
@@ -181,7 +181,7 @@ var TableProfilesController = function(profiles, $scope, $state, $location, $uib
             }
         });
         modalInstance.result.then(function(profiles) {
-            $location.path($location.path() + '/compare/' + profiles[0].id + '/' + profiles[1].id);
+            $location.path($location.path() + '/' + profiles[0].id + '/' + profiles[1].id + '/compare/diff');
         }, function () {
             // do nothing
         });
