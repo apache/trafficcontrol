ab8fc7a290daccc19a2b1c74e34e5d98faf3a9df
Michael Hoppal <Michael_Hoppal@comcast.com>
2019-10-08 16:51:56 -0600

    Minor cleanup

--- traffic_ops/app/db/migrations/20191005000000_add_server_server_capability.sql
+++ traffic_ops/app/db/migrations/20191005000000_add_server_server_capability.sql
@@ -12,6 +12,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 
