1df75118bca500519c73cd522747ae8eb8a40c51
Jeremy Mitchell <mitchell852@users.noreply.github.com>
2019-10-24 12:05:06 -0600

    TP: Ability to manager capabilities of servers (#3998)
    * adds server capbility management to a server
    
    * adds the ability to view the servers that are utilizing a server capability
    
    * removes unecessary click handler
    
    * sends query params correctly
    
    * fixes styling on table row and changes hover verbiage
    
    * adds context menu item to the servers table for managing a server's capabilities and also made verbiage consistent
    
    * adds the ability to click on a server capability of a server (in the table) and navigate to the server capability
    
    * adds UI tests for server to server capability assignment
    
    * changes per PR review
    
    * adds a simplified context menu to the server capability servers table
    
    * adds the ability to quickly remove the server from the capability
    
    * adds docs for assigning a server capability to a server
    
    * adds changelog entry for associating capabilities with servers.
    
    * fixes bug with UI message not showing on success

---
+++ traffic_portal/app/src/common/modules/table/serverCapabilityServers/TableServerCapabilityServersController.js
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+var TableServerCapabilityServersController = function(serverCapability, servers, $scope, $state, $controller, $uibModal, $window, locationUtils, serverService, messageModel) {
+
+	// extends the TableServersController to inherit common methods
+	angular.extend(this, $controller('TableServersController', { servers: servers, $scope: $scope }));
+
+	var removeCapability = function(serverId) {
+		serverService.removeServerCapability(serverId, serverCapability.name)
+			.then(
+				function(result) {
+					messageModel.setMessages(result.alerts, false);
+					$scope.refresh();
+				}
+			);
+	};
+
+	$scope.serverCapability = serverCapability;
+
+	$scope.contextMenuItems = [
+		{
+			text: 'Open Server in New Tab',
+			click: function ($itemScope) {
+				$window.open('/#!/servers/' + $itemScope.s.serverId, '_blank');
+			}
+		},
+		null, // Divider
+		{
+			text: 'Remove Capability from Server',
+			click: function ($itemScope) {
+				$scope.confirmRemoveCapability($itemScope.s.serverId);
+			}
+		},
+		null, // Divider
+		{
+			text: 'Edit Server',
+			click: function ($itemScope) {
+				$scope.editServer($itemScope.s.serverId);
+			}
+		},
+		{
+			text: 'Manage Server Capabilities',
+			click: function ($itemScope) {
+				locationUtils.navigateToPath('/servers/' + $itemScope.s.serverId + '/capabilities');
+			}
+		}
+	];
+
+	$scope.confirmRemoveCapability = function(serverId, $event) {
+		if ($event) {
+			$event.stopPropagation(); // this kills the click event so it doesn't trigger anything else
+		}
+
+		const params = {
+			title: 'Remove Capability from Server?',
+			message: 'Are you sure you want to remove the ' + serverCapability.name + ' capability from this server?'
+		};
+		const modalInstance = $uibModal.open({
+			templateUrl: 'common/modules/dialog/confirm/dialog.confirm.tpl.html',
+			controller: 'DialogConfirmController',
+			size: 'md',
+			resolve: {
+				params: function () {
+					return params;
+				}
+			}
+		});
+		modalInstance.result.then(function() {
+			removeCapability(serverId);
+		});
+	};
+
+	angular.element(document).ready(function () {
+		$('#serverCapabilityServersTable').dataTable({
+			"lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
+			"iDisplayLength": 25,
+			"aaSorting": []
+		});
+	});
+
+};
+
+TableServerCapabilityServersController.$inject = ['serverCapability', 'servers', '$scope', '$state', '$controller', '$uibModal', '$window', 'locationUtils', 'serverService', 'messageModel'];
+module.exports = TableServerCapabilityServersController;
