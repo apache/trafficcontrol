7e62d070db4a3ee1d4ed5d3127970ecba0702bf0
ocket8888 <ocket8888@gmail.com>
2019-10-29 13:22:28 -0600

    Rewrite /federations to Go - POST/PUT/DELETE (#4015)
    * Rewrote /federations to Go - POST handler
    
    * Rewrote /federations to Go - DELETE handler
    
    * Rewrote /federations to Go - PUT handler
    
    * Added unit tests
    
    * go fmt
    
    * Update documentation
    
    * Add Go client methods
    
    * Add Go client integration tests
    
    * fix shared handler test
    
    * Remove unused query
    
    * Add major version check
    
    * Fix nested alerts
    
    * Inverted version checking logic
    
    * Fix or instead of and

--- traffic_ops/client/federation.go
+++ traffic_ops/client/federation.go
@@ -25,25 +25,25 @@ import (
 	"github.com/apache/trafficcontrol/lib/go-tc"
 )
 
-func (to *Session) Federations() ([]tc.AllFederation, ReqInf, error) {
+func (to *Session) Federations() ([]tc.AllDeliveryServiceFederationsMapping, ReqInf, error) {
 	type FederationResponse struct {
-		Response []tc.AllFederation `json:"response"`
+		Response []tc.AllDeliveryServiceFederationsMapping `json:"response"`
 	}
 	data := FederationResponse{}
 	inf, err := get(to, apiBase+"/federations", &data)
 	return data.Response, inf, err
 }
 
-func (to *Session) AllFederations() ([]tc.AllFederation, ReqInf, error) {
+func (to *Session) AllFederations() ([]tc.AllDeliveryServiceFederationsMapping, ReqInf, error) {
 	type FederationResponse struct {
-		Response []tc.AllFederation `json:"response"`
+		Response []tc.AllDeliveryServiceFederationsMapping `json:"response"`
 	}
 	data := FederationResponse{}
 	inf, err := get(to, apiBase+"/federations/all", &data)
 	return data.Response, inf, err
 }
 
-func (to *Session) AllFederationsForCDN(cdnName string) ([]tc.AllFederation, ReqInf, error) {
+func (to *Session) AllFederationsForCDN(cdnName string) ([]tc.AllDeliveryServiceFederationsMapping, ReqInf, error) {
 	// because the Federations JSON array is heterogeneous (array members may be a AllFederation or AllFederationCDN), we have to try decoding each separately.
 	type FederationResponse struct {
 		Response []json.RawMessage `json:"response"`
@@ -54,9 +54,9 @@ func (to *Session) AllFederationsForCDN(cdnName string) ([]tc.AllFederation, Req
 		return nil, inf, err
 	}
 
-	feds := []tc.AllFederation{}
+	feds := []tc.AllDeliveryServiceFederationsMapping{}
 	for _, raw := range data.Response {
-		fed := tc.AllFederation{}
+		fed := tc.AllDeliveryServiceFederationsMapping{}
 		if err := json.Unmarshal([]byte(raw), &fed); err != nil {
 			// we don't actually need the CDN, but we want to return an error if we got something unexpected
 			cdnFed := tc.AllFederationCDN{}
@@ -142,4 +142,70 @@ func (to *Session) DeleteFederationUser(federationID, userID int) (tc.Alerts, Re
 		return tc.Alerts{}, reqInf, err
 	}
 	return alerts, reqInf, nil
+}
+
+// AddFederationResolverMappingsForCurrentUser adds Federation Resolver mappings to one or more
+// Delivery Services for the current user.
+func (to *Session) AddFederationResolverMappingsForCurrentUser(mappings tc.DeliveryServiceFederationResolverMappingRequest) (tc.Alerts, ReqInf, error) {
+	reqInf := ReqInf{CacheHitStatus: CacheHitStatusMiss}
+	var alerts tc.Alerts
+
+	bts, err := json.Marshal(mappings)
+	if err != nil {
+		return alerts, reqInf, err
+	}
+
+	resp, remoteAddr, err := to.request(http.MethodPost, apiBase + "/federations", bts)
+	reqInf.RemoteAddr = remoteAddr
+	if err != nil {
+		return alerts, reqInf, err
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&alerts)
+	return alerts, reqInf, err
+}
+
+// DeleteFederationResolverMappingsForCurrentUser removes ALL Federation Resolver mappings for ALL
+// Federations assigned to the currently authenticated user, as well as deleting ALL of the
+// Federation Resolvers themselves.
+func (to *Session) DeleteFederationResolverMappingsForCurrentUser() (tc.Alerts, ReqInf, error) {
+	reqInf := ReqInf{CacheHitStatus: CacheHitStatusMiss}
+	var alerts tc.Alerts
+
+	resp, remoteAddr, err := to.request(http.MethodDelete, apiBase + "/federations", nil)
+	reqInf.RemoteAddr = remoteAddr
+	if err != nil {
+		return alerts, reqInf, err
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&alerts)
+	return alerts, reqInf, err
+}
+
+// ReplaceFederationResolverMappingsForCurrentUser replaces any and all Federation Resolver mappings
+// on all Federations assigned to the currently authenticated user. This will first remove ALL
+// Federation Resolver mappings for ALL Federations assigned to the currently authenticated user, as
+// well as deleting ALL of the Federation Resolvers themselves. In other words, calling this is
+// equivalent to a call to DeleteFederationResolverMappingsForCurrentUser followed by a call to
+// AddFederationResolverMappingsForCurrentUser .
+func (to *Session) ReplaceFederationResolverMappingsForCurrentUser(mappings tc.DeliveryServiceFederationResolverMappingRequest) (tc.Alerts, ReqInf, error) {
+	reqInf := ReqInf{CacheHitStatus: CacheHitStatusMiss}
+	var alerts tc.Alerts
+
+	bts, err := json.Marshal(mappings)
+	if err != nil {
+		return alerts, reqInf, err
+	}
+
+	resp, remoteAddr, err := to.request(http.MethodPut, apiBase + "/federations", bts)
+	reqInf.RemoteAddr = remoteAddr
+	if err != nil {
+		return alerts, reqInf, err
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&alerts)
+	return alerts, reqInf, err
 }
