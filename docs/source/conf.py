f36275fdca278cad1ec461ec7fc41b3d9ce421a8
Derek Gelinas <derek_gelinas@cable.comcast.com>
2019-10-31 13:06:40 -0400

    update release ver

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,7 +75,7 @@ author = 'Apache Traffic Control'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3'
+release = '3.1.0'
 
 
 # -- General configuration ---------------------------------------------------
