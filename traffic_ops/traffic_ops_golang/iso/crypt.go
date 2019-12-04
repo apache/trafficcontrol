cb5b2c80f4ed627aa7539bddb4aace15100a6246
awilliams <pwnfactory@gmail.com>
2019-12-04 10:36:37 -0700

    Replace openssl passwd with GehirnInc/crypt library for crypt function

--- traffic_ops/traffic_ops_golang/iso/crypt.go
+++ traffic_ops/traffic_ops_golang/iso/crypt.go
@@ -21,38 +21,19 @@ package iso
 
 import (
 	"math/rand"
-	"os/exec"
 	"strings"
 	"time"
+
+	"github.com/GehirnInc/crypt/md5_crypt"
 )
 
 // crypt acts Perl's built-in crypt() function, which in turn
 // acts like the crypt(3) function in the C library.
-// Since there isn't support in the standard library for this, the choice
-// was made to shell out to `openssl`. This creates a dependency on openssl,
-// which the Traffic Ops RPM already includes.
-//
-// Alternatives would be to use CGO to invoke the C function or
-// implementing the algorithm in Go.
 func crypt(password, salt string) (string, error) {
-	var out strings.Builder
-
-	// Special case to match Perl's behavior. In practice,
-	// it's not expected to receive an empty password.
-	if password == "" {
-		password = "\n"
-	}
-
-	// https://www.openssl.org/docs/man1.1.0/man1/openssl-passwd.html
-	cmd := exec.Command("openssl", "passwd", "-1", "-salt", salt, "-stdin")
-	cmd.Stdin = strings.NewReader(password)
-	cmd.Stdout = &out
-
-	if err := cmd.Run(); err != nil {
-		return "", err
-	}
-
-	return strings.TrimSuffix(out.String(), "\n"), nil
+	h := md5_crypt.New()
+	// The MagicPrefix ('$1$') is used to identify the algorithm (MD5-based in this case).
+	// See https://en.wikipedia.org/wiki/Crypt_(C) for more information.
+	return h.Generate([]byte(password), []byte(md5_crypt.MagicPrefix+salt))
 }
 
 // saltChars are the possible characters rndSalt may use to generate a salt string.
