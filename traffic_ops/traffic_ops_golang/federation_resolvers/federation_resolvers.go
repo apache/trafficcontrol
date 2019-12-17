7045fe927be4508f7b497ddd353a0b473ae77cdf
ocket8888 <ocket8888@gmail.com>
2019-12-17 14:47:30 -0700

    fix function name to not shadow builtin

--- traffic_ops/traffic_ops_golang/federation_resolvers/federation_resolvers.go
+++ traffic_ops/traffic_ops_golang/federation_resolvers/federation_resolvers.go
@@ -161,7 +161,7 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	}
 	defer inf.Close()
 
-	userErr, sysErr, statusCode, alert, respObj := delete(inf)
+	userErr, sysErr, statusCode, alert, respObj := deleteFederationResolver(inf)
 	if userErr != nil || sysErr != nil {
 		api.HandleErr(w, r, tx, statusCode, userErr, sysErr)
 		return
@@ -170,7 +170,7 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	api.WriteRespAlertObj(w, r, tc.SuccessLevel, alert.Text, respObj)
 }
 
-func delete(inf *api.APIInfo) (error, error, int, tc.Alert, tc.FederationResolver) {
+func deleteFederationResolver(inf *api.APIInfo) (error, error, int, tc.Alert, tc.FederationResolver) {
 	var userErr error
 	var sysErr error
 	var statusCode = http.StatusOK
@@ -209,7 +209,7 @@ func DeleteByID(w http.ResponseWriter, r *http.Request) {
 	}
 	defer inf.Close()
 
-	userErr, sysErr, statusCode, alert, respObj := delete(inf)
+	userErr, sysErr, statusCode, alert, respObj := deleteFederationResolver(inf)
 	if userErr != nil || sysErr != nil {
 		api.HandleErr(w, r, tx, statusCode, userErr, sysErr)
 		return
