2d2ac308b4ba3ef075a4957eae14242cd3f50280
ocket8888 <ocket8888@gmail.com>
2020-01-16 14:09:14 -0700

    Greatly simplified 'getDSStats'

--- traffic_monitor/static/script.js
+++ traffic_monitor/static/script.js
@@ -168,93 +168,61 @@ const kilobitsInMegabit = 1000;
 */
 const dsDisplayFloat = (f) => { return f === 0 ? "" : f.toFixed(2); }
 
+/**
+ * Attempts to extract data from a deliveryService object, but falls back on "N/A" if it doesn't have that
+ * property.
+ * @param ds The Delivery Service from which to extract data
+ * @param prop The property being extracted. Technically, the extracted property is dsDisplayFloat(parseFloat(ds[prop][0].value)).
+*/
+function getDSProperty(ds, prop) {
+	try {
+		return dsDisplayFloat(parseFloat(ds[prop][0].value));
+	} catch (e) {
+		console.error(e);
+	}
+	return "N/A";
+}
+
+/**
+ * Fetches the current Delivery Service stats from TM and updates the "Delivery Service States"
+ * table with the results - replacing the current content.
+*/
 function getDsStats() {
 	var now = Date.now();
 
 	/// \todo add /api/delivery-service-stats which only returns the data needed by the UI, for efficiency
 	ajax("/publish/DsStats", function(r) {
-		var j = JSON.parse(r);
-		var jds = j.deliveryService
-		var deliveryServiceNames = Object.keys(jds); //debug
-		//decrementing for loop so DsNames are alphabetical A-Z
-		//TODO allow for filtering of columns so this isn't necessary
-			for (var i = deliveryServiceNames.length - 1; i >= 0; i--) {
-			var deliveryService = deliveryServiceNames[i];
+		const deliveryServices = new Map(Object.entries(JSON.parse(r)));
+		const table = document.createElement('TBODY');
+		table.id = "deliveryservice-stats";
 
-			if (!document.getElementById("deliveryservice-stats-" + deliveryService)) {
-				var row = document.getElementById("deliveryservice-stats").insertRow(0); //document.createElement("tr");
-				row.id = "deliveryservice-stats-" + deliveryService
-				row.insertCell(0).id = row.id + "-delivery-service";
-				row.insertCell(1).id = row.id + "-status";
-				row.insertCell(2).id = row.id + "-caches-reporting";
-				row.insertCell(3).id = row.id + "-bandwidth";
-				row.insertCell(4).id = row.id + "-tps";
-				row.insertCell(5).id = row.id + "-2xx";
-				row.insertCell(6).id = row.id + "-3xx";
-				row.insertCell(7).id = row.id + "-4xx";
-				row.insertCell(8).id = row.id + "-5xx";
-				row.insertCell(9).id = row.id + "-disabled-locations";
-				document.getElementById(row.id + "-delivery-service").textContent = deliveryService;
-				document.getElementById(row.id + "-delivery-service").style.whiteSpace = "nowrap";
-				document.getElementById(row.id + "-caches-reporting").style.textAlign = "right";
-				document.getElementById(row.id + "-bandwidth").style.textAlign = "right";
-				document.getElementById(row.id + "-tps").style.textAlign = "right";
-				document.getElementById(row.id + "-2xx").style.textAlign = "right";
-				document.getElementById(row.id + "-3xx").style.textAlign = "right";
-				document.getElementById(row.id + "-4xx").style.textAlign = "right";
-				document.getElementById(row.id + "-5xx").style.textAlign = "right";
-			}
-
-			// \todo check that array has a member before dereferencing [0]
-			if (jds[deliveryService].hasOwnProperty("isAvailable")) {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-status").textContent = jds[deliveryService]["isAvailable"][0].value == "true" ? "available" : "unavailable - " + jds[deliveryService]["error-string"][0].value;
-			}
-			if (jds[deliveryService].hasOwnProperty("caches-reporting") && jds[deliveryService].hasOwnProperty("caches-available") && jds[deliveryService].hasOwnProperty("caches-configured")) {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-caches-reporting").textContent = jds[deliveryService]['caches-reporting'][0].value + " / " + jds[deliveryService]['caches-available'][0].value + " / " + jds[deliveryService]['caches-configured'][0].value;
-			}
-			if (jds[deliveryService].hasOwnProperty("total.kbps")) {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-bandwidth").textContent = (jds[deliveryService]['total.kbps'][0].value / kilobitsInMegabit).toFixed(2);
-			} else {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-bandwidth").textContent = "N/A";
-			}
-			if (jds[deliveryService].hasOwnProperty("total.tps_total")) {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-tps").textContent = dsDisplayFloat(parseFloat(jds[deliveryService]['total.tps_total'][0].value));
-			} else {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-tps").textContent = "N/A";
-			}
-			if (jds[deliveryService].hasOwnProperty("total.tps_2xx")) {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-2xx").textContent = dsDisplayFloat(parseFloat(jds[deliveryService]['total.tps_2xx'][0].value));
-			} else {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-2xx").textContent = "N/A";
-			}
-			if (jds[deliveryService].hasOwnProperty("total.tps_3xx")) {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-3xx").textContent = dsDisplayFloat(parseFloat(jds[deliveryService]['total.tps_3xx'][0].value));
-			} else {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-3xx").textContent = "N/A";
-			}
-			if (jds[deliveryService].hasOwnProperty("total.tps_4xx")) {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-4xx").textContent = dsDisplayFloat(parseFloat(jds[deliveryService]['total.tps_4xx'][0].value));
-			} else {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-4xx").textContent = "N/A";
-			}
-			if (jds[deliveryService].hasOwnProperty("total.tps_5xx")) {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-5xx").textContent = dsDisplayFloat(parseFloat(jds[deliveryService]['total.tps_5xx'][0].value));
-			} else {
-				document.getElementById("deliveryservice-stats-" + deliveryService + "-5xx").textContent = "N/A";
-			}
-
-			// \todo implement disabled locations
-
-			var row = document.getElementById("deliveryservice-stats-" + deliveryService);
-			if (jds[deliveryService]["isAvailable"][0].value == "true") {
-				row.classList.add("stripes");
-				row.classList.remove("error");
-			} else {
+		for (const [dsName, deliveryService] of deliveryServices) {
+			const row = table.insertRow(0);
+			const available = !deliveryService.isAvailable || !deliveryService.isAvailable[0] || !deliveryService.isAvailable[0].value === "true";
+			if (available) {
 				row.classList.add("error");
-				row.classList.remove("stripes");
 			}
+
+			row.insertCell(0).textContent = dsName;
+			row.insertCell(1).textContent = available ? "available" : `unavailable - ${deliveryService["error-string"][0].value}`;
+			row.insertCell(2).textContent = (Object.prototype.hasOwnProperty.call(deliveryService, "caches-reporting") &&
+			                                 Object.prototype.hasOwnProperty.call(deliveryService, "caches-available") &&
+			                                 Object.prototype.hasOwnProperty.call(deliveryService, "caches-configured")) ?
+			                                 `${deliveryService['caches-reporting'][0].value} / ${deliveryService['caches-available'][0].value} / ${deliveryService['caches-configured'][0].value}`;
+
+			row.insertCell(3).textContent = Object.prototype.hasOwnProperty.call(deliveryService, "total.kbps") ? (jds[deliveryService]['total.kbps'][0].value / kilobitsInMegabit).toFixed(2) : "N/A";
+			row.insertCell(4).textContent = getDSProperty(deliveryService, "total.tps_total");
+			row.insertCell(5).textContent = getDSProperty(deliveryService, "total.tps_2xx");
+			row.insertCell(6).textContent = getDSProperty(deliveryService, "total.tps_3xx");
+			row.insertCell(7).textContent = getDSProperty(deliveryService, "total.tps_4xx");
+			row.insertCell(8).textContent = getDSProperty(deliveryService, "total.tps_5xx");
+			row.insertCell(9);
+			// \todo implement disabled locations
 		}
-	})
+
+		const oldtable = document.getElementById("deliveryservice-stats");
+		oldtable.parentNode.replaceChild(table, oldtable);
+	});
 }
 
 function getCacheStatuses() {
