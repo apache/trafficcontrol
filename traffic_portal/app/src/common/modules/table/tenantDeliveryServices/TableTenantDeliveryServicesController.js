f8662ef30d32722c05a0854005cc153f489242cf
Jeremy Mitchell <mitchell852@users.noreply.github.com>
2020-01-21 10:41:06 -0700

    globally adds the ability to rearrange/reorder table columns to ALL TP tables (#4294)

--- traffic_portal/app/src/common/modules/table/tenantDeliveryServices/TableTenantDeliveryServicesController.js
+++ traffic_portal/app/src/common/modules/table/tenantDeliveryServices/TableTenantDeliveryServicesController.js
@@ -38,9 +38,6 @@ var TableTenantDeliveryServicesController = function(tenant, deliveryServices, $
 			"iDisplayLength": 25,
 			"aaSorting": [],
 			"columns": $scope.columns,
-			"colReorder": {
-				realtime: false
-			},
 			"initComplete": function(settings, json) {
 				try {
 					// need to create the show/hide column checkboxes and bind to the current visibility
