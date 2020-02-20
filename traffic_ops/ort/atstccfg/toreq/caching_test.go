9f9d58c4e1ca341e84fc06ba3e77e721ad26ba70
Robert Butts <rob05c@users.noreply.github.com>
2020-02-20 16:13:55 -0700

    ORT Speed Improvements (#4419)
    * Change atstccfg to use gob,csv caching
    
    Changes atstccfg caching to use gob by default, with a custom csv
    format for delivery service servers which is huge and slow.
    
    It also abstracts the cache reading and writing, so it's easy
    to change to another format.
    
    It also leaves in a const to easily change it to JSON at compile
    time.
    
    On a large ORT run, this cuts the time down to about 1/3 what
    it was with JSON.
    
    * Change atstccfg default cache time to 15mins.
    
    Default was 1min, which was far too short for an ORT run.
    It's configurable, so users passing the flag won't be changed.
    
    * Add atstccfg time info log.
    
    * Add ort flag for cache time, rm cache dir on start
    
    * Add atstccfg err not panic for nil en/decode
    
    * Add atstccfg more informative error msg
    
    * Change ort atstccfg cache clearing to variable

--- traffic_ops/ort/atstccfg/toreq/caching_test.go
+++ traffic_ops/ort/atstccfg/toreq/caching_test.go
@@ -45,24 +45,65 @@ func TestWriteCacheJSON(t *testing.T) {
 `,
 		}
 
-		WriteCacheJSON(tmpDir, fileName, largeObj)
+		WriteCache(CacheFormatJSON.Encoder, tmpDir, fileName, largeObj)
 		loadedLargeObj := Obj{}
-		if err := GetJSONObjFromFile(tmpDir, fileName, time.Hour, &loadedLargeObj); err != nil {
-			t.Fatalf("GetJSONObjFromFile large error expected nil, actual: " + err.Error())
+		if err := ReadCache(CacheFormatJSON.Decoder, tmpDir, fileName, time.Hour, &loadedLargeObj); err != nil {
+			t.Fatalf("ReadCache large error expected nil, actual: " + err.Error())
 		} else if largeObj.S != loadedLargeObj.S {
-			t.Fatalf("GetJSONObjFromFile expected %+v actual %+v\n", largeObj.S, loadedLargeObj.S)
+			t.Fatalf("ReadCache expected %+v actual %+v\n", largeObj.S, loadedLargeObj.S)
 		}
 	}
 
 	{
 		// Write a smaller object to the same file, to make sure it properly truncates, and doesn't leave old text lying around (resulting in malformed json)
 		smallObj := Obj{S: `foo`}
-		WriteCacheJSON(tmpDir, fileName, smallObj)
+		WriteCache(CacheFormatJSON.Encoder, tmpDir, fileName, smallObj)
 		loadedSmallObj := Obj{}
-		if err := GetJSONObjFromFile(tmpDir, fileName, time.Hour, &loadedSmallObj); err != nil {
+		if err := ReadCache(CacheFormatJSON.Decoder, tmpDir, fileName, time.Hour, &loadedSmallObj); err != nil {
 			t.Fatalf("GetJSONObjFromFile small error expected nil, actual: " + err.Error())
 		} else if smallObj.S != loadedSmallObj.S {
 			t.Fatalf("GetJSONObjFromFile expected %+v actual %+v\n", smallObj.S, loadedSmallObj.S)
+		}
+	}
+}
+
+func TestWriteCacheGob(t *testing.T) {
+	type Obj struct {
+		S string `json:"s"`
+	}
+
+	fileName := "TestWriteCacheGob.gob"
+	tmpDir := os.TempDir()
+	filePath := filepath.Join(tmpDir, fileName)
+	defer os.Remove(filePath)
+
+	{
+		largeObj := Obj{
+			S: `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+    Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.
+`,
+		}
+
+		WriteCache(CacheFormatGob.Encoder, tmpDir, fileName, largeObj)
+		loadedLargeObj := Obj{}
+		if err := ReadCache(CacheFormatGob.Decoder, tmpDir, fileName, time.Hour, &loadedLargeObj); err != nil {
+			t.Fatalf("ReadCache large error expected nil, actual: " + err.Error())
+		} else if largeObj.S != loadedLargeObj.S {
+			t.Fatalf("ReadCache expected %+v actual %+v\n", largeObj.S, loadedLargeObj.S)
+		}
+	}
+
+	{
+		// Write a smaller object to the same file, to make sure it properly truncates, and doesn't leave old text lying around (resulting in malformed gob)
+		smallObj := Obj{S: `foo`}
+		WriteCache(CacheFormatGob.Encoder, tmpDir, fileName, smallObj)
+		loadedSmallObj := Obj{}
+		if err := ReadCache(CacheFormatGob.Decoder, tmpDir, fileName, time.Hour, &loadedSmallObj); err != nil {
+			t.Fatalf("ReadCache small error expected nil, actual: " + err.Error())
+		} else if smallObj.S != loadedSmallObj.S {
+			t.Fatalf("ReadCache expected %+v actual %+v\n", smallObj.S, loadedSmallObj.S)
 		}
 	}
 }
