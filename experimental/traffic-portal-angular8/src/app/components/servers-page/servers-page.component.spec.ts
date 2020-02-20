1a4f7a3d443b45b70ffc1d2dfc62a5d6c346f519
ocket8888 <ocket8888@gmail.com>
2020-02-20 07:50:33 -0700

    Minor testing fixes

--- experimental/traffic-portal-angular8/src/app/components/servers-page/servers-page.component.spec.ts
+++ experimental/traffic-portal-angular8/src/app/components/servers-page/servers-page.component.spec.ts
@@ -1,25 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { TpHeaderComponent } from '../tp-header/tp-header.component';
 
 import { ServersPageComponent } from './servers-page.component';
 
 describe('ServersPageComponent', () => {
-  let component: ServersPageComponent;
-  let fixture: ComponentFixture<ServersPageComponent>;
+	let component: ServersPageComponent;
+	let fixture: ComponentFixture<ServersPageComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ServersPageComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [
+				ServersPageComponent,
+				TpHeaderComponent
+			],
+			imports: [
+				FormsModule,
+				HttpClientModule,
+				ReactiveFormsModule,
+				RouterTestingModule
+			]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ServersPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ServersPageComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
