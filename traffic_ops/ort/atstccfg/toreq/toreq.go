693cd52a9de48d670eb3523ec441c9ab5b77f586
ocket8888 <ocket8888@apache.org>
2020-04-27 19:40:34 -0600

    rolled back update of vendored client

--- traffic_ops/ort/atstccfg/toreq/toreq.go
+++ traffic_ops/ort/atstccfg/toreq/toreq.go
@@ -37,7 +37,7 @@ import (
 	"github.com/apache/trafficcontrol/lib/go-log"
 	"github.com/apache/trafficcontrol/lib/go-tc"
 	"github.com/apache/trafficcontrol/lib/go-util"
-	toclient "github.com/apache/trafficcontrol/traffic_ops/v2-client"
+	toclient "github.com/apache/trafficcontrol/traffic_ops/client"
 	"github.com/apache/trafficcontrol/traffic_ops/ort/atstccfg/torequtil"
 )
 
