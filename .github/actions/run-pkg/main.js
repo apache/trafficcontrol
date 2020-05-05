60567e332efd37d1eca78f0d51988f7a5f740ace
ocket8888 <ocket8888@apache.org>
2020-05-05 11:41:04 -0600

    Slight refactor and now builds all RPMs

--- .github/actions/run-pkg/main.js
+++ .github/actions/run-pkg/main.js
@@ -1,13 +1,39 @@
-const GOPATH = '/go',
-    srcDir = `${GOPATH}/src/github.com/apache/trafficcontrol`,
-    component = 'traffic_ops';
-process.exit(
-    require('child_process')
-        .spawnSync('docker', ['run',
-                '-e', `GOPATH=${GOPATH}`,
-                '-v', `${process.env.GITHUB_WORKSPACE}:${srcDir}`,
-                `trafficcontrol/${component}_builder`,
-                `${srcDir}/build/build.sh`, component],
-            {stdio: 'inherit'})
-        .status
-);
+const child_process = require("child_process");
+
+const GOPATH = "/go";
+const srcDir = `${GOPATH}/src/github.com/apache/trafficcontrol`,
+const components = [
+	"traffic_monitor",
+	"traffic_ops",
+	"traffic_router",
+	"traffic_stats"
+];
+
+const dockerArgs = [
+	"run",
+	"-e",
+	`GOPATH=${GOPATH}`,
+	"-v",
+	`${process.env.GITHUB_WORKSPACE}:${srcDir}`
+];
+
+const spawnArgs = {stdio: "inherit"};
+
+for (const component of components) {
+	const proc = child_process.spawnSync(
+		"docker",
+		dockerArgs.concat([
+			`trafficcontrol/${component}_builder`,
+			`${srcDir}/build/build.sh`,
+			component
+		]),
+		spawnArgs
+	);
+
+	if (proc.status !== 0) {
+		console.error(`Build for ${component} failed; exiting.`);
+		process.exit(proc.status);
+	}
+}
+
+process.exit(0);
