c2f894a5a4f29e4b6b093b94d8e7be15975b9bfe
John Rushford <jrushford@apache.org>
2020-05-12 20:31:47 +0000

    ort perl transliteration to go

--- traffic_ops_ort/build/build_rpm.sh
+++ traffic_ops_ort/build/build_rpm.sh
@@ -64,6 +64,9 @@ initBuildArea() {
 	(cd atstccfg;
 	go build -v -gcflags "$gcflags" -ldflags "${ldflags} -X main.GitRevision=$(git rev-parse HEAD) -X main.BuildTimestamp=$(date +'%Y-%M-%dT%H:%M:%s') -X main.Version=${TC_VERSION}" -tags "$tags")
 
+	(cd t3c;
+	go build -v -gcflags "$gcflags" -ldflags "${ldflags} -X main.GitRevision=$(git rev-parse HEAD) -X main.BuildTimestamp=$(date +'%Y-%M-%dT%H:%M:%s') -X main.Version=${TC_VERSION}" -tags "$tags")
+
 	cp -p traffic_ops_ort.pl "$dest";
 	cp -p supermicro_udev_mapper.pl "$dest";
 	mkdir -p "${dest}/atstccfg";
