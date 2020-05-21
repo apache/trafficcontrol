56a123c8a4efcb5019b6ba05ea75def8f58641cc
shamrickus <shamrickus@gmail.com>
2020-05-21 10:22:11 -0600

    Fix typo

--- traffic_monitor/threadsafe/cacheavailablestatus.go
+++ traffic_monitor/threadsafe/cacheavailablestatus.go
@@ -7,7 +7,7 @@ package threadsafe
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License a{t
+ * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
