97f9c55d04afa485d99326c618171961d52ce414
ocket8888 <ocket8888@apache.org>
2020-05-27 10:34:57 -0600

    Added argument parsing, defaults and sanity check

--- traffic_ops/install/bin/postinstall.py
+++ traffic_ops/install/bin/postinstall.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+>>> [c for c in [[a for a in b if not a.config_var] for b in DEFAULTS.values()] if c]
+[]
+"""
 
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,8 +18,10 @@
 # limitations under the License.
 #
 
+import argparse
 import getpass
 import logging
+import os
 import sys
 import typing
 
@@ -43,6 +49,90 @@ CPAN_LOG_FILE = "/var/log/traffic_ops/cpan.log"
 
 # Configuration file output with answers which can be used as input to postinstall
 OUTPUT_CONFIG_FILE = "/opt/traffic_ops/install/bin/configuration_file.json"
+
+class Question():
+
+	def __init__(self, question: str, default: str, config_var: str, hidden: bool = False):
+		self.question = question
+		self.default = default
+		self.config_var = config_var
+		self.hidden = hidden
+
+	def __str__(self) -> str:
+		if self.default:
+			return f"{self.question} [{self.default}]: "
+		return f"{self.question}: "
+
+	def __repr__(self) -> str:
+		return f"Question(question='{self.question}', default='{self.default}', config_var='{self.config_var}', hidden={self.hidden})"
+
+	def ask(self) -> str:
+		if self.hidden:
+			while True:
+				pw = getpass.getpass(self)
+				if pw == getpass.getpass(f"Re-Enter {self.question}"):
+					return pw
+				print("Error: passwords do not match, try again")
+		ipt = input(self)
+		return ipt if ipt else self.default
+
+
+# The default question/answer set
+DEFAULTS = {
+	DATABASE_CONF_FILE: [
+		Question("Database type", "Pg", "type"),
+		Question("Database name", "traffic_ops", "dbname"),
+		Question("Database server hostname IP or FQDN", "localhost", "hostname"),
+		Question("Database port number", "5432", "port"),
+		Question("Traffic Ops database user", "traffic_ops", "user"),
+		Question("Password for Traffic Ops database user", "", "password", hidden=True)
+	],
+	DB_CONF_FILE: [
+		Question("Database server root (admin) user", "postgres", "pgUser"),
+		Question("Password for database server admin", "", "pgPassword", hidden=True),
+		Question("Download Maxmind Database?", "yes", "maxmind")
+	],
+	CDN_CONF_FILE: [
+		Question("Generate a new secret?", "yes", "genSecret"),
+		Question("Number of secrets to keep?", "1", "keepSecrets"),
+		Question("Port to serve on?", "443", "port"),
+		Question("Number of workers?", "12", "workers"),
+		Question("Traffic Ops url?", "http://localhost:3000", "base_url"),
+		Question("ldap.conf location?", "/opt/traffic_ops/app/conf/ldap.conf", "ldap_conf_location")
+	],
+	LDAP_CONF_FILE:[
+		Question("Do you want to set up LDAP?", "no", "setupLdap"),
+		Question("LDAP server hostname", "", "host"),
+		Question("LDAP Admin DN", "", "admin_dn"),
+		Question("LDAP Admin Password", "", "admin_pass", hidden=True),
+		Question("LDAP Search Base", "", "search_base"),
+		Question("LDAP Search Query", "", "search_query"),
+		Question("LDAP Skip TLS verify", "", "insecure"),
+		Question("LDAP Timeout Seconds", "", "ldap_timeout_secs")
+	],
+	USERS_CONF_FILE: [
+		Question("Administration username for Traffic Ops", "admin", "tmAdminUser"),
+		Question("Password for the admin user", "", "tmAdminPw", hidden=True)
+	],
+	PROFILES_CONF_FILE: [
+		Question("Add custom profiles?", "no", "custom_profiles")
+	],
+	OPENSSL_CONF_FILE: [
+		Question("Do you want to generate a certificate?", "yes", "genCert"),
+		Question("Country Name (2 letter code)", "", "country"),
+		Question("State or Province Name (full name)", "", "state"),
+		Question("Locality Name (eg, city)", "", "locality"),
+		Question("Organization Name (eg, company)", "", "company"),
+		Question("Organizational Unit Name (eg, section)", "", "org_unit"),
+		Question("Common Name (eg, your name or your server's hostname)", "", "common_name"),
+		Question("RSA Passphrase", "CHANGEME!!", "rsaPassword", hidden=True)
+	],
+	PARAM_CONF_FILE: [
+		Question("Traffic Ops url", "https://localhost", "tm.url"),
+		Question("Human-readable CDN Name. (No whitespace, please)", "kabletown_cdn", "cdn_name"),
+		Question("DNS sub-domain for which your CDN is authoritative", "cdn1.kabletown.net", "dns_subdomain")
+	]
+}
 
 def get_field(question: str, config_answer: str, hidden: bool = False) -> str:
 	if hidden:
@@ -79,3 +169,82 @@ def generate_db_conf(user_input: dict, fname: str, todb_fname: str, automatic: b
 	"""
 	"""
 	db_conf = get_config(user_input, fname, automatic)
+
+def sanity_check_config(cfg: dict):
+	diffs = 0
+
+	for fname, file in DEFAULTS:
+		if fname not in cfg:
+			logging.warning("File '%s' found in defaults but not config file", fname)
+			cfg[fname] = []
+
+		for defaultValue in file:
+			for configValue in cfg[fname]:
+				if defaultValue["config_var"] == configValue.get("config_var"):
+					break
+			else:
+				continue
+
+
+
+def main(automatic: bool, debug: bool, defaults: str = None, cfile: str = None) -> int:
+	if debug:
+		logging.getLogger().setLevel(logging.DEBUG)
+	else:
+		logging.getLogger().setLevel(logging.INFO)
+
+	# At this point, the Perl script... unzipped its own logfile?
+
+	logging.info("Starting postinstall")
+	# The Perl printed this whether or not the logger was actually at the debug level
+	# so we do too
+	logging.info("Debug is on")
+
+	if automatic:
+		logging.info("Running in automatic mode")
+
+	if defaults is not None:
+		try:
+			if defaults:
+				try:
+					with open(defaults, "w") as fd:
+						json.dump(DEFAULTS, fd, indent="\t")
+				except OSError as e:
+					logging.critical("Writing output: %s", e)
+					return 1
+			else:
+				json.dump(DEFAULTS, sys.stdout, indent="\t")
+		except ValueError as e:
+			logging.critical("Converting defaults to JSON: %s", e)
+			return 1
+		return 0
+
+	userInput = None
+	if not cfile:
+		logging.info("No input file given - using defaults")
+		userInput = DEFAULTS
+	else:
+		logging.info("Using input file %s", cfile)
+		try:
+			with open(cfile) as fd:
+				userInput = json.load(fd)
+			sanity_check_config(userInput)
+		except (OSError, ValueError) as e:
+			logging.critical("Reading in input file '%s': %s", cfile, e)
+			return 1
+	return 0
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-a", "--automatic", help="If there are questions in the config file which do not have answers, the script will look to the defaults for the answer. If the answer is not in the defaults the script will exit", action="store_true")
+	parser.add_argument("--cfile", help="An input config file used to ask and answer questions", type=str, default=None)
+	parser.add_argument("--debug", help="Enables verbose output", action="store_true")
+	parser.add_argument("--defaults", help="Writes out a configuration file with defaults which can be used as input", type=str, nargs="?", default=None, const="")
+	parser.add_argument("-n", "--no-root", help="Enable running as a non-root user (may cause failure)", action="store_true")
+
+	args = parser.parse_args()
+
+	if not args.no_root and os.getuid() != 0:
+		logging.error("You must run this script as the root user")
+		sys.exit(1)
+	sys.exit(main(args.automatic, args.debug, args.defaults, args.cfile))
