aa03fd37f85a572ec4ad0f8843967061192cec77
ocket8888 <ocket8888@apache.org>
2020-06-11 15:51:13 -0600

    Giving ort its own build (#2776)
    * Added TO_ORT build tree
    
    * Made TO_ORT an independent build
    
    * Specifically, TO build needs the TO spec file
    
    * Fixed build script; added failure messages
    
    * Changelog update
    
    * Documentation update
    
    * Added Go to build image since we need it now
    
    * Fixed spec file for new paths
    
    * Re-added Go build step that I forgot in the rebase
    
    * Fixed import paths
    
    * Fixed TC_DIR definition
    
    * Removed uneeded dependency
    
    * changed cp flags to be simpler
    
    * fixed TO build
    
    * Reduced necessary changes for Go version upgrade
    
    * Using official Apache builder
    
    * Added image pull to avoid breaking change
    
    * Combined build and pull commands
    
    Co-authored-by: Brennan Fieck <Brennan_WilliamFieck@comcast.com>

---
+++ traffic_ops_ort/atstccfg/cfgfile/servercachedotconfig.go
@@ -0,0 +1,58 @@
+package cfgfile
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/apache/trafficcontrol/lib/go-atscfg"
+	"github.com/apache/trafficcontrol/lib/go-tc"
+	"github.com/apache/trafficcontrol/traffic_ops_ort/atstccfg/config"
+)
+
+const ServerCacheDotConfigIncludeInactiveDSes = false // TODO move to lib/go-atscfg
+
+func GetConfigFileServerCacheDotConfig(toData *config.TOData) (string, string, string, error) {
+	// TODO TOAPI add /servers?cdn=1 query param
+
+	// TODO remove this, we generated the scope, we know it's right? Or should we have an extra safety check?
+	if !strings.HasPrefix(string(toData.Server.Type), tc.MidTypePrefix) {
+		// emulates Perl
+		return "", "", "", errors.New("Error - incorrect file scope for route used.  Please use the profiles route.")
+	}
+
+	dsData := map[tc.DeliveryServiceName]atscfg.ServerCacheConfigDS{}
+	for _, ds := range toData.DeliveryServices {
+		if ds.XMLID == nil || ds.Active == nil || ds.OrgServerFQDN == nil || ds.Type == nil {
+			// TODO orgserverfqdn is nil for some DSes - MSO? Verify.
+			continue
+			//			return "", fmt.Errorf("getting delivery services: got DS with nil values! '%v' %v %+v\n", *ds.XMLID, *ds.ID, ds)
+		}
+		if !ServerCacheDotConfigIncludeInactiveDSes && !*ds.Active {
+			continue
+		}
+		dsData[tc.DeliveryServiceName(*ds.XMLID)] = atscfg.ServerCacheConfigDS{OrgServerFQDN: *ds.OrgServerFQDN, Type: *ds.Type}
+	}
+
+	serverName := tc.CacheName(toData.Server.HostName)
+
+	return atscfg.MakeServerCacheDotConfig(serverName, toData.TOToolName, toData.TOURL, dsData), atscfg.ContentTypeCacheDotConfig, atscfg.LineCommentCacheDotConfig, nil
+}
---
+++ traffic_ops_ort/build/build_rpm.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -ex
+
+#----------------------------------------
+function importFunctions() {
+	local script=$(readlink -f "$0");
+	local scriptdir=$(dirname "$script");
+	export ORT_DIR=$(dirname "$scriptdir");
+	export TC_DIR=$(dirname "$ORT_DIR");
+	functions_sh="$TC_DIR/build/functions.sh";
+	if [[ ! -r $functions_sh ]]; then
+		echo "error: can't find $functions_sh" >&2;
+		exit 1;
+	fi
+	. "$functions_sh";
+}
+
+#----------------------------------------
+function initBuildArea() {
+	echo "Initializing the build area for Traffic Ops ORT";
+	mkdir -p "$RPMBUILD"/{SPECS,SOURCES,RPMS,SRPMS,BUILD,BUILDROOT}
+
+	local dest=$(createSourceDir traffic_ops_ort);
+	cd "$ORT_DIR";
+
+	echo "PATH: $PATH";
+	echo "GOPATH: $GOPATH";
+	go version;
+	go env;
+
+	go get -v golang.org/x/crypto/ed25519 golang.org/x/crypto/scrypt golang.org/x/net/ipv4 golang.org/x/net/ipv6 golang.org/x/sys/unix;
+
+	GC=(go build)
+	GFLAGS=(-v)
+	if [[ "$DEBUG_BUILD" == true ]]; then
+		echo "DEBUG_BUILD is enabled, building without optimization or inlining...";
+		GFLAGS+=(--gcflags 'all=-N -l');
+	fi;
+
+	pushd atstccfg;
+	"${GC[@]}" "${GCFLAGS[@]}" -ldflags "-X main.GitRevision=`git rev-parse HEAD` -X main.BuildTimestamp=`date +'%Y-%M-%dT%H:%M:%s'` -X main.Version=${TC_VERSION}";
+	popd;
+
+	cp -p traffic_ops_ort.pl "$dest";
+	cp -p supermicro_udev_mapper.pl "$dest";
+	mkdir -p "${dest}/atstccfg";
+	cp -a atstccfg/* "${dest}/atstccfg";
+	tar -czvf "$dest".tgz -C "$RPMBUILD"/SOURCES $(basename "$dest");
+	cp build/traffic_ops_ort.spec "$RPMBUILD"/SPECS/.;
+
+	echo "The build area has been initialized.";
+}
+
+#----------------------------------------
+importFunctions;
+checkEnvironment go;
+initBuildArea;
+buildRpm traffic_ops_ort;
