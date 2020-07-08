zrhoffman/atc

- Bourne Shell shebang   - Almquist shellcheck   - Error trap   - Strict shell options   - Return 1 from functions on error instead of exit 1 since we have an     error trap and the errexit option set

traffic_ops/build/build_rpm.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-#
-#
+#!/usr/bin/env sh
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,6 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# shellcheck shell=ash
+trap 'exit_code=$?; [ $exit_code -ne 0 ] && echo "Error on line ${LINENO} of ${0}" >/dev/stderr; exit $exit_code' EXIT;
+set -o errexit -o nounset -o pipefail;
 
 #----------------------------------------
 function importFunctions() {
@@ -24,7 +24,7 @@ function importFunctions() {
 	functions_sh="$TC_DIR/build/functions.sh"
 	if [[ ! -r $functions_sh ]]; then
 		echo "error: can't find $functions_sh"
-		exit 1
+		return 1
 	fi
 	. "$functions_sh"
 }
@@ -32,11 +32,11 @@ function importFunctions() {
 # ---------------------------------------
 function initBuildArea() {
 	echo "Initializing the build area."
-	mkdir -p "$RPMBUILD"/{SPECS,SOURCES,RPMS,SRPMS,BUILD,BUILDROOT} || { echo "Could not create $RPMBUILD: $?"; exit 1; }
+	mkdir -p "$RPMBUILD"/{SPECS,SOURCES,RPMS,SRPMS,BUILD,BUILDROOT} || { echo "Could not create $RPMBUILD: $?"; return 1; }
 
 	local to_dest=$(createSourceDir traffic_ops)
 	cd "$TO_DIR" || \
-		 { echo "Could not cd to $TO_DIR: $?"; exit 1; }
+		 { echo "Could not cd to $TO_DIR: $?"; return 1; }
 
 	echo "PATH: $PATH"
 	echo "GOPATH: $GOPATH"
@@ -45,7 +45,7 @@ function initBuildArea() {
 
 	# get x/* packages (everything else should be properly vendored)
 	go get -v golang.org/x/crypto/ed25519 golang.org/x/crypto/scrypt golang.org/x/net/ipv4 golang.org/x/net/ipv6 golang.org/x/sys/unix || \
-                { echo "Could not get go package dependencies"; exit 1; }
+                { echo "Could not get go package dependencies"; return 1; }
 
 	# compile traffic_ops_golang
 	pushd traffic_ops_golang
@@ -55,35 +55,35 @@ function initBuildArea() {
 		go_build+=(-gcflags 'all=-N -l');
 	fi;
 	"${go_build[@]}" -ldflags "-X main.version=traffic_ops-${TC_VERSION}-${BUILD_NUMBER}.${RHEL_VERSION} -B 0x$(git rev-parse HEAD)" || \
-                { echo "Could not build traffic_ops_golang binary"; exit 1; }
+                { echo "Could not build traffic_ops_golang binary"; return 1; }
 	popd
 
 	# compile db/admin
 	pushd app/db
 	"${go_build[@]}" -o admin || \
-                { echo "Could not build db/admin binary"; exit 1; }
+                { echo "Could not build db/admin binary"; return 1; }
 	popd
 
 	# compile TO profile converter
 	pushd install/bin/convert_profile
 	"${go_build[@]}" || \
-                { echo "Could not build convert_profile binary"; exit 1; }
+                { echo "Could not build convert_profile binary"; return 1; }
 	popd
 
 	# compile atstccfg
 	pushd ort/atstccfg
 	"${go_build[@]}" -ldflags "-X main.GitRevision=`git rev-parse HEAD` -X main.BuildTimestamp=`date +'%Y-%M-%dT%H:%M:%s'` -X main.Version=${TC_VERSION}" || \
-                { echo "Could not build atstccfg binary"; exit 1; }
+                { echo "Could not build atstccfg binary"; return 1; }
 	popd
 
 	rsync -av etc install "$to_dest"/ || \
-		 { echo "Could not copy to $to_dest: $?"; exit 1; }
+		 { echo "Could not copy to $to_dest: $?"; return 1; }
 	rsync -av app/{bin,conf,cpanfile,db,lib,public,script,templates} "$to_dest"/app/ || \
-		 { echo "Could not copy to $to_dest/app: $?"; exit 1; }
+		 { echo "Could not copy to $to_dest/app: $?"; return 1; }
 	tar -czvf "$to_dest".tgz -C "$RPMBUILD"/SOURCES $(basename "$to_dest") || \
-		 { echo "Could not create tar archive $to_dest.tgz: $?"; exit 1; }
+		 { echo "Could not create tar archive $to_dest.tgz: $?"; return 1; }
 	cp "$TO_DIR"/build/*.spec "$RPMBUILD"/SPECS/. || \
-		 { echo "Could not copy spec files: $?"; exit 1; }
+		 { echo "Could not copy spec files: $?"; return 1; }
 
 	# Create traffic_ops_ort source area
 	to_ort_dest=$(createSourceDir traffic_ops_ort)
@@ -93,7 +93,7 @@ function initBuildArea() {
 	cp -R -p ort/atstccfg/* "${to_ort_dest}/atstccfg"
 
 	tar -czvf "$to_ort_dest".tgz -C "$RPMBUILD"/SOURCES $(basename "$to_ort_dest") || \
-		 { echo "Could not create tar archive $to_ort_dest: $?"; exit 1; }
+		 { echo "Could not create tar archive $to_ort_dest: $?"; return 1; }
 
 	export PLUGINS=$(grep -l -P '(?<!func )AddPlugin\(' ${TO_DIR}/traffic_ops_golang/plugin/*.go | xargs -I '{}' basename {} '.go')
 

apache/trafficcontrol

traffic_ops/build/build_rpm.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-#
-#
+#!/usr/bin/env sh
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,6 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# shellcheck shell=ash
+trap 'exit_code=$?; [ $exit_code -ne 0 ] && echo "Error on line ${LINENO} of ${0}" >/dev/stderr; exit $exit_code' EXIT;
+set -o errexit -o nounset -o pipefail;
 
 #----------------------------------------
 function importFunctions() {
@@ -24,7 +24,7 @@ function importFunctions() {
 	functions_sh="$TC_DIR/build/functions.sh"
 	if [[ ! -r $functions_sh ]]; then
 		echo "error: can't find $functions_sh"
-		exit 1
+		return 1
 	fi
 	. "$functions_sh"
 }
@@ -32,11 +32,11 @@ function importFunctions() {
 # ---------------------------------------
 function initBuildArea() {
 	echo "Initializing the build area."
-	mkdir -p "$RPMBUILD"/{SPECS,SOURCES,RPMS,SRPMS,BUILD,BUILDROOT} || { echo "Could not create $RPMBUILD: $?"; exit 1; }
+	mkdir -p "$RPMBUILD"/{SPECS,SOURCES,RPMS,SRPMS,BUILD,BUILDROOT} || { echo "Could not create $RPMBUILD: $?"; return 1; }
 
 	local dest=$(createSourceDir traffic_ops)
 	cd "$TO_DIR" || \
-		 { echo "Could not cd to $TO_DIR: $?"; exit 1; }
+		 { echo "Could not cd to $TO_DIR: $?"; return 1; }
 
 	echo "PATH: $PATH"
 	echo "GOPATH: $GOPATH"
@@ -45,7 +45,7 @@ function initBuildArea() {
 
 	# get x/* packages (everything else should be properly vendored)
 	go get -v golang.org/x/crypto/ed25519 golang.org/x/crypto/scrypt golang.org/x/net/ipv4 golang.org/x/net/ipv6 golang.org/x/sys/unix || \
-                { echo "Could not get go package dependencies"; exit 1; }
+                { echo "Could not get go package dependencies"; return 1; }
 
 	# compile traffic_ops_golang
 	pushd traffic_ops_golang
@@ -55,29 +55,29 @@ function initBuildArea() {
 		go_build+=(-gcflags 'all=-N -l');
 	fi;
 	"${go_build[@]}" -ldflags "-X main.version=traffic_ops-${TC_VERSION}-${BUILD_NUMBER}.${RHEL_VERSION} -B 0x$(git rev-parse HEAD)" || \
-                { echo "Could not build traffic_ops_golang binary"; exit 1; }
+                { echo "Could not build traffic_ops_golang binary"; return 1; }
 	popd
 
 	# compile db/admin
 	pushd app/db
 	"${go_build[@]}" -o admin || \
-                { echo "Could not build db/admin binary"; exit 1; }
+                { echo "Could not build db/admin binary"; return 1; }
 	popd
 
 	# compile TO profile converter
 	pushd install/bin/convert_profile
 	"${go_build[@]}" || \
-                { echo "Could not build convert_profile binary"; exit 1; }
+                { echo "Could not build convert_profile binary"; return 1; }
 	popd
 
 	rsync -av etc install "$dest"/ || \
-		 { echo "Could not copy to $dest: $?"; exit 1; }
+		 { echo "Could not copy to $dest: $?"; return 1; }
 	rsync -av app/{bin,conf,cpanfile,db,lib,public,script,templates} "$dest"/app/ || \
-		 { echo "Could not copy to $dest/app: $?"; exit 1; }
+		 { echo "Could not copy to $dest/app: $?"; return 1; }
 	tar -czvf "$dest".tgz -C "$RPMBUILD"/SOURCES $(basename "$dest") || \
-		 { echo "Could not create tar archive $dest.tgz: $?"; exit 1; }
+		 { echo "Could not create tar archive $dest.tgz: $?"; return 1; }
 	cp "$TO_DIR"/build/traffic_ops.spec "$RPMBUILD"/SPECS/. || \
-		 { echo "Could not copy spec files: $?"; exit 1; }
+		 { echo "Could not copy spec files: $?"; return 1; }
 
 	export PLUGINS=$(grep -l -P '(?<!func )AddPlugin\(' ${TO_DIR}/traffic_ops_golang/plugin/*.go | xargs -I '{}' basename {} '.go')
 