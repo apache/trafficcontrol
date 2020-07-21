bf6782dd65a37b4f06a8e7e96517ff290661e203
Zach Hoffman <zach@zrhoffman.net>
2020-07-21 10:40:49 -0600

    Add legacy severs{host_name}/update_status handler to continue deprecated support for servers/all/update_status for API versions below 3.0

--- traffic_ops/traffic_ops_golang/server/servers_update_status.go
+++ traffic_ops/traffic_ops_golang/server/servers_update_status.go
@@ -119,3 +119,96 @@ ORDER BY s.id
 	}
 	return updateStatuses, nil
 }
+
+func GetServerUpdateStatusHandlerV2(w http.ResponseWriter, r *http.Request) {
+	inf, userErr, sysErr, errCode := api.NewInfo(r, []string{"host_name"}, nil)
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, errCode, userErr, sysErr)
+		return
+	}
+	defer inf.Close()
+
+	serverUpdateStatus, err := getServerUpdateStatusV2(inf.Tx.Tx, inf.Config, inf.Params["host_name"])
+	if err != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, http.StatusInternalServerError, nil, err)
+		return
+	}
+	api.WriteRespRaw(w, r, serverUpdateStatus)
+}
+
+// getServerUpdateStatusV2 supports /servers/all/update_status in addition to /servers/{host_name}/update_status
+// This special case is believed to be used nowhere.
+func getServerUpdateStatusV2(tx *sql.Tx, cfg *config.Config, hostName string) ([]tc.ServerUpdateStatus, error) {
+	// language=SQL
+	baseSelectStatement := `
+WITH parentservers AS (
+	SELECT ps.id, ps.cachegroup, ps.cdn_id, ps.upd_pending, ps.reval_pending
+	FROM server ps
+	LEFT JOIN status AS pstatus ON pstatus.id = ps.status
+	WHERE pstatus.name != 'OFFLINE'
+), use_reval_pending AS (
+	SELECT value::BOOLEAN
+	FROM parameter
+	WHERE name = 'use_reval_pending'
+	AND config_file = 'global'
+	UNION ALL SELECT FALSE FETCH FIRST 1 ROW ONLY
+)
+SELECT
+	s.id,
+	s.host_name,
+	type.name AS type,
+	(s.reval_pending::BOOLEAN) AS server_reval_pending,
+	use_reval_pending.value,
+	s.upd_pending,
+	status.name AS status,
+	COALESCE(BOOL_OR(ps.upd_pending), FALSE) AS parent_upd_pending,
+	COALESCE(BOOL_OR(ps.reval_pending), FALSE) AS parent_reval_pending
+	FROM use_reval_pending,
+		 server s
+LEFT JOIN status ON s.status = status.id
+LEFT JOIN cachegroup cg ON s.cachegroup = cg.id
+LEFT JOIN type ON type.id = s.type
+LEFT JOIN parentservers ps ON ps.cachegroup = cg.parent_cachegroup_id
+	AND ps.cdn_id = s.cdn_id
+	AND type.name = 'EDGE'
+` // remove the EDGE reference if other server types should have their parents processed
+
+	// language=SQL
+	groupBy := `
+GROUP BY s.id, s.host_name, type.name, server_reval_pending, use_reval_pending.value, s.upd_pending, status.name
+ORDER BY s.id
+`
+
+	updateStatuses := []tc.ServerUpdateStatus{}
+	var rows *sql.Rows
+	var err error
+	if hostName == "all" {
+		rows, err = tx.Query(baseSelectStatement + groupBy)
+		if err != nil {
+			log.Error.Printf("could not execute select server update status query: %s\n", err)
+			return nil, tc.DBError
+		}
+	} else {
+		rows, err = tx.Query(baseSelectStatement+` WHERE s.host_name = $1`+groupBy, hostName)
+		if err != nil {
+			log.Error.Printf("could not execute select server update status by hostname query: %s\n", err)
+			return nil, tc.DBError
+		}
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var serverUpdateStatus tc.ServerUpdateStatus
+		var serverType string
+		if err := rows.Scan(&serverUpdateStatus.HostId, &serverUpdateStatus.HostName, &serverType, &serverUpdateStatus.RevalPending, &serverUpdateStatus.UseRevalPending, &serverUpdateStatus.UpdatePending, &serverUpdateStatus.Status, &serverUpdateStatus.ParentPending, &serverUpdateStatus.ParentRevalPending); err != nil {
+			log.Error.Printf("could not scan server update status: %s\n", err)
+			return nil, tc.DBError
+		}
+		if hostName == "all" { //if we want to return the parent data for servers when all is used remove this block
+			serverUpdateStatus.ParentRevalPending = false
+			serverUpdateStatus.ParentPending = false
+		}
+		updateStatuses = append(updateStatuses, serverUpdateStatus)
+	}
+	return updateStatuses, nil
+}
