e7c9d8a7fe4f99951cfeaa3091e9cac78d66f919
Zach Hoffman <zach@zrhoffman.net>
2020-08-18 21:12:48 -0600

    Use docker-compose to build ATC components instead of using Docker directly

--- .github/actions/build-rpms/main.js
+++ .github/actions/build-rpms/main.js
@@ -14,56 +14,23 @@
 
 const child_process = require("child_process");
 
-const GOPATH = "/go";
-const srcDir = `${GOPATH}/src/github.com/apache/trafficcontrol`;
-const components = [
-	"traffic_monitor",
-	"traffic_ops",
-	"traffic_router",
-	"traffic_stats",
-	"traffic_ops_ort"
-];
-
-const dockerArgs = [
-	"run",
-	"-e",
-	`GOPATH=${GOPATH}`,
-	"-v",
-	`${process.env.GITHUB_WORKSPACE}:${srcDir}`,
-	"-w",
-	srcDir
+const dockerComposeArgs = [
+	"-f",
+	`${process.env.GITHUB_WORKSPACE}/infrastructure/docker/build/docker-compose.yml`,
+	"up"
 ];
 
 const spawnArgs = {stdio: "inherit", stderr: "inherit"};
 
-const timers = new Map();
+const proc = child_process.spawnSync(
+	"docker-compose",
+	dockerComposeArgs,
+	spawnArgs
+);
 
-for (const component of components) {
-	const then = new Date();
-	const proc = child_process.spawnSync(
-		"docker",
-		dockerArgs.concat([
-			`apache/${component}_builder:master`,
-			`build/build.sh`,
-			component
-		]),
-		spawnArgs
-	);
-	const diff = new Date() - then;
-
-	console.log(`Finished build for ${component} in ${diff}ms total`);
-
-	if (proc.status !== 0) {
-		console.error(`Build for ${component} failed; exiting.`);
-		process.exit(proc.status);
-	}
-
-	timers.set(component, diff);
+if (proc.status !== 0) {
+	console.error("Building the RPMs failed");
+} else {
+	console.log("Finished building RPMS");
 }
-
-console.log(`Finished building RPMS`)
-for (const [component, time] of timers) {
-	console.log(`Built ${component} in ${time}ms`);
-}
-
-process.exit(0);
+process.exit(proc.status);
