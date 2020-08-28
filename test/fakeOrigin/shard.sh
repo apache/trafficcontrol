46b4bd2dd0665cc53d65b40cd187b36d33876c40
Jonathan Gray <Jonathan_Gray@comcast.com>
2020-08-28 11:22:31 -0600

    License Header fix per PR feedback

--- test/fakeOrigin/shard.sh
+++ test/fakeOrigin/shard.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,7 +16,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-#!/bin/bash
 set -e
 
 # brew install ffmpeg --with-rtmp-dump
