a0f7f87ef907325a00d537721da5abcf74e81003
ocket8888 <ocket8888@apache.org>
2020-09-09 16:53:43 -0600

    Add legacy validation function

--- traffic_ops/traffic_ops_golang/deliveryservice/request/validate.go
+++ traffic_ops/traffic_ops_golang/deliveryservice/request/validate.go
@@ -20,16 +20,58 @@ package request
  */
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"strconv"
 
+	"github.com/apache/trafficcontrol/lib/go-log"
 	"github.com/apache/trafficcontrol/lib/go-tc"
 	"github.com/apache/trafficcontrol/lib/go-tc/tovalidate"
 	"github.com/apache/trafficcontrol/lib/go-util"
 
-	"github.com/go-ozzo/ozzo-validation"
+	validation "github.com/go-ozzo/ozzo-validation"
 )
+
+// Validate ensures all required fields are present and in correct form.  Also checks request JSON is complete and valid
+func validateLegacy(dsr tc.DeliveryServiceRequestV15, tx *sql.Tx) error {
+	if tx == nil {
+		log.Errorln("validating a legacy Delivery Service Request: nil transaction was passed")
+	}
+
+	fromStatus := tc.RequestStatusDraft
+	if dsr.ID != nil && *dsr.ID > 0 {
+		err := tx.QueryRow(`SELECT status FROM deliveryservice_request WHERE id=$1`, *dsr.ID).Scan(&fromStatus)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	validTransition := func(s interface{}) error {
+		if s == nil {
+			return errors.New("cannot transition to nil status")
+		}
+		toStatus, ok := s.(*tc.RequestStatus)
+		if !ok {
+			return fmt.Errorf("Expected *tc.RequestStatus type,  got %T", s)
+		}
+		return fromStatus.ValidTransition(*toStatus)
+	}
+
+	errMap := validation.Errors{
+		"changeType":      validation.Validate(dsr.ChangeType, validation.Required),
+		"deliveryservice": validation.Validate(dsr.DeliveryService, validation.Required),
+		"status":          validation.Validate(dsr.Status, validation.Required, validation.By(validTransition)),
+	}
+	errs := tovalidate.ToErrors(errMap)
+	// ensure the deliveryservice requested is valid
+	e := dsr.DeliveryService.Validate(tx)
+
+	errs = append(errs, e)
+
+	return util.JoinErrs(errs)
+}
 
 // Validate ensures all required fields are present and in correct form.  Also checks request JSON is complete and valid
 func (req *TODeliveryServiceRequest) Validate() error {
