92aedfb0e5660f203f82e7389bafc6bd83409519
ocket8888 <ocket8888@apache.org>
2020-09-16 16:32:32 -0600

    Replace tests for CRUDer-ism with assignee fetch test

--- traffic_ops/traffic_ops_golang/deliveryservice/request/requests_test.go
+++ traffic_ops/traffic_ops_golang/deliveryservice/request/requests_test.go
@@ -22,125 +22,151 @@ package request
 import (
 	"testing"
 
-	tc "github.com/apache/trafficcontrol/lib/go-tc"
-	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/api"
 	"github.com/jmoiron/sqlx"
-	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
-func TestInterfaces(t *testing.T) {
-	var i interface{}
-	i = &TODeliveryServiceRequest{}
-
-	if _, ok := i.(api.Creator); !ok {
-		t.Errorf("DeliveryServiceRequest must be Creator")
-	}
-	if _, ok := i.(api.Reader); !ok {
-		t.Errorf("DeliveryServiceRequest must be Reader")
-	}
-	if _, ok := i.(api.Updater); !ok {
-		t.Errorf("DeliveryServiceRequest must be Updater")
-	}
-	if _, ok := i.(api.Deleter); !ok {
-		t.Errorf("DeliveryServiceRequest must be Deleter")
-	}
-	if _, ok := i.(api.Identifier); !ok {
-		t.Errorf("DeliveryServiceRequest must be Identifier")
-	}
-	if _, ok := i.(api.Tenantable); !ok {
-		t.Errorf("DeliveryServiceRequest must be Tenantable")
-	}
-}
-
-func TestGetDeliveryServiceRequest(t *testing.T) {
-	s := "this is not a valid xmlid.  Bad characters and too long."
-	i := 1
-	b := true
-	u := "UPDATE"
-	st := tc.RequestStatusSubmitted
-	ds := tc.DeliveryServiceV30{}
-	ds.XMLID = &s
-	ds.CDNID = &i
-	ds.LogsEnabled = &b
-	ds.DSCP = nil
-	ds.GeoLimit = &i
-	ds.Active = &b
-	ds.TypeID = &i
-	r := &TODeliveryServiceRequest{DeliveryServiceRequestNullable: tc.DeliveryServiceRequestNullable{
-		ChangeType:      &u,
-		Status:          &st,
-		DeliveryService: &ds,
-	}}
-
-	expectedErrors := []string{
-		/*
-			`'regionalGeoBlocking' is required`,
-			`'xmlId' cannot contain spaces`,
-			`'dscp' is required`,
-			`'displayName' cannot be blank`,
-			`'geoProvider' is required`,
-			`'typeId' is required`,
-		*/
+func TestGetAssignee(t *testing.T) {
+	req := assignmentRequest{
+		AssigneeID: nil,
+		Assignee:   nil,
 	}
 
-	r.SetKeys(map[string]interface{}{"id": 10})
-	keys, _ := r.GetKeys()
-	if keys["id"].(int) != 10 {
-		t.Errorf("expected ID to be %d,  not %d", 10, keys["id"].(int))
-	}
-	exp := "10"
-	if s != r.GetAuditName() {
-		t.Errorf("expected AuditName to be '%s',  not '%s'", s, r.GetAuditName())
-	}
-	exp = "deliveryservice_request"
-	if r.GetType() != "deliveryservice_request" {
-		t.Errorf("expected Type to be %s,  not %s", exp, r.GetType())
-	}
-
-	var errs []error
-	mockDB, _, err := sqlmock.New()
+	mockDB, mock, err := sqlmock.New()
 	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		t.Fatalf("opening mock database: %v", err)
 	}
 	defer mockDB.Close()
 
 	db := sqlx.NewDb(mockDB, "sqlmock")
 	defer db.Close()
 
-	/* TODO: this section panics when deliveryservice.Validate() tries to get the type name.
-	q := `insert into type (name, description, use_in_table) values ('HTTP', 'HTTP Content Routing', 'deliveryservice') ON CONFLICT (name) DO NOTHING;`
-	qe := `insert into type \(name, description, use_in_table\) values \('HTTP', 'HTTP Content Routing', 'deliveryservice'\) ON CONFLICT \(name\) DO NOTHING;`
-	mock.ExpectExec(qe).WillReturnResult(sqlmock.NewResult(1, 1))
-	res, err := db.Exec(q)
-
-	// db.Exec(`insert into type (name, description, use_in_table) values ('HTTP_NO_CACHE', 'HTTP Content Routing, no caching', 'deliveryservice') ON CONFLICT (name) DO NOTHING;`)
-	//db.Exec(`insert into type (name, description, use_in_table) values ('HTTP_LIVE', 'HTTP Content routing cache in RAM', 'deliveryservice') ON CONFLICT (name) DO NOTHING;`)
-	if err != nil {
-		t.Error(err)
+	// check simple case, no Assignee or ID means no change
+	mock.ExpectBegin()
+	_, _, userErr, sysErr := getAssignee(&req, "test", db.MustBegin().Tx)
+	if userErr != nil {
+		t.Errorf("unexpected user error: %v", userErr)
 	}
-	mock.ExpectQuery(`SELECT name from type where id=\$1`).WillReturnRows(sqlmock.NewRows([]string{"name"}))
-
-	errs := r.Validate(db)
-	*/
-	if len(errs) != len(expectedErrors) {
-		for _, e := range errs {
-			t.Error(e)
-		}
+	if sysErr != nil {
+		t.Errorf("unexpected system error: %v", sysErr)
+	}
+	if req.AssigneeID != nil {
+		t.Errorf("assignee ID was somehow set to: %d", *req.AssigneeID)
+	}
+	if req.Assignee != nil {
+		t.Errorf("assignee was somehow set to: %s", *req.Assignee)
 	}
 
-	for e := range expectedErrors {
-		t.Error(e)
+	expectID := 12
+	expectName := "test assignee"
+
+	req.Assignee = &expectName
+
+	rows := sqlmock.NewRows([]string{"id"})
+	rows.AddRow(expectID)
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT id").WillReturnRows(rows)
+
+	// check case where getting Assignee ID from username
+	_, _, userErr, sysErr = getAssignee(&req, "test", db.MustBegin().Tx)
+	if userErr != nil {
+		t.Errorf("unexpected user error: %v", userErr)
+	}
+	if sysErr != nil {
+		t.Errorf("unexpected system error: %v", sysErr)
 	}
 
-	/*
-		if r.Update(db *sqlx.Tx, ctx context.Context) {
-			t.Errorf("expected ID to be %d,  not %d", 10, r.GetID())
-		}
-		if r.Insert(db *sqlx.Tx, ctx context.Context) {
-			t.Errorf("expected ID to be %d,  not %d", 10, r.GetID())
-		}
-		if r.Delete(db *sqlx.Tx, ctx context.Context) {
-			t.Errorf("expected ID to be %d,  not %d", 10, r.GetID())
-		}
-	*/
+	if req.Assignee == nil {
+		t.Error("Expected assignee to not be nil after getting assignee")
+	} else if *req.Assignee != expectName {
+		t.Errorf("Incorrect assignee; expected: '%s', got: '%s'", expectName, *req.Assignee)
+	}
+
+	if req.AssigneeID == nil {
+		t.Error("Expected assignee ID to not be nil after getting assignee")
+	} else if *req.AssigneeID != expectID {
+		t.Errorf("Incorrect assignee ID; expected: %d, got: %d", expectID, *req.AssigneeID)
+	}
+
+	req.Assignee = nil
+	req.AssigneeID = &expectID
+
+	rows = sqlmock.NewRows([]string{"username"})
+	rows.AddRow(expectName)
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT username").WillReturnRows(rows)
+
+	// check case where getting username from Assignee ID
+	_, _, userErr, sysErr = getAssignee(&req, "test", db.MustBegin().Tx)
+	if userErr != nil {
+		t.Errorf("unexpected user error: %v", userErr)
+	}
+	if sysErr != nil {
+		t.Errorf("unexpected system error: %v", sysErr)
+	}
+
+	if req.Assignee == nil {
+		t.Error("Expected assignee to not be nil after getting assignee")
+	} else if *req.Assignee != expectName {
+		t.Errorf("Incorrect assignee; expected: '%s', got: '%s'", expectName, *req.Assignee)
+	}
+
+	if req.AssigneeID == nil {
+		t.Error("Expected assignee ID to not be nil after getting assignee")
+	} else if *req.AssigneeID != expectID {
+		t.Errorf("Incorrect assignee ID; expected: %d, got: %d", expectID, *req.AssigneeID)
+	}
+
+	req.Assignee = new(string)
+	*req.Assignee = expectName + " - but not actually"
+	req.AssigneeID = &expectID
+	rows = sqlmock.NewRows([]string{"username"})
+	rows.AddRow(expectName)
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT username").WillReturnRows(rows)
+
+	// check that Assignee ID has precedence over Assignee
+	_, _, userErr, sysErr = getAssignee(&req, "test", db.MustBegin().Tx)
+	if userErr != nil {
+		t.Errorf("unexpected user error: %v", userErr)
+	}
+	if sysErr != nil {
+		t.Errorf("unexpected system error: %v", sysErr)
+	}
+
+	if req.Assignee == nil {
+		t.Error("Expected assignee to not be nil after getting assignee")
+	} else if *req.Assignee != expectName {
+		t.Errorf("Incorrect assignee; expected: '%s', got: '%s'", expectName, *req.Assignee)
+	}
+
+	if req.AssigneeID == nil {
+		t.Error("Expected assignee ID to not be nil after getting assignee")
+	} else if *req.AssigneeID != expectID {
+		t.Errorf("Incorrect assignee ID; expected: %d, got: %d", expectID, *req.AssigneeID)
+	}
+
+	req.Assignee = nil
+	req.AssigneeID = &expectID
+	rows = sqlmock.NewRows([]string{"username"})
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT username").WillReturnRows(rows)
+
+	// check that looking for ID of non-existent username is an error
+	_, _, userErr, sysErr = getAssignee(&req, "test", db.MustBegin().Tx)
+	if userErr == nil {
+		t.Error("Expected a user error, but didn't get one")
+	}
+
+	req.Assignee = &expectName
+	req.AssigneeID = nil
+	rows = sqlmock.NewRows([]string{"id"})
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT id").WillReturnRows(rows)
+
+	// check that looking for username of non-existent Assignee is an error
+	_, _, userErr, sysErr = getAssignee(&req, "test", db.MustBegin().Tx)
+	if userErr == nil {
+		t.Error("Expected a user error, but didn't get one")
+	}
 }
