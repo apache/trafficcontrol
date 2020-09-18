049ba5a8494ca7ed1bb1dc6aa8746fcdeac46e1e
Robert Butts <rob@apache.org>
2020-09-18 08:42:08 -0600

    Add ORT flag to update ip_allow.config in syncds

--- traffic_ops_ort/traffic_ops_ort.pl
+++ traffic_ops_ort/traffic_ops_ort.pl
@@ -42,6 +42,7 @@ my $rev_proxy_disable = 0;
 my $skip_os_check = 0;
 my $override_hostname_short = '';
 my $to_timeout_ms = 30000;
+my $syncds_updates_ipallow = 0;
 
 GetOptions( "dispersion=i"       => \$dispersion, # dispersion (in seconds)
             "retries=i"          => \$retries,
@@ -51,6 +52,7 @@ GetOptions( "dispersion=i"       => \$dispersion, # dispersion (in seconds)
             "skip_os_check=i" => \$skip_os_check,
             "override_hostname_short=s" => \$override_hostname_short,
             "to_timeout_ms=i" => \$to_timeout_ms,
+            "syncds_updates_ipallow=i" => \$syncds_updates_ipallow,
           );
 
 if ( $#ARGV < 1 ) {
@@ -345,6 +347,7 @@ sub usage {
 	print "\t   skip_os_check=<0|1>            => bypass the check for a supported CentOS version. Default = 0.\n";
 	print "\t   override_hostname_short=<text> => override the short hostname of the OS for config generation. Default = ''.\n";
 	print "\t   to_timeout_ms=<time>           => the Traffic Ops request timeout in milliseconds. Default = 30000 (30 seconds).\n";
+	print "\t   syncds_updates_ipallow=<0|1>   => Update ip_allow.config in syncds mode, which may trigger an ATS bug blocking random addresses on load! Default = 0, only update on badass and restart.\n";
 	print "====-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-====\n";
 	exit 1;
 }
@@ -412,7 +415,7 @@ sub process_cfg_file {
 		}
 	}
 
-	if ($change_needed && $cfg_file eq "ip_allow.config") {
+	if ($change_needed && $cfg_file eq "ip_allow.config" && $syncds_updates_ipallow != 1) {
 		if ($script_mode == $BADASS) {
 			$trafficserver_restart_needed++;
 		} else {
