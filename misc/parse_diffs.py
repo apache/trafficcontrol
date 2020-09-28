b5de8dadda487e7c8de2e4d3c943d12454106519
ocket8888 <ocket8888@apache.org>
2020-09-28 14:20:47 -0600

    add main function and full diff parsing function

--- misc/parse_diffs.py
+++ misc/parse_diffs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import re
+import sys
 import typing
 
 from enum import Enum
@@ -121,3 +122,95 @@ def parseFile(contents: str) -> typing.List[Annotation]:
 
 	return annotations
 
+def parseDiff(diff: str) -> typing.List[Annotation]:
+	"""
+	parseDiff parses a git diff output and returns the corresponding annotations.
+
+	>>> diff= '''diff --git a/test b/test
+	... index c9072dcb7..2b7686061 100644
+	... --- a/test
+	... +++ b/test
+	... @@ -24,7 +24,7 @@ package tc
+	...  // in: body
+	...  type ASNsResponse struct {
+	...         // in: body
+	... -       Response []ASN `json:"response"`
+	... +Response []ASN `json:"response"`
+	...         Alerts
+	...  }
+	...
+	... @@ -85,7 +85,7 @@ type ASNNullable struct {
+	...         // ID of the ASN
+	...         //
+	...         // required: true
+	... -       ID *int `json:"id" db:"id"`
+	... +ID *int `json:"id" db:"id"`
+	...
+	...         // LastUpdated
+	...         //
+	... diff --git a/quest b/quest
+	... index 283901f14..0c1e2b0c1 100644
+	... --- a/quest
+	... +++ b/quest
+	... @@ -1,7 +1,7 @@
+	...  package tc
+	...
+	...  import (
+	... -       "database/sql"
+	... +"database/sql"
+	...  )
+	...
+	...  /*
+	... '''
+	>>> anns = parseDiff(diff)
+	>>> len(anns)
+	3
+	>>> anns[0]
+	Annotation(level=Level.ERROR, file='test', line=27)
+	>>> anns[1]
+	Annotation(level=Level.ERROR, file='test', line=88)
+	>>> anns[2]
+	Annotation(level=Level.ERROR, file='quest', line=4)
+	"""
+
+	lines = diff.splitlines()
+	if len(lines) < 5:
+		raise ValueError(f"'{diff}' does not represent a git diff")
+
+	match = FILE_HEADER_PATTERN.match(lines[0])
+	if not match or len(match.groups()) != 2:
+		raise ValueError(f"invalid git diff file header: '{lines[0]}''")
+
+	file = lines[:4]
+	lines = lines[4:]
+	annotations = []
+	for line in lines:
+		if FILE_HEADER_PATTERN.match(line):
+			annotations += parseFile("\n".join(file))
+			file = []
+		file.append(line)
+
+	if file:
+		annotations += parseFile("\n".join(file))
+
+	return annotations
+
+def main(args) -> int:
+	"""
+	Runs the main program, based on the passed-in arguments.
+
+	Returns an exit code based on success or failure of the script - NOT based
+	on the presence of any error-level annotations.
+	"""
+	try:
+		print(*parseDiff(sys.stdin.read()), sep="\n")
+		return 0
+	except ValueError as e:
+		print("Error:", e, file=sys.stderr)
+		return 1
+	except OSError as e:
+		print("Error reading input:", e, file=sys.stderr)
+		return 2
+
+if __name__ == "__main__":
+	sys.exit(main(None))
