2158a21ee289dc2836d620a3293bd29504e21549
jpappa200 <joseph_pappano@cable.comcast.com>
2020-10-07 20:18:35 -0400

    fix a couple typos

--- traffic_ops/app/bin/checks/DnssecRefresh/ToDnssecRefresh.go
+++ traffic_ops/app/bin/checks/DnssecRefresh/ToDnssecRefresh.go
@@ -1,4 +1,5 @@
 package main
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -17,8 +18,6 @@ package main
  * specific language governing permissions and limitations
  * under the License.
  */
-
-package main
 
 import (
 "bytes"
