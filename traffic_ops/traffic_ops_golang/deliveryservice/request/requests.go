0a3cb73ada8bee9bbca8881fe848722cfd6d6e83
ocket8888 <ocket8888@apache.org>
2020-10-08 11:05:49 -0600

    Fix duplicate import from rebase errors

--- traffic_ops/traffic_ops_golang/deliveryservice/request/requests.go
+++ traffic_ops/traffic_ops_golang/deliveryservice/request/requests.go
@@ -30,8 +30,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/util/ims"
-
 	"github.com/apache/trafficcontrol/lib/go-log"
 	"github.com/apache/trafficcontrol/lib/go-rfc"
 	"github.com/apache/trafficcontrol/lib/go-tc"
