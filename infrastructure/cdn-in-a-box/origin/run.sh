89918a14f33db51e96bf5c8f20362044e530870d
Zach Hoffman <zrhoffman@apache.org>
2020-10-15 23:03:53 -0600

    CDN-in-a-Box Multi-Site Origin configuration with 1 origin

--- infrastructure/cdn-in-a-box/origin/run.sh
+++ infrastructure/cdn-in-a-box/origin/run.sh
@@ -50,6 +50,6 @@ while ! to-ping 2>/dev/null; do
   sleep 3
 done
 
-# no need to enroll the origin -- gets created when deliveryservice is created
+to-enroll origin "$CDN_NAME" 'CDN_in_a_Box_Origin' || (while true; do echo "enroll failed."; sleep 3 ; done)
 
 lighttpd -t -f /etc/lighttpd/lighttpd.conf && lighttpd -D -f /etc/lighttpd/lighttpd.conf
