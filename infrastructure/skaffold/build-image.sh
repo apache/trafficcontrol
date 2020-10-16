ae266f7f47a66077ddc39aff7f56990ae05027f5
Chris Hayen <chris@bayesianhealth.com>
2020-10-16 08:40:38 -0400

    Stub skaffold and build script

---
+++ infrastructure/skaffold/build-image.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+IMAGE=notset
+WORKDIR=$(mktemp -d)
+
+while getopts i: flag
+do
+    case "${flag}" in
+        i) IMAGE=${OPTARG};;
+    esac
+done
+
+
+
+# Traffic ops
+if [ "ops" == "$IMAGE" ]; then
+  echo "OPS"
+fi
+
+echo "$WORKDIR"
+echo "$IMAGE"
