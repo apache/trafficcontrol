8901f55bdda5c005226bbdb38acc88493f367823
ocket8888 <ocket8888@apache.org>
2020-10-21 17:43:26 -0600

    Miscellaneous fixes (#5101)
    * Fix compile errors, missing docstrings in experimental Go TR
    
    * Fix compile errors in DNSSEC suite tests
    
    * Move vendored packages up so that traffic_vault_util can build
    
    * Fix incorrect method name in t3c test
    
    * Fix license paths
    
    * Fix protobuf license path
    
    * Finally fix protobuf license problems
    
    * Revert changing imports from . to _

--- experimental/traffic_router_golang/toutil/toutil.go
+++ experimental/traffic_router_golang/toutil/toutil.go
@@ -22,38 +22,47 @@ package toutil
 
 import (
 	"errors"
+	"net/url"
 	"strconv"
 
-	to "github.com/apache/trafficcontrol/traffic_ops/client"
+	"github.com/apache/trafficcontrol/lib/go-tc"
+	client "github.com/apache/trafficcontrol/traffic_ops/v3-client"
 )
 
-const TrafficMonitorType = "RASCAL"
-
+// MonitorAvailableStatuses returns a set of the status names that indicate a
+// Traffic Monitor instance is "available".
 func MonitorAvailableStatuses() map[string]struct{} {
 	return map[string]struct{}{
-		"ONLINE":   struct{}{},
-		"REPORTED": struct{}{},
+		"ONLINE":   {},
+		"REPORTED": {},
 	}
 }
 
-func GetMonitorURIs(toc *to.Session, cdn string) ([]string, error) {
-	servers, reqInf, err := toc.GetServersByType(map[string][]string{"type": {TrafficMonitorType}})
+// GetMonitorURIs returns a list of URLs for the root of all Traffic Monitor
+// instances in the given CDN - or an error if one occurred.
+func GetMonitorURIs(toc *client.Session, cdn string) ([]string, error) {
+	params := url.Values{}
+	params.Set("type", tc.MonitorTypeName)
+	servers, reqInf, err := toc.GetServersWithHdr(&params, nil)
 	if err != nil {
 		return nil, errors.New("getting servers by type from '" + reqInf.RemoteAddr.String() + "':" + err.Error())
 	}
 	availableStatuses := MonitorAvailableStatuses()
 
 	monitors := []string{}
-	for _, server := range servers {
-		if server.CDNName != cdn {
+	for _, server := range servers.Response {
+		if server.CDNName == nil || server.HostName == nil || server.DomainName == nil || server.Status == nil {
 			continue
 		}
-		if _, ok := availableStatuses[server.Status]; !ok {
+		if *server.CDNName != cdn {
 			continue
 		}
-		m := "http://" + server.HostName + "." + server.DomainName
-		if server.TCPPort > 0 && server.TCPPort != 80 {
-			m += ":" + strconv.Itoa(server.TCPPort)
+		if _, ok := availableStatuses[*server.Status]; !ok {
+			continue
+		}
+		m := "http://" + *server.HostName + "." + *server.DomainName
+		if server.TCPPort != nil && *server.TCPPort > 0 && *server.TCPPort != 80 {
+			m += ":" + strconv.Itoa(*server.TCPPort)
 		}
 		monitors = append(monitors, m)
 	}
