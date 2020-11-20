7541b204c064036d2d0f048d4f0315eb3433478e
Rima Shah <Rima_Shah@comcast.com>
2020-11-20 10:56:15 -0700

    Updated code based on review feedback

--- traffic_ops/traffic_ops_golang/dbhelpers/db_helpers.go
+++ traffic_ops/traffic_ops_golang/dbhelpers/db_helpers.go
@@ -1015,18 +1015,18 @@ GROUP BY t.name, ds.topology
 func CheckOriginServerInCacheGroupTopology(tx *sql.Tx, dsID int, dsTopology string) (error, error, int) {
 	// get servers and respective cachegroup name that have ORG type in a delivery service
 	q := `
-		SELECT s2.host_name, c.name 
-		FROM server s2
-			INNER JOIN deliveryservice_server ds ON ds.server = s2.id
-			inner join type t on t.id = s2.type
-			INNER JOIN cachegroup c ON c.id = s2.cachegroup
-		WHERE ds.deliveryservice=$1 and t.name='ORG'
-		GROUP BY s2.host_name, c.name
+		SELECT s.host_name, c.name 
+		FROM server s
+			INNER JOIN deliveryservice_server ds ON ds.server = s.id
+			INNER JOIN type t ON t.id = s.type
+			INNER JOIN cachegroup c ON c.id = s.cachegroup
+		WHERE ds.deliveryservice=$1 AND t.name=$2
+		GROUP BY s.host_name, c.name
 	`
 
 	serverName := ""
 	cacheGroupName := ""
-	rows, err := tx.Query(q, dsID)
+	rows, err := tx.Query(q, dsID, tc.OriginTypeName)
 	if err != nil {
 		return nil, fmt.Errorf("querying deliveryservice origin server: %s", err), http.StatusInternalServerError
 	}
@@ -1035,24 +1035,29 @@ func CheckOriginServerInCacheGroupTopology(tx *sql.Tx, dsID int, dsTopology stri
 			return nil, fmt.Errorf("querying deliveryservice origin server: %s", err), http.StatusInternalServerError
 		}
 	}
-	fmt.Println("CacheName:", cacheGroupName)
 
 	servers := make(map[string]string)
 	servers[cacheGroupName] = serverName
-	fmt.Println(servers)
+	offendingSCG := []string{}
+	topoCachegroups := make(map[string]string)
 
 	if len(servers) > 0 {
 		cachegroups, sysErr := GetTopologyCachegroups(tx, dsTopology)
 		if sysErr != nil {
 			return nil, fmt.Errorf("validating %s servers in topology: %v", tc.OriginTypeName, sysErr), http.StatusInternalServerError
 		}
+		// put slice values into map
 		for _, cg := range cachegroups {
-			if _, ok := servers[cg]; ok {
-				return nil, nil, http.StatusOK
-			} else {
-				fmt.Printf("server %s doesnot exists in cachegroup %s", servers[cg], cg)
+			topoCachegroups[cg] = ""
+		}
+		for cg, s := range servers {
+			if _, ok := topoCachegroups[cg]; !ok {
+				offendingSCG = append(offendingSCG, fmt.Sprintf("%s-%s", s, cg))
 			}
 		}
+	}
+	if len(offendingSCG) > 0 {
+		return nil, errors.New("offending list of server-cachegroup: " + strings.Join(offendingSCG, ", ")), http.StatusBadRequest
 	}
 	return nil, nil, http.StatusOK
 }
