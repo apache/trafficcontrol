767f38ea240cbab92fa1c72d190ba1d2a4fb1824
shamrickus <shamrickus@gmail.com>
2020-11-20 18:42:37 -0700

    Add dev config file

--- traffic_portal/conf/configDev.js
+++ traffic_portal/conf/configDev.js
@@ -25,21 +25,23 @@ module.exports = {
     sslPort: 443, // set to https port
     // if useSSL is true, generate ssl certs and provide the proper locations.
     ssl: {
-        key:    '/etc/pki/tls/private/localhost.key',
-        cert:   '/etc/pki/tls/certs/localhost.crt',
-        ca:     [ '/etc/pki/tls/certs/ca-bundle.crt' ]
+        key:    '../localhost.key',
+        cert:   '../localhost.cert',
+        ca:     [ '../localhost.cert' ]
     },
     // set api 'base_url' to the traffic ops api url (all api calls made from the traffic portal will be proxied to the api base_url)
     api: {
-        base_url: 'https://trafficops.CHANGEME.domain.com/api/'
+        base_url: 'https://localhost:6443/api/'
     },
     // default static files location (this is where the traffic portal html, css and javascript was installed. rpm installs these files at /opt/traffic_portal/public
+    // change this to ./app/dist/public/ if you are running locally for development
     files: {
-        static: '/opt/traffic_portal/public'
+        static: './app/dist/public'
     },
     // default log location (this is where traffic_portal logs are written)
+    // change this to ./server/log/access.log if you are running traffic portal locally for development
     log: {
-        stream: '/var/log/traffic_portal/access.log'
+        stream: './server/log/access.log'
     },
     reject_unauthorized: 0 // 0 if using self-signed certs, 1 if trusted certs
 };
