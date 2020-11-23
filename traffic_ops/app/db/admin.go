c00b5d0439a177748405e928d5fab21892e5295a
ocket8888 <ocket8888@apache.org>
2020-11-23 16:52:13 -0700

    Update admin to use sqitch

--- traffic_ops/app/db/admin.go
+++ traffic_ops/app/db/admin.go
@@ -1,3 +1,36 @@
+/*
+
+Admin automates interactions with the Traffic Ops database.
+
+Example:  admin --env=test reset
+
+Purpose:  This script is used to manage database. The environments are defined in the dbconf.yml, as well as the database names.
+
+NOTE:
+Postgres Superuser: The 'postgres' superuser needs to be created to run ` + programName + ` and setup databases.
+If the 'postgres' superuser has not been created or password has not been set then run the following commands accordingly.
+
+Create the 'postgres' user as a super user (if not created):
+
+	$ createuser postgres --superuser --createrole --createdb --login --pwprompt
+
+Modify your ~/.pgpass file which allows for easy command line access by defaulting the user and password for the database
+without prompts.
+
+Postgres .pgpass file format:
+
+	hostname:port:database:username:password
+
+Example Contents:
+
+	*:*:*:postgres:your-postgres-password
+	*:*:*:traffic_ops:the-password-in-dbconf.yml
+
+Save the following example into ~/.pgpass with the permissions of this file so only your user can read and write.
+
+	$ chmod 0600 ~/.pgpass
+
+*/
 package main
 
 /*
@@ -32,6 +65,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// DBConfig holds configuration information for all the different environments.
 type DBConfig struct {
 	Development GooseConfig `yaml:"development"`
 	Test        GooseConfig `yaml:"test"`
@@ -39,9 +73,14 @@ type DBConfig struct {
 	Production  GooseConfig `yaml:"production"`
 }
 
+// GooseConfig contains configuration information for a particular environment.
 type GooseConfig struct {
+	// Driver describes the type of database to which to connect. Only
+	// 'postgres' is supported.
 	Driver string `yaml:"driver"`
-	Open   string `yaml:"open"`
+	// Open is a string containing enough information to connect to and
+	// authenticate with the database.
+	Open string `yaml:"open"`
 }
 
 func (conf DBConfig) getGooseConfig(env string) (GooseConfig, error) {
@@ -59,21 +98,25 @@ func (conf DBConfig) getGooseConfig(env string) (GooseConfig, error) {
 	}
 }
 
+// The possible environments that may be used.
 const (
-	// the possible environments to use
 	EnvDevelopment = "development"
 	EnvTest        = "test"
 	EnvIntegration = "integration"
 	EnvProduction  = "production"
+)
 
-	// keys in the goose config's "open" string value
+// Keys in the goose config's "open" string value.
+const (
 	HostKey     = "host"
 	PortKey     = "port"
 	UserKey     = "user"
 	PasswordKey = "password"
 	DBNameKey   = "dbname"
+)
 
-	// available commands
+// Available commands.
+const (
 	CmdCreateDB      = "createdb"
 	CmdDropDB        = "dropdb"
 	CmdCreateUser    = "create_user"
@@ -90,23 +133,39 @@ const (
 	CmdLoadSchema    = "load_schema"
 	CmdReverseSchema = "reverse_schema"
 	CmdPatch         = "patch"
+)
 
-	// goose commands that don't match the commands for this tool
+// Goose commands that don't match the commands for this tool.
+const (
 	GooseUp = "up"
+)
 
-	DBConfigPath       = "db/dbconf.yml"
-	DBSeedsPath        = "db/seeds.sql"
-	DBSchemaPath       = "db/create_tables.sql"
-	DBPatchesPath      = "db/patches.sql"
+// Various filepaths - relative to {{Traffic Ops installation dir}}/app/.
+const (
+	DBConfigPath  = "dbconf.yml"
+	DBSeedsPath   = "seeds.sql"
+	DBSchemaPath  = "create_tables.sql"
+	DBPatchesPath = "patches.sql"
+)
+
+// DBPath is the path to the database directory containing configuration and
+// seeds/patches/creation scripts.
+var DBPath = "db"
+
+// Defaults for configurable values.
+const (
 	DefaultEnvironment = EnvDevelopment
 	DefaultDBSuperUser = "postgres"
 )
 
+// Globals that are passed in via CLI flags and used in commands.
 var (
-	// globals that are passed in via CLI flags and used in commands
 	Environment string
+	UseSqitch   bool
+)
 
-	// globals that are parsed out of DBConfigFile and used in commands
+// Globals that are parsed out of DBConfigFile and used in commands.
+var (
 	DBName      string
 	DBSuperUser = DefaultDBSuperUser
 	DBUser      string
@@ -116,9 +175,10 @@ var (
 )
 
 func parseDBConfig() error {
-	confBytes, err := ioutil.ReadFile(DBConfigPath)
+	configPath := strings.Join([]string{DBPath, DBConfigPath}, "/")
+	confBytes, err := ioutil.ReadFile(configPath)
 	if err != nil {
-		return errors.New("reading DB conf '" + DBConfigPath + "': " + err.Error())
+		return fmt.Errorf("reading DB conf '%s': %v", configPath, err)
 	}
 
 	dbConfig := DBConfig{}
@@ -171,6 +231,10 @@ func parseDBConfig() error {
 	return nil
 }
 
+func targetURI() string {
+	return fmt.Sprintf("db:pg://%s:%s@%s:%s/%s", DBSuperUser, DBPassword, HostIP, HostPort, DBName)
+}
+
 func createDB() {
 	dbExistsCmd := exec.Command("psql", "-h", HostIP, "-U", DBSuperUser, "-p", HostPort, "-tAc", "SELECT 1 FROM pg_database WHERE datname='"+DBName+"'")
 	out, err := dbExistsCmd.Output()
@@ -183,9 +247,10 @@ func createDB() {
 	}
 	createDBCmd := exec.Command("createdb", "-h", HostIP, "-p", HostPort, "-U", DBSuperUser, "-e", "--owner", DBUser, DBName)
 	out, err = createDBCmd.CombinedOutput()
-	fmt.Printf("%s", out)
+	fmt.Println(string(out))
 	if err != nil {
-		die("Can't create db " + DBName)
+		fmt.Fprintf(os.Stderr, "Can't create db '%s': %v\n", DBName, err)
+		os.Exit(1)
 	}
 }
 
@@ -193,9 +258,10 @@ func dropDB() {
 	fmt.Println("Dropping database: " + DBName)
 	cmd := exec.Command("dropdb", "-h", HostIP, "-p", HostPort, "-U", DBSuperUser, "-e", "--if-exists", DBName)
 	out, err := cmd.CombinedOutput()
-	fmt.Printf("%s", out)
+	fmt.Println(string(out))
 	if err != nil {
-		die("Can't drop db " + DBName)
+		fmt.Fprintf(os.Stderr, "Can't drop db '%s': %v\n", DBName, err)
+		os.Exit(1)
 	}
 }
 
@@ -212,27 +278,29 @@ func createUser() {
 	}
 	createUserCmd := exec.Command("psql", "-h", HostIP, "-p", HostPort, "-U", DBSuperUser, "-etAc", "CREATE USER "+DBUser+" WITH LOGIN ENCRYPTED PASSWORD '"+DBPassword+"'")
 	out, err = createUserCmd.CombinedOutput()
-	fmt.Printf("%s", out)
+	fmt.Println(string(out))
 	if err != nil {
-		die("Can't create user " + DBUser)
+		fmt.Fprintf(os.Stderr, "Can't create user '%s': %v\n", DBUser, err)
+		os.Exit(1)
 	}
 }
 
 func dropUser() {
 	cmd := exec.Command("dropuser", "-h", HostIP, "-p", HostPort, "-U", DBSuperUser, "-i", "-e", DBUser)
 	out, err := cmd.CombinedOutput()
-	fmt.Printf("%s", out)
+	fmt.Println(string(out))
 	if err != nil {
-		die("Can't drop user " + DBUser)
+		fmt.Fprintf(os.Stderr, "Can't drop user '%s': %v\n", DBUser, err)
+		os.Exit(1)
 	}
 }
 
 func showUsers() {
 	cmd := exec.Command("psql", "-h", HostIP, "-p", HostPort, "-U", DBSuperUser, "-ec", `\du`)
 	out, err := cmd.CombinedOutput()
-	fmt.Printf("%s", out)
+	fmt.Println(string(out))
 	if err != nil {
-		die("Can't show users")
+		fmt.Fprintf(os.Stderr, "Can't show users: %v\n", err)
 	}
 }
 
@@ -245,17 +313,29 @@ func reset() {
 }
 
 func upgrade() {
-	goose(GooseUp)
+	if UseSqitch {
+		sqitch("deploy")
+	} else {
+		goose(GooseUp)
+	}
 	seed()
 	patch()
 }
 
 func migrate() {
-	goose(GooseUp)
+	if UseSqitch {
+		sqitch("deploy")
+	} else {
+		goose(GooseUp)
+	}
 }
 
 func down() {
-	goose(CmdDown)
+	if UseSqitch {
+		sqitch("revert")
+	} else {
+		goose(CmdDown)
+	}
 }
 
 func redo() {
@@ -263,7 +343,11 @@ func redo() {
 }
 
 func status() {
-	goose(CmdStatus)
+	if UseSqitch {
+		sqitch("status")
+	} else {
+		goose(CmdStatus)
+	}
 }
 
 func dbVersion() {
@@ -272,25 +356,30 @@ func dbVersion() {
 
 func seed() {
 	fmt.Println("Seeding database w/ required data.")
-	seedsBytes, err := ioutil.ReadFile(DBSeedsPath)
+	seedsPath := strings.Join([]string{DBPath, DBSeedsPath}, "/")
+	seedsBytes, err := ioutil.ReadFile(seedsPath)
 	if err != nil {
-		die("unable to read '" + DBSeedsPath + "': " + err.Error())
+		fmt.Fprintf(os.Stderr, "unable to read '%s': %v\n", seedsPath, err)
+		os.Exit(1)
 	}
 	cmd := exec.Command("psql", "-h", HostIP, "-p", HostPort, "-d", DBName, "-U", DBUser, "-e", "-v", "ON_ERROR_STOP=1")
 	cmd.Stdin = bytes.NewBuffer(seedsBytes)
 	cmd.Env = append(os.Environ(), "PGPASSWORD="+DBPassword)
 	out, err := cmd.CombinedOutput()
-	fmt.Printf("%s", out)
+	fmt.Println(string(out))
 	if err != nil {
-		die("Can't patch database w/ required data")
+		fmt.Fprintf(os.Stderr, "Can't patch database w/ required data: %v\n", err)
+		os.Exit(1)
 	}
 }
 
 func loadSchema() {
 	fmt.Println("Creating database tables.")
-	schemaBytes, err := ioutil.ReadFile(DBSchemaPath)
+	schemaPath := strings.Join([]string{DBPath, DBSchemaPath}, "/")
+	schemaBytes, err := ioutil.ReadFile(schemaPath)
 	if err != nil {
-		die("unable to read '" + DBSchemaPath + "': " + err.Error())
+		fmt.Fprintf(os.Stderr, "unable to read '%s': %v\n", schemaPath, err)
+		os.Exit(1)
 	}
 	cmd := exec.Command("psql", "-h", HostIP, "-p", HostPort, "-d", DBName, "-U", DBUser, "-e", "-v", "ON_ERROR_STOP=1")
 	cmd.Stdin = bytes.NewBuffer(schemaBytes)
@@ -298,35 +387,46 @@ func loadSchema() {
 	out, err := cmd.CombinedOutput()
 	fmt.Printf("%s", out)
 	if err != nil {
-		die("Can't create database tables")
+		fmt.Fprintf(os.Stderr, "Can't create database tables: %v\n", err)
+		os.Exit(1)
 	}
 }
 
 func reverseSchema() {
 	fmt.Fprintf(os.Stderr, "WARNING: the '%s' command will be removed with Traffic Ops Perl because it will no longer be necessary\n", CmdReverseSchema)
-	cmd := exec.Command("db/reverse_schema.pl")
+	reversePath := strings.Join([]string{DBPath, "reverse_schema.pl"}, "/")
+	cmd := exec.Command(reversePath)
 	cmd.Env = append(os.Environ(), "MOJO_MODE="+Environment)
 	out, err := cmd.CombinedOutput()
 	fmt.Printf("%s", out)
 	if err != nil {
-		die("Can't run `db/reverse_schema.pl`: " + err.Error())
+		fmt.Fprintf(os.Stderr, "Can't run `%s`: %v\n", reversePath, err)
 	}
 }
 
 func patch() {
 	fmt.Println("Patching database with required data fixes.")
-	patchesBytes, err := ioutil.ReadFile(DBPatchesPath)
+	patchesPath := strings.Join([]string{DBPath, DBPatchesPath}, "/")
+	patchesBytes, err := ioutil.ReadFile(patchesPath)
 	if err != nil {
-		die("unable to read '" + DBPatchesPath + "': " + err.Error())
+		fmt.Fprintf(os.Stderr, "unable to read '%s': %v\n", patchesPath, err)
 	}
 	cmd := exec.Command("psql", "-h", HostIP, "-p", HostPort, "-d", DBName, "-U", DBUser, "-e", "-v", "ON_ERROR_STOP=1")
 	cmd.Stdin = bytes.NewBuffer(patchesBytes)
 	cmd.Env = append(os.Environ(), "PGPASSWORD="+DBPassword)
 	out, err := cmd.CombinedOutput()
-	fmt.Printf("%s", out)
+	fmt.Println(string(out))
 	if err != nil {
-		die("Can't patch database w/ required data")
+		fmt.Fprintf(os.Stderr, "Can't patch database w/ required data: %v\n", err)
+		os.Exit(1)
 	}
+}
+
+func verify() {
+	if !UseSqitch {
+		die("This command can only be invoked with --use-sqitch")
+	}
+	sqitch("verify")
 }
 
 func goose(arg string) {
@@ -339,105 +439,135 @@ func goose(arg string) {
 	}
 }
 
+func sqitch(arg string) {
+	fmt.Printf("Running: sqitch %s\n", arg)
+	cmd := exec.Command("sqitch", arg, targetURI())
+	out, err := cmd.CombinedOutput()
+	fmt.Println(string(out))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't run sqitch: %v", err)
+		os.Exit(1)
+	}
+}
+
 func die(message string) {
-	fmt.Println(message)
+	fmt.Fprintln(os.Stderr, message)
 	os.Exit(1)
 }
 
-func usage() string {
-	programName := os.Args[0]
-	home := "$HOME"
-	home = os.Getenv("HOME")
-	return `
-Usage:  ` + programName + ` [--env (development|test|production|integration)] [arguments]
+// Command represents a command that may be invoked. The name of the command is
+// given by its key in Commands.
+type Command struct {
+	// A function that runs the command's logic.
+	Function func()
+	// A human-readable string that describes what the command does.
+	Help string
+}
 
-Example:  ` + programName + ` --env=test reset
+// Commands contains all of the available commands that may be run.
+var Commands = map[string]Command{
+	"createdb": {
+		Function: createDB,
+		Help:     "Execute db 'createdb' the database for the current environment.",
+	},
+	"create_user": {
+		Function: createUser,
+		Help:     "Execute 'create_user' the user for the current environment (traffic_ops).",
+	},
+	"dropdb": {
+		Function: dropDB,
+		Help:     "Execute db 'dropdb' on the database for the current environment.",
+	},
+	"down": {
+		Function: down,
+		Help:     "Roll back a single migration from the current version.",
+	},
+	"drop_user": {
+		Function: dropUser,
+		Help:     "Execute 'drop_user' the user for the current environment (traffic_ops).",
+	},
+	"patch": {
+		Function: patch,
+		Help:     "Execute sql from db/patches.sql for loading post-migration data patches.",
+	},
+	"redo": {
+		Function: redo,
+		Help:     "Roll back the most recently applied migration, then run it again.",
+	},
+	"reset": {
+		Function: reset,
+		Help:     "Execute db 'dropdb', 'createdb', load_schema, migrate on the database for the current environment.",
+	},
+	"reverse_schema": {
+		Function: reverseSchema,
+		Help:     "Reverse engineer the lib/Schema/Result files from the environment database.",
+	},
+	"seed": {
+		Function: seed,
+		Help:     "Execute sql from db/seeds.sql for loading static data.",
+	},
+	"show_users": {
+		Function: showUsers,
+		Help:     "Execute sql to show all of the user for the current environment.",
+	},
+	"status": {
+		Function: status,
+		Help:     "Print the status of all migrations.",
+	},
+	"upgrade": {
+		Function: upgrade,
+		Help:     "Execute migrate, seed, and patches on the database for the current environment.",
+	},
+	"migrate": {
+		Function: migrate,
+		Help:     "Execute migrate (without seeds or patches) on the database for the current environment.",
+	},
+	"verify": {
+		Function: verify,
+		Help:     "Run database verification tests",
+	},
+}
 
-Purpose:  This script is used to manage database. The environments are
-          defined in the dbconf.yml, as well as the database names.
-
-NOTE:
-Postgres Superuser: The 'postgres' superuser needs to be created to run ` + programName + ` and setup databases.
-If the 'postgres' superuser has not been created or password has not been set then run the following commands accordingly.
-
-Create the 'postgres' user as a super user (if not created):
-
-     $ createuser postgres --superuser --createrole --createdb --login --pwprompt
-
-Modify your ` + home + `/.pgpass file which allows for easy command line access by defaulting the user and password for the database
-without prompts.
-
- Postgres .pgpass file format:
- hostname:port:database:username:password
-
- ----------------------
- Example Contents
- ----------------------
- *:*:*:postgres:your-postgres-password
- *:*:*:traffic_ops:the-password-in-dbconf.yml
- ----------------------
-
- Save the following example into this file ` + home + `/.pgpass with the permissions of this file
- so only your user can read and write.
-
-     $ chmod 0600 ` + home + `/.pgpass
-
-===================================================================================================================
-` + programName + ` arguments:
-
-createdb  - Execute db 'createdb' the database for the current environment.
-create_user  - Execute 'create_user' the user for the current environment (traffic_ops).
-dropdb  - Execute db 'dropdb' on the database for the current environment.
-down  - Roll back a single migration from the current version.
-drop_user  - Execute 'drop_user' the user for the current environment (traffic_ops).
-patch  - Execute sql from db/patches.sql for loading post-migration data patches.
-redo  - Roll back the most recently applied migration, then run it again.
-reset  - Execute db 'dropdb', 'createdb', load_schema, migrate on the database for the current environment.
-reverse_schema  - Reverse engineer the lib/Schema/Result files from the environment database.
-seed  - Execute sql from db/seeds.sql for loading static data.
-show_users  - Execute sql to show all of the user for the current environment.
-status  - Print the status of all migrations.
-upgrade  - Execute migrate, seed, and patches on the database for the current environment.
-migrate  - Execute migrate (without seeds or patches) on the database for the current environment.
-`
+func usage() {
+	fmt.Printf("Usage: %s [--env ENVIRONMENT] [--use-sqitch] COMMAND\n", os.Args[0])
+	fmt.Printf("       %s --help\n", os.Args[0])
+	flag.PrintDefaults()
+	fmt.Println("  COMMAND")
+	fmt.Println("        An operation to perform on the database. One of the following:")
+	for cmdName, cmd := range Commands {
+		fmt.Printf("          %s\n            %s\n", cmdName, cmd.Help)
+	}
 }
 
 func main() {
-	flag.StringVar(&Environment, "env", DefaultEnvironment, "The environment to use (defined in "+DBConfigPath+").")
+	flag.CommandLine.SetOutput(os.Stdout)
+	flag.StringVar(&Environment, "env", DefaultEnvironment, "The environment to use (defined in "+DBConfigPath+")")
+	flag.StringVar(&DBPath, "db-path", "db", "Path to the database directory (containing create_tables.sql)")
+	flag.BoolVar(&UseSqitch, "use-sqitch", false, "Use Sqitch instead of goose (applies only Sqitch migrations!)")
+	help := flag.Bool("help", false, "Print usage information and exit")
+	flag.Usage = usage
 	flag.Parse()
-	if len(flag.Args()) != 1 || flag.Arg(0) == "" {
-		die(usage())
+	if help != nil && *help {
+		flag.Usage()
+		return
+	}
+	if flag.NArg() != 1 || flag.Arg(0) == "" {
+		flag.Usage()
+		os.Exit(1)
 	}
 	if Environment == "" {
-		die(usage())
+		flag.Usage()
+		os.Exit(1)
 	}
 	if err := parseDBConfig(); err != nil {
 		die(err.Error())
 	}
-	commands := make(map[string]func())
-
-	commands[CmdCreateDB] = createDB
-	commands[CmdDropDB] = dropDB
-	commands[CmdCreateUser] = createUser
-	commands[CmdDropUser] = dropUser
-	commands[CmdShowUsers] = showUsers
-	commands[CmdReset] = reset
-	commands[CmdUpgrade] = upgrade
-	commands[CmdMigrate] = migrate
-	commands[CmdDown] = down
-	commands[CmdRedo] = redo
-	commands[CmdStatus] = status
-	commands[CmdDBVersion] = dbVersion
-	commands[CmdSeed] = seed
-	commands[CmdLoadSchema] = loadSchema
-	commands[CmdReverseSchema] = reverseSchema
-	commands[CmdPatch] = patch
 
 	userCmd := flag.Arg(0)
-	if cmd, ok := commands[userCmd]; ok {
-		cmd()
+	if cmd, ok := Commands[userCmd]; ok {
+		cmd.Function()
 	} else {
-		fmt.Println(usage())
+		flag.Usage()
 		die("invalid command: " + userCmd)
 	}
 }
