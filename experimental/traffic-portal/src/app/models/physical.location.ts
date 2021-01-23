9452816dc72404050803f60e4cd2471be3eb7bcf
ocket8888 <ocket8888@apache.org>
2021-01-23 02:25:19 -0700

    Add services for Types and Physical Locations

--- experimental/traffic-portal/src/app/models/physical.location.ts
+++ experimental/traffic-portal/src/app/models/physical.location.ts
@@ -12,11 +12,19 @@
 * limitations under the License.
 */
 
-export * from "./CacheGroupService";
-export * from "./CDNService";
-export * from "./DeliveryServiceService";
-export * from "./InvalidationJobService";
-export * from "./ProfileService";
-export * from "./ServerService";
-export * from "./TypeService";
-export * from "./UserService";
+export interface PhysicalLocation {
+	address: string;
+	city: string;
+	comments: string | null;
+	email: string | null;
+	id?: number;
+	lastUpdated?: Date;
+	name: string;
+	phone: string | null;
+	poc: string | null;
+	region: string | null;
+	regionId: number;
+	shortName: string;
+	state: string;
+	zip: string;
+}
