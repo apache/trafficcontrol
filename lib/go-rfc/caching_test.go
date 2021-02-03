3dacbedc3e11bf0b3358cc56bdcdd44606215204
ocket8888 <ocket8888@apache.org>
2021-02-03 12:20:24 -0500

    Code QL workflow (#5459)
    * Add CodeQL workflow
    
    * remove Java from the runner
    
    * Add cron line comment
    
    * Convert full workflow to schedule/push only
    
    * Add JS codeql workflow
    
    * Add trivial comment change to Javascript to demonstrate workflow
    
    * fix duplicate workflow name
    
    * Add Go CodeQL workflow
    
    * Fix non-niladic example test
    
    * Add Python CodeQL workflow
    
    * Add trivial docstring change to Python to demonstrate workflow
    
    * Fix flaky test
    
    * Get rid of 'push' workflow, add 'push' directive to each individual language

--- lib/go-rfc/caching_test.go
+++ lib/go-rfc/caching_test.go
@@ -19,18 +19,29 @@ package rfc
  * under the License.
  */
 
-import "fmt"
-import "net/http"
-import "testing"
-import "time"
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
 
-func ExampleParseCacheControl(t *testing.T) {
+func ExampleParseCacheControl() {
 	hdrs := http.Header{}
 
 	hdrs.Set(CacheControl, "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
-	fmt.Println(ParseCacheControl(hdrs))
+	cchm := ParseCacheControl(hdrs)
+	fmt.Println(cchm.Has("no-store"))
+	fmt.Println(cchm.Has("no-cache"))
+	fmt.Println(cchm.Has("must-revalidate"))
+	fmt.Println(cchm["post-check"])
+	fmt.Println(cchm["pre-check"])
 
-	// Output: Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0
+	// Output: true
+	// true
+	// true
+	// 0
+	// 0
 }
 
 func ExampleCacheControlMap_Has() {
