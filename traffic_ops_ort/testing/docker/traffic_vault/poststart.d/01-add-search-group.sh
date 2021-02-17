01aa2b9d298356218b5c99067eadd78797f8f946
John J. Rushford <jrushford@apache.org>
2021-02-17 16:28:18 -0700

    Updates ORT integration test traffic_vault container to improve automations (#5520)
    for CI.

--- traffic_ops_ort/testing/docker/traffic_vault/poststart.d/01-add-search-group.sh
+++ traffic_ops_ort/testing/docker/traffic_vault/poststart.d/01-add-search-group.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -6,9 +5,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -16,24 +15,13 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# Script for running the Dockerfile for Traffic Vault.
-# The Dockerfile sets up a Docker image which can be used for any new container;
-# This script, which should be run when the container is run (it's the ENTRYPOINT), will configure the container.
-#
-# The following environment variables must be set (ordinarily by `docker run -e` arguments):
-# TV_ADMIN_PASS
-# RIAK_USER_PASS
-# CERT_COUNTRY
-# CERT_STATE
-# CERT_CITY
-# CERT_COMPANY
-# TO_URI
-# TO_USER
-# TO_PASS
-# TV_DOMAIN
-# IP
-# GATEWAY
-# CREATE_TO_DB_ENTRY (If set to yes, create the TO db entry for this server if set to no, assume it it already there)
+# The following environment variables are required, see 'riak-cluster.sh'
+# RIAK_ADMIN
 
-/usr/lib/riak/riak-cluster.sh
-
+$RIAK_ADMIN security grant search.admin on schema to admin
+$RIAK_ADMIN security grant search.admin on index to admin
+$RIAK_ADMIN security grant search.query on index to admin
+$RIAK_ADMIN security grant search.query on index sslkeys to admin
+$RIAK_ADMIN security grant search.query on index to riakuser
+$RIAK_ADMIN security grant search.query on index sslkeys to riakuser
+$RIAK_ADMIN security grant riak_core.set_bucket on any to admin
