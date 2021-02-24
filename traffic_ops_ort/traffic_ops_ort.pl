a1a80e4cfa968eccfb25ae59b1659b93ad05f4e0
jpappa200 <joseph_pappano@cable.comcast.com>
2021-02-24 16:37:35 -0500

    line 1337 removed exclusion for .lua files. so remap.config will be touched if lua files are changed.

--- traffic_ops_ort/traffic_ops_ort.pl
+++ traffic_ops_ort/traffic_ops_ort.pl
@@ -1334,7 +1334,7 @@ sub check_plugins {
 						( my @parts ) = split( /\//, $plugin_config_file );
 						$plugin_config_file = $parts[$#parts];
 						$plugin_config_file =~ s/\s+//g;
-						if ( !exists($cfg_file_tracker->{$plugin_config_file}->{'remap_plugin_config_file'} ) && $plugin_config_file !~ /.lua$/ ) {
+						if ( !exists($cfg_file_tracker->{$plugin_config_file}->{'remap_plugin_config_file'} )) {
 							$cfg_file_tracker->{$plugin_config_file}->{'remap_plugin_config_file'} = 1;
 						}
 					}
