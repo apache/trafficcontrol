3712949d0355ce03c50dfcd3a80ea54332ae8b5b
Zach Hoffman <zrhoffman@apache.org>
2021-03-30 21:57:09 -0600

    Log to stdout, not stderr

--- traffic_ops/install/bin/postinstall.test.sh
+++ traffic_ops/install/bin/postinstall.test.sh
@@ -366,12 +366,11 @@ cat <<- EOF > "$ROOT_DIR/defaults.json"
 }
 EOF
 
-"$python_bin" "$MY_DIR/_postinstall" --no-root --root-directory="$ROOT_DIR" --no-restart-to --no-database --ops-user="$(whoami)" --ops-group="$(id -gn)" --automatic --cfile="$ROOT_DIR/defaults.json" --debug 2>>"$ROOT_DIR/stderr" | tee -a "$ROOT_DIR/stdout"
+"$python_bin" "$MY_DIR/_postinstall" --no-root --root-directory="$ROOT_DIR" --no-restart-to --no-database --ops-user="$(whoami)" --ops-group="$(id -gn)" --automatic --cfile="$ROOT_DIR/defaults.json" --debug > >(tee -a "$ROOT_DIR/stdout") 2> >(tee -a "$ROOT_DIR/stderr" >&2);
 
-if grep -q 'ERROR' $ROOT_DIR/stderr; then
+if grep -q 'ERROR' $ROOT_DIR/stdout; then
 	echo "Errors found in script logs" >&2;
-	cat "$ROOT_DIR/stderr";
-	cat "$ROOT_DIR/stdout";
+	cat "$ROOT_DIR/stdout" "$ROOT_DIR/stderr";
 	exit 1;
 fi
 
