d518014faf95c224458d045675db1c8fb2d6fe5d
Alejandra Bricio <alejandra.bricio@gmail.com>
2021-04-28 15:10:08 -0500

    Fix struct field tag issues

--- traffic_monitor/health/event.go
+++ traffic_monitor/health/event.go
@@ -42,8 +42,8 @@ type Event struct {
 	Hostname      string `json:"hostname"`
 	Type          string `json:"type"`
 	Available     bool   `json:"isAvailable"`
-	IPv4Available bool   `json:"isAvailable"`
-	IPv6Available bool   `json:"isAvailable"`
+	IPv4Available bool   `json:"isIPv4Available"`
+	IPv6Available bool   `json:"isIPv6Available"`
 }
 
 // Events provides safe access for multiple goroutines readers and a single writer to a stored Events slice.
