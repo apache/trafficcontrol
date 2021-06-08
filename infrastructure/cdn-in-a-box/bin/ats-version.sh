a2a18f830b67bc9f8c684c71105272bd09644239
Zach Hoffman <zrhoffman@apache.org>
2021-06-08 09:38:26 -0600

    Consistent whitespace

--- infrastructure/cdn-in-a-box/bin/ats-version.sh
+++ infrastructure/cdn-in-a-box/bin/ats-version.sh
@@ -24,29 +24,29 @@ script_dir="$(dirname "$0")"
 ats_version_file="${script_dir}/../cache/ATS_VERSION"
 
 remote_ats_version() {
-  local gitbox_url=https://gitbox.apache.org/repos/asf
-  local repo="${project}.git"
+	local gitbox_url=https://gitbox.apache.org/repos/asf
+	local repo="${project}.git"
 	local branch refs commit last_tag release
-  branch="$(grep 'ATS_VERSION=' "${script_dir}/../../../cache-config/testing/docker/docker-compose-ats-build.yml" | cut -d= -f2)"
-  refs="$(curl -fs "${gitbox_url}/${repo}/info/refs?service=git-upload-pack" |
+	branch="$(grep 'ATS_VERSION=' "${script_dir}/../../../cache-config/testing/docker/docker-compose-ats-build.yml" | cut -d= -f2)"
+	refs="$(curl -fs "${gitbox_url}/${repo}/info/refs?service=git-upload-pack" |
 		sed -E 's/^00[0-9a-f]{2}//g' |
 		tr -d '\0')"
-  commit="$(<<<"$refs" grep "refs/heads/${branch}$" | awk '{print $1}')"
+	commit="$(<<<"$refs" grep "refs/heads/${branch}$" | awk '{print $1}')"
 
-  # $last_tag is the latest tag before the commit at the head of $branch.
-  last_tag="$(<<<"$refs" grep -oE 'refs/tags/[0-9.]+$' |
-    cut -d/ -f3 |
-    grep -F "${branch::-1}" |
-    tail -n1)"
+	# $last_tag is the latest tag before the commit at the head of $branch.
+	last_tag="$(<<<"$refs" grep -oE 'refs/tags/[0-9.]+$' |
+		cut -d/ -f3 |
+		grep -F "${branch::-1}" |
+		tail -n1)"
 
-  # $release is the number of commits between $release to $branch.
-  release="$(curl -fs "${gitbox_url}?p=${repo};a=shortlog;h=${branch};hp=${last_tag}" | grep -c 'class="link"')"
-  <<<"${last_tag}-${release}.${commit:0:9}" tee "$ats_version_file"
+	# $release is the number of commits between $release to $branch.
+	release="$(curl -fs "${gitbox_url}?p=${repo};a=shortlog;h=${branch};hp=${last_tag}" | grep -c 'class="link"')"
+	<<<"${last_tag}-${release}.${commit:0:9}" tee "$ats_version_file"
 }
 
 # Reuse the ATS version file if it was generated within the last day
 if [[ -n "$(find "$ats_version_file" -mtime -1 2>/dev/null)" ]]; then
-  cat "$ats_version_file"
+	cat "$ats_version_file"
 else
-  remote_ats_version
+	remote_ats_version
 fi
