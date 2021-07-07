d5596cc7f5416a522fef5a10c4553ce67e064d04
Rima Shah <Rima_Shah@comcast.com>
2021-07-06 20:44:15 -0600

    updated label and heading for Change Log Count

--- traffic_portal/app/src/common/modules/table/users/TableUsersController.js
+++ traffic_portal/app/src/common/modules/table/users/TableUsersController.js
@@ -30,7 +30,7 @@ var TableUsersController = function(users, $scope, $state, locationUtils) {
         { "name": "Tenant", "visible": true, "searchable": true },
         { "name": "Role", "visible": true, "searchable": true },
         { "name": "Registration Sent", "visible": false, "searchable": true },
-        { "name": "ChangeLog Count", "visible": false, "searchable": false }
+        { "name": "Change Log Count", "visible": false, "searchable": false }
     ];
 
     $scope.editUser = function(id) {
