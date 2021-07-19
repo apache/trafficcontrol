d6e5872bcda02e019b0efd15f898ba2af977018f
ocket8888 <ocket8888@apache.org>
2021-07-19 13:34:27 -0600

    Add package documentation for lib/go-tc/tovalidate/

---
+++ lib/go-tc/tovalidate/doc.go
@@ -0,0 +1,19 @@
+// Package tovalidate contains helpful utility functions and
+// ozzo-validator-compatible rules and rule generators for validating data in
+// the Traffic Ops API.
+//
+// Actually, the functionality shouldn't be specific to Traffic Ops (usually),
+// but that is the only place that uses it at the time of this writing.
+package tovalidate
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
