48b9abe603dadebc98d623eb2e80c20a011302f1
ocket8888 <ocket8888@apache.org>
2021-07-20 11:22:54 -0600

    Add/Update/Correct GoDoc comments on sting/error-joining-related utilities

--- lib/go-util/join.go
+++ lib/go-util/join.go
@@ -25,6 +25,11 @@ import (
 	"strings"
 )
 
+// JoinErrsStr returns a string representation of all of the passed errors.
+//
+// This is equivalent to calling JoinErrs(errs).Error(), but in the case that
+// JoinErrs returns nil that would panic. This checks for that case and instead
+// returns an empty string.
 func JoinErrsStr(errs []error) string {
 	joined := JoinErrs(errs)
 
@@ -35,6 +40,8 @@ func JoinErrsStr(errs []error) string {
 	return joined.Error()
 }
 
+// ErrsToStrs converts a slice of errors to a slice of their string
+// representations.
 func ErrsToStrs(errs []error) []string {
 	errorStrs := []string{}
 	for _, errType := range errs {
@@ -44,10 +51,24 @@ func ErrsToStrs(errs []error) []string {
 	return errorStrs
 }
 
+// JoinErrs joins the passed errors into a single error with a message that is
+// a combination of their error messages.
+//
+// This is equivalent to calling JoinErrsSep(errs, "").
 func JoinErrs(errs []error) error {
 	return JoinErrsSep(errs, "")
 }
 
+// JoinErrsSep joins the passed errors into a single error with a message that
+// is a combination of their error messages, joined by the given separator.
+//
+// If the given separator is an empty string, the default (", ") is used.
+//
+// Note that this DOES NOT preserve error identity. For example:
+//
+//     err := JoinErrsSep([]error{sql.ErrNoRows, errors.New("foo")})
+//     fmt.Println(errors.Is(err, sql.ErrNoRows))
+//     // Output: false
 func JoinErrsSep(errs []error, separator string) error {
 	if separator == "" {
 		separator = ", "
@@ -70,6 +91,13 @@ func JoinErrsSep(errs []error, separator string) error {
 	return fmt.Errorf("%s", joinedErrors)
 }
 
+// CamelToSnakeCase returns a case transformation of the input string from
+// assumed "camelCase" (or "PascalCase") to "snake_case".
+//
+// Note that the transformation applied to strings that contain non-"word"
+// characters is undefined. Also, this doesn't handle names that contain
+// abbreviations, initialisms, and/or acronyms very well in general, like e.g.
+// "IPAddress".
 func CamelToSnakeCase(s string) string {
 	return strings.ToLower(regexp.MustCompile("([a-z0-9])([A-Z])").ReplaceAllString(s, "${1}_${2}"))
 }
