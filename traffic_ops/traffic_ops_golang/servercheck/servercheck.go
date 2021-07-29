5008892056ef7d7689bcd75fdad89210262dd5f3
Steve Malenfant <steve.malenfant@cox.com>
2021-07-29 08:00:19 -0400

    Removing not used go-log

--- traffic_ops/traffic_ops_golang/servercheck/servercheck.go
+++ traffic_ops/traffic_ops_golang/servercheck/servercheck.go
@@ -27,7 +27,6 @@ import (
 
 	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/dbhelpers"
 
-	"github.com/apache/trafficcontrol/lib/go-log"
 	"github.com/apache/trafficcontrol/lib/go-tc"
 	"github.com/apache/trafficcontrol/lib/go-util"
 
