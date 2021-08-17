1ea43195e7551a0d2015f7374fe96ecc27589b9d
Zach Hoffman <zrhoffman@apache.org>
2021-08-16 21:05:07 -0600

    Use commit hashes

--- .github/actions/build-rpms/build-rpms.sh
+++ .github/actions/build-rpms/build-rpms.sh
@@ -23,7 +23,7 @@ export DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1
 
 pkg_command=(./pkg -v)
 if [[ -n "$GITHUB_BASE_REF" ]]; then
-	ref="$(git merge-base "${GITHUB_BASE_REF}" "${GITHUB_HEAD_REF}")...${GITHUB_HEAD_REF}"
+	ref="$(git merge-base "$INPUT_BASE_COMMIT" "$GITHUB_SHA")...${GITHUB_SHA}"
 else
 	ref="$GITHUB_SHA"
 fi
