ab74c4b7ee7c3efbd57a280aab02a080b368f371
ocket8888 <ocket8888@apache.org>
2021-08-23 07:34:05 -0600

    Fix compilation errors in the API client

--- traffic_portal/test/integration/CommonUtils/API.ts
+++ traffic_portal/test/integration/CommonUtils/API.ts
@@ -102,7 +102,7 @@ export class API {
     }
 
     /** The randomization string used to generate testing API data. */
-    protected readonly randomize;
+    protected readonly randomize: string;
 
     /**
      * @param cfg The testing configuration.
@@ -375,7 +375,7 @@ export class API {
             return null;
         }
         for (const request of data.getRequest) {
-            const query = `?${encodeURIComponent(request.queryKey)}=${encodeURIComponent(request.queryValue)}${randomize}`;
+            const query = `?${encodeURIComponent(request.queryKey)}=${encodeURIComponent(request.queryValue)}${this.randomize}`;
             const response = await this.getResponse("get", request.route + query)
 
             if (response.status == 200) {
