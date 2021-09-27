2884f9d2af853c174f884a093620b8dee9543737
Devanand Mohan <devanand_mohan@comcast.com>
2021-09-27 21:59:58 +0530

    removed default values in logs only for V4

--- traffic_ops/traffic_ops_golang/logs/log.go
+++ traffic_ops/traffic_ops_golang/logs/log.go
@@ -26,6 +26,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/apache/trafficcontrol/lib/go-tc"
@@ -36,12 +37,49 @@ import (
 
 // These are the default values for query string parameters if not provided.
 const (
+	// For the 'limit' parameter.
+	DefaultLogLimit = 1000
+	// For the 'limit' parameter when 'days' is given and 'limit' is not.
+	DefaultLogLimitForDays = 1000000
 	// For the 'days' parameter.
 	DefaultLogDays = 30
 )
 
-// Get is the handler for GET requests to /logs.
 func Get(w http.ResponseWriter, r *http.Request) {
+	inf, userErr, sysErr, errCode := api.NewInfo(r, nil, []string{"days", "limit"})
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, errCode, userErr, sysErr)
+		return
+	}
+	defer inf.Close()
+
+	limit := DefaultLogLimit
+	days := DefaultLogDays
+	if pDays, ok := inf.IntParams["days"]; ok {
+		days = pDays
+		limit = DefaultLogLimitForDays
+	}
+	if pLimit, ok := inf.IntParams["limit"]; ok {
+		limit = pLimit
+	}
+
+	a := tc.Alerts{}
+	setLastSeenCookie(w)
+	logs, count, err := getLog(inf, days, limit)
+	if err != nil {
+		a.AddNewAlert(tc.ErrorLevel, err.Error())
+		api.WriteAlerts(w, r, http.StatusInternalServerError, a)
+		return
+	}
+	if a.HasAlerts() {
+		api.WriteAlertsObj(w, r, 200, a, logs)
+	} else {
+		api.WriteRespWithSummary(w, r, logs, count)
+	}
+}
+
+// Get is the handler for GET requests to /logs V4.0.
+func Getv40(w http.ResponseWriter, r *http.Request) {
 	inf, userErr, sysErr, errCode := api.NewInfo(r, nil, []string{"days", "limit"})
 	if userErr != nil || sysErr != nil {
 		api.HandleErr(w, r, inf.Tx.Tx, errCode, userErr, sysErr)
@@ -55,7 +93,7 @@ func Get(w http.ResponseWriter, r *http.Request) {
 
 	a := tc.Alerts{}
 	setLastSeenCookie(w)
-	logs, count, err := getLog(inf, days)
+	logs, count, err := getLogV40(inf, days)
 
 	if err != nil {
 		a.AddNewAlert(tc.ErrorLevel, err.Error())
@@ -125,7 +163,7 @@ FROM "log" as l JOIN tm_user as u ON l.tm_user = u.id`
 
 const countQuery = `SELECT count(l.tm_user) FROM log as l`
 
-func getLog(inf *api.APIInfo, days int) ([]tc.Log, uint64, error) {
+func getLogV40(inf *api.APIInfo, days int) ([]tc.Log, uint64, error) {
 	var count = uint64(0)
 	var whereCount string
 
@@ -147,6 +185,62 @@ func getLog(inf *api.APIInfo, days int) ([]tc.Log, uint64, error) {
 		whereCount = where
 		where = "\nWHERE " + timeInterval
 	}
+	queryCount := countQuery + whereCount
+	rowCount, err := inf.Tx.NamedQuery(queryCount, queryValues)
+	if err != nil {
+		return nil, count, errors.New("querying log count for a given user: " + err.Error())
+	}
+	defer rowCount.Close()
+	for rowCount.Next() {
+		if err = rowCount.Scan(&count); err != nil {
+			return nil, count, errors.New("scanning logs: " + err.Error())
+		}
+	}
+
+	query := selectFromQuery + where + "\n ORDER BY last_updated DESC" + pagination
+	rows, err := inf.Tx.NamedQuery(query, queryValues)
+	if err != nil {
+		return nil, count, errors.New("querying logs: " + err.Error())
+	}
+	defer rows.Close()
+	ls := []tc.Log{}
+	for rows.Next() {
+		l := tc.Log{}
+		if err = rows.Scan(&l.ID, &l.Level, &l.Message, &l.User, &l.TicketNum, &l.LastUpdated); err != nil {
+			return nil, count, errors.New("scanning logs: " + err.Error())
+		}
+		ls = append(ls, l)
+	}
+	return ls, count, nil
+}
+
+func getLog(inf *api.APIInfo, days int, limit int) ([]tc.Log, uint64, error) {
+	var count = uint64(0)
+	var whereCount string
+	if _, ok := inf.Params["limit"]; !ok {
+		inf.Params["limit"] = strconv.Itoa(DefaultLogLimit)
+	} else {
+		inf.Params["limit"] = strconv.Itoa(limit)
+	}
+
+	queryParamsToQueryCols := map[string]dbhelpers.WhereColumnInfo{
+		"username": {Column: "u.username", Checker: nil},
+	}
+	where, _, pagination, queryValues, errs :=
+		dbhelpers.BuildWhereAndOrderByAndPagination(inf.Params, queryParamsToQueryCols)
+	if len(errs) > 0 {
+		return nil, 0, util.JoinErrs(errs)
+	}
+
+	timeInterval := fmt.Sprintf("l.last_updated > now() - INTERVAL '%v' DAY", days)
+	if where != "" {
+		whereCount = ", tm_user as u\n" + where + " AND l.tm_user = u.id"
+		where = where + " AND " + timeInterval
+	} else {
+		whereCount = where
+		where = "\nWHERE " + timeInterval
+	}
+
 	queryCount := countQuery + whereCount
 	rowCount, err := inf.Tx.NamedQuery(queryCount, queryValues)
 	if err != nil {
