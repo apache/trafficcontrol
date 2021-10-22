54675a9eae8327c4e15478a4a2419b34b54e0074
Srijeet Chatterjee <srijeet0406@gmail.com>
2021-10-22 17:28:51 -0600

    test

--- traffic_ops/app/db/migrations/202110191500000000_add_permissions.up.sql
+++ traffic_ops/app/db/migrations/202110191500000000_add_permissions.up.sql
@@ -15,7 +15,7 @@
  * the License.
  */
 
--- add permissions to roles
+-- add permissions to roles in order of decreasing priv level
 INSERT INTO role_capability SELECT id, perm FROM public.role CROSS JOIN (VALUES
 ('ASN:CREATE'),
 ('ASN:DELETE'),
