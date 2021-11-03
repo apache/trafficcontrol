1244b5776ab3170f765c2c947730004b9900fed4
Zach Hoffman <zrhoffman@apache.org>
2021-11-03 17:30:59 -0600

    Cache ATS RPMs by latest ATS branch tag and latest branch commit

--- .github/actions/fetch-github-branch-sha/entrypoint.sh
+++ .github/actions/fetch-github-branch-sha/entrypoint.sh
@@ -43,5 +43,16 @@ fi
 
 echo "::set-output name=sha::${_sha}"
 
-exit 0
+branch_prefix_pattern="^$(<<<"$INPUT_BRANCH" grep -o '.*\.' | sed 's/\./\\./g')[0-9.]+$"
 
+if ! tags_info="$(curl --silent "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPO}/tags")"; then
+	echo "Error: failed to fetch tag info ${INPUT_BRANCH}"
+	exit 2
+fi
+
+latest_tag="$(<<<"$tags_info" jq -r --arg BRANCH_PREFIX_PATTERN "$branch_prefix_pattern" '.[] | .name | select(test($BRANCH_PREFIX_PATTERN))' |
+	head -n1)"
+
+echo "::set-output name=latest-tag::${latest_tag}"
+
+exit 0
