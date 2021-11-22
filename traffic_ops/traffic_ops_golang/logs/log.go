60c3da08326bdd06545d19e04b871fc03d0ed2fe
ocket8888 <ocket8888@apache.org>
2021-11-21 19:28:05 -0700

    Update logs handling for v4

--- traffic_ops/traffic_ops_golang/logs/log.go
+++ traffic_ops/traffic_ops_golang/logs/log.go
@@ -152,17 +152,17 @@ func getLastSeenCookie(r *http.Request) (time.Time, bool) {
 }
 
 const selectFromQuery = `
-SELECT l.id, l.level, l.message, u.username as user, l.ticketnum, l.last_updated
-FROM "log" as l JOIN tm_user as u ON l.tm_user = u.id`
+SELECT l.message, u.username as user, l.date
+FROM "log" as l JOIN tm_user as u ON l.user = u.id`
 
-const countQuery = `SELECT count(l.tm_user) FROM log as l`
+const countQuery = `SELECT count(l.user) FROM log as l`
 
-func getLogV40(inf *api.APIInfo, days int) ([]tc.Log, uint64, error) {
+func getLogV40(inf *api.APIInfo, days int) ([]tc.LogV4, uint64, error) {
 	var count = uint64(0)
 	var whereCount string
 
 	queryParamsToQueryCols := map[string]dbhelpers.WhereColumnInfo{
-		"username": {Column: "u.username", Checker: nil},
+		"user": {Column: "u.username", Checker: nil},
 	}
 	where, _, pagination, queryValues, errs :=
 		dbhelpers.BuildWhereAndOrderByAndPagination(inf.Params, queryParamsToQueryCols)
@@ -171,13 +171,15 @@ func getLogV40(inf *api.APIInfo, days int) ([]tc.Log, uint64, error) {
 		return nil, 0, util.JoinErrs(errs)
 	}
 
-	timeInterval := fmt.Sprintf("l.last_updated > now() - INTERVAL '%d' DAY", days)
-	if where != "" {
-		whereCount = ", tm_user as u\n" + where + " AND l.tm_user = u.id"
-		where = where + " AND " + timeInterval
-	} else {
-		whereCount = where
-		where = "\nWHERE " + timeInterval
+	if days > 0 {
+		timeInterval := fmt.Sprintf("l.date > now() - INTERVAL '%d' DAY", days)
+		if where != "" {
+			whereCount = ", tm_user as u\n" + where + " AND l.user = u.id"
+			where = where + " AND " + timeInterval
+		} else {
+			whereCount = where
+			where = "\nWHERE " + timeInterval
+		}
 	}
 	queryCount := countQuery + whereCount
 	rowCount, err := inf.Tx.NamedQuery(queryCount, queryValues)
@@ -191,16 +193,16 @@ func getLogV40(inf *api.APIInfo, days int) ([]tc.Log, uint64, error) {
 		}
 	}
 
-	query := selectFromQuery + where + "\n ORDER BY last_updated DESC" + pagination
+	query := selectFromQuery + where + "\n ORDER BY date DESC" + pagination
 	rows, err := inf.Tx.NamedQuery(query, queryValues)
 	if err != nil {
 		return nil, count, fmt.Errorf("querying logs: %w", err)
 	}
 	defer rows.Close()
-	ls := []tc.Log{}
+	ls := []tc.LogV4{}
 	for rows.Next() {
-		l := tc.Log{}
-		if err = rows.Scan(&l.ID, &l.Level, &l.Message, &l.User, &l.TicketNum, &l.LastUpdated); err != nil {
+		var l tc.LogV4
+		if err = rows.Scan(&l.Message, &l.User, &l.Date); err != nil {
 			return nil, count, fmt.Errorf("scanning logs: %w", err)
 		}
 		ls = append(ls, l)
@@ -228,9 +230,9 @@ func getLog(inf *api.APIInfo, days int, limit int) ([]tc.Log, uint64, error) {
 		return nil, 0, util.JoinErrs(errs)
 	}
 
-	timeInterval := fmt.Sprintf("l.last_updated > now() - INTERVAL '%v' DAY", days)
+	timeInterval := fmt.Sprintf("l.date > now() - INTERVAL '%v' DAY", days)
 	if where != "" {
-		whereCount = ", tm_user as u\n" + where + " AND l.tm_user = u.id"
+		whereCount = ", tm_user as u\n" + where + " AND l.user = u.id"
 		where = where + " AND " + timeInterval
 	} else {
 		whereCount = where
@@ -249,19 +251,23 @@ func getLog(inf *api.APIInfo, days int, limit int) ([]tc.Log, uint64, error) {
 		}
 	}
 
-	query := selectFromQuery + where + "\n ORDER BY last_updated DESC" + pagination
+	query := selectFromQuery + where + "\n ORDER BY date DESC" + pagination
 	rows, err := inf.Tx.NamedQuery(query, queryValues)
 	if err != nil {
 		return nil, count, errors.New("querying logs: " + err.Error())
 	}
 	defer rows.Close()
 	ls := []tc.Log{}
+	i := 0
 	for rows.Next() {
 		l := tc.Log{}
-		if err = rows.Scan(&l.ID, &l.Level, &l.Message, &l.User, &l.TicketNum, &l.LastUpdated); err != nil {
+		if err = rows.Scan(&l.Message, &l.User, &l.LastUpdated); err != nil {
 			return nil, count, errors.New("scanning logs: " + err.Error())
 		}
+		l.ID = util.IntPtr(i)
+		l.Level = util.StrPtr(api.ApiChange)
 		ls = append(ls, l)
+		i++
 	}
 	return ls, count, nil
 }
