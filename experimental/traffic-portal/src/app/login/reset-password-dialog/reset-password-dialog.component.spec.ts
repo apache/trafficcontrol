4ac965a8c2e108eda5a6d4a87773c788c3c57f94
ocket8888 <ocket8888@apache.org>
2022-01-07 12:56:14 -0700

    Fix tests

--- experimental/traffic-portal/src/app/login/reset-password-dialog/reset-password-dialog.component.spec.ts
+++ experimental/traffic-portal/src/app/login/reset-password-dialog/reset-password-dialog.component.spec.ts
@@ -13,6 +13,9 @@
 */
 import { HttpClientModule } from "@angular/common/http";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatDialogRef } from "@angular/material/dialog";
+
+import { UserService } from "src/app/shared/api/UserService";
 
 import { ResetPasswordDialogComponent } from "./reset-password-dialog.component";
 
@@ -21,9 +24,18 @@ describe("ResetPasswordDialogComponent", () => {
 	let fixture: ComponentFixture<ResetPasswordDialogComponent>;
 
 	beforeEach(async () => {
+		const mockCurrentUserService = jasmine.createSpyObj(["updateCurrentUser", "login", "logout"]);
 		await TestBed.configureTestingModule({
 			declarations: [ ResetPasswordDialogComponent ],
-			imports: [HttpClientModule]
+			imports: [HttpClientModule],
+			providers: [
+				// The controller doesn't pass any arguments or check the return
+				// value of `close` - this literally needs to do nothing but be
+				// callable.
+				// eslint-disable-next-line @typescript-eslint/no-empty-function
+				{ provide: MatDialogRef, useValue: {close: (): void => {}} },
+				{ provide: UserService, useValue: mockCurrentUserService }
+			]
 		})
 			.compileComponents();
 	});
