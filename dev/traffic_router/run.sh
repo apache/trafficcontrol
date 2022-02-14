942b7f82d764fa2c97a93e239d5a57cd5d8b9aa5
ocket8888 <ocket8888@apache.org>
2022-02-14 10:12:59 -0700

    Fix creating a ton of things owned by root in the TR container

--- dev/traffic_router/run.sh
+++ dev/traffic_router/run.sh
@@ -23,6 +23,7 @@ cd "$TC/traffic_router"
 mvn -Dmaven.test.skip=true compile -P \!rpm-build
 mvn -Dmaven.test.skip=true package -P \!rpm-build
 
+chmod -R a+rw "$TC/dev/traffic_router/"
 /opt/tomcat/bin/catalina.sh jpda run
 # java -agentlib:jdwp=transport=dt_socket,address=5005,server=y,suspend=n StartTrafficRouter
 
