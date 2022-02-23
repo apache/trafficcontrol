19bf1d9ab02b4fb22d7dfbd307387022f970c4f4
ocket8888 <ocket8888@apache.org>
2022-02-23 13:09:02 -0700

    remove unused code

--- dev/traffic_router/run.sh
+++ dev/traffic_router/run.sh
@@ -24,15 +24,3 @@ mvn -Dmaven.test.skip=true compile package -P \!rpm-build
 
 chmod -R a+rw "$TC/dev/traffic_router/"
 /opt/tomcat/bin/catalina.sh jpda run
-# java -agentlib:jdwp=transport=dt_socket,address=5005,server=y,suspend=n StartTrafficRouter
-
-# while inotifywait --exclude '.*(\.md|_test\.go|\.gitignore|__debug_bin)$' -e modify -r . ; do
-# 	kill "$(netstat -nlp | grep ':443' | grep __debug_bin | head -n1 | tr -s ' ' | cut -d ' ' -f7 | cut -d '/' -f1)"
-# 	kill "$(netstat -nlp | grep ':6444' | grep dlv | head -n1 | tr -s ' ' | cut -d ' ' -f7 | cut -d '/' -f1)"
-# 	dlv --accept-multiclient --continue --listen=:6444 --headless --api-version=2 debug -- --cfg=../../dev/traffic_ops/cdn.json --dbcfg=../../dev/traffic_ops/db.config.json &
-# 	# for whatever reason, without this the repeated call to inotifywait will
-# 	# sometimes lose track of th current directory. It spits out:
-# 	# Couldn't watch .: No such file or directory
-# 	# which is a bit odd.
-# 	sleep 0.5
-# done;
