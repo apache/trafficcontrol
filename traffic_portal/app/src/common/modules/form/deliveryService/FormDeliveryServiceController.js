425fdcf264f9f922ad5d7c6d65e746677462e3c2
shamrickus <shamrickus@gmail.com>
2022-03-07 15:44:50 -0700

    Remove superfluous log

--- traffic_portal/app/src/common/modules/form/deliveryService/FormDeliveryServiceController.js
+++ traffic_portal/app/src/common/modules/form/deliveryService/FormDeliveryServiceController.js
@@ -142,7 +142,6 @@ var FormDeliveryServiceController = function(deliveryService, dsCurrent, origin,
                     .then(function(tenants) {
                         $scope.tenants = tenantUtils.hierarchySort(tenantUtils.groupTenantsByParent(tenants), tenant.parentId, []);
                         tenantUtils.addLevels($scope.tenants);
-                        console.log(tenants, $scope.tenants);
                     });
             });
     };
