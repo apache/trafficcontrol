f1249ccce9851d456598abbc6d248280e3481364
Zach Hoffman <zrhoffman@apache.org>
2022-03-22 09:12:20 -0600

    Enable software collections environment before building openssl

--- cache-config/testing/docker/trafficserver/run.sh
+++ cache-config/testing/docker/trafficserver/run.sh
@@ -30,6 +30,13 @@ echo "OS_DISTRO:${OS_DISTRO}"
 echo "OS_VERSION:${OS_VERSION}"
 echo "ATS_VERSION:${ATS_VERSION}"
 
+# setup the environment to use the devtoolset-9 tools.
+if [ "${OS_VERSION%%.*}" -le 7 ]; then
+  source scl_source enable devtoolset-9
+else
+  source scl_source enable gcc-toolset-9
+fi
+
 mkdir -p /opt/build
 cd /opt/build
 
@@ -74,13 +81,6 @@ echo "Building a RPM for ATS version: $ATS_VERSION and OS version: $OS_VERSION"
 
 # add the 'ats' user
 id ats &>/dev/null || /usr/sbin/useradd -u 176 -r ats -s /sbin/nologin -d /
-
-# setup the environment to use the devtoolset-9 tools.
-if [ "${OS_VERSION%%.*}" -le 7 ]; then 
-  source scl_source enable devtoolset-9
-else
-  source scl_source enable gcc-toolset-9
-fi
 
 cd /root
 # prep build environment
