3f5a8d9b57c7e934ac3f1c9d870b76133b4d2eca
eholgu401 <eric_holguin@comcast.com>
2022-03-24 13:17:09 -0600

    Fix missing parameter prereqs

--- traffic_ops/testing/api/v3/readonlycannotmodify_test.go
+++ traffic_ops/testing/api/v3/readonlycannotmodify_test.go
@@ -25,7 +25,7 @@ import (
 )
 
 func TestReadOnlyCannotModify(t *testing.T) {
-	WithObjs(t, []TCObj{CDNs, Types, Tenants, Profiles, Statuses, Divisions, Regions, PhysLocations, CacheGroups, Servers, Topologies, DeliveryServices, Users}, func() {
+	WithObjs(t, []TCObj{CDNs, Types, Tenants, Parameters, Profiles, Statuses, Divisions, Regions, PhysLocations, CacheGroups, Servers, Topologies, DeliveryServices, Users}, func() {
 		CreateTestCDNWithReadOnlyUser(t)
 	})
 }
