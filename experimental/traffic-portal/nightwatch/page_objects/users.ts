e1147b2b9f0ff62f82ac7bf215ed730b826aa3d2
ocket8888 <ocket8888@apache.org>
2022-04-21 11:16:10 -0600

    Add users table test
    There's a second test but it's unusable until the "View User Details"
    button actually does anything.

---
+++ experimental/traffic-portal/nightwatch/page_objects/users.ts
@@ -0,0 +1,81 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import {
+	EnhancedElementInstance,
+	EnhancedPageObject, EnhancedSectionInstance, NightwatchAPI
+} from "nightwatch";
+
+/**
+ * Defines the commands for the users table section.
+ */
+interface UsersTableSectionCommands extends EnhancedSectionInstance, EnhancedElementInstance<EnhancedPageObject> {
+	getColumnState(column: string): Promise<boolean>;
+	searchText(text: string): this;
+	toggleColumn(column: string): this;
+}
+
+const usersPageObject = {
+	api: {} as NightwatchAPI,
+	sections: {
+		usersTable: {
+			commands: {
+				async getColumnState(column: string): Promise<boolean> {
+					return new Promise((resolve, reject) => {
+						this.click("@columnMenu").getElementProperty(`input[name='column-${column}']`, "checked",
+							result => {
+								if (typeof result.value !== "boolean") {
+									console.error("incorrect type for 'checked' DOM property:", result.value);
+									reject(new Error(`incorrect type for 'checked' DOM property: ${typeof result.value}`));
+									return;
+								}
+								resolve(result.value);
+							}
+						).click("@columnMenu");
+					});
+				},
+				searchText(text: string): UsersTableSectionCommands  {
+					 return this.setValue("@searchbox", text);
+				},
+				toggleColumn(column: string): UsersTableSectionCommands {
+					return this.click("@columnMenu").click(`input[name='${column}']`).click("@columnMenu");
+				},
+			} as UsersTableSectionCommands,
+			elements: {
+				columnMenu: {
+					selector: "button.dropdown-toggle"
+				},
+				searchbox: {
+					selector: "input[name='fuzzControl']"
+				},
+			},
+			selector: "main > main"
+		}
+	},
+	url(): string {
+		return `${this.api.launchUrl}/core/users`;
+	}
+};
+
+/**
+ * Defines the users table section.
+ */
+type UsersTableSection = EnhancedSectionInstance<UsersTableSectionCommands, typeof usersPageObject.sections.usersTable.elements>;
+
+/**
+ * The type of the users table page object as provided by the Nightwatch API at
+ * runtime.
+ */
+export type UsersPageObject = EnhancedPageObject<{}, {}, { usersTable: UsersTableSection }>;
+
+export default usersPageObject;
---
+++ experimental/traffic-portal/nightwatch/tests/users.spec.ts
@@ -0,0 +1,61 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import type { NightwatchBrowser } from "nightwatch";
+import { LoginPageObject } from "nightwatch/page_objects/login";
+
+import type { GlobalConfig } from "../globals/globals";
+import type { UsersPageObject } from "../page_objects/users";
+
+module.exports = {
+	"Filter by username": async (browser: NightwatchBrowser): Promise<void> => {
+		const username = (browser.globals as GlobalConfig).adminUser;
+		const password = (browser.globals as GlobalConfig).adminPass;
+
+		const loginPage: LoginPageObject = browser.page.login();
+		loginPage.navigate().section.loginForm.login(username, password);
+
+		const page: UsersPageObject = browser.waitForElementPresent("main").page.users();
+		let tbl = page.navigate().waitForElementPresent(".ag-row").section.usersTable;
+		if (! await tbl.getColumnState("Username")) {
+			tbl = tbl.toggleColumn("Username");
+		}
+
+		tbl = tbl.searchText(username);
+		tbl.parent.assert.urlContains(`search=${username}`);
+
+		tbl.api.elements("css selector", ".ag-row:not(.ag-hidden .ag-row)",
+			result => {
+				if (result.status === 1) {
+					browser.assert.equal(true, false, `failed to select ag-grid rows: ${result.value.message}`);
+					return;
+				}
+				browser.assert.equal(result.value.length, 1);
+			}
+		);
+	},
+	// Uncomment when user details page exists
+	// "View user details":  (browser: NightwatchBrowser): void => {
+	// 	const username = (browser.globals as GlobalConfig).adminUser;
+	// 	const password = (browser.globals as GlobalConfig).adminPass;
+
+	// 	const loginPage: LoginPageObject = browser.page.login();
+	// 	loginPage.navigate().section.loginForm.login(username, password);
+
+	// 	const page: UsersPageObject = browser.waitForElementPresent("main").page.users();
+	// 	const tbl = page.navigate().waitForElementPresent(".ag-row").section.usersTable.searchText(username);
+
+	// 	const userRow = tbl.parent.api.moveToElement(".ag-row:not(.ag-hidden .ag-row)", 2, 2, 100, {pointer: 0, viewport: 0});
+	// 	userRow.mouseButtonClick("right").click("button[name=View-User-Details]").assert.urlContains(username);
+	// }
+};
