29bf16887c4d5550b9a4d2f2fc653e9e1452b5a6
ocket8888 <ocket8888@apache.org>
2022-04-28 17:04:27 -0600

    Squash set_provider_not_null

--- traffic_ops/app/db/trafficvault/create_tables.sql
+++ traffic_ops/app/db/trafficvault/create_tables.sql
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS sslkey (
     deliveryservice text NOT NULL,
     cdn text NOT NULL,
     version text NOT NULL,
-    provider text,
+    provider text NOT NULL,
     expiration timestamp with time zone NOT NULL DEFAULT now(),
     last_updated timestamp with time zone DEFAULT now() NOT NULL
 );
