866d7a169d9291c925446013a75f3d7f7c39c10b
Steve Hamrick <shamrickus@users.noreply.github.com>
2022-05-10 11:06:30 -0600

    Add Various E2E TPv2 DS Tests (#6805)
    * Add DS e2e tests
    
    * Run without PR
    
    * Cleanup
    
    * Dont use silly ids, actually create the data before test runs
    
    * Lint fix
    
    * Code review changes
    
    * Allow tests to be ran multiple times
    
    * Dont define custom types, use module augmentation
    
    * Dont call login or end session in each suite
    
    * Wait for callback
    
    * Lint fix

--- experimental/traffic-portal/nightwatch/globals/globals.ts
+++ experimental/traffic-portal/nightwatch/globals/globals.ts
@@ -12,20 +12,156 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import {type NightwatchGlobals} from "nightwatch";
+import * as https from "https";
 
-/**
- * Defines the configuration used for the testing environment
- */
-export interface GlobalConfig extends NightwatchGlobals {
-	adminPass: string;
-	adminUser: string;
-	trafficOpsURL: string;
+import axios, {AxiosError} from "axios";
+import {NightwatchBrowser} from "nightwatch";
+import type {CommonPageObject} from "nightwatch/page_objects/common";
+import type {DeliveryServiceCardPageObject} from "nightwatch/page_objects/deliveryServiceCard";
+import type {DeliveryServiceDetailPageObject} from "nightwatch/page_objects/deliveryServiceDetail";
+import type {DeliveryServiceInvalidPageObject} from "nightwatch/page_objects/deliveryServiceInvalidationJobs";
+import type {LoginPageObject} from "nightwatch/page_objects/login";
+import type {ServersPageObject} from "nightwatch/page_objects/servers";
+import type {UsersPageObject} from "nightwatch/page_objects/users";
+import {
+	CDN,
+	GeoLimit, GeoProvider, LoginRequest,
+	Protocol,
+	RequestDeliveryService,
+	ResponseCDN,
+	ResponseDeliveryService
+} from "trafficops-types";
+
+declare module "nightwatch" {
+	/**
+	 * Defines the global nightwatch browser type with our types mixed in.
+	 */
+	export interface NightwatchCustomPageObjects {
+		common: () => CommonPageObject;
+		deliveryServiceCard: () => DeliveryServiceCardPageObject;
+		deliveryServiceDetail: () => DeliveryServiceDetailPageObject;
+		deliveryServiceInvalidationJobs: () => DeliveryServiceInvalidPageObject;
+		login: () => LoginPageObject;
+		servers: () => ServersPageObject;
+		users: () => UsersPageObject;
+	}
+
+	/**
+	 * Defines the additional types needed for the test environment.
+	 */
+	export interface NightwatchGlobals {
+		adminPass: string;
+		adminUser: string;
+		trafficOpsURL: string;
+		apiVersion: string;
+		uniqueString: string;
+	}
 }
-const config = {
+
+const globals = {
 	adminPass: "twelve12",
 	adminUser: "admin",
-	trafficOpsURL: "https://localhost:6443"
+	afterEach: (browser: NightwatchBrowser, done: () => void): void => {
+		browser.end(() => {
+			done();
+		});
+	},
+	apiVersion: "4.0",
+	before: async (done: () => void): Promise<void> => {
+		const apiUrl = `${globals.trafficOpsURL}/api/${globals.apiVersion}`;
+		const client = axios.create({
+			httpsAgent: new https.Agent({
+				rejectUnauthorized: false
+			})
+		});
+		let accessToken = "";
+		const loginReq: LoginRequest = {
+			p: globals.adminPass,
+			u: globals.adminUser
+		};
+		try {
+			const resp = await client.post(`${apiUrl}/user/login`, JSON.stringify(loginReq));
+			if(resp.headers["set-cookie"]) {
+				for (const cookie of resp.headers["set-cookie"]) {
+					if(cookie.indexOf("access_token") > -1) {
+						accessToken = cookie;
+						break;
+					}
+				}
+			}
+		} catch (e) {
+			console.error((e as AxiosError).message);
+			throw e;
+		}
+		if(accessToken === "") {
+			console.error("Access token is not set");
+			return Promise.reject();
+		}
+		client.defaults.headers.common = { Cookie: accessToken };
+
+		const cdn: CDN = {
+			dnssecEnabled: false, domainName: `tests${globals.uniqueString}.com`, name: `testCDN${globals.uniqueString}`
+		};
+		let respCDN: ResponseCDN;
+		try {
+			let resp = await client.post(`${apiUrl}/cdns`, JSON.stringify(cdn));
+			respCDN = resp.data.response;
+
+			const ds: RequestDeliveryService = {
+				active: false,
+				cacheurl: null,
+				cdnId: respCDN.id,
+				displayName: `test DS${globals.uniqueString}`,
+				dscp: 0,
+				ecsEnabled: false,
+				edgeHeaderRewrite: null,
+				fqPacingRate: null,
+				geoLimit: GeoLimit.NONE,
+				geoProvider: GeoProvider.MAX_MIND,
+				httpBypassFqdn: null,
+				infoUrl: null,
+				initialDispersion: 1,
+				ipv6RoutingEnabled: false,
+				logsEnabled: false,
+				maxOriginConnections: 0,
+				maxRequestHeaderBytes: 0,
+				midHeaderRewrite: null,
+				missLat: 0,
+				missLong: 0,
+				multiSiteOrigin: false,
+				orgServerFqdn: "http://test.com",
+				profileId: 1,
+				protocol: Protocol.HTTP,
+				qstringIgnore: 0,
+				rangeRequestHandling: 0,
+				regionalGeoBlocking: false,
+				remapText: null,
+				routingName: "test",
+				signed: false,
+				tenantId: 1,
+				typeId: 1,
+				xmlId: `testDS${globals.uniqueString}`
+			};
+			resp = await client.post(`${apiUrl}/deliveryservices`, JSON.stringify(ds));
+			const respDS: ResponseDeliveryService = resp.data.response[0];
+			console.log(`Successfully created DS '${respDS.displayName}'`);
+		} catch(e) {
+			console.error((e as AxiosError).message);
+			throw e;
+		}
+		done();
+	},
+	beforeEach: (browser: NightwatchBrowser, done: () => void): void => {
+		browser.page.login()
+			.navigate().section.loginForm
+			.loginAndWait(browser.globals.adminUser, browser.globals.adminPass);
+		// This ensures that we call done after loginAndWait is finished
+		browser.pause(1, () => {
+			done();
+		});
+	},
+	trafficOpsURL: "https://localhost:6443",
+	uniqueString: new Date().getTime().toString()
 };
 
-module.exports = config;
+module.exports = globals;
---
+++ experimental/traffic-portal/nightwatch/tests/servers/servers.spec.ts
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+describe("Servers Spec", () => {
+	it("Filter by hostname", async () => {
+		const page = browser.page.servers();
+		page.navigate()
+			.waitForElementPresent("input[name=fuzzControl]");
+		page.section.serversTable.searchText("edge");
+		page.assert.urlContains("search=edge");
+	});
+});
