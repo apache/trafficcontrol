24afd8dd772cb50508740264f9e1857a6298af44
eholgu401 <eric_holguin@comcast.com>
2022-05-25 11:25:47 -0600

    Add GoDocs and functions return value instead of pointer

--- traffic_ops/traffic_ops_golang/test/rand.go
+++ traffic_ops/traffic_ops_golang/test/rand.go
@@ -21,68 +21,81 @@ import (
 	"net"
 )
 
-func RandStr() *string {
+// RandStr returns, as a string, a random 100 character alphanumeric, including '-' and '_'.
+func RandStr() string {
 	chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_"
 	num := 100
 	s := ""
 	for i := 0; i < num; i++ {
 		s += string(chars[rand.Intn(len(chars))])
 	}
-	return &s
+	return s
 }
+
+// RandStrArray returns, as an array of 100 strings, with 100 character random alphanumeric, including '-' and '_'.
 func RandStrArray() []string {
 	num := 100
 	sArray := make([]string, num)
 	for i := 0; i < num; i++ {
-		sArray[i] = *RandStr()
+		sArray[i] = RandStr()
 	}
 	return sArray
 }
 
-func RandBool() *bool {
+// RandBool returns a random boolean value.
+func RandBool() bool {
 	b := rand.Int()%2 == 0
-	return &b
+	return b
 }
 
-func RandInt() *int {
+// RandInt returns a random int.
+func RandInt() int {
 	i := rand.Int()
-	return &i
+	return i
 }
-func RandInt64() *int64 {
+
+// RandInt64 returns a random signed 64-bit int.
+func RandInt64() int64 {
 	i := rand.Int63()
-	return &i
+	return i
 }
 
-func RandUint64() *uint64 {
+// RandUint64 returns a random unsigned 64-bit int.
+func RandUint64() uint64 {
 	i := uint64(rand.Int63())
-	return &i
+	return i
 }
 
-func RandUint() *uint {
+// RandUint returns a random unsigned int.
+func RandUint() uint {
 	i := uint(rand.Int())
-	return &i
+	return i
 }
 
-func RandIntn(n int) *int {
+// RandIntn returns a random int in the half-open interval [0,n).
+func RandIntn(n int) int {
 	i := rand.Intn(n)
-	return &i
+	return i
 }
 
-func RandFloat64() *float64 {
+// RandFloat64 returns a random float64.
+func RandFloat64() float64 {
 	f := rand.Float64()
-	return &f
+	return f
 }
 
-func RandomIPv4() *string {
+// RandomIPv4 returns, as a string, a random IP address.
+func RandomIPv4() string {
 	first := rand.Int31n(256)
 	second := rand.Int31n(256)
 	third := rand.Int31n(256)
 	fourth := rand.Int31n(256)
 	str := fmt.Sprintf("%d.%d.%d.%d", first, second, third, fourth)
-	return &str
+	return str
 }
 
-func RandomIPv6() *string {
+// RandomIPv6 returns, as a string, a random IPv6 address.
+func RandomIPv6() string {
 	ip := net.IP([]byte{
 		uint8(rand.Int31n(256)),
 		uint8(rand.Int31n(256)),
@@ -101,5 +114,5 @@ func RandomIPv6() *string {
 		uint8(rand.Int31n(256)),
 		uint8(rand.Int31n(256)),
 	}).String()
-	return &ip
+	return ip
 }
