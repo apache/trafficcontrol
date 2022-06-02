dab18d5554db3ee4be798860d698cb72260153d7
Srijeet Chatterjee <srijeet0406@gmail.com>
2022-06-02 12:44:11 -0600

    Default routeID to 0, if it is not present in the request context

--- traffic_ops/traffic_ops_golang/routing/middleware/wrappers.go
+++ traffic_ops/traffic_ops_golang/routing/middleware/wrappers.go
@@ -212,7 +212,11 @@ func WrapAccessLog(secret string, h http.Handler) http.HandlerFunc {
 					imsType = IMSMISS
 				}
 			}
-			log.EventfRaw(`%s - %s [%s] "%v %v?%v %s" %v %v %v "%v" %v %s`, r.RemoteAddr, user, time.Now().Format(AccessLogTimeFormat), r.Method, r.URL.Path, r.URL.RawQuery, r.Proto, iw.Code, iw.ByteCount, int(time.Now().Sub(start)/time.Millisecond), r.UserAgent(), r.Context().Value(RouteID), imsType)
+			routeID := r.Context().Value(RouteID)
+			if routeID == nil {
+				routeID = 0
+			}
+			log.EventfRaw(`%s - %s [%s] "%v %v?%v %s" %v %v %v "%v" %v %s`, r.RemoteAddr, user, time.Now().Format(AccessLogTimeFormat), r.Method, r.URL.Path, r.URL.RawQuery, r.Proto, iw.Code, iw.ByteCount, int(time.Now().Sub(start)/time.Millisecond), r.UserAgent(), routeID, imsType)
 		}()
 		h.ServeHTTP(iw, r)
 	}
