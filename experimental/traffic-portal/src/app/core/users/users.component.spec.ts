7ec32fc987958ee6725e2b03cb557fba2ae76c4d
shamrickus <shamrickus@gmail.com>
2022-07-20 16:59:26 -0600

    Fix tests

--- experimental/traffic-portal/src/app/core/users/users.component.spec.ts
+++ experimental/traffic-portal/src/app/core/users/users.component.spec.ts
@@ -143,11 +143,6 @@ describe("UsersComponent", () => {
 		if (isAction(item)) {
 			return fail("the first context menu item should've been a link but it was an action");
 		}
-		if (!item.disabled) {
-			return fail("missing 'disabled' property");
-		}
-		expect(item.disabled([])).toBe(true, "should be disabled for multiple selected users");
-		expect(item.disabled(testUser)).toBe(false, "should be enabled for single selected user");
 		if (!item.href) {
 			return fail("missing 'href' property");
 		}
@@ -169,11 +164,6 @@ describe("UsersComponent", () => {
 			return fail("the second context menu item should've been a link but it was an action");
 		}
 		expect(item.newTab).toBeTrue();
-		if (!item.disabled) {
-			return fail("missing 'disabled' property");
-		}
-		expect(item.disabled([])).toBe(true, "should be disabled for multiple selected users");
-		expect(item.disabled(testUser)).toBe(false, "should be enabled for single selected user");
 		if (!item.href) {
 			return fail("missing 'href' property");
 		}
@@ -194,11 +184,6 @@ describe("UsersComponent", () => {
 		if (isAction(item)) {
 			return fail("the third context menu item should've been a link but it was an action");
 		}
-		if (!item.disabled) {
-			return fail("missing 'disabled' property");
-		}
-		expect(item.disabled([])).toBe(true, "should be disabled for multiple selected users");
-		expect(item.disabled(testUser)).toBe(false, "should be enabled for single selected user");
 		if (!item.href) {
 			return fail("missing 'href' property");
 		}
