1bb142190e1389c2c479a05e63f9e61d872ddb17
ocket8888 <ocket8888@apache.org>
2022-07-20 18:20:18 -0600

    Remove _ from servers controller

--- traffic_portal/app/src/common/modules/form/server/FormServerController.js
+++ traffic_portal/app/src/common/modules/form/server/FormServerController.js
@@ -52,13 +52,10 @@ var FormServerController = function(server, $scope, $location, $state, $uibModal
             });
     };
 
-    var getProfiles = function(cdnId) {
-        profileService.getProfiles({ orderby: 'name', cdn: cdnId })
-            .then(function(result) {
-                $scope.profiles = _.filter(result, function(profile) {
-                    return profile.type != 'DS_PROFILE'; // DS profiles are not intended for servers
-                });
-            });
+	/** @param {number} cdnId */
+    async function getProfiles(cdnId) {
+        const result = await profileService.getProfiles({ orderby: "name", cdn: cdnId })
+		$scope.profiles = result.filter(profile => profile.type !== "DS_PROFILE"); // DS profiles are not intended for servers
     };
 
     $scope.getProfileID = function(profileName) {
