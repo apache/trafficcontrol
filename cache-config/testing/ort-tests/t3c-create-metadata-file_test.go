1c206fe5cb4a81ab1e9d7e71ec49a8782f60def3
Robert O Butts <rob05c@users.noreply.github.com>
2022-07-22 12:25:34 -0600

    Add t3c metadata file (#6905)

---
+++ cache-config/testing/ort-tests/t3c-create-metadata-file_test.go
@@ -0,0 +1,61 @@
+package orttest
+
+/*
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/apache/trafficcontrol/cache-config/testing/ort-tests/tcdata"
+	"github.com/apache/trafficcontrol/cache-config/testing/ort-tests/util"
+)
+
+func TestT3cCreateMetaDataFile(t *testing.T) {
+	// t3c should create a metadata file
+	tcd.WithObjs(t, []tcdata.TCObj{
+		tcdata.CDNs, tcdata.Types, tcdata.Tenants, tcdata.Parameters,
+		tcdata.Profiles, tcdata.ProfileParameters,
+		tcdata.Divisions, tcdata.Regions, tcdata.PhysLocations,
+		tcdata.CacheGroups, tcdata.Servers, tcdata.Topologies,
+		tcdata.DeliveryServices}, func() {
+
+		err := t3cUpdateCreateEmptyFile(DefaultCacheHostName, "badass")
+		if err != nil {
+			t.Fatalf("t3c badass failed: %v", err)
+		}
+
+		const metaDataFileName = `t3c-apply-metadata.json`
+
+		filePath := filepath.Join(TestConfigDir, metaDataFileName)
+
+		if !util.FileExists(filePath) {
+			t.Fatalf("missing metadata file '%s'", filePath)
+		}
+
+		mdFileBts, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			t.Fatalf("reading file '%s': %v", filePath, err)
+		}
+
+		// Test that the file is a valid JSON object.
+		// Other than that, we don't want to assert any particular data.
+		mdObj := map[string]interface{}{}
+		if err := json.Unmarshal(mdFileBts, &mdObj); err != nil {
+			t.Errorf("expected metadata file '%s' to be a json object, actual: %s", filePath, err)
+		}
+	})
+}
