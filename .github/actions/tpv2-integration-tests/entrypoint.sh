412ac7111e771c1ad849d17881f44cbb0bf4feb4
ocket8888 <ocket8888@apache.org>
2022-09-30 12:44:15 -0600

    Fix missing path part in main script exec

--- .github/actions/tpv2-integration-tests/entrypoint.sh
+++ .github/actions/tpv2-integration-tests/entrypoint.sh
@@ -26,7 +26,7 @@ envsubst <../../.github/actions/tpv2-integration-tests/cdn.json >./cdn.conf
 ./traffic_ops_golang --cfg ./cdn.conf --dbcfg ../../.github/actions/tpv2-integration-tests/database.json > out.log 2>&1 &
 
 cd "${GITHUB_WORKSPACE}/experimental/traffic-portal"
-node ./dist/traffic-portal/main.js --port 4200 -k -t 'https://localhost:6443/'
+node ./dist/traffic-portal/server/main.js --port 4200 -k -t 'https://localhost:6443/'
 
 timeout 15m bash <<TMOUT
 	while ! curl -k "http://localhost:4200/api/4.0/ping" >/dev/null 2>&1; do
