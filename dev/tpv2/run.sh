57b28a9a160a10e7d2c6f047bfa739517fee0191
Zach Hoffman <zrhoffman@apache.org>
2022-11-14 02:32:47 -0700

    Dev CiaB: Use Almquist shell for TP/TPv2 since the base image is Alpine

--- dev/tpv2/run.sh
+++ dev/tpv2/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 #  limitations under the License.
 #
 
-set -o errexit
+set -o errexit -o nounset
 
 cd "$TC/experimental/traffic-portal"
 
