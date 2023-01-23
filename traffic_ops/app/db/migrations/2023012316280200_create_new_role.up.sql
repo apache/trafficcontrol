5e367d770165554fdff6c24dd72898e4493dec6b
Kurtis Michie <kurtis.michie@gmail.com>
2023-01-23 16:39:47 -0700

    Migration for new role trouter with limited permissions for user trouter1

---
+++ traffic_ops/app/db/migrations/2023012316280200_create_new_role.up.sql
@@ -0,0 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership.  The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+INSERT INTO public."role"
+(name, description, priv_level)
+VALUES('trouter', 'Limited role for Traffic Router calls to Traffic Ops', 10);
+
+INSERT INTO public.role_capability
+(SELECT id FROM public."role" WHERE name="trouter", perm)
+FROM public.role
+        CROSS JOIN ( VALUES
+            ('CDN:READ', 'DELIVERY-SERVICE:READ', 'DNS-SEC:READ', 'FEDERATION:READ', 'STEERING:READ', 'FEDERATION-RESOLVER:READ', 'DS-SECURITY-KEY:READ')
+) AS perms(perm)
+WHERE priv_level > 10
+    ON CONFLICT DO NOTHING;
