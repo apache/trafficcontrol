353669f12cac00d7dfa56421a6cc9071515778a3
jpappa200 <joseph_pappano@cable.comcast.com>
2023-02-10 09:14:32 -0500

    added comment and time units to variable name

--- cache-config/t3c-apply/t3c-apply.go
+++ cache-config/t3c-apply/t3c-apply.go
@@ -147,13 +147,14 @@ func Main() int {
 
 	if cfg.UseGit == config.UseGitYes || cfg.UseGit == config.UseGitAuto {
 		//need to see if there is an old lock file laying around.
-		const gitMaxLockAge = 5
-		oldLock, err := util.IsGitLockFileOld(cfg, time.Now(), gitMaxLockAge)
+		//older than 5 minutes
+		const gitMaxLockAgeMinutes = 5
+		oldLock, err := util.IsGitLockFileOld(cfg, time.Now(), gitMaxLockAgeMinutes)
 		if err != nil {
 			log.Errorln("checking for git lock file: " + err.Error())
 		}
 		if oldLock {
-			log.Errorf("removing git lock file older than %dm", gitMaxLockAge)
+			log.Errorf("removing git lock file older than %dm", gitMaxLockAgeMinutes)
 			err := util.RemoveGitLock(cfg)
 			if err != nil {
 				log.Errorf("couldn't remove git lock file: %v", err.Error())
