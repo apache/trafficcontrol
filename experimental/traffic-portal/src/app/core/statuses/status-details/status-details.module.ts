057fa66955bfea0c9aa4c132ccd751ff709c90f2
gbkannan89 <111871677+gbkannan89@users.noreply.github.com>
2023-02-27 12:29:08 +0530

    breadcrumbs removed,license added,model from lib

--- experimental/traffic-portal/src/app/core/statuses/status-details/status-details.module.ts
+++ experimental/traffic-portal/src/app/core/statuses/status-details/status-details.module.ts
@@ -1,3 +1,16 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatusDetailsComponent } from './status-details.component';
@@ -9,6 +22,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatButtonModule } from '@angular/material/button';
+import { StatusesService } from 'src/app/api/statuses.service';
 
 const StatusDetailRouting = RouterModule.forChild([
   {
@@ -31,6 +45,9 @@ const StatusDetailRouting = RouterModule.forChild([
     MatCardModule,
     MatButtonModule,
     SharedModule
+  ],
+  providers:[
+    StatusesService
   ]
 })
 export class StatusDetailsModule { }
