2c9c9cb5ae9c63e338a68eba7a5ffb59cd83289e
gbkannan89 <111871677+gbkannan89@users.noreply.github.com>
2023-02-28 18:32:21 +0530

    statuses test cases added

--- experimental/traffic-portal/src/app/core/statuses/status-details/status-details.component.spec.ts
+++ experimental/traffic-portal/src/app/core/statuses/status-details/status-details.component.spec.ts
@@ -11,20 +11,54 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ComponentFixture, fakeAsync, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatInputModule } from "@angular/material/input";
+import { BrowserDynamicTestingModule } from "@angular/platform-browser-dynamic/testing";
+import { Router } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { StatusesService } from "src/app/api/statuses.service";
+import { DecisionDialogComponent } from "src/app/shared/dialogs/decision-dialog/decision-dialog.component";
+import { SharedModule } from "src/app/shared/shared.module";
 import { StatusDetailsComponent } from "./status-details.component";
+
+const status = { id: 1, name: 'test', description: 'test', lastUpdated: new Date('02/02/2023') };
 
 describe("StatusDetailsComponent", () => {
 	let component: StatusDetailsComponent;
 	let fixture: ComponentFixture<StatusDetailsComponent>;
+	let router: Router;
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [ StatusDetailsComponent ]
+			imports: [
+				HttpClientTestingModule,
+				RouterTestingModule.withRoutes([
+					{ path: 'core/statuses/:id', component: StatusDetailsComponent }
+				]),
+				ReactiveFormsModule,
+				MatFormFieldModule,
+				MatInputModule,
+				MatGridListModule,
+				MatCardModule,
+				MatButtonModule,
+				SharedModule
+			],
+			declarations: [StatusDetailsComponent, DecisionDialogComponent],
+			providers: [StatusesService]
 		})
 			.compileComponents();
-
+		TestBed.overrideModule(BrowserDynamicTestingModule, {
+			set: {
+				entryComponents: [DecisionDialogComponent]
+			}
+		});
+		router = TestBed.inject(Router);
 		fixture = TestBed.createComponent(StatusDetailsComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
@@ -33,4 +67,28 @@ describe("StatusDetailsComponent", () => {
 	it("should create", () => {
 		expect(component).toBeTruthy();
 	});
+
+	it("submits a update status request", fakeAsync(() => {
+		const service = TestBed.inject(StatusesService);
+		component.statusDetailsForm.setValue(status);
+		spyOn(service, 'updateStatus').and.returnValue(Promise.resolve(status));
+		component.updateStatus();
+
+		service.updateStatus(component.statusDetailsForm.value, 1).then((result) => {
+			expect(result).toEqual(status);
+		})
+	}));
+
+	it("submits a status creation request", fakeAsync(() => {
+		const service = TestBed.inject(StatusesService);
+		component.statusDetailsForm.setValue(status);
+		spyOn(service, 'createStatus').and.returnValue(Promise.resolve(status));
+		component.createStatus();
+
+		service.createStatus(component.statusDetailsForm.value).then((result) => {
+			expect(result).toEqual(status);
+			router.navigate(['/core/statuses/1']);
+		})
+	}));
+
 });
