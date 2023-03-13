693e7c56ff389b803b7a20bdf8cc51b7ac49a108
Gokula Krishnan S <67802669+gokulakrishnansvm@users.noreply.github.com>
2023-03-13 12:06:17 +0530

    Update test_cdns.py

--- traffic_ops/testing/api_contract/v4/test_cdns.py
+++ traffic_ops/testing/api_contract/v4/test_cdns.py
@@ -9,7 +9,7 @@ PyTest Fixture to store keys for cdns endpoint
 @pytest.fixture
 def get_cdn_keys():
     # Response keys for cdns endpoint
-    with open('post_data.json', 'r') as f:
+    with open('prerequisite_data.json', 'r') as f:
         data = json.load(f)
     cdn_keys = list(data["cdns"].keys())
     return cdn_keys
@@ -18,23 +18,21 @@ def get_cdn_keys():
 """
 Test step to validate keys from cdns endpoint response and POST method
 """
-def test_get_cdn(to_login, get_cdn_keys, cdn_post_data):
+def test_get_cdn(to_login, get_cdn_keys, cdn_prereq):
     # validate CDN keys from cdns get response
     logging.info("Accessing Cdn endpoint through Traffic ops session")
-    cdn_name = cdn_post_data[0]["cdns"]["name"]
+    cdn_name = cdn_prereq[0]["cdns"]["name"]
     cdn_get_response = to_login.get_cdns(query_params={"name": str(cdn_name)})
     try:
         cdn_data = cdn_get_response[0]
         cdn_keys = list(cdn_data[0].keys())
         logging.info(
             "CDN Keys from cdns endpoint response {}".format(cdn_keys))
-        # validate cdn values from post data in cdns get response
-        post_data = [cdn_post_data[0]["cdns"]['name'], cdn_post_data[0]
-                     ["cdns"]['domainName'], cdn_post_data[0]["cdns"]['dnssecEnabled']]
-        get_data = [cdn_data[0]['name'], cdn_data[0]
-                    ['domainName'], cdn_data[0]['dnssecEnabled']]
+        # validate cdn values from prereq data in cdns get response
+        prereq_data = [cdn_prereq[0]["cdns"]['name'], cdn_prereq[0]["cdns"]['domainName'], cdn_prereq[0]["cdns"]['dnssecEnabled']]
+        get_data = [cdn_data[0]['name'], cdn_data[0]['domainName'], cdn_data[0]['dnssecEnabled']]
         assert cdn_keys.sort() == get_cdn_keys.sort()
-        assert get_data == post_data
+        assert get_data == prereq_data
     except IndexError:
         logging.error("No CDN data from cdns get request")
         pytest.fail("Response is empty from get request , Failing test_get_cdn")
@@ -42,9 +40,9 @@ def test_get_cdn(to_login, get_cdn_keys, cdn_post_data):
 
 """
 Delete CDN after test execution to avoid redundancy"""
-def pytest_sessionfinish(cdn_post_data, to_login):
+def pytest_sessionfinish(cdn_prereq, to_login):
     try:
-        cdn_response = cdn_post_data[1]
+        cdn_response = cdn_prereq[1]
         id = cdn_response["id"]
         to_login.delete_cdn_by_id(cdn_id=id)
     except IndexError:
