d18d90eedb678a04b9112a69824eb70762a302c4
Gokula krishnan <gokulakrishnan_sugumaran@comcast.com>
2023-03-14 15:50:32 +0530

    Addressed review comments

--- traffic_ops/testing/api_contract/v4/conftest.py
+++ traffic_ops/testing/api_contract/v4/conftest.py
@@ -1,35 +1,36 @@
+"""This module is used to create a Traffic Ops session 
+and to store prerequisite data for endpoints"""
+import json
+import logging
+import sys
+from random import randint
+from urllib.parse import urlparse
 import pytest
 from trafficops.tosession import TOSession
-from urllib.parse import urlparse
-import sys 
-import json
-from random import randint
-import logging
+
+# Create and configure logger
+logger = logging.getLogger()
 
 
-""" 
-Passing in Traffic Ops Arguments [Username, Password, Url and Hostname] from Command Line
-"""
 def pytest_addoption(parser):
+    """Passing in Traffic Ops Arguments [Username, Password, Url and Hostname] from Command Line"""
     parser.addoption(
-        '--to_user', action='store', help='User name for Traffic Ops Session'
+        '--to_user', action='store', default='admin', help='User name for Traffic Ops Session'
     )
     parser.addoption(
-        '--to_password', action='store',  help='Password for Traffic Ops Session'
+        '--to_password', action='store', default='twelve',  help='Password for Traffic Ops Session'
     )
     parser.addoption(
-        '--to_url', action='store',  help='Traffic Ops URL'
+        '--to_url', action='store', default='https://localhost/api', help='Traffic Ops URL'
     )
     parser.addoption(
-        '--hostname', action='store',  help='Traffic Ops hostname'
+        '--hostname', action='store', default='localhost', help='Traffic Ops hostname'
     )
 
 
-""" 
-PyTest fixture to store Traffic ops Arguments passed from command line
-"""
-@pytest.fixture
+@pytest.fixture(name="to_args")
 def to_data(pytestconfig):
+    """PyTest fixture to store Traffic ops Arguments passed from command line"""
     args = {}
     args['user'] = pytestconfig.getoption('--to_user')
     args['password'] = pytestconfig.getoption('--to_password')
@@ -38,57 +39,54 @@ def to_data(pytestconfig):
     return args
 
 
-"""
-PyTest Fixture to create a Traffic Ops session from Traffic Ops Arguments
-passed as command line arguments in to_data fixture in conftest
-"""
-@pytest.fixture()
-def to_login(to_data):
-
+@pytest.fixture(name="to_session")
+def to_login(to_args):
+    """PyTest Fixture to create a Traffic Ops session from Traffic Ops Arguments 
+    passed as command line arguments in to_args fixture in conftest"""
     # Create a Traffic Ops V4 session and login
-    print("Parsed TO args {}".format(to_data))
-    if to_data["user"] == None:
-        f = open('to_data.json')
-        data = json.load(f)
-        to_data = data["test"]
-    to_url = urlparse(to_data["url"])
+    with open('to_data.json', encoding="utf-8", mode='r') as session_file:
+        data = json.load(session_file)
+        session_file.close()
+    session_data = data["test"]
+    api_version = session_data["api_version"]
+    port = session_data["port"]
+    if to_args["user"] is None:
+        logger.info(
+            "Traffic Ops session data were not passed from Command line Args")
+    else:
+        logger.info("Parsed Traffic ops session data from args %s", to_args)
+        session_data = to_args
+    to_url = urlparse(session_data["url"])
     to_host = to_url.hostname
-
-    TO=TOSession(host_ip=to_host,host_port=443,api_version='4.0',ssl=True,verify_cert=False)
-    print("established connection")
-
+    to_session = TOSession(host_ip=to_host, host_port=port,
+                           api_version=api_version, ssl=True, verify_cert=False)
+    logger.info("Established Traffic Ops Session")
     # Login To TO_API
-    TO.login(to_data["user"], to_data["password"])
+    to_session.login(session_data["user"], session_data["password"])
 
-    if not TO.logged_in:
-        print("Failure Logging into Traffic Ops")
+    if not to_session.logged_in:
+        logger.error("Failure Logging into Traffic Ops")
         sys.exit(-1)
     else:
-        print("Successfully logged into Traffic Ops")
-    return TO
+        logger.info("Successfully logged into Traffic Ops")
+    return to_session
 
 
-"""
-PyTest Fixture to create POST data for cdns endpoint
-"""
 @pytest.fixture()
-def cdn_prereq(to_login):
+def cdn_prereq(to_session, get_cdn_data):
+    """PyTest Fixture to create POST data for cdns endpoint"""
 
-    #Return new post data and post response from cdns POST request
-    f = open('prerequisite_data.json')
-    data = json.load(f)
-
-    data["cdns"]["name"] =data["cdns"]["name"][:4]+str(randint(0,1000))
-    data["cdns"]["domainName"] = data["cdns"]["domainName"][:5] + str(randint(0,1000))
-    logging.info("New post data to hit POST method {}".format(data))
-    with open('prerequisite_data.json', 'w') as f:
-         json.dump(data, f)
-    f.close()
-    #Hitting cdns POST methed
-    response = to_login.create_cdn(data=data["cdns"])
+    # Return new post data and post response from cdns POST request
+    data = get_cdn_data
+    data["name"] = data["name"][:4]+str(randint(0, 1000))
+    data["domainName"] = data["domainName"][:5] + str(randint(0, 1000))
+    logger.info("New cdn data to hit POST method %s", data)
+    # Hitting cdns POST methed
+    response = to_session.create_cdn(data=data)
     try:
         cdn_response = response[0]
         prerequisite_data = [data, cdn_response]
         return prerequisite_data
     except IndexError:
-        logging.error("No CDN response data from cdns POST request")
+        logger.error("No CDN response data from cdns POST request")
+        return None
--- traffic_ops/testing/api_contract/v4/test_cdns.py
+++ traffic_ops/testing/api_contract/v4/test_cdns.py
@@ -1,49 +1,51 @@
-import pytest
+"""Api Contract Test Case for cdns endpoint"""
 import json
 import logging
+import pytest
+
+# Create and configure logger
+logger = logging.getLogger()
 
 
-"""
-PyTest Fixture to store keys for cdns endpoint
-"""
-@pytest.fixture
-def get_cdn_keys():
+@pytest.fixture(name="get_cdn_data")
+def cdn_get_data():
+    """PyTest Fixture to store prereq data for cdns endpoint"""
     # Response keys for cdns endpoint
-    with open('prerequisite_data.json', 'r') as f:
-        data = json.load(f)
-    cdn_keys = list(data["cdns"].keys())
-    return cdn_keys
+    with open('prerequisite_data.json', encoding="utf-8", mode='r') as prereq_file:
+        data = json.load(prereq_file)
+        prereq_file.close()
+    cdn_data = data["cdns"]
+    return cdn_data
 
 
-"""
-Test step to validate keys from cdns endpoint response and POST method
-"""
-def test_get_cdn(to_login, get_cdn_keys, cdn_prereq):
+def test_get_cdn(to_session, get_cdn_data, cdn_prereq):
+    """Test step to validate keys from cdns endpoint response and POST method"""
     # validate CDN keys from cdns get response
-    logging.info("Accessing Cdn endpoint through Traffic ops session")
-    cdn_name = cdn_prereq[0]["cdns"]["name"]
-    cdn_get_response = to_login.get_cdns(query_params={"name": str(cdn_name)})
+    logger.info("Accessing Cdn endpoint through Traffic ops session")
+    cdn_name = cdn_prereq[0]["name"]
+    cdn_get_response = to_session.get_cdns(query_params={"name": str(cdn_name)})
     try:
         cdn_data = cdn_get_response[0]
         cdn_keys = list(cdn_data[0].keys())
-        logging.info(
-            "CDN Keys from cdns endpoint response {}".format(cdn_keys))
+        logger.info(
+            "CDN Keys from cdns endpoint response %s", cdn_keys)
         # validate cdn values from prereq data in cdns get response
-        prereq_data = [cdn_prereq[0]["cdns"]['name'], cdn_prereq[0]["cdns"]['domainName'], cdn_prereq[0]["cdns"]['dnssecEnabled']]
-        get_data = [cdn_data[0]['name'], cdn_data[0]['domainName'], cdn_data[0]['dnssecEnabled']]
-        assert cdn_keys.sort() == get_cdn_keys.sort()
+        prereq_data = [cdn_prereq[0]['name'], cdn_prereq[0]
+                       ['domainName'], cdn_prereq[0]['dnssecEnabled']]
+        get_data = [cdn_data[0]['name'], cdn_data[0]
+                    ['domainName'], cdn_data[0]['dnssecEnabled']]
+        assert cdn_keys.sort() == list(get_cdn_data.keys()).sort()
         assert get_data == prereq_data
     except IndexError:
-        logging.error("No CDN data from cdns get request")
-        pytest.fail("Response is empty from get request , Failing test_get_cdn")
+        logger.error("No CDN data from cdns get request")
+        pytest.fail("Response from get request is empty, Failing test_get_cdn")
 
 
-"""
-Delete CDN after test execution to avoid redundancy"""
-def pytest_sessionfinish(cdn_prereq, to_login):
+def pytest_sessionfinish(cdn_prereq, to_session):
+    """Delete CDN after test execution to avoid redundancy"""
     try:
         cdn_response = cdn_prereq[1]
-        id = cdn_response["id"]
-        to_login.delete_cdn_by_id(cdn_id=id)
+        cdn_id = cdn_response["id"]
+        to_session.delete_cdn_by_id(cdn_id=cdn_id)
     except IndexError:
-        logging.error("CDN doesn't created")
+        logger.error("CDN doesn't created")
