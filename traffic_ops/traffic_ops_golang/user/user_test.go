292bc75defab57fc2393f4890ee8af1a5684bf8b
shamrickus <shamrickus@gmail.com>
2023-04-06 12:51:17 -0600

    Missed a file

--- traffic_ops/traffic_ops_golang/user/user_test.go
+++ traffic_ops/traffic_ops_golang/user/user_test.go
@@ -89,67 +89,67 @@ func TestDowngrade(t *testing.T) {
 	old := user.Downgrade()
 
 	if *old.FullName != *oldUser.FullName {
-		t.Fatalf("expected FullName to be equal, got %v and %v", *old.FullName, *oldUser.FullName)
+		t.Fatalf("expected FullName to be equal, got %s and %s", *old.FullName, *oldUser.FullName)
 	}
 	if *old.NewUser != *oldUser.NewUser {
-		t.Fatalf("expected NewUser to be equal, got %v and %v", *old.NewUser, *oldUser.NewUser)
+		t.Fatalf("expected NewUser to be equal, got %t and %t", *old.NewUser, *oldUser.NewUser)
 	}
 	if *old.RoleName != *oldUser.RoleName {
-		t.Fatalf("expected RoleName to be equal, got %v and %v", *old.RoleName, *oldUser.RoleName)
+		t.Fatalf("expected RoleName to be equal, got %s and %s", *old.RoleName, *oldUser.RoleName)
 	}
 	if *old.TenantID != *oldUser.TenantID {
-		t.Fatalf("expected TenantID to be equal, got %v and %v", *old.TenantID, *oldUser.TenantID)
+		t.Fatalf("expected TenantID to be equal, got %d and %d", *old.TenantID, *oldUser.TenantID)
 	}
 	if *old.Username != *oldUser.Username {
-		t.Fatalf("expected Username to be equal, got %v and %v", *old.Username, *oldUser.Username)
+		t.Fatalf("expected Username to be equal, got %s and %s", *old.Username, *oldUser.Username)
 	}
 	if *old.AddressLine1 != *oldUser.AddressLine1 {
-		t.Fatalf("expected AddressLine1 to be equal, got %v and %v", *old.AddressLine1, *oldUser.AddressLine1)
+		t.Fatalf("expected AddressLine1 to be equal, got %s and %s", *old.AddressLine1, *oldUser.AddressLine1)
 	}
 	if *old.AddressLine2 != *oldUser.AddressLine2 {
-		t.Fatalf("expected AddressLine2 to be equal, got %v and %v", *old.AddressLine2, *oldUser.AddressLine2)
+		t.Fatalf("expected AddressLine2 to be equal, got %s and %s", *old.AddressLine2, *oldUser.AddressLine2)
 	}
 	if *old.City != *oldUser.City {
-		t.Fatalf("expected City to be equal, got %v and %v", *old.City, *oldUser.City)
+		t.Fatalf("expected City to be equal, got %s and %s", *old.City, *oldUser.City)
 	}
 	if *old.Company != *oldUser.Company {
-		t.Fatalf("expected Company to be equal, got %v and %v", *old.Company, *oldUser.Company)
+		t.Fatalf("expected Company to be equal, got %s and %s", *old.Company, *oldUser.Company)
 	}
 	if old.ConfirmLocalPassword != nil {
-		t.Fatalf("expected ConfirmLocalPassword to be nil, got %v", *old.ConfirmLocalPassword)
+		t.Fatalf("expected ConfirmLocalPassword to be nil, got %s", *old.ConfirmLocalPassword)
 	}
 	if *old.Country != *oldUser.Country {
-		t.Fatalf("expected Country to be equal, got %v and %v", *old.Country, *oldUser.Country)
+		t.Fatalf("expected Country to be equal, got %s and %s", *old.Country, *oldUser.Country)
 	}
 	if *old.Email != *oldUser.Email {
-		t.Fatalf("expected Email to be equal, got %v and %v", *old.Email, *oldUser.Email)
+		t.Fatalf("expected Email to be equal, got %s and %s", *old.Email, *oldUser.Email)
 	}
 	if old.GID != nil {
-		t.Fatalf("expected GID to be null, got %v", *old.GID)
+		t.Fatalf("expected GID to be null, got %d", *old.GID)
 	}
 	if *old.ID != *oldUser.ID {
-		t.Fatalf("expected ID to be equal, got %v and %v", *old.ID, *oldUser.ID)
+		t.Fatalf("expected ID to be equal, got %d and %d", *old.ID, *oldUser.ID)
 	}
 	if old.LocalPassword != nil {
-		t.Fatalf("expected LocalPassword to be nil, got %v", *old.LocalPassword)
+		t.Fatalf("expected LocalPassword to be nil, got %s", *old.LocalPassword)
 	}
 	if *old.PhoneNumber != *oldUser.PhoneNumber {
-		t.Fatalf("expected PhoneNumber to be equal, got %v and %v", *old.PhoneNumber, *oldUser.PhoneNumber)
+		t.Fatalf("expected PhoneNumber to be equal, got %s and %s", *old.PhoneNumber, *oldUser.PhoneNumber)
 	}
 	if old.PublicSSHKey != nil {
-		t.Fatalf("expected PublicSSHKey to be null, got %v", *old.PublicSSHKey)
+		t.Fatalf("expected PublicSSHKey to be null, got %s", *old.PublicSSHKey)
 	}
 	if *old.StateOrProvince != *oldUser.StateOrProvince {
-		t.Fatalf("expected StateOrProvince to be equal, got %v and %v", *old.StateOrProvince, *oldUser.StateOrProvince)
+		t.Fatalf("expected StateOrProvince to be equal, got %s and %s", *old.StateOrProvince, *oldUser.StateOrProvince)
 	}
 	if old.Tenant != nil {
-		t.Fatalf("expected Tenant to be null, got %v", *old.Tenant)
+		t.Fatalf("expected Tenant to be null, got %s", *old.Tenant)
 	}
 	if old.Token != nil {
-		t.Fatalf("expected Token to be null, got %v", *old.Token)
+		t.Fatalf("expected Token to be null, got %s", *old.Token)
 	}
 	if old.UID != nil {
-		t.Fatalf("expected UID to be null, got %v", *old.UID)
+		t.Fatalf("expected UID to be null, got %d", *old.UID)
 	}
 }
 
@@ -158,64 +158,64 @@ func TestUpgrade(t *testing.T) {
 	newUser := oldUser.Upgrade()
 
 	if *user.FullName != *newUser.FullName {
-		t.Fatalf("expected FullName to be equal, got %v and %v", *user.FullName, *newUser.FullName)
+		t.Fatalf("expected FullName to be equal, got %s and %s", *user.FullName, *newUser.FullName)
 	}
 	if user.NewUser != newUser.NewUser {
-		t.Fatalf("expected NewUser to be equal, got %v and %v", user.NewUser, newUser.NewUser)
+		t.Fatalf("expected NewUser to be equal, got %t and %t", user.NewUser, newUser.NewUser)
 	}
 	if user.Role != newUser.Role {
-		t.Fatalf("expected Role to be equal, got %v and %v", user.Role, newUser.Role)
+		t.Fatalf("expected Role to be equal, got %s and %s", user.Role, newUser.Role)
 	}
 	if user.TenantID != newUser.TenantID {
-		t.Fatalf("expected TenantID to be equal, got %v and %v", user.TenantID, newUser.TenantID)
+		t.Fatalf("expected TenantID to be equal, got %d and %d", user.TenantID, newUser.TenantID)
 	}
 	if user.Username != newUser.Username {
-		t.Fatalf("expected Username to be equal, got %v and %v", user.Username, newUser.Username)
+		t.Fatalf("expected Username to be equal, got %s and %s", user.Username, newUser.Username)
 	}
 	if *user.AddressLine1 != *newUser.AddressLine1 {
-		t.Fatalf("expected AddressLine1 to be equal, got %v and %v", *user.AddressLine1, *newUser.AddressLine1)
+		t.Fatalf("expected AddressLine1 to be equal, got %s and %s", *user.AddressLine1, *newUser.AddressLine1)
 	}
 	if *user.AddressLine2 != *newUser.AddressLine2 {
-		t.Fatalf("expected AddressLine2 to be equal, got %v and %v", *user.AddressLine2, *newUser.AddressLine2)
+		t.Fatalf("expected AddressLine2 to be equal, got %s and %s", *user.AddressLine2, *newUser.AddressLine2)
 	}
 	if *user.City != *newUser.City {
-		t.Fatalf("expected City to be equal, got %v and %v", *user.City, *newUser.City)
+		t.Fatalf("expected City to be equal, got %s and %s", *user.City, *newUser.City)
 	}
 	if *user.Company != *newUser.Company {
-		t.Fatalf("expected Company to be equal, got %v and %v", *user.Company, *newUser.Company)
+		t.Fatalf("expected Company to be equal, got %s and %s", *user.Company, *newUser.Company)
 	}
 	if *user.Country != *newUser.Country {
-		t.Fatalf("expected Country to be equal, got %v and %v", *user.Country, *newUser.Country)
+		t.Fatalf("expected Country to be equal, got %s and %s", *user.Country, *newUser.Country)
 	}
 	if *user.Email != *newUser.Email {
-		t.Fatalf("expected Email to be equal, got %v and %v", *user.Email, *newUser.Email)
+		t.Fatalf("expected Email to be equal, got %s and %s", *user.Email, *newUser.Email)
 	}
 	if user.GID != nil {
-		t.Fatalf("expected GID to be null, got %v", *user.GID)
+		t.Fatalf("expected GID to be null, got %d", *user.GID)
 	}
 	if *user.ID != *newUser.ID {
-		t.Fatalf("expected ID to be equal, got %v and %v", *user.ID, *newUser.ID)
+		t.Fatalf("expected ID to be equal, got %d and %d", *user.ID, *newUser.ID)
 	}
 	if *user.LocalPassword != *newUser.LocalPassword {
-		t.Fatalf("expected LocalPassword to be equal, got %v and %v", *user.LocalPassword, *newUser.LocalPassword)
+		t.Fatalf("expected LocalPassword to be equal, got %s and %s", *user.LocalPassword, *newUser.LocalPassword)
 	}
 	if *user.PhoneNumber != *newUser.PhoneNumber {
-		t.Fatalf("expected PhoneNumber to be equal, got %v and %v", *user.PhoneNumber, *newUser.PhoneNumber)
+		t.Fatalf("expected PhoneNumber to be equal, got %s and %s", *user.PhoneNumber, *newUser.PhoneNumber)
 	}
 	if user.PublicSSHKey != nil {
-		t.Fatalf("expected PublicSSHKey to be null, got %v", *user.PublicSSHKey)
+		t.Fatalf("expected PublicSSHKey to be null, got %s", *user.PublicSSHKey)
 	}
 	if *user.StateOrProvince != *newUser.StateOrProvince {
-		t.Fatalf("expected StateOrProvince to be equal, got %v and %v", *user.StateOrProvince, *newUser.StateOrProvince)
+		t.Fatalf("expected StateOrProvince to be equal, got %s and %s", *user.StateOrProvince, *newUser.StateOrProvince)
 	}
 	if user.Tenant != nil {
-		t.Fatalf("expected Tenant to be null, got %v", *user.Tenant)
+		t.Fatalf("expected Tenant to be null, got %s", *user.Tenant)
 	}
 	if user.Token != nil {
-		t.Fatalf("expected Token to be null, got %v", *user.Token)
+		t.Fatalf("expected Token to be null, got %s", *user.Token)
 	}
 	if user.UID != nil {
-		t.Fatalf("expected UID to be null, got %v", *user.UID)
+		t.Fatalf("expected UID to be null, got %d", *user.UID)
 	}
 }
 
@@ -223,64 +223,64 @@ func TestUpgradeCurrent(t *testing.T) {
 	newUser := legacyUser.Upgrade(user.RegistrationSent, user.LastAuthenticated, user.UCDN, user.ChangeLogCount)
 
 	if *user.FullName != *newUser.FullName {
-		t.Fatalf("expected FullName to be equal, got %v and %v", *user.FullName, *newUser.FullName)
+		t.Fatalf("expected FullName to be equal, got %s and %s", *user.FullName, *newUser.FullName)
 	}
 	if user.NewUser != newUser.NewUser {
-		t.Fatalf("expected NewUser to be equal, got %v and %v", user.NewUser, newUser.NewUser)
+		t.Fatalf("expected NewUser to be equal, got %s and %s", user.NewUser, newUser.NewUser)
 	}
 	if user.Role != newUser.Role {
-		t.Fatalf("expected Role to be equal, got %v and %v", user.Role, newUser.Role)
+		t.Fatalf("expected Role to be equal, got %s and %s", user.Role, newUser.Role)
 	}
 	if user.TenantID != newUser.TenantID {
-		t.Fatalf("expected TenantID to be equal, got %v and %v", user.TenantID, newUser.TenantID)
+		t.Fatalf("expected TenantID to be equal, got %s and %s", user.TenantID, newUser.TenantID)
 	}
 	if user.Username != newUser.Username {
-		t.Fatalf("expected Username to be equal, got %v and %v", user.Username, newUser.Username)
+		t.Fatalf("expected Username to be equal, got %s and %s", user.Username, newUser.Username)
 	}
 	if *user.AddressLine1 != *newUser.AddressLine1 {
-		t.Fatalf("expected AddressLine1 to be equal, got %v and %v", *user.AddressLine1, *newUser.AddressLine1)
+		t.Fatalf("expected AddressLine1 to be equal, got %s and %s", *user.AddressLine1, *newUser.AddressLine1)
 	}
 	if *user.AddressLine2 != *newUser.AddressLine2 {
-		t.Fatalf("expected AddressLine2 to be equal, got %v and %v", *user.AddressLine2, *newUser.AddressLine2)
+		t.Fatalf("expected AddressLine2 to be equal, got %s and %s", *user.AddressLine2, *newUser.AddressLine2)
 	}
 	if *user.City != *newUser.City {
-		t.Fatalf("expected City to be equal, got %v and %v", *user.City, *newUser.City)
+		t.Fatalf("expected City to be equal, got %s and %s", *user.City, *newUser.City)
 	}
 	if *user.Company != *newUser.Company {
-		t.Fatalf("expected Company to be equal, got %v and %v", *user.Company, *newUser.Company)
+		t.Fatalf("expected Company to be equal, got %s and %s", *user.Company, *newUser.Company)
 	}
 	if *user.Country != *newUser.Country {
-		t.Fatalf("expected Country to be equal, got %v and %v", *user.Country, *newUser.Country)
+		t.Fatalf("expected Country to be equal, got %s and %s", *user.Country, *newUser.Country)
 	}
 	if *user.Email != *newUser.Email {
-		t.Fatalf("expected Email to be equal, got %v and %v", *user.Email, *newUser.Email)
+		t.Fatalf("expected Email to be equal, got %s and %s", *user.Email, *newUser.Email)
 	}
 	if user.GID != nil {
-		t.Fatalf("expected GID to be null, got %v", *user.GID)
+		t.Fatalf("expected GID to be null, got %s", *user.GID)
 	}
 	if *user.ID != *newUser.ID {
-		t.Fatalf("expected ID to be equal, got %v and %v", *user.ID, *newUser.ID)
+		t.Fatalf("expected ID to be equal, got %s and %s", *user.ID, *newUser.ID)
 	}
 	if newUser.LocalPassword != nil {
-		t.Fatalf("expected LocalPassword to be nil, got %v", newUser.LocalPassword)
+		t.Fatalf("expected LocalPassword to be nil, got %s", newUser.LocalPassword)
 	}
 	if *user.PhoneNumber != *newUser.PhoneNumber {
-		t.Fatalf("expected PhoneNumber to be equal, got %v and %v", *user.PhoneNumber, *newUser.PhoneNumber)
+		t.Fatalf("expected PhoneNumber to be equal, got %s and %s", *user.PhoneNumber, *newUser.PhoneNumber)
 	}
 	if user.PublicSSHKey != nil {
-		t.Fatalf("expected PublicSSHKey to be null, got %v", *user.PublicSSHKey)
+		t.Fatalf("expected PublicSSHKey to be null, got %s", *user.PublicSSHKey)
 	}
 	if *user.StateOrProvince != *newUser.StateOrProvince {
-		t.Fatalf("expected StateOrProvince to be equal, got %v and %v", *user.StateOrProvince, *newUser.StateOrProvince)
+		t.Fatalf("expected StateOrProvince to be equal, got %s and %s", *user.StateOrProvince, *newUser.StateOrProvince)
 	}
 	if user.Tenant != nil {
-		t.Fatalf("expected Tenant to be null, got %v", *user.Tenant)
+		t.Fatalf("expected Tenant to be null, got %s", *user.Tenant)
 	}
 	if user.Token != nil {
-		t.Fatalf("expected Token to be null, got %v", *user.Token)
+		t.Fatalf("expected Token to be null, got %s", *user.Token)
 	}
 	if user.UID != nil {
-		t.Fatalf("expected UID to be null, got %v", *user.UID)
+		t.Fatalf("expected UID to be null, got %s", *user.UID)
 	}
 }
 
@@ -288,60 +288,60 @@ func TestDowngradeCurrent(t *testing.T) {
 	old := user.ToLegacyCurrentUser(*legacyUser.Role, *legacyUser.LocalUser)
 
 	if *old.FullName != *oldUser.FullName {
-		t.Fatalf("expected FullName to be equal, got %v and %v", *old.FullName, *oldUser.FullName)
+		t.Fatalf("expected FullName to be equal, got %s and %s", *old.FullName, *oldUser.FullName)
 	}
 	if *old.NewUser != *oldUser.NewUser {
-		t.Fatalf("expected NewUser to be equal, got %v and %v", *old.NewUser, *oldUser.NewUser)
+		t.Fatalf("expected NewUser to be equal, got %t and %t", *old.NewUser, *oldUser.NewUser)
 	}
 	if *old.RoleName != *oldUser.RoleName {
-		t.Fatalf("expected RoleName to be equal, got %v and %v", *old.RoleName, *oldUser.RoleName)
+		t.Fatalf("expected RoleName to be equal, got %s and %s", *old.RoleName, *oldUser.RoleName)
 	}
 	if *old.TenantID != *oldUser.TenantID {
-		t.Fatalf("expected TenantID to be equal, got %v and %v", *old.TenantID, *oldUser.TenantID)
+		t.Fatalf("expected TenantID to be equal, got %d and %d", *old.TenantID, *oldUser.TenantID)
 	}
 	if *old.UserName != *oldUser.Username {
-		t.Fatalf("expected Username to be equal, got %v and %v", *old.UserName, *oldUser.Username)
+		t.Fatalf("expected Username to be equal, got %s and %s", *old.UserName, *oldUser.Username)
 	}
 	if *old.AddressLine1 != *oldUser.AddressLine1 {
-		t.Fatalf("expected AddressLine1 to be equal, got %v and %v", *old.AddressLine1, *oldUser.AddressLine1)
+		t.Fatalf("expected AddressLine1 to be equal, got %s and %s", *old.AddressLine1, *oldUser.AddressLine1)
 	}
 	if *old.AddressLine2 != *oldUser.AddressLine2 {
-		t.Fatalf("expected AddressLine2 to be equal, got %v and %v", *old.AddressLine2, *oldUser.AddressLine2)
+		t.Fatalf("expected AddressLine2 to be equal, got %s and %s", *old.AddressLine2, *oldUser.AddressLine2)
 	}
 	if *old.City != *oldUser.City {
-		t.Fatalf("expected City to be equal, got %v and %v", *old.City, *oldUser.City)
+		t.Fatalf("expected City to be equal, got %s and %s", *old.City, *oldUser.City)
 	}
 	if *old.Company != *oldUser.Company {
-		t.Fatalf("expected Company to be equal, got %v and %v", *old.Company, *oldUser.Company)
+		t.Fatalf("expected Company to be equal, got %s and %s", *old.Company, *oldUser.Company)
 	}
 	if *old.Country != *oldUser.Country {
-		t.Fatalf("expected Country to be equal, got %v and %v", *old.Country, *oldUser.Country)
+		t.Fatalf("expected Country to be equal, got %s and %s", *old.Country, *oldUser.Country)
 	}
 	if *old.Email != *oldUser.Email {
-		t.Fatalf("expected Email to be equal, got %v and %v", *old.Email, *oldUser.Email)
+		t.Fatalf("expected Email to be equal, got %s and %s", *old.Email, *oldUser.Email)
 	}
 	if old.GID != nil {
-		t.Fatalf("expected GID to be null, got %v", *old.GID)
+		t.Fatalf("expected GID to be null, got %d", *old.GID)
 	}
 	if *old.ID != *oldUser.ID {
-		t.Fatalf("expected ID to be equal, got %v and %v", *old.ID, *oldUser.ID)
+		t.Fatalf("expected ID to be equal, got %d and %d", *old.ID, *oldUser.ID)
 	}
 	if *old.PhoneNumber != *oldUser.PhoneNumber {
-		t.Fatalf("expected PhoneNumber to be equal, got %v and %v", *old.PhoneNumber, *oldUser.PhoneNumber)
+		t.Fatalf("expected PhoneNumber to be equal, got %s and %s", *old.PhoneNumber, *oldUser.PhoneNumber)
 	}
 	if old.PublicSSHKey != nil {
-		t.Fatalf("expected PublicSSHKey to be null, got %v", *old.PublicSSHKey)
+		t.Fatalf("expected PublicSSHKey to be null, got %s", *old.PublicSSHKey)
 	}
 	if *old.StateOrProvince != *oldUser.StateOrProvince {
-		t.Fatalf("expected StateOrProvince to be equal, got %v and %v", *old.StateOrProvince, *oldUser.StateOrProvince)
+		t.Fatalf("expected StateOrProvince to be equal, got %s and %s", *old.StateOrProvince, *oldUser.StateOrProvince)
 	}
 	if old.Tenant != nil {
-		t.Fatalf("expected Tenant to be null, got %v", *old.Tenant)
+		t.Fatalf("expected Tenant to be null, got %s", *old.Tenant)
 	}
 	if old.Token != nil {
-		t.Fatalf("expected Token to be null, got %v", *old.Token)
+		t.Fatalf("expected Token to be null, got %s", *old.Token)
 	}
 	if old.UID != nil {
-		t.Fatalf("expected UID to be null, got %v", *old.UID)
+		t.Fatalf("expected UID to be null, got %d", *old.UID)
 	}
 }
