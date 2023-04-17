0648d080c722438f38b46df63c44be93531c2a95
shamrickus <shamrickus@gmail.com>
2023-04-17 09:16:29 -0600

    Also raise timeouts

--- infrastructure/cdn-in-a-box/traffic_portal_v2_e2e_test/run.sh
+++ infrastructure/cdn-in-a-box/traffic_portal_v2_e2e_test/run.sh
@@ -44,7 +44,7 @@ TMOUT
 
 cd /lang/traffic-portal
 
-jq --arg TPURL $TP_URL --arg TOURL https://$TO_FQDN:$TO_PORT '.tp_url = $TPURL | .to_url = $TOURL | .disableColors = true' \
+jq --arg TPURL $TP_URL --arg TOURL https://$TO_FQDN:$TO_PORT '.tp_url = $TPURL | .to_url = $TOURL | .disableColors = true | .retryAssertionTimeoutMS = 10000 | .waitForConditionTimeoutMS = 10000' \
 	nightwatch/config.json > config.tmp.json && mv config.tmp.json nightwatch/config.json
 
 npm run e2e:ci
