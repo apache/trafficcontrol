a0c5569d72b2b76bc26fb3c3a8625afe071e1063
Rima Shah <rima_shah@comcast.com>
2023-05-04 10:40:07 -0600

    Fixed Api version and browser page path

--- experimental/traffic-portal/nightwatch/tests/users/role/table.spec.ts
+++ experimental/traffic-portal/nightwatch/tests/users/role/table.spec.ts
@@ -14,9 +14,11 @@
 
 describe("Roles Spec", () => {
 	it("Loads elements", async () => {
-		browser.page.users.roles().navigate()
-			.waitForElementPresent("input[name=fuzzControl]");
-		browser.elements("css selector", "div.ag-row", rows => {
+		await browser.page.common()
+			.section.sidebar
+			.navigateToNode("roles", ["usersContainer"]);
+		await browser.waitForElementPresent("input[name=fuzzControl]");
+		await browser.elements("css selector", "div.ag-row", rows => {
 			browser.assert.ok(rows.status === 0);
 			browser.assert.ok((rows.value as []).length >= 1);
 		});
