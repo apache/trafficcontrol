9806fadb7d7b264ae0c4e31d0c607eaf3bc2ae11
Srijeet Chatterjee <srijeet0406@gmail.com>
2023-06-07 17:14:17 -0600

    wip

--- lib/go-tc/deliveryservice_request_comments.go
+++ lib/go-tc/deliveryservice_request_comments.go
@@ -1,5 +1,12 @@
 package tc
 
+import (
+	"encoding/json"
+	"errors"
+	"github.com/apache/trafficcontrol/lib/go-util"
+	"time"
+)
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,6 +25,170 @@ package tc
  * specific language governing permissions and limitations
  * under the License.
  */
+
+type DeliveryServiceRequestCommentV5 DeliveryServiceRequestCommentV50
+
+type DeliveryServiceRequestCommentV50 struct {
+	AuthorID                 IDNoMod   `json:"authorId" db:"author_id"`
+	Author                   string    `json:"author"`
+	DeliveryServiceRequestID int       `json:"deliveryServiceRequestId" db:"deliveryservice_request_id"`
+	ID                       int       `json:"id" db:"id"`
+	LastUpdated              time.Time `json:"lastUpdated" db:"last_updated"`
+	Value                    string    `json:"value" db:"value"`
+	XMLID                    string    `json:"xmlId" db:"xml_id"`
+}
+
+type DeliveryServiceRequestCommentsResponseV5 DeliveryServiceRequestCommentsResponseV50
+
+type DeliveryServiceRequestCommentsResponseV50 struct {
+	Response []DeliveryServiceRequestCommentV5 `json:"response"`
+	Alerts
+}
+
+func (d DeliveryServiceRequestCommentsResponse) Upgrade() (DeliveryServiceRequestCommentsResponseV5, error) {
+	deliveryServiceRequestCommentsResponse := DeliveryServiceRequestCommentsResponseV5{}
+	deliveryServiceRequestCommentsResponse.Response = make([]DeliveryServiceRequestCommentV5, 0)
+	for _, dsrc := range d.Response {
+		dsrcV5, err := dsrc.Upgrade()
+		if err != nil {
+			return DeliveryServiceRequestCommentsResponseV5{}, err
+		}
+		deliveryServiceRequestCommentsResponse.Response = append(deliveryServiceRequestCommentsResponse.Response, dsrcV5)
+	}
+	deliveryServiceRequestCommentsResponse.Alerts = d.Alerts
+	return deliveryServiceRequestCommentsResponse, nil
+}
+
+func (d DeliveryServiceRequestCommentsResponseV5) Downgrade() DeliveryServiceRequestCommentsResponse {
+	deliveryServiceRequestCommentsResponse := DeliveryServiceRequestCommentsResponse{}
+	deliveryServiceRequestCommentsResponse.Response = make([]DeliveryServiceRequestComment, 0)
+	for _, dsrc := range d.Response {
+		dsrcLegacy := dsrc.Downgrade()
+		deliveryServiceRequestCommentsResponse.Response = append(deliveryServiceRequestCommentsResponse.Response, dsrcLegacy)
+	}
+	deliveryServiceRequestCommentsResponse.Alerts = d.Alerts
+	return deliveryServiceRequestCommentsResponse
+}
+
+func (d DeliveryServiceRequestCommentV5) DowngradeToNullable() DeliveryServiceRequestCommentNullable {
+	t := TimeNoModFromTime(d.LastUpdated)
+	deliveryServiceRequestComment := DeliveryServiceRequestCommentNullable{
+		AuthorID:                 &d.AuthorID,
+		Author:                   &d.Author,
+		DeliveryServiceRequestID: &d.DeliveryServiceRequestID,
+		ID:                       &d.ID,
+		LastUpdated:              t,
+		Value:                    &d.Value,
+		XMLID:                    &d.XMLID,
+	}
+	return deliveryServiceRequestComment
+}
+
+func (d DeliveryServiceRequestCommentNullable) UpgradeFromNullable() (DeliveryServiceRequestCommentV5, error) {
+	deliveryServiceRequestCommentV5 := DeliveryServiceRequestCommentV5{}
+	if d.AuthorID != nil {
+		deliveryServiceRequestCommentV5.AuthorID = *d.AuthorID
+	}
+	if d.Author != nil {
+		deliveryServiceRequestCommentV5.Author = *d.Author
+	}
+	if d.DeliveryServiceRequestID != nil {
+		deliveryServiceRequestCommentV5.DeliveryServiceRequestID = *d.DeliveryServiceRequestID
+	}
+	if d.ID != nil {
+		deliveryServiceRequestCommentV5.ID = *d.ID
+	}
+	if d.LastUpdated != nil {
+		t := d.LastUpdated.Time
+		updatedTime, err := util.ConvertTimeFormat(t, time.RFC3339)
+		if err != nil {
+			return DeliveryServiceRequestCommentV5{}, err
+		}
+		deliveryServiceRequestCommentV5.LastUpdated = *updatedTime
+	}
+	if d.Value != nil {
+		deliveryServiceRequestCommentV5.Value = *d.Value
+	}
+	if d.XMLID != nil {
+		deliveryServiceRequestCommentV5.XMLID = *d.XMLID
+	}
+	return deliveryServiceRequestCommentV5, nil
+}
+
+func (d DeliveryServiceRequestComment) Upgrade() (DeliveryServiceRequestCommentV5, error) {
+	t := d.LastUpdated.Time
+	updatedTime, err := util.ConvertTimeFormat(t, time.RFC3339)
+	if err != nil {
+		return DeliveryServiceRequestCommentV5{}, err
+	}
+	deliveryServiceRequestCommentV5 := DeliveryServiceRequestCommentV5{
+		Author:                   d.Author,
+		AuthorID:                 d.AuthorID,
+		DeliveryServiceRequestID: d.DeliveryServiceRequestID,
+		ID:                       d.ID,
+		LastUpdated:              *updatedTime,
+		Value:                    d.Value,
+		XMLID:                    d.XMLID,
+	}
+	return deliveryServiceRequestCommentV5, nil
+}
+
+func (d DeliveryServiceRequestCommentV5) Downgrade() DeliveryServiceRequestComment {
+	t := TimeNoModFromTime(d.LastUpdated)
+	return DeliveryServiceRequestComment{
+		AuthorID:                 d.AuthorID,
+		Author:                   d.Author,
+		DeliveryServiceRequestID: d.DeliveryServiceRequestID,
+		ID:                       d.ID,
+		LastUpdated:              *t,
+		Value:                    d.Value,
+		XMLID:                    d.XMLID,
+	}
+}
+
+// UnmarshalJSON implements the encoding/json.Unmarshaler interface with a
+// customized decoding to force the date format on LastUpdated.
+func (d *DeliveryServiceRequestCommentV5) UnmarshalJSON(data []byte) error {
+	type Alias DeliveryServiceRequestCommentV5
+	resp := struct {
+		LastUpdated string `json:"lastUpdated"`
+		*Alias
+	}{
+		Alias: (*Alias)(d),
+	}
+	err := json.Unmarshal(data, &resp)
+	if err != nil {
+		return err
+	}
+
+	d.LastUpdated, err = parseTimeV5(resp.LastUpdated)
+	if err != nil {
+		return errors.New("invalid timestamp given for lastUpdated: " + err.Error())
+	}
+	return nil
+}
+
+func parseTimeV5(ts string) (time.Time, error) {
+	rt, err := time.Parse(time.RFC3339, ts)
+	if err == nil {
+		return rt, err
+	}
+	return time.Parse(dateFormat, ts)
+}
+
+// MarshalJSON implements the encoding/json.Marshaler interface with a
+// customized encoding to force the date format on LastUpdated.
+func (d DeliveryServiceRequestCommentV5) MarshalJSON() ([]byte, error) {
+	type Alias DeliveryServiceRequestCommentV5
+	resp := struct {
+		LastUpdated string `json:"lastUpdated"`
+		Alias
+	}{
+		LastUpdated: d.LastUpdated.Format(time.RFC3339),
+		Alias:       (Alias)(d),
+	}
+	return json.Marshal(&resp)
+}
 
 // DeliveryServiceRequestCommentsResponse is a list of
 // DeliveryServiceRequestComments as a response.
--- traffic_ops/traffic_ops_golang/deliveryservice/request/comment/comments.go
+++ traffic_ops/traffic_ops_golang/deliveryservice/request/comment/comments.go
@@ -20,7 +20,11 @@ package comment
  */
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/apache/trafficcontrol/lib/go-log"
+	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/util/ims"
 	"net/http"
 	"strconv"
 	"time"
@@ -47,6 +51,15 @@ func (v *TODeliveryServiceRequestComment) GetLastUpdated() (*time.Time, bool, er
 func (v *TODeliveryServiceRequestComment) SetLastUpdated(t tc.TimeNoMod) { v.LastUpdated = &t }
 func (v *TODeliveryServiceRequestComment) InsertQuery() string           { return insertQuery() }
 func (v *TODeliveryServiceRequestComment) SelectMaxLastUpdatedQuery(where, orderBy, pagination, tableName string) string {
+	return `SELECT max(t) from (
+		SELECT max(dsrc.last_updated) as t from deliveryservice_request_comment dsrc
+JOIN tm_user a ON dsrc.author_id = a.id
+JOIN deliveryservice_request dsr ON dsrc.deliveryservice_request_id = dsr.id ` + where + orderBy + pagination +
+		` UNION ALL
+	select max(last_updated) as t from last_deleted l where l.table_name='deliveryservice_request_comment') as res`
+}
+
+func selectMaxLastUpdatedQuery(where, orderBy, pagination string) string {
 	return `SELECT max(t) from (
 		SELECT max(dsrc.last_updated) as t from deliveryservice_request_comment dsrc
 JOIN tm_user a ON dsrc.author_id = a.id
@@ -185,4 +198,193 @@ WHERE id=:id RETURNING last_updated`
 
 func deleteQuery() string {
 	return `DELETE FROM deliveryservice_request_comment WHERE id = :id`
+}
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	var maxTime time.Time
+	var runSecond bool
+	inf, userErr, sysErr, errCode := api.NewInfo(r, nil, nil)
+	tx := inf.Tx.Tx
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, tx, errCode, userErr, sysErr)
+		return
+	}
+	defer inf.Close()
+
+	cols := map[string]dbhelpers.WhereColumnInfo{
+		"authorId":                 dbhelpers.WhereColumnInfo{Column: "dsrc.author_id"},
+		"author":                   dbhelpers.WhereColumnInfo{Column: "a.username"},
+		"deliveryServiceRequestId": dbhelpers.WhereColumnInfo{Column: "dsrc.deliveryservice_request_id"},
+		"id":                       dbhelpers.WhereColumnInfo{Column: "dsrc.id", Checker: api.IsInt},
+	}
+
+	where, orderBy, pagination, queryValues, errs := dbhelpers.BuildWhereAndOrderByAndPagination(inf.Params, cols)
+	if len(errs) > 0 {
+		errCode = http.StatusBadRequest
+		userErr = util.JoinErrs(errs)
+		api.HandleErr(w, r, tx, errCode, userErr, nil)
+		return
+	}
+
+	if inf.Config.UseIMS {
+		runSecond, maxTime = ims.TryIfModifiedSinceQuery(inf.Tx, r.Header, queryValues, selectMaxLastUpdatedQuery(where, orderBy, pagination))
+		if !runSecond {
+			log.Debugln("IMS HIT")
+			api.AddLastModifiedHdr(w, maxTime)
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+		log.Debugln("IMS MISS")
+	} else {
+		log.Debugln("Non IMS request")
+	}
+
+	deliveryServiceRequestComments := []tc.DeliveryServiceRequestCommentV5{}
+	query := selectQuery() + where + orderBy + pagination
+	rows, err := inf.Tx.NamedQuery(query, queryValues)
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, errors.New("querying cdn locks: "+err.Error()))
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var deliveryServiceRequestComment tc.DeliveryServiceRequestCommentNullable
+		if err = rows.StructScan(&deliveryServiceRequestComment); err != nil {
+			api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, errors.New("scanning delivery service request comments: "+err.Error()))
+			return
+		}
+		dsrcV5, err := deliveryServiceRequestComment.UpgradeFromNullable()
+		if err != nil {
+			api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, errors.New("converting to deliveryServiceRequestCommentV5: "+err.Error()))
+			return
+		}
+		deliveryServiceRequestComments = append(deliveryServiceRequestComments, dsrcV5)
+	}
+	api.WriteResp(w, r, deliveryServiceRequestComments)
+}
+
+func Validate(dsrc tc.DeliveryServiceRequestCommentV5) error {
+	errs := validation.Errors{
+		"deliveryServiceRequestId": validation.Validate(dsrc.DeliveryServiceRequestID, validation.NotNil),
+		"value":                    validation.Validate(dsrc.Value, validation.NotNil),
+	}
+	return util.JoinErrs(tovalidate.ToErrors(errs))
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	var deliveryServiceRequestComment tc.DeliveryServiceRequestCommentV5
+
+	inf, userErr, sysErr, errCode := api.NewInfo(r, []string{"id"}, nil)
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, errCode, userErr, sysErr)
+		return
+	}
+	defer inf.Close()
+
+	tx := inf.Tx.Tx
+	if err := json.NewDecoder(r.Body).Decode(&deliveryServiceRequestComment); err != nil {
+		api.HandleErr(w, r, tx, http.StatusBadRequest, err, nil)
+		return
+	}
+
+	if err := Validate(deliveryServiceRequestComment); err != nil {
+		api.HandleErr(w, r, tx, http.StatusBadRequest, err, nil)
+		return
+	}
+
+	var current TODeliveryServiceRequestComment
+	err := inf.Tx.QueryRowx(selectQuery() + `WHERE dsrc.id=` + inf.Params["id"]).StructScan(&current)
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, errors.New("scanning deliveryservice_request_comment: "+err.Error()))
+		return
+	}
+
+	userID := tc.IDNoMod(inf.User.ID)
+	if *current.AuthorID != userID {
+		api.HandleErr(w, r, tx, http.StatusBadRequest, errors.New("comments can only be updated by the author"), nil)
+		return
+	}
+
+	idParam := inf.Params["id"]
+	id, parseErr := strconv.Atoi(idParam)
+	if parseErr != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, http.StatusBadRequest, errors.New("id must be an integer"), nil)
+		return
+	}
+	deliveryServiceRequestComment.ID = id
+	userErr, sysErr, sc := api.CheckIfUnModified(r.Header, inf.Tx, id, "deliveryservice_request_comment")
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, tx, sc, userErr, sysErr)
+		return
+	}
+
+	rows, err := inf.Tx.NamedQuery(updateQuery(), deliveryServiceRequestComment)
+	if err != nil {
+		userErr, sysErr, sc := api.ParseDBError(err)
+		api.HandleErr(w, r, tx, sc, userErr, sysErr)
+		return
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		api.HandleErr(w, r, tx, http.StatusNotFound, errors.New("no deliveryservice_request_comment found with this id"), nil)
+		return
+	}
+	lastUpdated := tc.TimeNoMod{}
+	if err := rows.Scan(&lastUpdated); err != nil {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, errors.New("scanning lastUpdated from deliveryservice_request_comment insert: "+err.Error()))
+		return
+	}
+	deliveryServiceRequestComment.LastUpdated = time.Now()
+
+	t := lastUpdated.Time
+	updatedTime, err := util.ConvertTimeFormat(t, time.RFC3339)
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, errors.New("converting last_updated to RFC3339 format: "+err.Error()))
+		return
+	}
+	deliveryServiceRequestComment.LastUpdated = *updatedTime
+
+	if rows.Next() {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, errors.New("deliveryservice_request_comment update affected too many rows: >1"))
+		return
+	}
+
+	alerts := tc.CreateAlerts(tc.SuccessLevel, "deliveryservice_request_comment was updated.")
+	api.WriteAlertsObj(w, r, http.StatusOK, alerts, deliveryServiceRequestComment)
+	changeLogMsg := fmt.Sprintf("DELIVERYSERVICE_REQUEST_COMMENT: %d, ID: %d, ACTION: Updated deliveryservice_request_comment", deliveryServiceRequestComment.ID, deliveryServiceRequestComment.ID)
+	api.CreateChangeLogRawTx(api.ApiChange, changeLogMsg, inf.User, tx)
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	inf, userErr, sysErr, errCode := api.NewInfo(r, []string{"id"}, nil)
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, errCode, userErr, sysErr)
+		return
+	}
+	defer inf.Close()
+
+	tx := inf.Tx.Tx
+
+	idParam := inf.Params["id"]
+	id, parseErr := strconv.Atoi(idParam)
+	if parseErr != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, http.StatusBadRequest, errors.New("id must be an integer"), nil)
+		return
+	}
+	rows, err := tx.Query(deleteQuery(), id)
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, fmt.Errorf("deleting deliveryservice_request_comment: %w", err))
+		return
+	}
+	rows.Close()
+	alerts := tc.CreateAlerts(tc.SuccessLevel, "deliveryservice_request_comment was deleted.")
+	api.WriteAlerts(w, r, http.StatusOK, alerts)
+	changeLogMsg := fmt.Sprintf("DELIVERYSERVICE_REQUEST_COMMENT: %d, ACTION: Deleted Deliveryservice_request_comment", id)
+	api.CreateChangeLogRawTx(api.ApiChange, changeLogMsg, inf.User, tx)
 }
