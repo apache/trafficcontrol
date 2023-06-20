20e470e3f5cbb9ae4c5b84d1a0b4b3d47f18589e
Zach Hoffman <zrhoffman@apache.org>
2023-06-20 08:27:49 -0600

    Reformat TreeNavNode[] literal

--- experimental/traffic-portal/src/app/shared/navigation/navigation.service.ts
+++ experimental/traffic-portal/src/app/shared/navigation/navigation.service.ts
@@ -111,40 +111,41 @@ export class NavigationService {
 			href: "/core",
 			name: "Dashboard"
 		}, {
-			children: [{
-				href: "/core/servers",
-				name: "Servers"
-			}, {
-				href: "/core/phys-locs",
-				name: "Physical Locations"
-			},
-			{
-				href: "/core/statuses",
-				name: "Statuses"
-			},
-			{
-				href: "/core/capabilities",
-				name: "Capabilities",
-			},
-			{
-				children: [{
-					href: "/core/cache-groups",
+			children: [
+				{
+					href: "/core/servers",
+					name: "Servers"
+				}, {
+					href: "/core/phys-locs",
+					name: "Physical Locations"
+				},
+				{
+					href: "/core/statuses",
+					name: "Statuses"
+				},
+				{
+					href: "/core/capabilities",
+					name: "Capabilities",
+				},
+				{
+					children: [{
+						href: "/core/cache-groups",
+						name: "Cache Groups"
+					}, {
+						href: "/core/coordinates",
+						name: "Coordinates"
+					}, {
+						href: "/core/divisions",
+						name: "Divisions"
+					}, {
+						href: "/core/regions",
+						name: "Regions"
+					}, {
+						href: "/core/asns",
+						name: "ASNs"
+					}],
 					name: "Cache Groups"
-				}, {
-					href: "/core/coordinates",
-					name: "Coordinates"
-				}, {
-					href: "/core/divisions",
-					name: "Divisions"
-				}, {
-					href: "/core/regions",
-					name: "Regions"
-				}, {
-					href: "/core/asns",
-					name: "ASNs"
 				}],
-				name: "Cache Groups"
-			}],
 			name: "Servers"
 		}, {
 			children: [
