00085e68910beca11ba7f446cf6b34a76866db4c
Srijeet Chatterjee <srijeet0406@gmail.com>
2023-07-11 15:42:16 -0600

    Traffic Router zone (Delivery service) calculation optimizations

--- traffic_router/core/src/main/java/org/apache/traffic_control/traffic_router/core/dns/NameServer.java
+++ traffic_router/core/src/main/java/org/apache/traffic_control/traffic_router/core/dns/NameServer.java
@@ -369,6 +369,14 @@ public class NameServer {
 			return;
 		}
 
+		if (clientAddress != null && clientAddress.getHostName().equals("_")) {
+			response.getHeader().setRcode(Rcode.NXDOMAIN);
+			response.getHeader().setFlag(Flags.AA);
+			addDenialOfExistence(qname, zone, response, flags);
+			addSOA(zone, response, Section.AUTHORITY, flags);
+			return;
+		}
+
 		final SetResponse sr = zone.findRecords(qname, qtype);
 
 		if (sr.isSuccessful()) {
