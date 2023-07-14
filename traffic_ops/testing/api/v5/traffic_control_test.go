db67a94b4f564cabb99ecb9af473fa861fe8d0bf
Kannan.G.B <111871677+gbkannan89@users.noreply.github.com>
2023-07-14 10:25:05 +0530

    Fixed lastUpdated timestamp format in api v5 federation resolvers (#7596)
    * fr changes
    
    * fr changes
    
    * changelog added
    
    * type import correction
    
    * fr codefix
    
    * fr code changes
    
    * comment correction
    
    * fixes
    
    * comments addressed
    
    * changelog correction
    
    * fr changed to give rfc format res
    
    * removed unused function
    
    * version revert for deletion
    
    * space added
    
    * comments addressed
    
    * comments addressed
    
    * reference mistake correction

--- traffic_ops/testing/api/v5/traffic_control_test.go
+++ traffic_ops/testing/api/v5/traffic_control_test.go
@@ -36,7 +36,7 @@ type TrafficControl struct {
 	TopologyBasedDeliveryServicesRequiredCapabilities []tc.DeliveryServicesRequiredCapability `json:"topologyBasedDeliveryServicesRequiredCapabilities"`
 	Divisions                                         []tc.DivisionV5                         `json:"divisions"`
 	Federations                                       []tc.CDNFederation                      `json:"federations"`
-	FederationResolvers                               []tc.FederationResolver                 `json:"federation_resolvers"`
+	FederationResolvers                               []tc.FederationResolverV5               `json:"federation_resolvers"`
 	Jobs                                              []tc.InvalidationJobCreateV4            `json:"jobs"`
 	Origins                                           []tc.Origin                             `json:"origins"`
 	Profiles                                          []tc.Profile                            `json:"profiles"`
