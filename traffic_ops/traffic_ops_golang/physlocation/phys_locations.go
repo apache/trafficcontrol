13ff9f51c36dfbdbd963e42dc42585f03026e0b1
Jagan Parthiban <JaganParthiban.JP@gmail.com>
2023-07-19 11:56:53 +0530

    PR Review comment Updates for  https://github.com/apache/trafficcontrol/issues/7630

--- traffic_ops/traffic_ops_golang/physlocation/phys_locations.go
+++ traffic_ops/traffic_ops_golang/physlocation/phys_locations.go
@@ -296,6 +296,22 @@ func CreatePhysLocation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// checks to see if the supplied region name and ID in the phys_location body correspond to each other.
+	if physLocation.RegionName != "" {
+		regionName, ok, err := dbhelpers.GetRegionNameFromID(tx, physLocation.RegionID)
+		if err != nil {
+			api.HandleErr(w, r, tx, http.StatusInternalServerError, fmt.Errorf("error fetching name from region ID: %w", err), nil)
+			return
+		} else if !ok {
+			api.HandleErr(w, r, tx, http.StatusNotFound, errors.New("no such region"), nil)
+			return
+		}
+		if regionName != physLocation.RegionName {
+			api.HandleErr(w, r, tx, http.StatusBadRequest, errors.New("region name and ID do not match"), nil)
+			return
+		}
+	}
+
 	// check if phys_location already exists
 	var count int
 	err := tx.QueryRow("SELECT count(*) from phys_location where name = $1", physLocation.Name).Scan(&count)
