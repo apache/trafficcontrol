7f2c5f2df883e57c0b46d84459fe1dca09ccf8bf
jpappa200 <joseph_pappano@cable.comcast.com>
2023-08-15 16:24:13 -0400

    add self-healing to the slice plugin automatically

--- lib/go-atscfg/remapdotconfig.go
+++ lib/go-atscfg/remapdotconfig.go
@@ -423,7 +423,7 @@ func getServerConfigRemapDotConfigForMid(
 		if ds.RangeRequestHandling != nil && (*ds.RangeRequestHandling == tc.RangeRequestHandlingCacheRangeRequest || *ds.RangeRequestHandling == tc.RangeRequestHandlingSlice) {
 			remapTags.RangeRequests = `@plugin=cache_range_requests.so` +
 				paramsStringFor(dsConfigParamsMap["cache_range_requests.pparam"], &warnings)
-			if *ds.RangeRequestHandling == tc.RangeRequestHandlingSlice {
+			if !strings.Contains(remapTags.RangeRequests, `@pparam=--consider-ims`) && *ds.RangeRequestHandling == tc.RangeRequestHandlingSlice {
 				remapTags.RangeRequests += ` @pparam=--consider-ims`
 			}
 		}
@@ -760,7 +760,6 @@ func buildEdgeRemapLine(
 	rangeReqTxt := ""
 	if ds.RangeRequestHandling != nil {
 		crr := false
-		addIms := false
 
 		if *ds.RangeRequestHandling == tc.RangeRequestHandlingBackgroundFetch {
 			rangeReqTxt = `@plugin=background_fetch.so @pparam=--config=bg_fetch.config` +
@@ -771,7 +770,6 @@ func buildEdgeRemapLine(
 				paramsStringFor(dsConfigParamsMap["slice.pparam"], &warnings)
 			rangeReqTxt += ` `
 			crr = true
-			addIms = true
 		} else if *ds.RangeRequestHandling == tc.RangeRequestHandlingCacheRangeRequest {
 			crr = true
 		}
@@ -779,9 +777,9 @@ func buildEdgeRemapLine(
 		if crr {
 			rangeReqTxt += `@plugin=cache_range_requests.so ` +
 				paramsStringFor(dsConfigParamsMap["cache_range_requests.pparam"], &warnings)
-		}
-		if addIms {
-			rangeReqTxt += `@pparam=--consider-ims `
+			if !strings.Contains(rangeReqTxt, `@pparam=--consider-ims`) && *ds.RangeRequestHandling == tc.RangeRequestHandlingSlice && ds.RangeSliceBlockSize != nil {
+				rangeReqTxt += ` @pparam=--consider-ims`
+			}
 		}
 	}
 
