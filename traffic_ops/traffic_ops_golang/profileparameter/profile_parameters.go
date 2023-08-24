7ce921431500b7fd0c567c5d0726c35ca7f78f25
Jagan Parthiban <33490370+jagan-parthiban@users.noreply.github.com>
2023-08-24 07:29:42 +0530

    Fixes ProfileParameters V5 apis to respond with RFC3339 date/time Format (#7738)
    * RFC3339 Changes for ProfileParameters. Fixes https://github.com/apache/trafficcontrol/issues/7737
    
    * CHANGELOG.md entry added
    
    * Fixed PR review comments
    
    * Fixed PR review comments
    
    * Remove strconv function and use APIInfo

--- traffic_ops/traffic_ops_golang/profileparameter/profile_parameters.go
+++ traffic_ops/traffic_ops_golang/profileparameter/profile_parameters.go
@@ -20,16 +20,23 @@ package profileparameter
  */
 
 import (
+	"database/sql"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"reflect"
 	"strconv"
 	"time"
 
+	"github.com/apache/trafficcontrol/lib/go-log"
 	"github.com/apache/trafficcontrol/lib/go-tc"
 	"github.com/apache/trafficcontrol/lib/go-tc/tovalidate"
 	"github.com/apache/trafficcontrol/lib/go-util"
 	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/api"
 	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/dbhelpers"
+	"github.com/apache/trafficcontrol/traffic_ops/traffic_ops_golang/util/ims"
 
 	validation "github.com/go-ozzo/ozzo-validation"
 )
@@ -211,4 +218,299 @@ func deleteQuery() string {
 	query := `DELETE FROM profile_parameter
 	WHERE profile=:profile_id and parameter=:parameter_id`
 	return query
+}
+
+func GetProfileParameter(w http.ResponseWriter, r *http.Request) {
+	var runSecond bool
+	var maxTime time.Time
+	inf, userErr, sysErr, errCode := api.NewInfo(r, nil, nil)
+	tx := inf.Tx
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, tx.Tx, errCode, userErr, sysErr)
+		return
+	}
+	defer inf.Close()
+
+	// Query Parameters to Database Query column mappings
+	queryParamsToQueryCols := map[string]dbhelpers.WhereColumnInfo{
+		"profileId":   {Column: "pp.profile"},
+		"parameterId": {Column: "pp.parameter"},
+		"lastUpdated": {Column: "pp.last_updated"},
+	}
+	if _, ok := inf.Params["orderby"]; !ok {
+		inf.Params["orderby"] = "parameter"
+	}
+	where, orderBy, pagination, queryValues, errs := dbhelpers.BuildWhereAndOrderByAndPagination(inf.Params, queryParamsToQueryCols)
+	if len(errs) > 0 {
+		api.HandleErr(w, r, tx.Tx, http.StatusBadRequest, util.JoinErrs(errs), nil)
+		return
+	}
+
+	if inf.Config.UseIMS {
+		runSecond, maxTime = ims.TryIfModifiedSinceQuery(tx, r.Header, queryValues, selectMaxLastUpdatedQuery(where))
+		if !runSecond {
+			log.Debugln("IMS HIT")
+			api.AddLastModifiedHdr(w, maxTime)
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+		log.Debugln("IMS MISS")
+	} else {
+		log.Debugln("Non IMS request")
+	}
+
+	query := selectQuery() + where + orderBy + pagination
+	rows, err := tx.NamedQuery(query, queryValues)
+	if err != nil {
+		api.HandleErr(w, r, tx.Tx, http.StatusInternalServerError, nil, fmt.Errorf("Profile Parameter read: error getting Profile Parameter(s): %w", err))
+		return
+	}
+	defer log.Close(rows, "unable to close DB connection")
+
+	profileParams := tc.ProfileParametersNullableV5{}
+	profileParamsList := []tc.ProfileParametersNullableV5{}
+	for rows.Next() {
+		if err = rows.Scan(&profileParams.LastUpdated, &profileParams.Parameter, &profileParams.Profile); err != nil {
+			api.HandleErr(w, r, tx.Tx, http.StatusInternalServerError, nil, fmt.Errorf("error getting profile parameter(s): %w", err))
+			return
+		}
+
+		profileParamsList = append(profileParamsList, profileParams)
+	}
+
+	api.WriteResp(w, r, profileParamsList)
+	return
+}
+
+func CreateProfileParameter(w http.ResponseWriter, r *http.Request) {
+	inf, userErr, sysErr, errCode := api.NewInfo(r, nil, nil)
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, inf.Tx.Tx, errCode, userErr, sysErr)
+		return
+	}
+	defer inf.Close()
+	tx := inf.Tx.Tx
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusBadRequest, errors.New("error reading request body"), nil)
+		return
+	}
+	defer r.Body.Close()
+
+	// Initial Unmarshal to validate request body
+	var data interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusBadRequest, errors.New("invalid request format"), nil)
+		return
+	}
+
+	// This code block decides if the request body is a slice of parameters or a single object.
+	var profileParams []tc.ProfileParameterCreationRequest
+	switch reflect.TypeOf(data).Kind() {
+	case reflect.Slice:
+		if err := json.Unmarshal(body, &profileParams); err != nil {
+			api.HandleErr(w, r, tx, http.StatusBadRequest, errors.New("error unmarshalling slice"), nil)
+			return
+		}
+	case reflect.Map:
+		// If it is a single object it is still converted to a slice for code simplicity.
+		var profileParam tc.ProfileParameterCreationRequest
+		if err := json.Unmarshal(body, &profileParam); err != nil {
+			api.HandleErr(w, r, tx, http.StatusBadRequest, errors.New("error unmarshalling single object"), nil)
+			return
+		}
+		profileParams = append(profileParams, profileParam)
+	default:
+		api.HandleErr(w, r, tx, http.StatusBadRequest, errors.New("invalid request format"), nil)
+		return
+	}
+
+	// Validate all objects of the every profile parameter from the request slice
+	for _, profileParameter := range profileParams {
+		readValErr := validateRequestProfileParameter(profileParameter)
+		if readValErr != nil {
+			api.HandleErr(w, r, tx, http.StatusBadRequest, readValErr, nil)
+			return
+		}
+	}
+
+	// Check user Permissions on all Profiles requested
+	for _, profileParameter := range profileParams {
+		cdnName, err := dbhelpers.GetCDNNameFromProfileID(tx, profileParameter.ProfileID)
+		if err != nil {
+			api.HandleErr(w, r, tx, http.StatusInternalServerError, err, nil)
+			return
+		}
+		userErr, sysErr, errCode = dbhelpers.CheckIfCurrentUserCanModifyCDN(tx, string(cdnName), inf.User.UserName)
+		if userErr != nil || sysErr != nil {
+			api.HandleErr(w, r, tx, errCode, userErr, sysErr)
+			return
+		}
+	}
+
+	// Check if any of the profile parameter from the request slice already exists
+	for _, profileParameter := range profileParams {
+		var count int
+		err = tx.QueryRow("SELECT count(*) from profile_parameter where profile = $1 and parameter = $2", profileParameter.ProfileID, profileParameter.ParameterID).Scan(&count)
+		if err != nil {
+			api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, fmt.Errorf("error: %w, when checking if profile parameter with profile_id %d and parameter_id %d exists", err, profileParameter.ProfileID, profileParameter.ParameterID))
+			return
+		}
+		if count == 1 {
+			api.HandleErr(w, r, tx, http.StatusBadRequest, fmt.Errorf("profile parameter with profile_id %d and parameter_id %d already exists", profileParameter.ProfileID, profileParameter.ParameterID), nil)
+			return
+		}
+	}
+
+	// Create all profile parameters from the request slice
+	var objProfileParams []tc.ProfileParameterV5
+	for _, profileParameter := range profileParams {
+		query := `
+				INSERT INTO profile_parameter (
+				profile, 
+				parameter
+				) VALUES (
+				$1, $2
+				) RETURNING profile, parameter, last_updated
+`
+		var objProfileParam tc.ProfileParameterV5
+		err = tx.QueryRow(
+			query,
+			profileParameter.ProfileID,
+			profileParameter.ParameterID,
+		).Scan(
+			&objProfileParam.ProfileID,
+			&objProfileParam.ParameterID,
+			&objProfileParam.LastUpdated,
+		)
+
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				api.HandleErr(w, r, tx, http.StatusInternalServerError, fmt.Errorf("error: %w in profile_parameter with with profile_id %d and parameter_id %d", err, profileParameter.ProfileID, profileParameter.ParameterID), nil)
+				return
+			}
+			usrErr, sysErr, code := api.ParseDBError(err)
+			api.HandleErr(w, r, tx, code, usrErr, sysErr)
+			return
+		}
+
+		// Fetch the Profile Name from ID to insert in type ProfileParameterV5
+		profileName, ok, err := dbhelpers.GetProfileNameFromID(profileParameter.ProfileID, tx)
+		if err != nil {
+			api.HandleErr(w, r, inf.Tx.Tx, http.StatusInternalServerError, nil, errors.New("getting profile name from id: "+err.Error()))
+			return
+		} else if !ok {
+			api.HandleErr(w, r, inf.Tx.Tx, http.StatusNotFound, errors.New("profile not found"), nil)
+			return
+		}
+
+		// Fetch the Parameter Name from ID to insert in type ProfileParameterV5
+		parameterName, ok, err := dbhelpers.GetParamNameByID(tx, profileParameter.ParameterID)
+		if err != nil {
+			api.HandleErr(w, r, inf.Tx.Tx, http.StatusInternalServerError, nil, errors.New("getting parameter name from id: "+err.Error()))
+			return
+		} else if !ok {
+			api.HandleErr(w, r, inf.Tx.Tx, http.StatusNotFound, errors.New("parameter not found"), nil)
+			return
+		}
+
+		objProfileParam.Profile = profileName
+		objProfileParam.Parameter = parameterName
+		objProfileParams = append(objProfileParams, objProfileParam)
+	}
+	alerts := tc.CreateAlerts(tc.SuccessLevel, "All Requested ProfileParameters were created.")
+	api.WriteAlertsObj(w, r, http.StatusCreated, alerts, objProfileParams)
+	return
+
+}
+
+func DeleteProfileParameter(w http.ResponseWriter, r *http.Request) {
+	inf, userErr, sysErr, errCode := api.NewInfo(r, nil, []string{"profileId"})
+	tx := inf.Tx.Tx
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, tx, errCode, userErr, sysErr)
+		return
+	}
+	defer inf.Close()
+
+	profileID := inf.Params["profileId"]
+	parameterID := inf.Params["parameterId"]
+	intProfileID := inf.IntParams["profileId"]
+
+	if profileID == "" || parameterID == "" {
+		api.HandleErr(w, r, tx, http.StatusBadRequest, fmt.Errorf("couldn't delete Profile_Parameter. profileID  & parameterID Cannot be empty for Delete Operation"), nil)
+		return
+	}
+
+	cdnName, err := dbhelpers.GetCDNNameFromProfileID(tx, intProfileID)
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, err, nil)
+		return
+	}
+	userErr, sysErr, errCode = dbhelpers.CheckIfCurrentUserCanModifyCDN(tx, string(cdnName), inf.User.UserName)
+	if userErr != nil || sysErr != nil {
+		api.HandleErr(w, r, tx, errCode, userErr, sysErr)
+		return
+	}
+
+	exists, err := dbhelpers.ProfileParameterExists(tx, profileID, parameterID)
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, err)
+		return
+	}
+	if !exists {
+		api.HandleErr(w, r, tx, http.StatusNotFound, fmt.Errorf("no profile_parameter exists by profile_id: %s & parameter_id: %s", profileID, parameterID), nil)
+		return
+	}
+
+	res, err := tx.Exec("DELETE FROM profile_parameter AS pp WHERE pp.profile=$1 and parameter=$2", profileID, parameterID)
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, err)
+		return
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, nil, fmt.Errorf("error determining rows affected for delete profile_parameter: %w", err))
+		return
+	}
+	if rowsAffected == 0 {
+		api.HandleErr(w, r, tx, http.StatusInternalServerError, fmt.Errorf("no rows deleted for profile_parameter"), nil)
+		return
+	}
+	alerts := tc.CreateAlerts(tc.SuccessLevel, "profile_parameter"+
+		" was deleted.")
+	api.WriteAlerts(w, r, http.StatusOK, alerts)
+	return
+}
+
+func selectMaxLastUpdatedQuery(where string) string {
+	return `
+        SELECT max(t) from (
+            SELECT max(pp.last_updated) as t FROM profile_parameter pp
+            JOIN profile prof ON prof.id = pp.profile
+            JOIN parameter param ON param.id = pp.parameter ` + where +
+		` UNION ALL
+            SELECT max(last_updated) as t FROM last_deleted l WHERE l.table_name = 'profile_parameter'
+        ) as res
+    `
+}
+
+// validateRequestProfileParameter validate the JSON objects
+func validateRequestProfileParameter(profileParameter tc.ProfileParameterCreationRequest) error {
+	errs := make(map[string]error)
+
+	errs[ProfileIDQueryParam] = validation.Validate(profileParameter.ProfileID, validation.Required)
+	errs[ParameterIDQueryParam] = validation.Validate(profileParameter.ParameterID, validation.Required)
+
+	if len(errs) > 0 {
+		var errorSlice []error
+		for _, err := range errs {
+			errorSlice = append(errorSlice, err)
+		}
+		userErr := util.JoinErrs(errorSlice)
+		return userErr
+	}
+	return nil
 }
