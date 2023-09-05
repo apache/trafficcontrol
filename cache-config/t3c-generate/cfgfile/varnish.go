c571cf349c0d45081070b76c2614aaa75e416b96
AbdelrahmanElawady <60783742+AbdelrahmanElawady@users.noreply.github.com>
2023-09-05 16:55:09 +0300

    Add HTTPS support to Varnish cache using Hitch (#7725)
    * Add Hitch config and include Hitch in CIAB
    
    * Add license

--- cache-config/t3c-generate/cfgfile/varnish.go
+++ cache-config/t3c-generate/cfgfile/varnish.go
@@ -20,6 +20,9 @@ package cfgfile
  */
 
 import (
+	"errors"
+	"path/filepath"
+
 	"github.com/apache/trafficcontrol/cache-config/t3c-generate/config"
 	"github.com/apache/trafficcontrol/cache-config/t3cutil"
 	"github.com/apache/trafficcontrol/lib/varnishcfg"
@@ -42,5 +45,29 @@ func GetVarnishConfigs(toData *t3cutil.ConfigData, cfg config.Cfg) ([]t3cutil.AT
 		LineComment: "//",
 		Secure:      false,
 	})
-	return configs, err
+	txt, hitchWarnings := varnishcfg.GetHitchConfig(toData.DeliveryServices, filepath.Join(cfg.Dir, "ssl/"))
+	warnings = append(warnings, hitchWarnings...)
+	logWarnings("Generating hitch configuration files: ", hitchWarnings)
+
+	configs = append(configs, t3cutil.ATSConfigFile{
+		Name:        "hitch.conf",
+		Text:        txt,
+		Path:        cfg.Dir,
+		ContentType: "text/plain; charset=us-ascii",
+		LineComment: "//",
+		Secure:      false,
+	})
+
+	sslConfigs, err := GetSSLCertsAndKeyFiles(toData)
+	if err != nil {
+		return nil, errors.New("getting ssl key and cert config files: " + err.Error())
+	}
+	for i := range sslConfigs {
+		// path changed manually because GetSSLCertsAndKeyFiles hardcodes the directory certs and keys are written to.
+		// will be removed once GetSSLCertsAndKeyFiles uses proxy.config.ssl.server.cert.path parameter.
+		sslConfigs[i].Path = filepath.Join(cfg.Dir, "ssl/")
+	}
+	configs = append(configs, sslConfigs...)
+
+	return configs, nil
 }
