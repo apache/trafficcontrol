06504e38606a57d23d509dd5132ac0dc6287d00a
Steve Hamrick <shamrickus@gmail.com>
2023-09-08 09:12:44 -0600

    Fix the TP GHA Tests (#7791)
    Fix TP GHA
    
    Co-authored-by: Steve Hamrick <shamrick@apache.org>

--- .github/actions/tp-integration-tests/entrypoint.sh
+++ .github/actions/tp-integration-tests/entrypoint.sh
@@ -27,7 +27,7 @@ onFail() {
   if [[ -f "${REPO_DIR}/traffic_ops/traffic_ops_golang/out.log" ]]; then
     mv "${REPO_DIR}/traffic_ops/traffic_ops_golang/out.log" Reports/to.log
   fi
-  docker logs $CHROMIUM_CONTAINER > Reports/chromium.log 2>&1;
+  docker logs $CHROME_CONTAINER > Reports/chromium.log 2>&1;
   docker logs $HUB_CONTAINER > Reports/hub.log 2>&1;
   echo "Detailed logs produced info Reports artifact"
   exit 1
@@ -82,9 +82,9 @@ sudo apt-get install -y --no-install-recommends gettext curl
 
 sudo npm i -g pm2 grunt sass
 
-CHROMIUM_CONTAINER=$(docker ps -qf name=chromium)
+CHROME_CONTAINER=$(docker ps -qf name=chrome)
 HUB_CONTAINER=$(docker ps -qf name=hub)
-CHROMIUM_VER=$(docker exec "$CHROMIUM_CONTAINER" chromium --version | grep -Eo '[0-9.]+')
+CHROME_VER=$(docker exec "$CHROME_CONTAINER" google-chrome --version | grep -Eo '[0-9.]+')
 
 export GOPATH="${HOME}/go"
 readonly ORG_DIR="$GOPATH/src/github.com/apache"
@@ -124,7 +124,7 @@ popd
 cd "${REPO_DIR}/traffic_portal/test/integration"
 npm ci
 
-npx webdriver-manager update --gecko false --versions.chrome "LATEST_RELEASE_$CHROMIUM_VER"
+npx webdriver-manager update --gecko false --versions.chrome "LATEST_RELEASE_$CHROME_VER"
 
 jq " .capabilities.chromeOptions.args = [
     \"--headless\",
@@ -134,8 +134,6 @@ jq " .capabilities.chromeOptions.args = [
   ] | .params.apiUrl = \"${to_fqdn}/api/5.0\" | .params.baseUrl =\"${tp_fqdn}\"
   | .capabilities[\"goog:chromeOptions\"].w3c = false | .capabilities.chromeOptions.w3c = false" \
   config.json > config.json.tmp && mv config.json.tmp config.json
-
-npm run build
 
 # Wait for tp/to build
 timeout 5m bash <<TMOUT
