8c6fab954e80de5d4a30b0f277669bdbf5c25e2c
The Anh Nguyen <ntheanh201@gmail.com>
2023-12-01 09:53:10 +0700

    Add show/hide comment field in properties

--- traffic_portal/app/src/common/modules/table/parameters/TableParametersController.js
+++ traffic_portal/app/src/common/modules/table/parameters/TableParametersController.js
@@ -27,8 +27,9 @@
  * @param {import("../../../api/ParameterService")} parameterService
  * @param {import("../../../api/ProfileService")} profileService
  * @param {import("../../../models/MessageModel")} messageModel
+ * @param {import("../../../models/PropertiesModel")} propertiesModel
  */
-var TableParametersController = function(parameters, $scope, $state, $uibModal, $window, locationUtils, parameterService, profileService, messageModel) {
+var TableParametersController = function(parameters, $scope, $state, $uibModal, $window, locationUtils, parameterService, profileService, messageModel, propertiesModel) {
 
     let parametersTable;
 
@@ -154,6 +155,13 @@ var TableParametersController = function(parameters, $scope, $state, $uibModal, 
         return true;
     };
 
+    $scope.show = function (colName) {
+        if (colName === "Comment") {
+            return propertiesModel.properties.parameters.comment.show;
+        }
+        return true;
+    }
+
     angular.element(document).ready(function () {
         parametersTable = $('#parametersTable').DataTable({
             "aLengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
@@ -176,5 +184,5 @@ var TableParametersController = function(parameters, $scope, $state, $uibModal, 
 
 };
 
-TableParametersController.$inject = ['parameters', '$scope', '$state', '$uibModal', '$window', 'locationUtils', 'parameterService', 'profileService', 'messageModel'];
+TableParametersController.$inject = ['parameters', '$scope', '$state', '$uibModal', '$window', 'locationUtils', 'parameterService', 'profileService', 'messageModel', 'propertiesModel'];
 module.exports = TableParametersController;
