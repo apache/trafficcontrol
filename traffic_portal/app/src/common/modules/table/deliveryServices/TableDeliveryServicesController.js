73ae8a163f96f412633b527e41bb66266cf23d95
Rima Shah <rima_shah@comcast.com>
2023-12-04 13:31:34 -0700

    Updated broken links in DS's right click menu

--- traffic_portal/app/src/common/modules/table/deliveryServices/TableDeliveryServicesController.js
+++ traffic_portal/app/src/common/modules/table/deliveryServices/TableDeliveryServicesController.js
@@ -657,14 +657,14 @@ function TableDeliveryServicesController(tableName, deliveryServices, steeringTa
 
 	$scope.contextMenuOptions = [
 		{
-			getHref: ds => `/delivery-services/${ds.id}?dsType=${ds.type}`,
+			getHref: ds => `#!/delivery-services/${ds.id}?dsType=${ds.type}`,
 			getText: ds => `Open ${ds.xmlId} in a new tab`,
 			newTab: true,
 			type: 2
 		},
 		{type: 0},
 		{
-			getHref: ds => `/delivery-services/${ds.id}?dsType=${ds.type}`,
+			getHref: ds => `#!/delivery-services/${ds.id}?dsType=${ds.type}`,
 			text: "Edit",
 			type: 2
 		},
