878b83c979c966c753682886b8bc7e562cf51d15
ocket8888 <ocket8888@apache.org>
2024-01-04 16:09:18 -0700

    Extract login to a custom command for reuse

--- experimental/traffic-portal/cypress/support/e2e.ts
+++ experimental/traffic-portal/cypress/support/e2e.ts
@@ -28,4 +28,4 @@
 // ***********************************************************
 
 // When a command from ./commands is ready to use, import with `import './commands'` syntax
-// import './commands';
+import "./commands";
