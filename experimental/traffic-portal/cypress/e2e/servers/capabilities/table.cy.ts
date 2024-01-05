bb2f3c2f42d54a58bdcf91362cad23355ada51ea
ocket8888 <ocket8888@apache.org>
2024-01-05 16:02:21 -0700

    Convert server Capability tests to Cypress

--- experimental/traffic-portal/cypress/e2e/servers/capabilities/table.cy.ts
+++ experimental/traffic-portal/cypress/e2e/servers/capabilities/table.cy.ts
@@ -12,15 +12,13 @@
  * limitations under the License.
  */
 
-describe("Capabilities Spec", () => {
+describe("Capabilities table page", () => {
+	beforeEach(() => {
+		cy.login();
+	});
 	it("Loads elements", async () => {
-		await browser.page.common()
-			.section.sidebar
-			.navigateToNode("capabilities", ["serversContainer"]);
-		await browser.waitForElementPresent("input[name=fuzzControl]");
-		await browser.elements("css selector", "div.ag-row", rows => {
-			browser.assert.ok(rows.status === 0);
-			browser.assert.ok((rows.value as []).length >= 2);
-		});
+		cy.visit("/core/capabilities");
+		cy.get("input[name=fuzzControl]");
+		cy.get("div.ag-row").should("have.length.at.least", 2);
 	});
 });
