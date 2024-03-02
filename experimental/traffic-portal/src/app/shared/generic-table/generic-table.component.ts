86d77ed7d3e823a854f304af47de1054473eb0a3
Steve Hamrick <shamrick@apache.org>
2024-03-02 02:22:47 -0700

    Fix rebase

--- experimental/traffic-portal/src/app/shared/generic-table/generic-table.component.ts
+++ experimental/traffic-portal/src/app/shared/generic-table/generic-table.component.ts
@@ -833,15 +833,27 @@ export class GenericTableComponent<T> implements OnInit, OnDestroy {
 	 * Downloads the table data as a CSV file.
 	 */
 	public download(): void {
-		const params: CsvExportParams = {
-			onlySelected: this.gridAPI.getSelectedNodes().length > 0,
-		};
-
-		if (this.context) {
-			params.fileName = `${this.context}.csv`;
-		}
-
-		this.gridAPI.exportDataAsCsv(params);
+		const nodes = this.gridAPI.getSelectedNodes();
+		const model = this.gridAPI.getModel();
+		let visible = 0;
+		let all = 0;
+		model.forEachNode(rowNode => {
+			if(rowNode.displayed) {
+				visible++;
+			}
+			all++;
+		});
+		this.dialog.open(DownloadOptionsDialogComponent, {
+			data: {
+				allRows: all,
+				columns: this.gridAPI.getColumnDefs() ?? [],
+				name: this.context,
+				selectedRows: nodes.length > 0 ? nodes.length : undefined,
+				visibleRows: visible
+			}
+		}).afterClosed().subscribe(value => {
+			this.gridAPI.exportDataAsCsv(value);
+		});
 	}
 
 	/**
